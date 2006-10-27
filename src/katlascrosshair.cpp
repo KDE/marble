7bc9ec7ddd93df841803fa62c5f77f5b977d6bd0
Torsten Rahn <tackat@kde.org>
2006-10-27 17:15:42 +0000

    Those were surely missing
    svn path=/trunk/playground/base/marble/; revision=599576

---
+++ src/katlascrosshair.cpp
@@ -0,0 +1,20 @@
+#include "katlascrosshair.h"
+
+#include <QDebug>
+
+KAtlasCrossHair::KAtlasCrossHair(QObject* parent) : QObject(parent) {
+	m_enabled = false;
+}
+
+void KAtlasCrossHair::paintCrossHair( QPainter* painter, int width, int height){
+
+	if ( m_enabled == true ){ 
+		int centerx = width / 2;
+		int centery = height / 2;
+		int halfsize = 5;
+
+		painter->setPen(QColor(Qt::white));
+		painter->drawLine( centerx - halfsize, centery, centerx + halfsize , centery );
+		painter->drawLine( centerx, centery - halfsize, centerx, centery + halfsize );
+	}	
+}
---
+++ src/katlascrosshair.h
@@ -0,0 +1,38 @@
+//
+// C++ Interface: katlascrosshair
+//
+// Description: KAtlasCrossHair 
+
+// KAtlasWindRose paints a crosshair
+//
+// Author: Torsten Rahn <tackat@kde.org>, (C) 2006
+//
+// Copyright: See COPYING file that comes with this distribution
+
+#ifndef KATLASCROSSHAIR_H
+#define KATLASCROSSHAIR_H
+
+#include <QObject>
+#include <QPainter>
+
+/**
+@author Torsten Rahn
+*/
+
+class KAtlasCrossHair : public QObject {
+
+Q_OBJECT
+
+public:
+	KAtlasCrossHair(QObject *parent = 0);
+
+	void paintCrossHair( QPainter*, int, int );
+
+	const bool isEnabled() const { return m_enabled; }
+	void setEnabled( bool enabled ){ m_enabled = enabled; }
+
+protected:
+	bool m_enabled;
+};
+
+#endif // KATLASCROSSHAIR_H
