37ac4c1933e8af333c77b4b448121d614c19dda8
Inge Wallin <inge@lysator.liu.se>
2007-04-08 21:37:43 +0000

    clean FastMath
    svn path=/trunk/playground/base/marble/; revision=651684

---
+++ src/FastMath.cpp
@@ -0,0 +1,29 @@
+#Include "FastMath.h"
+
+/*
+namespace FastMath {
+    int ACos[32768];
+    int ATan2[32768];
+}
+*/
+
+void FastMath::cpuid( unsigned long function,
+		      unsigned long& out_eax, unsigned long& out_ebx,
+		      unsigned long& out_ecx, unsigned long& out_edx )
+{
+    asm("cpuid": "=a" ( out_eax ),
+	         "=b" ( out_ebx ),
+		 "=c" ( out_ecx ),
+		 "=d" ( out_edx ) 
+		 : "a" ( function )
+	);
+}
+
+
+bool FastMath::haveSSE()
+{
+    unsigned long eax,ebx,edx,unused;
+
+    cpuid( 1,eax,ebx,unused,edx );
+    return ( edx & 0x2000000L ) != 0;
+}
