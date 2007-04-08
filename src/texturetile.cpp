033ca5ff8ffb0421f386b3338c0bd72dd1ffcce1
Inge Wallin <inge@lysator.liu.se>
2007-04-08 22:44:21 +0000

    clean TextureTile
    svn path=/trunk/playground/base/marble/; revision=651696

--- src/texturetile.cpp
+++ src/texturetile.cpp
@@ -5,139 +5,155 @@
 #include "katlasdirs.h"
 #include "TileLoader.h"
 
+
 static uint **jumpTableFromQImage32( QImage &img )
 {
-	const int height = img.height();
-	const int bpl = img.bytesPerLine()/4;
-	uint *data = (QRgb*)(img.scanLine(0));
-	uint **jumpTable = new uint*[height];
-	for ( int y = 0; y < height; ++y ) {
-		jumpTable[ y ] = data;
-		data += bpl;
-	}
-	return jumpTable;
+    const int  height = img.height();
+    const int  bpl    = img.bytesPerLine() / 4;
+    uint      *data = (QRgb*)(img.scanLine(0));
+    uint     **jumpTable = new uint*[height];
+
+    for ( int y = 0; y < height; ++y ) {
+	jumpTable[ y ] = data;
+	data += bpl;
+    }
+
+    return jumpTable;
 }
+
 
 static uchar **jumpTableFromQImage8( QImage &img )
 {
-	const int height = img.height();
-	const int bpl = img.bytesPerLine();
-	uchar *data = img.bits();
-	uchar **jumpTable = new uchar*[height];
-	for ( int y = 0; y < height; ++y ) {
-		jumpTable[ y ] = data;
-		data += bpl;
-	}
-	return jumpTable;
+    const int  height = img.height();
+    const int  bpl    = img.bytesPerLine();
+    uchar     *data = img.bits();
+    uchar    **jumpTable = new uchar*[height];
+
+    for ( int y = 0; y < height; ++y ) {
+	jumpTable[ y ] = data;
+	data += bpl;
+    }
+
+    return jumpTable;
 }
+
 
 TextureTile::TextureTile( int x, int y, int level, const QString& theme )
-		 : QObject(){
-	loadTile( x, y, level, theme, false );
-
-//	slotLoadTile( "test/2/0003/0003_0004.jpg" );
+    : QObject()
+{
+    loadTile( x, y, level, theme, false );
 }
 
-void TextureTile::loadTile( int x, int y, int level, const QString& theme, bool requestRepaint ){
-	m_used=true;
-	QString absfilename;
-//	qDebug() << "Requested tile level" << level;
 
-//	If the tile level offers the requested tile then load it. Otherwise cycle from 
-//	the requested tilelevel down to one where the requested area is covered. Then 
-//	scale the area to create a replacement for the tile that has been requested.
+void TextureTile::loadTile( int x, int y, int level, 
+			     const QString& theme, bool requestRepaint )
+{
+    m_used = true;
 
-	for ( int i = level; i > -1; --i ) {
+    QString  absfilename;
+    // qDebug() << "Requested tile level" << level;
 
-		float origx1 = (float)(x) / (float)( TileLoader::levelToRow( level ) );
-		float origy1 = (float)(y) / (float)( TileLoader::levelToColumn( level ) );
-		float testx1 = origx1 * (float)( TileLoader::levelToRow( i ) ) ;
-		float testy1 = origy1 * (float)( TileLoader::levelToColumn( i ) );
+    // If the tile level offers the requested tile then load it.
+    // Otherwise cycle from the requested tilelevel down to one where
+    // the requested area is covered.  Then scale the area to create a
+    // replacement for the tile that has been requested.
 
-		QString relfilename = QString("%1/%2/%3/%3_%4.jpg").arg(theme).arg(i).arg( (int)(testy1), 4, 10, QChar('0') ).arg( (int)(testx1), 4, 10, QChar('0') );
+    for ( int i = level; i > -1; --i ) {
 
-		absfilename = KAtlasDirs::path( relfilename );
-		if ( QFile::exists( absfilename ) ){
-			m_rawtile = new QImage( absfilename );
-//			qDebug() << absfilename;
-			if ( !m_rawtile->isNull() ){
+	float origx1 = (float)(x) / (float)( TileLoader::levelToRow( level ) );
+	float origy1 = (float)(y) / (float)( TileLoader::levelToColumn( level ) );
+	float testx1 = origx1 * (float)( TileLoader::levelToRow( i ) ) ;
+	float testy1 = origy1 * (float)( TileLoader::levelToColumn( i ) );
 
-				if ( level != i ){	
-					QSize tilesize = m_rawtile->size();
-					float origx2 = (float)(x + 1) / (float)( TileLoader::levelToRow( level ) );
-					float origy2 = (float)(y + 1) / (float)( TileLoader::levelToColumn( level ) );
-					float testx2 = origx2 * (float)( TileLoader::levelToRow( i ) );
-					float testy2 = origy2 * (float)( TileLoader::levelToColumn( i ) );
+	QString relfilename = QString("%1/%2/%3/%3_%4.jpg").arg(theme).arg(i).arg( (int)(testy1), 4, 10, QChar('0') ).arg( (int)(testx1), 4, 10, QChar('0') );
+
+	absfilename = KAtlasDirs::path( relfilename );
+	if ( QFile::exists( absfilename ) ){
+	    m_rawtile = new QImage( absfilename );
+	    // qDebug() << absfilename;
+	    if ( !m_rawtile->isNull() ) {
+
+		if ( level != i ) { 
+		    QSize tilesize = m_rawtile->size();
+		    float origx2 = (float)(x + 1) / (float)( TileLoader::levelToRow( level ) );
+		    float origy2 = (float)(y + 1) / (float)( TileLoader::levelToColumn( level ) );
+		    float testx2 = origx2 * (float)( TileLoader::levelToRow( i ) );
+		    float testy2 = origy2 * (float)( TileLoader::levelToColumn( i ) );
 	
-					QPoint topleft( (int)( ( testx1 - (int)(testx1) ) * m_rawtile->width() ), (int)( ( testy1 - (int)(testy1) ) * m_rawtile->height() ) );
-					QPoint bottomright( (int)( ( testx2 - (int)(testx1) ) * m_rawtile->width() ) - 1, (int)( ( testy2 - (int)(testy1) ) * m_rawtile->height() ) - 1 );
+		    QPoint topleft( (int)( ( testx1 - (int)(testx1) ) * m_rawtile->width() ), (int)( ( testy1 - (int)(testy1) ) * m_rawtile->height() ) );
+		    QPoint bottomright( (int)( ( testx2 - (int)(testx1) ) * m_rawtile->width() ) - 1, (int)( ( testy2 - (int)(testy1) ) * m_rawtile->height() ) - 1 );
 				
-//					qDebug() << "x1: " << topleft.x() << "y1: " << topleft.y() << "x2: " << bottomright.x() << "y2: " << bottomright.y();  
-					*m_rawtile = m_rawtile->copy( QRect( topleft, bottomright ) );
-					*m_rawtile = m_rawtile->scaled( tilesize ); // TODO: use correct size
-				}
+		    //					qDebug() << "x1: " << topleft.x() << "y1: " << topleft.y() << "x2: " << bottomright.x() << "y2: " << bottomright.y();  
+		    *m_rawtile = m_rawtile->copy( QRect( topleft, bottomright ) );
+		    *m_rawtile = m_rawtile->scaled( tilesize ); // TODO: use correct size
+		}
 
-				break;
-			}
-		}
-		else {
-			emit downloadTile( relfilename );
-		}
+		break;
+	    }
 	}
+	else {
+	    emit downloadTile( relfilename );
+	}
+    }
 	
-	if ( m_rawtile->isNull() ){
-		qDebug() << "An essential tile is missing. Please rerun the application.";
-		exit(-1);
-	}
+    if ( m_rawtile->isNull() ){
+	qDebug() << "An essential tile is missing. Please rerun the application.";
+	exit(-1);
+    }
 
 
-	m_depth = m_rawtile->depth();
+    m_depth = m_rawtile->depth();
 
-	switch ( m_depth ) {
-	case 32:
-//		qDebug("32");
-		jumpTable32=jumpTableFromQImage32(*m_rawtile);
-		break;
-	case 8:
-//		qDebug("8");
-		jumpTable8=jumpTableFromQImage8(*m_rawtile);
-		break;
-	default:
-		qDebug() << QString("Color m_depth %1 of tile %2 could not be retrieved. Exiting.").arg(m_depth).arg(absfilename);
-		exit(-1);
-	}
+    switch ( m_depth ) {
+    case 32:
+	// qDebug("32");
+	jumpTable32=jumpTableFromQImage32(*m_rawtile);
+	break;
+    case 8:
+	// qDebug("8");
+	jumpTable8=jumpTableFromQImage8(*m_rawtile);
+	break;
+    default:
+	qDebug() << QString("Color m_depth %1 of tile %2 could not be retrieved. Exiting.").arg(m_depth).arg(absfilename);
+	exit(-1);
+    }
 
-	if ( requestRepaint == true )
-		emit tileUpdate();
+    if ( requestRepaint == true )
+	emit tileUpdate();
 }
 
-TextureTile::~TextureTile(){
-	switch ( m_depth ) {
-	case 32:
-		delete [] jumpTable32;
-		break;
-	case 8:
-		delete [] jumpTable8;
-		break;
-	default:
-		qDebug("Color m_depth of a tile could not be retrieved. Exiting.");
-		exit(-1);
-	}
-	delete m_rawtile;
+
+TextureTile::~TextureTile()
+{
+    switch ( m_depth ) {
+    case 32:
+	delete [] jumpTable32;
+	break;
+    case 8:
+	delete [] jumpTable8;
+	break;
+    default:
+	qDebug("Color m_depth of a tile could not be retrieved. Exiting.");
+	exit(-1);
+    }
+
+    delete m_rawtile;
 }
 
-void TextureTile::slotLoadTile( const QString& path ){
-//	QString relfilename = QString("%1/%2/%3/%3_%4.jpg").arg(theme).arg(i).arg( (int)(testy1), 4, 10, QChar('0') ).arg( (int)(testx1), 4, 10, QChar('0') );
-	QString filename = path.section( '/', -1 );
-	int x = filename.section( '_', 0, 0 ).toInt();
-	int y = filename.section( '_', 1, 1 ).section( '.', 0, 0 ).toInt();
-	int level = path.section( '/', 1, 1 ).toInt();
-	QString theme = path.section( '/', 0, 0 );
+void TextureTile::slotLoadTile( const QString& path )
+{
+    // QString relfilename = QString("%1/%2/%3/%3_%4.jpg").arg(theme).arg(i).arg( (int)(testy1), 4, 10, QChar('0') ).arg( (int)(testx1), 4, 10, QChar('0') );
+    QString filename = path.section( '/', -1 );
+    int x = filename.section( '_', 0, 0 ).toInt();
+    int y = filename.section( '_', 1, 1 ).section( '.', 0, 0 ).toInt();
+    int level = path.section( '/', 1, 1 ).toInt();
+    QString theme = path.section( '/', 0, 0 );
 
-//	qDebug() << "Test: |" << theme << "|" << level << "|" << x << "|" << y;
+    //	qDebug() << "Test: |" << theme << "|" << level << "|" << x << "|" << y;
 
-	loadTile( x, y, level, theme, true );
+    loadTile( x, y, level, theme, true );
 }
+
 
 #ifndef Q_OS_MACX
 #include "texturetile.moc"
--- src/texturetile.h
+++ src/texturetile.h
@@ -8,47 +8,60 @@
 //
 // Copyright: See COPYING file that comes with this distribution
 
+
 #ifndef __MARBLE__TEXTURETILE_H
 #define __MARBLE__TEXTURETILE_H
+
 
 #include <QtCore/QObject>
 #include <QtCore/QHash>
 #include <QtGui/QImage>
 #include <QtCore/QString>
 
+
 /**
-@author Torsten Rahn
-*/
+ *@author Torsten Rahn
+ *
+ * This class contains a Tile with a texture in it.  It is used for
+ * drawing the map.
+ */
 
 class TextureTile : public QObject {
-	Q_OBJECT
+    Q_OBJECT
 
-public:
-	TextureTile( int x, int y, int level, const QString& theme );
+ public:
+    TextureTile( int x, int y, int level, const QString& theme );
 
-	virtual ~TextureTile();
+    virtual ~TextureTile();
 
-	const int depth() const { return m_depth; }
-	const bool used() const { return m_used; }
-	void setUsed( bool used ){ m_used = used; }
+    const int  depth() const        { return m_depth; }
 
-	QImage* rawtile() { return m_rawtile; }
+    const bool used() const         { return m_used; }
+    void       setUsed( bool used ) { m_used = used; }
+
+    QImage    *rawtile()            { return m_rawtile; }
  
-	uchar **jumpTable8;
-	uint **jumpTable32;
-signals:
-	void downloadTile( const QString& filename );
-	void tileUpdate();
+    uchar  **jumpTable8;
+    uint   **jumpTable32;
 
-public slots:
-	void slotLoadTile( const QString& path );
+ signals:
+    void downloadTile( const QString& filename );
+    void tileUpdate();
 
-protected:
-	QImage* m_rawtile;
+ public slots:
+    void slotLoadTile( const QString& path );
 
-	int m_depth;
-	bool m_used;
-	void loadTile( int x, int y, int level, const QString& theme, bool requestRepaint = true );
+ protected:
+    void loadTile( int x, int y, int level, 
+		   const QString& theme, bool requestRepaint = true );
+
+ protected:
+    QImage  *m_rawtile;
+
+    int      m_depth;
+
+    bool     m_used;
 };
+
 
 #endif // __MARBLE__TEXTURETILE_H
