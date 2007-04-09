21bd2eb5bcdab39d5da3f1e28247dff202f8f5e3
Inge Wallin <inge@lysator.liu.se>
2007-04-09 16:30:13 +0000

    Cleaned up main.cpp
    svn path=/trunk/playground/base/marble/; revision=651909

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,25 @@
 #include <QtGui/QApplication>
 #include <QtCore/QFile>
 #include <QtCore/QTime>
+
 #include "katlascontrol.h"
+
 
 #if STATIC_BUILD
  Q_IMPORT_PLUGIN(qjpeg)
  Q_IMPORT_PLUGIN(qsvg)
 #endif
 
+
 int main(int argc, char *argv[])
 {
-    QApplication   app(argc, argv);
-    KAtlasControl  katlascontrol(0);
+    QApplication   app( argc, argv );
+    KAtlasControl  katlascontrol( 0 );
 
     katlascontrol.show();
     // katlascontrol.addPlaceMarkFile( "../data/placemarks/stdplacemarks.kml" );
     // Parse all arguments
     for ( int i = 1; i < argc; ++i ) {
-	if ( QFile::exists( app.arguments().at( i ) ) == true )
-	    katlascontrol.addPlaceMarkFile( argv[i] );
-
 	if ( strcmp( argv[ i ], "--timedemo" ) == 0 ) {
 	    qDebug( "Running timedemo, stand by..." );
 	    QTime t;
@@ -32,23 +32,25 @@ int main(int argc, char *argv[])
 		for ( int k = 0; k < 10; ++k ) {
 		    katlascontrol.moveLeft();
 		}
-		//				katlascontrol.moveUp();
+		// katlascontrol.moveUp();
 	    }
-	    /*
-	      for ( int j = 0; j < 10; ++j ) {
+#if 0
+            for ( int j = 0; j < 10; ++j ) {
 
-	      for ( int i = 0; i < 5; ++i ){
-	      katlascontrol.moveLeft();
-	      }
-	      for ( int i = 0; i < 5; ++i ){
-	      katlascontrol.moveRight();
-	      }
-	      }
-	    */
+                for ( int i = 0; i < 5; ++i ){
+                    katlascontrol.moveLeft();
+                }
+                for ( int i = 0; i < 5; ++i ){
+                    katlascontrol.moveRight();
+                }
+            }
+#endif
 	    qDebug( "Timedemo finished in %ims", t.elapsed() );
 	    qDebug() <<  QString("= %1 fps").arg(2000*1000/(float)(t.elapsed()));
 	    return 0;
-	}			 
+	}
+	else if ( QFile::exists( app.arguments().at( i ) ) )
+	    katlascontrol.addPlaceMarkFile( argv[i] );
     }
 
     return app.exec();
