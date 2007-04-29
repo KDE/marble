546fcacf67931136a9730662ea914ad23e74560c
Inge Wallin <inge@lysator.liu.se>
2007-04-29 16:59:34 +0000

    Cleaned class KAtlasCrossHair
    svn path=/trunk/playground/base/marble/; revision=659189

--- src/katlascrosshair.cpp
+++ src/katlascrosshair.cpp
@@ -1,22 +1,31 @@
 #include "katlascrosshair.h"
+
 #include <QtCore/QDebug>
 
-KAtlasCrossHair::KAtlasCrossHair(QObject* parent) : QObject(parent) {
-	m_enabled = false;
+
+KAtlasCrossHair::KAtlasCrossHair(QObject* parent)
+    : QObject(parent)
+{
+    m_enabled = false;
 }
 
-void KAtlasCrossHair::paintCrossHair( QPainter* painter, int width, int height){
 
-	if ( m_enabled == true ){ 
-		int centerx = width / 2;
-		int centery = height / 2;
-		int halfsize = 5;
+void KAtlasCrossHair::paintCrossHair( QPainter* painter, int width, int height)
+{
 
-		painter->setPen(QColor(Qt::white));
-		painter->drawLine( centerx - halfsize, centery, centerx + halfsize , centery );
-		painter->drawLine( centerx, centery - halfsize, centerx, centery + halfsize );
-	}	
+    if ( m_enabled ) { 
+        int  centerx  = width / 2;
+        int  centery  = height / 2;
+        int  halfsize = 5;
+
+        painter->setPen( QColor( Qt::white ) );
+        painter->drawLine( centerx - halfsize, centery,
+                           centerx + halfsize, centery );
+        painter->drawLine( centerx, centery - halfsize,
+                           centerx, centery + halfsize );
+    }	
 }
+
 
 #ifndef Q_OS_MACX
 #include "katlascrosshair.moc"
--- src/katlascrosshair.h
+++ src/katlascrosshair.h
@@ -3,36 +3,40 @@
 //
 // Description: KAtlasCrossHair 
 
-// KAtlasWindRose paints a crosshair
+// KAtlasCrossHair paints a crosshair
 //
 // Author: Torsten Rahn <tackat@kde.org>, (C) 2006
 //
 // Copyright: See COPYING file that comes with this distribution
 
+
 #ifndef KATLASCROSSHAIR_H
 #define KATLASCROSSHAIR_H
 
+
 #include <QtCore/QObject>
 #include <QtGui/QPainter>
+
 
 /**
 @author Torsten Rahn
 */
 
-class KAtlasCrossHair : public QObject {
+class KAtlasCrossHair : public QObject
+{
+    Q_OBJECT
 
-Q_OBJECT
+ public:
+    KAtlasCrossHair(QObject *parent = 0);
 
-public:
-	KAtlasCrossHair(QObject *parent = 0);
+    void paintCrossHair( QPainter*, int, int );
 
-	void paintCrossHair( QPainter*, int, int );
+    const bool enabled()            const { return m_enabled; }
+    void       setEnabled( bool enabled ) { m_enabled = enabled; }
 
-	const bool isEnabled() const { return m_enabled; }
-	void setEnabled( bool enabled ){ m_enabled = enabled; }
-
-protected:
-	bool m_enabled;
+ protected:
+    bool  m_enabled;
 };
+
 
 #endif // KATLASCROSSHAIR_H
