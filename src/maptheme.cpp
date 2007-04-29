6a3d3ace0a39c68e8b248c638b6efa6c96579b12
Inge Wallin <inge@lysator.liu.se>
2007-04-29 14:06:54 +0000

    Cleaned class MapTheme
    svn path=/trunk/playground/base/marble/; revision=659116

--- src/maptheme.cpp
+++ src/maptheme.cpp
@@ -1,261 +1,280 @@
 #include "maptheme.h"
+
 #include <QtCore/QDebug>
 #include <QtCore/QDir>
-#include <QtXml/QDomDocument>
 #include <QtCore/QFile>
-#include <QtGui/QIcon>
 #include <QtCore/QStringList>
+#include <QtXml/QDomDocument>
+#include <QtGui/QIcon>
 #include <QtGui/QStandardItemModel>
 
 #include "katlasdirs.h"
 #include "TileLoader.h"
+
 
 MapTheme::MapTheme(QObject *parent)
     : QObject(parent)
 {
 }
 
-int MapTheme::open( QString path ){
-	QDomDocument doc( "DeskGlobeML" );
-	QFile file( path );
-	if( !file.open( QIODevice::ReadOnly ) ){
-		qDebug() << QString("Could not open ") + path;
-		return -1;
-	}
-	if( !doc.setContent( &file ) ){
-		qDebug() << QString("Parse error!");
-		file.close();
-		return -2;
-	}
-	file.close();
-	QDomElement element = doc.documentElement();
-	if( element.tagName() != "DeskGlobeML" ){
-		qDebug("File is not a DeskGlobe file");
-		return -3;
-	}
 
-	element = element.firstChildElement();
+int MapTheme::open( QString path )
+{
+    QDomDocument  doc( "DeskGlobeML" );
+    QFile         file( path );
 
-	while( !element.isNull() ){
+    if ( !file.open( QIODevice::ReadOnly ) ) {
+        qDebug() << QString("Could not open ") + path;
+        return -1;
+    }
 
-		if( element.tagName().toLower() == "mapstyle" ){
-//			qDebug("Parsing MapStyleNode");
+    if ( !doc.setContent( &file ) ) {
+        qDebug() << QString("Parse error!");
+        file.close();
+        return -2;
+    }
+    file.close();
 
-			QDomElement mapStyleSibling = element.firstChildElement();
-			while( !mapStyleSibling.isNull() ){
+    QDomElement  element = doc.documentElement();
+    if ( element.tagName() != "DeskGlobeML" ) {
+        qDebug("File is not a DeskGlobe file");
+        return -3;
+    }
 
-				if( mapStyleSibling.tagName().toLower() == "name" ){
-					m_name=mapStyleSibling.text();
-//					qDebug() << m_name;
-				}
+    element = element.firstChildElement();
+    while ( !element.isNull() ) {
 
-				if( mapStyleSibling.tagName().toLower() == "prefix" ){
-					m_prefix=mapStyleSibling.text();
-//					qDebug() << m_prefix;
-				}
+        if ( element.tagName().toLower() == "mapstyle" ) {
+            // qDebug("Parsing MapStyleNode");
 
-				if( mapStyleSibling.tagName().toLower() == "icon" ){
-					m_icon=mapStyleSibling.text();
-//					qDebug() << m_icon;
-				}
+            QDomElement  mapStyleSibling = element.firstChildElement();
+            while ( !mapStyleSibling.isNull() ) {
+
+                if ( mapStyleSibling.tagName().toLower() == "name" ) {
+                    m_name = mapStyleSibling.text();
+                    // qDebug() << m_name;
+                }
+
+                if ( mapStyleSibling.tagName().toLower() == "prefix" ) {
+                    m_prefix = mapStyleSibling.text();
+                    // qDebug() << m_prefix;
+                }
+
+                if ( mapStyleSibling.tagName().toLower() == "icon" ) {
+                    m_icon = mapStyleSibling.text();
+                    // qDebug() << m_icon;
+                }
 				
-				if( mapStyleSibling.tagName().toLower() == "description" ){
-					m_description=mapStyleSibling.text();
-//					qDebug() << m_description;
-				}
+                if ( mapStyleSibling.tagName().toLower() == "description" ) {
+                    m_description = mapStyleSibling.text();
+                    // qDebug() << m_description;
+                }
 
-				if( mapStyleSibling.tagName().toLower() == "installmap" ){
-					m_installmap=mapStyleSibling.text();
-//					qDebug() << m_installmap;
-				}
+                if ( mapStyleSibling.tagName().toLower() == "installmap" ) {
+                    m_installmap = mapStyleSibling.text();
+                    // qDebug() << m_installmap;
+                }
 
-				m_labelcolor = QColor( 0, 0, 0, 255 );
-				if( mapStyleSibling.tagName().toLower() == "labelstyle" ){
-					QDomElement labelStyleSibling = mapStyleSibling.firstChildElement();
-					while( !labelStyleSibling.isNull() ){
-						if( labelStyleSibling.tagName().toLower() == "color" ){
-							m_labelcolor.setNamedColor( "#" + labelStyleSibling.text() );
-//							qDebug() << "#" + labelStyleSibling.text();
-						}
-						labelStyleSibling = labelStyleSibling.nextSiblingElement();
-					}
-				}
+                m_labelcolor = QColor( 0, 0, 0, 255 );
+                if ( mapStyleSibling.tagName().toLower() == "labelstyle" ) {
+                    QDomElement labelStyleSibling = mapStyleSibling.firstChildElement();
+                    while ( !labelStyleSibling.isNull() ) {
+                        if ( labelStyleSibling.tagName().toLower() == "color" ) {
+                            m_labelcolor.setNamedColor( "#" + labelStyleSibling.text() );
+                            // qDebug() << "#" + labelStyleSibling.text();
+                        }
+                        labelStyleSibling = labelStyleSibling.nextSiblingElement();
+                    }
+                }
 
-				m_labelcolor.setAlpha(255);
+                m_labelcolor.setAlpha(255);
+                if ( mapStyleSibling.tagName().toLower() == "layer" ) {
 
-				if( mapStyleSibling.tagName().toLower() == "layer" ){
+                    m_bitmaplayer.enabled = false;
+                    if ( mapStyleSibling.attribute( "type", "" ) == "bitmap" ) {
+                        m_bitmaplayer.enabled = true;
+                        m_bitmaplayer.name    = mapStyleSibling.attribute( "name", "" );
+                        m_bitmaplayer.type    = mapStyleSibling.attribute( "type", "" );
+                        m_bitmaplayer.dem     = mapStyleSibling.attribute( "dem", "" );
+                        if ( m_bitmaplayer.dem == "" )
+                            m_bitmaplayer.dem = "false";
+                    }
 
-					m_bitmaplayer.enabled = false;
-					if ( mapStyleSibling.attribute( "type", "" ) == "bitmap" ){
-						m_bitmaplayer.enabled = true;
-						m_bitmaplayer.name = mapStyleSibling.attribute( "name", "" );
-						m_bitmaplayer.type = mapStyleSibling.attribute( "type", "" );
-						m_bitmaplayer.dem = mapStyleSibling.attribute( "dem", "" );
-						if ( m_bitmaplayer.dem == "" ) m_bitmaplayer.dem = "false";
-					}
+                    m_vectorlayer.enabled = false;
+                    if ( mapStyleSibling.attribute( "type", "" ) == "vector" ) {
+                        m_vectorlayer.enabled = true;
+                        m_vectorlayer.name    = mapStyleSibling.attribute( "name", "" );
+                        m_vectorlayer.type    = mapStyleSibling.attribute( "type", "" );
+                    }					
+                    // qDebug() << m_layer.name << " " << m_layer.type;
+                }
 
-					m_vectorlayer.enabled = false;
-					if ( mapStyleSibling.attribute( "type", "" ) == "vector" ){
-						m_vectorlayer.enabled = true;
-						m_vectorlayer.name = mapStyleSibling.attribute( "name", "" );
-						m_vectorlayer.type = mapStyleSibling.attribute( "type", "" );
-					}					
-//					qDebug() << m_layer.name << " " << m_layer.type;
-				}
+                mapStyleSibling = mapStyleSibling.nextSiblingElement();
+            }
+        }
 
-				mapStyleSibling = mapStyleSibling.nextSiblingElement();
+        element = element.nextSiblingElement();
+    }
 
-			}
+    // detectMaxTileLevel();
 
-		}
-
-		element = element.nextSiblingElement();
-	}
-
-//	detectMaxTileLevel();
-
-	return 0;
+    return 0;
 }
 
-QStringList MapTheme::findMapThemes( const QString& path ){
 
-	QDir localdirs = QDir( KAtlasDirs::localDir() + "/" + path );
-	QDir sysdirs = QDir( KAtlasDirs::systemDir() + "/" + path );
-	QDir unixdirs = QDir( KAtlasDirs::unixDir() + "/" + path );
+QStringList MapTheme::findMapThemes( const QString& path )
+{
+    QDir  localdirs = QDir( KAtlasDirs::localDir() + "/" + path );
+    QDir  sysdirs   = QDir( KAtlasDirs::systemDir() + "/" + path );
+    QDir  unixdirs  = QDir( KAtlasDirs::unixDir() + "/" + path );
 
-	QStringList localmappaths = localdirs.entryList( QStringList( "*" ),
-                                    QDir::AllDirs | QDir::NoSymLinks | QDir::NoDotAndDotDot);
-	QStringList sysmappaths = sysdirs.entryList( QStringList( "*" ),
-                                    QDir::AllDirs | QDir::NoSymLinks | QDir::NoDotAndDotDot);
-	QStringList unixmappaths = unixdirs.entryList( QStringList( "*" ),
-                                    QDir::AllDirs | QDir::NoSymLinks | QDir::NoDotAndDotDot);
-	
+    QStringList  localmappaths = localdirs.entryList( QStringList( "*" ),
+                                                      QDir::AllDirs
+                                                      | QDir::NoSymLinks
+                                                      | QDir::NoDotAndDotDot );
+    QStringList sysmappaths = sysdirs.entryList( QStringList( "*" ),
+                                                 QDir::AllDirs
+                                                 | QDir::NoSymLinks
+                                                 | QDir::NoDotAndDotDot );
+    QStringList unixmappaths = unixdirs.entryList( QStringList( "*" ),
+                                                   QDir::AllDirs
+                                                   | QDir::NoSymLinks
+                                                   | QDir::NoDotAndDotDot );
 
-	QStringList localmapdirs;
-	QStringList sysmapdirs;
-	QStringList unixmapdirs;
 
-	for (int i = 0; i < localmappaths.size(); ++i){
-//		qDebug() << "Map dirs: "  << KAtlasDirs::localDir() + "/maps/" + localmappaths.at(i);
-		localmapdirs << KAtlasDirs::localDir() + "/maps/earth/" + localmappaths.at(i);
-	}
+    QStringList  localmapdirs;
+    QStringList  sysmapdirs;
+    QStringList  unixmapdirs;
 
-	for (int i = 0; i < sysmappaths.size(); ++i){
-//		qDebug() << "Map dirs: " << KAtlasDirs::systemDir() + "/maps/" + sysmappaths.at(i);
-		sysmapdirs << KAtlasDirs::systemDir() + "/maps/earth/" + sysmappaths.at(i);
-	}
+    for ( int i = 0; i < localmappaths.size(); ++i ) {
+        // qDebug() << "Map dirs: "
+        // << KAtlasDirs::localDir() + "/maps/" + localmappaths.at(i);
+        localmapdirs << KAtlasDirs::localDir() + "/maps/earth/" + localmappaths.at(i);
+    }
 
-	for (int i = 0; i < unixmappaths.size(); ++i){
-//		qDebug() << "Map dirs: " << KAtlasDirs::unixDir() + "/maps/" + unixmappaths.at(i);
-		unixmapdirs << KAtlasDirs::unixDir() + "/maps/earth/" + unixmappaths.at(i);
-	}
+    for ( int i = 0; i < sysmappaths.size(); ++i ) {
+        // qDebug() << "Map dirs: " << KAtlasDirs::systemDir() + "/maps/" + sysmappaths.at(i);
+        sysmapdirs << KAtlasDirs::systemDir() + "/maps/earth/" + sysmappaths.at(i);
+    }
+
+    for ( int i = 0; i < unixmappaths.size(); ++i ) {
+        // qDebug() << "Map dirs: " << KAtlasDirs::unixDir() + "/maps/" + unixmappaths.at(i);
+        unixmapdirs << KAtlasDirs::unixDir() + "/maps/earth/" + unixmappaths.at(i);
+    }
         
-        QStringList mapfiles;
-	QStringList tmp;
-	QString themedir, themedirname;
-	QString themexml;
+    QStringList  mapfiles;
+    QStringList  tmp;
+    QString      themedir;
+    QString      themedirname;
+    QString      themexml;
 
-	QStringListIterator i( localmapdirs );
-	while (i.hasNext()){
-		themedir = i.next() + "/";
-		themedirname = QDir( themedir ).dirName();
-//		qDebug() << themedir;
+    QStringListIterator  i( localmapdirs );
+    while ( i.hasNext() ) {
+        themedir = i.next() + "/";
+        themedirname = QDir( themedir ).dirName();
+        // qDebug() << themedir;
 
-		tmp = ( QDir( themedir ) ).entryList( QStringList( "*.dgml" ),
-                                    QDir::Files | QDir::NoSymLinks );
-		if (!tmp.isEmpty()){
-			QStringListIterator k( tmp );
-			while (k.hasNext()){
-				themexml = k.next();
-//				qDebug() << themedirname + "/" + themexml;
-				mapfiles << themedirname + "/" + themexml;
-			}
-		}
-//		else qDebug("Empty *.dgml list!");
-	}
+        tmp = ( QDir( themedir ) ).entryList( QStringList( "*.dgml" ),
+                                              QDir::Files | QDir::NoSymLinks );
+        if ( !tmp.isEmpty() ) {
+            QStringListIterator  k( tmp );
+            while ( k.hasNext() ) {
+                themexml = k.next();
+                // qDebug() << themedirname + "/" + themexml;
+                mapfiles << themedirname + "/" + themexml;
+            }
+        }
+        // else qDebug("Empty *.dgml list!");
+    }
 
-	QStringListIterator j( sysmapdirs );
-	while (j.hasNext()){
-		themedir = j.next();
-//		qDebug() << themedir;
-		themedirname = QDir( themedir ).dirName();
+    QStringListIterator  j( sysmapdirs );
+    while ( j.hasNext() ) {
+        themedir = j.next();
+        // qDebug() << themedir;
+        themedirname = QDir( themedir ).dirName();
 
-		tmp = ( QDir( themedir ) ).entryList( QStringList( "*.dgml" ),
-                                    QDir::Files | QDir::NoSymLinks );
-		if (!tmp.isEmpty()){
-			QStringListIterator l( tmp );
-			while (l.hasNext()){
-				themexml = l.next();
-//				qDebug() << themedirname + "/" + themexml;
-				mapfiles << themedirname + "/" + themexml;
-			}
-		}
-//		else qDebug("Empty *.dgml list!");
-	}
+        tmp = ( QDir( themedir ) ).entryList( QStringList( "*.dgml" ),
+                                              QDir::Files | QDir::NoSymLinks );
+        if ( !tmp.isEmpty() ) {
+            QStringListIterator  l( tmp );
+            while ( l.hasNext() ) {
+                themexml = l.next();
+                // qDebug() << themedirname + "/" + themexml;
+                mapfiles << themedirname + "/" + themexml;
+            }
+        }
+        // else qDebug("Empty *.dgml list!");
+    }
 
-	QStringListIterator k( unixmapdirs );
-	while (k.hasNext()){
-		themedir = k.next();
-//		qDebug() << themedir;
-		themedirname = QDir( themedir ).dirName();
+    QStringListIterator  k( unixmapdirs );
+    while ( k.hasNext() ) {
+        themedir = k.next();
+        // qDebug() << themedir;
+        themedirname = QDir( themedir ).dirName();
 
-		tmp = ( QDir( themedir ) ).entryList( QStringList( "*.dgml" ),
-                                    QDir::Files | QDir::NoSymLinks );
-		if (!tmp.isEmpty()){
-			QStringListIterator l( tmp );
-			while (l.hasNext()){
-				themexml = l.next();
-//				qDebug() << themedirname + "/" + themexml;
-				mapfiles << themedirname + "/" + themexml;
-			}
-		}
-//		else qDebug("Empty *.dgml list!");
-	}
+        tmp = ( QDir( themedir ) ).entryList( QStringList( "*.dgml" ),
+                                              QDir::Files | QDir::NoSymLinks );
+        if ( !tmp.isEmpty() ) {
+            QStringListIterator  l( tmp );
+            while ( l.hasNext() ) {
+                themexml = l.next();
+                // qDebug() << themedirname + "/" + themexml;
+                mapfiles << themedirname + "/" + themexml;
+            }
+        }
+        // else qDebug("Empty *.dgml list!");
+    }
 
-	mapfiles.sort();
+    mapfiles.sort();
 
-	for (int i = 1; i < mapfiles.size(); ++i){
-		if ( mapfiles.at(i) == mapfiles.at(i-1) ){
-			mapfiles.removeAt(i);
-			--i;
-		}
-	}
+    for ( int i = 1; i < mapfiles.size(); ++i ) {
+        if ( mapfiles.at(i) == mapfiles.at( i - 1 ) ) {
+            mapfiles.removeAt(i);
+            --i;
+        }
+    }
 
-//	for (int i = 0; i < mapfiles.size(); ++i)
-//		qDebug() << "Files: " << mapfiles.at(i);
+    // for (int i = 0; i < mapfiles.size(); ++i)
+    //	   qDebug() << "Files: " << mapfiles.at(i);
 
-	return mapfiles;
+    return mapfiles;
 }
 
-QStandardItemModel* MapTheme::mapThemeModel( const QStringList& stringlist ){
 
-	QStandardItemModel* mapthememodel = new QStandardItemModel();
+QStandardItemModel* MapTheme::mapThemeModel( const QStringList& stringlist )
+{
+    QStandardItemModel  *mapthememodel = new QStandardItemModel();
 
-	QModelIndex parent;
-	mapthememodel->insertColumns(0, 3, parent);
+    QModelIndex  parent;
+    mapthememodel->insertColumns(0, 3, parent);
 
-        mapthememodel->setHeaderData(0, Qt::Horizontal, tr("Name"));
-        mapthememodel->setHeaderData(1, Qt::Horizontal, tr("Description"));
-        mapthememodel->setHeaderData(2, Qt::Horizontal, tr("Path"));
+    mapthememodel->setHeaderData(0, Qt::Horizontal, tr("Name"));
+    mapthememodel->setHeaderData(1, Qt::Horizontal, tr("Description"));
+    mapthememodel->setHeaderData(2, Qt::Horizontal, tr("Path"));
 
-	QStringListIterator it(stringlist);
+    QStringListIterator  it(stringlist);
+    MapTheme            *maptheme = new MapTheme();
 
-	MapTheme* maptheme = new MapTheme();
+    int  row = 0;
+    while ( it.hasNext() ) {
+        QString currentmaptheme = it.next();
+        maptheme->open( KAtlasDirs::path( "maps/earth/" + currentmaptheme ) );
 
-	int row = 0;
-	while (it.hasNext()){
-		QString currentmaptheme = it.next();
-		maptheme->open( KAtlasDirs::path( "maps/earth/" + currentmaptheme ) );
+        mapthememodel->insertRows(row, 1, QModelIndex());
+        mapthememodel->setData( mapthememodel->index( row, 0, QModelIndex() ),
+                                maptheme->name(), Qt::DisplayRole );
+        mapthememodel->setData( mapthememodel->index( row, 0, QModelIndex() ),
+                                QIcon( KAtlasDirs::path( "maps/earth/" +  maptheme->prefix() + "/" + maptheme->icon() ) ), 
+                                Qt::DecorationRole );
+        mapthememodel->setData( mapthememodel->index( row, 1, QModelIndex() ),
+                                maptheme->description(), Qt::ToolTipRole);
+        mapthememodel->setData( mapthememodel->index( row, 2, QModelIndex() ),
+                                currentmaptheme );
+    }
 
-		mapthememodel->insertRows(row, 1, QModelIndex());
-		mapthememodel->setData( mapthememodel->index(row, 0, QModelIndex()), maptheme->name(), Qt::DisplayRole );
-		mapthememodel->setData( mapthememodel->index(row, 0, QModelIndex()), QIcon( KAtlasDirs::path( "maps/earth/" +  maptheme->prefix() + "/" + maptheme->icon() ) ), Qt::DecorationRole );
-		mapthememodel->setData( mapthememodel->index(row, 1, QModelIndex()), maptheme->description(), Qt::ToolTipRole);
-		mapthememodel->setData( mapthememodel->index(row, 2, QModelIndex()), currentmaptheme );
-	}
-
-	return mapthememodel;
+    return mapthememodel;
 }
+
 
 #ifndef Q_OS_MACX
 #include "maptheme.moc"
--- src/maptheme.h
+++ src/maptheme.h
@@ -1,51 +1,58 @@
 #ifndef MAPTHEME_H
 #define MAPTHEME_H
 
+
 #include <QtCore/QObject>
 #include <QtGui/QColor>
 
+
 class QStandardItemModel;
 
-typedef struct {
-	bool enabled;
-	QString type;
-	QString name;
-	QString dem;
+
+typedef struct
+{
+    bool     enabled;
+    QString  type;
+    QString  name;
+    QString  dem;
 } DgmlLayer;
+
 
 class MapTheme : public QObject
 {
     Q_OBJECT
 
 public:
-	MapTheme(QObject *parent = 0);
-	int open( QString path );
+    MapTheme(QObject *parent = 0);
+    int open( QString path );
 
-	QString name() const { return m_name; }
-	QString prefix() const { return m_prefix; }
-	QString icon() const { return m_icon; }
-	QColor labelColor() const { return m_labelcolor; }
-	QString tilePrefix() const { return ( m_prefix ); }
-	QString description() const { return m_description; }
-	QString installMap() const { return m_installmap; }
-	DgmlLayer bitmaplayer() const { return m_bitmaplayer; }
-	DgmlLayer vectorlayer() const { return m_vectorlayer; }
+    QString name()          const { return m_name;        }
+    QString prefix()        const { return m_prefix;      }
+    QString icon()          const { return m_icon;        }
+    QColor labelColor()     const { return m_labelcolor;  }
+    QString tilePrefix()    const { return m_prefix;      }
+    QString description()   const { return m_description; }
+    QString installMap()    const { return m_installmap;  }
+    DgmlLayer bitmaplayer() const { return m_bitmaplayer; }
+    DgmlLayer vectorlayer() const { return m_vectorlayer; }
 
-	int maxTileLevel() const { return m_maxtilelevel; }
+    int maxTileLevel() const { return m_maxtilelevel; }
 
-	static QStringList findMapThemes( const QString& );
-	static QStandardItemModel* mapThemeModel( const QStringList& stringlist );
+    static QStringList findMapThemes( const QString& );
+    static QStandardItemModel* mapThemeModel( const QStringList& stringlist );
+
 private:
-	QString m_name;
-	QString m_prefix;
-	QString m_icon;
-	QColor m_labelcolor;
-	QString m_tileprefix;
-	QString m_description;
-	QString m_installmap;
-	DgmlLayer m_vectorlayer;
-	DgmlLayer m_bitmaplayer;
-	int m_maxtilelevel;
+    QString    m_name;
+    QString    m_prefix;
+    QString    m_icon;
+    QColor     m_labelcolor;
+    QString    m_tileprefix;
+    QString    m_description;
+    QString    m_installmap;
+    DgmlLayer  m_vectorlayer;
+    DgmlLayer  m_bitmaplayer;
+    int        m_maxtilelevel;
 };
+
 
 #endif // MAPTHEME_H
