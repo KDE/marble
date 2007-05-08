61827d8cfadc26515b03128dbf0f4117f16e182f
Torsten Rahn <tackat@kde.org>
2007-05-08 12:24:29 +0000

    - Marble got a docbook handbook now.
    svn path=/trunk/playground/base/marble/; revision=662494

--- src/QtMainWindow.cpp
+++ src/QtMainWindow.cpp
@@ -19,7 +19,7 @@
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
 {
-    m_katlascontrol = new KAtlasControl(0);
+    m_katlascontrol = new KAtlasControl(this);
 
     setWindowTitle( tr("Marble - Desktop Globe") );
     setCentralWidget(m_katlascontrol);
