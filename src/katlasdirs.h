49fc5770d30cfd001561ce6ccdf88c22db6ea1bb
Inge Wallin <inge@lysator.liu.se>
2007-05-21 21:44:22 +0000

    clean katlasdirs.h
    svn path=/trunk/kdereview/marble/; revision=667112

--- src/katlasdirs.h
+++ src/katlasdirs.h
@@ -1,50 +1,52 @@
 #ifndef KATLASDIRS_H
 #define KATLASDIRS_H
 
-#include <QtGui/QApplication>
+
 #include <QtCore/QFile>
 #include <QtCore/QDir>
+#include <QtGui/QApplication>
+
 
 class KAtlasDirs
 {
-  public:
+ public:
     static QString path( const QString& path ) 
-	{ 
-		QString fullpath = systemDir() + "/" + path;	// system path
-		QString localpath = localDir() + "/" + path;	// local path
-		QString unixpath = unixDir() + "/" + path;	// unix path
+    { 
+        QString  fullpath  = systemDir() + "/" + path;	// system path
+        QString  localpath = localDir() + "/" + path;	// local path
+        QString  unixpath  = unixDir() + "/" + path;	// unix path
 	
-		if( QFile::exists( unixpath ) )
-			fullpath = unixpath;
-		else if( QFile::exists( localpath ) )
-			fullpath = localpath;
+        if ( QFile::exists( unixpath ) )
+            fullpath = unixpath;
+        else if ( QFile::exists( localpath ) )
+            fullpath = localpath;
 
-        	return QDir(fullpath).canonicalPath(); 
-	}
+        return QDir( fullpath ).canonicalPath(); 
+    }
 
     static QString systemDir() 
-	{ 
-          return QDir( qApp->applicationDirPath() 
+    {
+        return QDir( qApp->applicationDirPath() 
 #if defined(Q_OS_MACX)
-	+ QLatin1String("/Resources/data")
+                     + QLatin1String( "/Resources/data" )
 #elif defined(Q_OS_UNIX)
-	+ QLatin1String("/../share/apps/marble/data")
+                     + QLatin1String( "/../share/apps/marble/data" )
 #elif defined(Q_OS_WIN)
-	+ QLatin1String("/data")
+                     + QLatin1String( "/data" )
 #endif
-	  ).canonicalPath();
-        }
+                     ).canonicalPath();
+    }
 
     static QString localDir() 
-	{ 
-		return QString( QDir::homePath() + "/.marble/data");	// local path
-	}
+    { 
+        return QString( QDir::homePath() + "/.marble/data" ); // local path
+    }
 
     static QString unixDir()
-        {
-		return QString("/usr/share/marble/data");	// unix system path
-	}
-
+    {
+        return QString( "/usr/share/marble/data" );	// unix system path
+    }
 };
+
 
 #endif // KATLASDIRS_H
