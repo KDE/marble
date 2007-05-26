38afdddd085f876f4d12818a35d4b83fe3781f72
Inge Wallin <inge@lysator.liu.se>
2007-05-26 16:42:28 +0000

    Krazy: Added Copyright and License
    svn path=/trunk/kdereview/marble/; revision=668510

--- tools/svg2pnt/svg2pnt.cpp
+++ tools/svg2pnt/svg2pnt.cpp
@@ -1,6 +1,20 @@
+//
+// This file is part of the Marble Desktop Globe.
+//
+// This program is free software licensed under the GNU LGPL. You can
+// find a copy of this license in LICENSE.txt in the top directory of
+// the source code.
+//
+// Copyright 2006-2007 Torsten Rahn <tackat@kde.org>"
+// Copyright 2007      Inge Wallin  <ingwa@kde.org>"
+//
+
+
 #include <QApplication>
-#include "svgxmlhandler.h"
 #include <QDebug>
+
+#include "svgxmlhandler.h"
+
 
 int main(int argc, char *argv[])
 {
