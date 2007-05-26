9277bab4ca3f3c90ed9506bbd1b7b5fb330181e5
Inge Wallin <inge@lysator.liu.se>
2007-05-26 19:37:00 +0000

    Clean tools/svg2pnt/svg2png.cpp
    svn path=/trunk/kdereview/marble/; revision=668573

--- tools/svg2pnt/svg2pnt.cpp
+++ tools/svg2pnt/svg2pnt.cpp
@@ -18,27 +18,28 @@
 
 int main(int argc, char *argv[])
 {
-	QString sourcefile;
-	QString targetfile;
+    QString  sourcefile;
+    QString  targetfile;
 
-	QApplication app(argc, argv);
+    QApplication  app(argc, argv);
 
-	for ( int i = 1; i < argc; ++i ) {
-		if ( strcmp( argv[ i ], "-o" ) == 0 ) {
-			targetfile = QString(argv[i+1]);
-			sourcefile = QString(argv[i+2]);
+    for ( int i = 1; i < argc; ++i ) {
+        if ( strcmp( argv[ i ], "-o" ) == 0 ) {
+            targetfile = QString( argv[i+1] );
+            sourcefile = QString( argv[i+2] );
 
-			SVGXmlHandler handler(targetfile);
-			QFile xmlFile(sourcefile);
-			QXmlInputSource inputSource(&xmlFile);
-			QXmlSimpleReader reader;
+            SVGXmlHandler     handler( targetfile );
+            QFile             xmlFile( sourcefile );
+            QXmlInputSource   inputSource(&xmlFile);
+            QXmlSimpleReader  reader;
 
-			reader.setContentHandler(&handler);
-			reader.parse( inputSource );
-			return 0;
-		}
-	}
+            reader.setContentHandler(&handler);
+            reader.parse( inputSource );
 
-	qDebug(" svg2pnt -o targetfile sourcefile");
-	app.exit();
+            return 0;
+        }
+    }
+
+    qDebug( " svg2pnt -o targetfile sourcefile" );
+    app.exit();
 }
