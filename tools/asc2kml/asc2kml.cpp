f25074d66e7cd101b697e9ccbeb32422c36932f9
Inge Wallin <inge@lysator.liu.se>
2007-05-26 16:53:54 +0000

    Cleaned asc2kml.cpp
    svn path=/trunk/kdereview/marble/; revision=668513

--- tools/asc2kml/asc2kml.cpp
+++ tools/asc2kml/asc2kml.cpp
@@ -11,140 +11,155 @@
 
 
 // #include <zlib.h>
-#include <QApplication>
-#include <QDebug>
 
 #include <QFile>
 #include <QStringList>
+#include <QApplication>
+#include <QDebug>
 
-QString escapeXml( const QString &str ) {
-	QString xml = str;
-	xml.replace("&", "&amp;");
-	xml.replace("<", "&lt;");
-	xml.replace(">", "&gt;");
-	xml.replace("'", "&apos;");
-	xml.replace("\"", "&quot;");
-	return xml;
+
+QString escapeXml( const QString &str )
+{
+    QString xml = str;
+    xml.replace("&", "&amp;");
+    xml.replace("<", "&lt;");
+    xml.replace(">", "&gt;");
+    xml.replace("'", "&apos;");
+    xml.replace("\"", "&quot;");
+
+    return xml;
 }
+
 
 int main(int argc, char *argv[])
 {
-	QString sourcefilename;
-	QString targetfilename;
+    QString  sourcefilename;
+    QString  targetfilename;
 
-	QApplication app(argc, argv);
+    QApplication  app( argc, argv );
 
-	for ( int i = 1; i < argc; ++i ) {
-		if ( strcmp( argv[ i ], "-o" ) == 0 ) {
-			targetfilename = QString(argv[i+1]);
-			sourcefilename = QString(argv[i+2]);
+    for ( int i = 1; i < argc; ++i ) {
+        if ( strcmp( argv[ i ], "-o" ) != 0 )
+            continue;
 
-			qDebug() << "Source: " << sourcefilename;
-			qDebug() << "Target: " << targetfilename;
+        targetfilename = QString(argv[i+1]);
+        sourcefilename = QString(argv[i+2]);
 
-			QFile sourcefile( sourcefilename );
-			sourcefile.open( QIODevice::ReadOnly );
-			QTextStream sourcestream( &sourcefile );  // read the data serialized from the file
-			sourcestream.setCodec("UTF-8");
+        qDebug() << "Source: " << sourcefilename;
+        qDebug() << "Target: " << targetfilename;
 
-			QFile targetfile( targetfilename );
-			targetfile.open( QIODevice::ReadWrite );
-			QTextStream targetstream( &targetfile );
-			targetstream.setCodec("UTF-8");
+        QFile  sourcefile( sourcefilename );
+        sourcefile.open( QIODevice::ReadOnly );
 
-//			gzFile gzDoc = gzopen( targetfilename.toLatin1(), "w");
-//			QTextStream targetstream( new QString() );
+        // Read the data serialized from the file.
+        QTextStream  sourcestream( &sourcefile );
+        sourcestream.setCodec("UTF-8");
 
-			targetstream << "<?xml version=\"1.0\" encoding=\"UTF-8\"?> \n"
-				<< "<kml xmlns=\"http://earth.google.com/kml/2.0\"> \n"
-				<< "<Document> \n";
+        QFile  targetfile( targetfilename );
+        targetfile.open( QIODevice::ReadWrite );
 
-			targetstream << "    <SimpleField> \n"
-				<< "        <name>pop</name> \n"
-				<< "        <type>int</type> \n"
-				<< "    </SimpleField> \n";
+        QTextStream  targetstream( &targetfile );
+        targetstream.setCodec("UTF-8");
 
-			targetstream << "    <SimpleField> \n"
-				<< "        <name>state</name> \n"
-				<< "        <type>string</type> \n"
-				<< "    </SimpleField> \n";
+        // gzFile gzDoc = gzopen( targetfilename.toLatin1(), "w");
+        // QTextStream targetstream( new QString() );
 
-			targetstream << "    <SimpleField> \n"
-				<< "        <name>CountryNameCode</name> \n"
-				<< "        <type>string</type> \n"
-				<< "    </SimpleField> \n";
+        targetstream << "<?xml version=\"1.0\" encoding=\"UTF-8\"?> \n"
+                     << "<kml xmlns=\"http://earth.google.com/kml/2.0\"> \n"
+                     << "<Document> \n";
 
-			targetstream << "    <SimpleField> \n"
-				<< "        <name>role</name> \n"
-				<< "        <type>string</type> \n"
-				<< "    </SimpleField> \n";
+        targetstream << "    <SimpleField> \n"
+                     << "        <name>pop</name> \n"
+                     << "        <type>int</type> \n"
+                     << "    </SimpleField> \n";
 
-			targetstream << "\n";
+        targetstream << "    <SimpleField> \n"
+                     << "        <name>state</name> \n"
+                     << "        <type>string</type> \n"
+                     << "    </SimpleField> \n";
 
-			QString rawline;
-			QString name, state, country, role, popstring, latstring, lngstring; 
-			float lat, lng;
-			int population;
-			QStringList splitline;
+        targetstream << "    <SimpleField> \n"
+                     << "        <name>CountryNameCode</name> \n"
+                     << "        <type>string</type> \n"
+                     << "    </SimpleField> \n";
 
-			while(!sourcestream.atEnd()){
+        targetstream << "    <SimpleField> \n"
+                     << "        <name>role</name> \n"
+                     << "        <type>string</type> \n"
+                     << "    </SimpleField> \n";
 
-				rawline=sourcestream.readLine();
-				splitline = rawline.split("\t");
+        targetstream << "\n";
 
-				name = splitline[0];
+        QString  rawline;
+        QString  name;
+        QString  state;
+        QString  country;
+        QString  role;
+        QString  popstring;
+        QString  latstring;
+        QString  lngstring; 
+        float    lat;
+        float    lng;
+        int          population;
+        QStringList  splitline;
 
-				state = splitline[1];
-				country = splitline[2];
-				role = splitline[3];
+        while ( !sourcestream.atEnd() ) {
 
-				popstring = splitline[4];
-				latstring = splitline[5];
-				lngstring = splitline[6];
+            rawline=sourcestream.readLine();
+            splitline = rawline.split("\t");
 
- 				population = (int) ( 1000 * popstring.toFloat() );
+            name    = splitline[0];
+            state   = splitline[1];
+            country = splitline[2];
+            role    = splitline[3];
 
-				lng = lngstring.left(lngstring.size()-2).toFloat();
-				if (lngstring.contains("W")) lng=-lng;
+            popstring = splitline[4];
+            latstring = splitline[5];
+            lngstring = splitline[6];
 
-				lat = latstring.left(latstring.size()-2).toFloat();
-				if (latstring.contains("S")) lat=-lat;
+            population = (int) ( 1000 * popstring.toFloat() );
 
+            lng = lngstring.left( lngstring.size() - 2 ).toFloat();
+            if ( lngstring.contains( "W" ) )
+                lng=-lng;
 
-				targetstream << "    <Placemark> \n";
-				targetstream << "        <name>" << escapeXml( name ) << "</name> \n";
-				targetstream << "        <state>" << escapeXml( state ) << "</state> \n";
-				targetstream << "        <Country>\n"
-						<< "	     <CountryNameCode>" << escapeXml( country.toUpper() ) << "</CountryNameCode>\n"
-					     << "        </Country> \n";
-				targetstream << "        <role>" << escapeXml( role ) << "</role> \n";
-				targetstream << "        <pop>" 
-					<< escapeXml( QString::number( population ) ) << "</pop> \n";
-				targetstream << "        <Point>\n"
-					<< "            <coordinates>" 
-					<< escapeXml( QString::number( lng ) )
-					<< ","
-					<< escapeXml( QString::number( lat ) )
-					<< "</coordinates> \n"
-					<< "        </Point> \n";
-				targetstream << "    </Placemark> \n";
-			}
+            lat = latstring.left( latstring.size() - 2 ).toFloat();
+            if ( latstring.contains( "S" ) )
+                lat=-lat;
 
-			targetstream << "</Document> \n"
-				<< "</kml> \n";
-			qDebug("Putting");
+            targetstream << "    <Placemark> \n";
+            targetstream << "        <name>" << escapeXml( name ) << "</name> \n";
+            targetstream << "        <state>" << escapeXml( state ) << "</state> \n";
+            targetstream << "        <Country>\n"
+                         << "	     <CountryNameCode>" << escapeXml( country.toUpper() ) << "</CountryNameCode>\n"
+                         << "        </Country> \n";
+            targetstream << "        <role>" << escapeXml( role ) << "</role> \n";
+            targetstream << "        <pop>" 
+                         << escapeXml( QString::number( population ) ) << "</pop> \n";
+            targetstream << "        <Point>\n"
+                         << "            <coordinates>" 
+                         << escapeXml( QString::number( lng ) )
+                         << ","
+                         << escapeXml( QString::number( lat ) )
+                         << "</coordinates> \n"
+                         << "        </Point> \n";
+            targetstream << "    </Placemark> \n";
+        }
 
-//			gzputs( gzDoc, targetstream.readAll().toUtf8() );
-//			gzclose( gzDoc );
+        targetstream << "</Document> \n"
+                     << "</kml> \n";
+        qDebug("Putting");
+
+        // gzputs( gzDoc, targetstream.readAll().toUtf8() );
+        // gzclose( gzDoc );
 			
-			sourcefile.close();
-			targetfile.close();
+        sourcefile.close();
+        targetfile.close();
 
-			qDebug("Finished!");
-			return 0;
-		}
-	}
+        qDebug("Finished!");
+        return 0;
+    }
 
-	qDebug(" asc2kml -o targetfile sourcefile");
-	app.exit();
+    qDebug(" asc2kml -o targetfile sourcefile");
+    app.exit();
 }
