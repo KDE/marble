41a32ba8de7cc4cbe225133020296647d7cb1004
Torsten Rahn <tackat@kde.org>
2007-06-01 11:31:01 +0000

    Adding the plugin dir + class files already.
    svn path=/trunk/KDE/kdeedu/marble/; revision=670403

---
+++ src/plugins/MarbleWidgetPlugin.cpp
@@ -0,0 +1,106 @@
+//
+// This file is part of the Marble Desktop Globe.
+//
+// This program is free software licensed under the GNU LGPL. You can
+// find a copy of this license in LICENSE.txt in the top directory of
+// the source code.
+//
+// Copyright 2005-2007 Torsten Rahn <tackat@kde.org>"
+//
+
+#include "katlasglobe.h"
+#include "MarbleWidget.h"
+#include "MarbleWidgetPlugin.h"
+
+#include <QtPlugin>
+
+MarbleWidgetPlugin::MarbleWidgetPlugin(QObject *parent)
+    : QObject(parent)
+{
+    initialized = false;
+}
+
+MarbleWidgetPlugin::~MarbleWidgetPlugin()
+{
+}
+
+void MarbleWidgetPlugin::initialize(QDesignerFormEditorInterface * /* core */)
+{
+    if (initialized)
+        return;
+
+    initialized = true;
+}
+
+bool MarbleWidgetPlugin::isInitialized() const
+{
+    return initialized;
+}
+
+QWidget *MarbleWidgetPlugin::createWidget(QWidget *parent)
+{
+    // Create the Model (Globe) and one view.
+    KAtlasGlobe *globe = new KAtlasGlobe( parent );
+    return new MarbleWidget( globe, parent );
+}
+
+QString MarbleWidgetPlugin::name() const
+{
+    return "MarbleWidget";
+}
+
+QString MarbleWidgetPlugin::group() const
+{
+    return "Marble Desktop Globe";
+}
+
+QIcon MarbleWidgetPlugin::icon() const
+{
+    return QIcon();
+}
+
+QString MarbleWidgetPlugin::toolTip() const
+{
+    return "";
+}
+
+QString MarbleWidgetPlugin::whatsThis() const
+{
+    return "";
+}
+
+bool MarbleWidgetPlugin::isContainer() const
+{
+    return false;
+}
+
+QString MarbleWidgetPlugin::domXml() const
+{
+    return "<widget class=\"MarbleWidget\" name=\"MarbleWidget\">\n"
+           " <property name=\"geometry\">\n"
+           "  <rect>\n"
+           "   <x>0</x>\n"
+           "   <y>0</y>\n"
+           "   <width>200</width>\n"
+           "   <height>200</height>\n"
+           "  </rect>\n"
+           " </property>\n"
+           " <property name=\"toolTip\" >\n"
+           "  <string>The Earth</string>\n"
+           " </property>\n"
+           " <property name=\"whatsThis\" >\n"
+           "  <string>The Marble widget displays a virtual globe.</string>\n"
+           " </property>\n"
+           "</widget>\n";
+}
+
+QString MarbleWidgetPlugin::includeFile() const
+{
+    return "MarbleWidget.h";
+}
+
+Q_EXPORT_PLUGIN2(MarbleWidgetPlugin, MarbleWidgetPlugin)
+
+#ifndef Q_OS_MACX
+#include "MarbleWidgetPlugin.moc"
+#endif
---
+++ src/plugins/MarbleWidgetPlugin.h
@@ -0,0 +1,47 @@
+//
+// This file is part of the Marble Desktop Globe.
+//
+// This program is free software licensed under the GNU LGPL. You can
+// find a copy of this license in LICENSE.txt in the top directory of
+// the source code.
+//
+// Copyright 2005-2007 Torsten Rahn <tackat@kde.org>"
+// Copyright 2007      Inge Wallin   <ingwa@kde.org>"
+//
+
+//
+// This class is responsible for offering plugin capabilities.
+//
+
+#ifndef MARBLEWIDGETPLUGIN_H
+#define MARBLEWIDGETPLUGIN_H
+
+#include <QtDesigner/QDesignerCustomWidgetInterface>
+
+class MarbleWidgetPlugin : public QObject, public QDesignerCustomWidgetInterface
+{
+    Q_OBJECT
+    Q_INTERFACES(QDesignerCustomWidgetInterface)
+
+public:
+    MarbleWidgetPlugin(QObject *parent = 0);
+    virtual ~MarbleWidgetPlugin();
+
+    bool isContainer() const;
+    bool isInitialized() const;
+    QIcon icon() const;
+    QString domXml() const;
+    QString name() const;
+    QString group() const;
+    QString includeFile() const;
+    QString toolTip() const;
+    QString whatsThis() const;
+    QWidget *createWidget(QWidget *parent);
+
+    void initialize(QDesignerFormEditorInterface *core);
+
+private:
+    bool initialized;
+};
+
+#endif
