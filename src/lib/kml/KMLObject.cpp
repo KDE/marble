b671e0e4396263ff4af0e2d40accd634ef662510
Inge Wallin <inge@lysator.liu.se>
2007-06-05 11:08:56 +0000

    Fix Krazy issues 4 and 14 (license & copyright)
    svn path=/trunk/KDE/kdeedu/marble/; revision=671696

--- src/lib/kml/KMLObject.cpp
+++ src/lib/kml/KMLObject.cpp
@@ -1,3 +1,14 @@
+//
+// This file is part of the Marble Desktop Globe.
+//
+// This program is free software licensed under the GNU LGPL. You can
+// find a copy of this license in LICENSE.txt in the top directory of
+// the source code.
+//
+// Copyright 2007      Murad Tagirov <x@y>
+//
+
+
 #include "KMLObject.h"
 
 KMLObject::KMLObject()
