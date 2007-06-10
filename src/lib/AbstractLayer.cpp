faa84d0fa03cf07f2b2762a1df4b78ff23addb50
Andrew Manson <g.real.ate@gmail.com>
2007-06-10 16:08:41 +0000

    just adding the the .cpp file because of not including it in the last commit
    svn path=/trunk/KDE/kdeedu/marble/; revision=673578

---
+++ src/lib/AbstractLayer.cpp
@@ -0,0 +1,38 @@
+//
+// This file is part of the Marble Desktop Globe.
+//
+// This program is free software licensed under the GNU LGPL. You can
+// find a copy of this license in LICENSE.txt in the top directory of
+// the source code.
+//
+// Copyright 2006-2007 Torsten Rahn <tackat@kde.org>"
+// Copyright 2007      Inge Wallin  <ingwa@kde.org>"
+//
+
+#include <QtCore/QSize>
+#include <QtCore/QObject>
+#include "AbstractLayer.h"
+#include "Quaternion.h"
+
+AbstractLayer::AbstractLayer(QObject *parent) : QObject(parent){
+    //no layer common things have been so far identified
+}
+
+bool AbstractLayer::getPixelPosFromGeoPoint(float _long, float _lat, QSize screenSize, 
+        Quaternion invRotAxis, int radius, int &xOut,
+        int &yOut)
+{
+    Quaternion qpos(_long,_lat); //temp
+    qpos.rotateAroundAxis(invRotAxis);
+
+    if(qpos.v[Q_Z]>0){
+        xOut = (int)( ( screenSize.width()/2 ) 
+                + ( radius * qpos.v[Q_X] ) );
+        yOut = (int)( ( screenSize.height()/2 ) 
+                + ( radius * qpos.v[Q_Y] ) );
+        return true;
+    }else{
+        return false;
+    }
+}
+
