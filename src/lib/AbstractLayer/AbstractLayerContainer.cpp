58cfa9e8be12db11ada21726253e80454764198e
Andrew Manson <g.real.ate@gmail.com>
2007-06-19 14:13:09 +0000

    adding cpp files for previous commit
    svn path=/trunk/KDE/kdeedu/marble/; revision=677605

---
+++ src/lib/AbstractLayer/AbstractLayerContainer.cpp
@@ -0,0 +1,52 @@
+//
+// This file is part of the Marble Desktop Globe.
+//
+// This program is free software licensed under the GNU LGPL. You can
+// find a copy of this license in LICENSE.txt in the top directory of
+// the source code.
+//
+// Copyright 2007      Andrew Manson    <g.real.ate@gmail.com>
+//
+
+#include "AbstractLayerContainer.h"
+
+AbstractLayerContainer::AbstractLayerContainer( int size ){
+    m_data = new QVector<AbstractLayerData*>( size );
+    m_visible = new QBitArray( size );
+    
+    m_name = 0;
+}
+
+AbstractLayerContainer::AbstractLayerContainer(const QString &name,
+                                               int size ){
+    m_data = new QVector<AbstractLayerData*>( size );
+    m_visible = new QBitArray( size );
+    
+    *m_name = name;
+}
+AbstractLayerContainer::~AbstractLayerContainer(){
+
+}
+
+void AbstractLayerContainer::processVisible(){
+    QVector<AbstractLayerData*>::const_iterator i = m_data -> begin();
+    int temp;
+    
+    for( ; i < m_data -> end() ; ++i ){
+        if ( (*i) -> visible() ){
+            //iterator safety
+            temp = m_data -> indexOf ( *i );
+            m_visible -> setBit ( temp, true );
+        }
+    }
+}
+
+void AbstractLayerContainer::manageMemory(){
+    for( int i = 0 ; i < m_visible -> size () ; ++i ){
+        if ( m_visible -> testBit( i ) ){
+            if ( ! ( this -> contains ( m_data -> at( i ) ) ) ){
+                this -> append( m_data -> at( i ));
+            }
+        }
+    }
+}
---
+++ src/lib/AbstractLayer/AbstractLayerData.cpp
@@ -0,0 +1,24 @@
+//
+// This file is part of the Marble Desktop Globe.
+//
+// This program is free software licensed under the GNU LGPL. You can
+// find a copy of this license in LICENSE.txt in the top directory of
+// the source code.
+//
+// Copyright 2007      Andrew Manson    <g.real.ate@gmail.com>
+//
+
+#include "AbstractLayerData.h"
+
+AbstractLayerData::AbstractLayerData( GeoPoint position ){
+    m_position = new GeoPoint;
+    *m_position = position;
+}
+
+AbstractLayerData::AbstractLayerData( double lat, double lon){
+    m_position = new GeoPoint( lon, lat );
+}
+
+AbstractLayerData::~AbstractLayerData(){
+    delete m_position;
+}
