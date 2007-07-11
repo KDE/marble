a5c3d3a029df2dc62b317ee6121977fb56acd382
Inge Wallin <inge@lysator.liu.se>
2007-07-11 16:42:06 +0000

    Fix Krazy issue 4: Acceptable copyright.
    svn path=/trunk/KDE/kdeedu/marble/; revision=686558

--- src/lib/gps/Track.cpp
+++ src/lib/gps/Track.cpp
@@ -1,4 +1,16 @@
+//
+// This file is part of the Marble Desktop Globe.
+//
+// This program is free software licensed under the GNU LGPL. You can
+// find a copy of this license in LICENSE.txt in the top directory of
+// the source code.
+//
+// Copyright 2007      Andrew Manson  <g.real.ate@gmail.com>
+//
+
+
 #include "Track.h"
+
 
 Track::Track() : AbstractLayerData(0,0)
 {
@@ -9,7 +21,8 @@ Track::~Track()
 {
 }
 
-void Track::addTrackSeg(WaypointContainer trackSeg)
+
+void Track::addTrackSeg( WaypointContainer trackSeg )
 {
-    m_trackSegs->append(trackSeg);
+    m_trackSegs->append( trackSeg );
 }
