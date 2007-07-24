b5594e18644f892893c6e7ea1295113cb8e57fb5
Inge Wallin <inge@lysator.liu.se>
2007-07-24 22:29:23 +0000

    Fix krazy issue 4: Check for an acceptable copyright.
    svn path=/trunk/KDE/kdeedu/marble/; revision=692041

--- src/lib/utils.h
+++ src/lib/utils.h
@@ -1,15 +1,18 @@
+//
 // This file is part of the Marble Desktop Globe.
-// //
-// // This program is free software licensed under the GNU LGPL. You can
-// // find a copy of this license in LICENSE.txt in the top directory of
-// // the source code.
-// //
 //
-// //
-// //
+// This program is free software licensed under the GNU LGPL. You can
+// find a copy of this license in LICENSE.txt in the top directory of
+// the source code.
 //
+// Copyright 2007      Torsten Rahn <tackat@kde.org>"
+// Copyright 2007      Inge Wallin  <ingwa@kde.org>"
+//
+
+
 #ifndef UTILS_H
 #define UTILS_H
 
 const double RAD2INT = 21600.0 / M_PI;
+
 #endif
