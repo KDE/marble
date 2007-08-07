6525b2ad56a5bfc4d6f30d7ba7c4995a2e9c96eb
Pino Toscano <pino@kde.org>
2007-08-07 16:11:56 +0000

    A class with virtual functions needs a virtual destructor.
    svn path=/trunk/KDE/kdeedu/marble/; revision=697381

--- src/lib/kml/Serializable.h
+++ src/lib/kml/Serializable.h
@@ -6,6 +6,8 @@ class QDataStream;
 class Serializable
 {
  public:
+    virtual ~Serializable() {}
+
     virtual void pack( QDataStream& stream ) const = 0;
     virtual void unpack( QDataStream& stream ) = 0;
 };
