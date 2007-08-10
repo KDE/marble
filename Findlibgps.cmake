b8bed58410dfca4e3d85431ef1fe5b8f6ed4b11b
Laurent Montel <montel@kde.org>
2007-08-10 19:51:33 +0000

    Not necessary to add path
    svn path=/trunk/KDE/kdeedu/marble/; revision=698707

--- Findlibgps.cmake
+++ Findlibgps.cmake
@@ -5,10 +5,10 @@
 # Copyright (c) 2006, Andrew Manson, <g.real.ate@gmail.com>
 #
 
-FIND_PATH(libgps_INCLUDES libgpsmm.h /usr/include)
+FIND_PATH(libgps_INCLUDES libgpsmm.h )
 
 
-FIND_LIBRARY(libgps_LIBRARIES gps /usr/lib)
+FIND_LIBRARY(libgps_LIBRARIES gps)
 
 IF( NOT libgps_LIBRARIES)
     MESSAGE(STATUS "Not building with Gpsd Support")
