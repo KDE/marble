28a052a567d7eee06ee96aae73235723c6b45f34
Inge Wallin <inge@lysator.liu.se>
2007-08-11 21:12:36 +0000

    Fix some warnings
    svn path=/trunk/KDE/kdeedu/marble/; revision=699037

--- src/lib/gps/GpsTracking.cpp
+++ src/lib/gps/GpsTracking.cpp
@@ -18,17 +18,18 @@
 
 GpsTracking::GpsTracking( Track *track, TrackingMethod method)
 {
+    Q_UNUSED( method );
     
-    m_gpsCurrentPosition = new TrackPoint( 0,0 );
+    m_gpsCurrentPosition  = new TrackPoint( 0,0 );
     m_gpsPreviousPosition = new TrackPoint( 0,0 );
-    m_gpsTracking           = new TrackPoint( 0,0 );
+    m_gpsTracking         = new TrackPoint( 0,0 );
     
-    m_gpsTrack = track;
+    m_gpsTrack    = track;
     m_gpsTrackSeg = 0;
     
 #ifdef HAVE_LIBGPS
-    m_gpsd = new gpsmm();
-    m_gpsdData = m_gpsd -> open( "127.0.0.1", "2947" );
+    m_gpsd     = new gpsmm();
+    m_gpsdData = m_gpsd->open( "127.0.0.1", "2947" );
     
 #endif
 }
@@ -37,6 +38,7 @@ GpsTracking::GpsTracking( Track *track, TrackingMethod method)
 GpsTracking::~GpsTracking()
 {
 }
+
 
 void GpsTracking::construct( const QSize &canvasSize, double radius,
                              Quaternion invRotAxis )
@@ -59,36 +61,40 @@ void GpsTracking::construct( const QSize &canvasSize, double radius,
     if ( !draw ) {
         return;
     }
+
     double distance = sqrt( AbstractLayer::distance( position,
                                                   previousPosition) );
-    
     if (distance == 0) {
         return;
     }
     
     QPointF unitVector = ( position - previousPosition )
                                     / distance;
-    //the perpindicular of the unit vector between first and second
+    // The normal of the unit vector between first and second
     QPointF unitVector2 = QPointF ( -unitVector.y(), unitVector.x());
     
-    qDebug()<<"unit vectors" << unitVector << unitVector2;
+    qDebug() <<"unit vectors" << unitVector << unitVector2;
     
     previousDraw = currentDraw;
     
     currentDraw.clear();
-    currentDraw   << position
-                  << ( position - ( unitVector * 9 ) 
-                                + ( unitVector2 * 9 ) )
-                  << ( position + ( unitVector * 19.0 ) )
-                  << ( position - ( unitVector * 9 ) 
-                                - ( unitVector2 * 9 ) );
+    currentDraw << position
+                << ( position - ( unitVector * 9 ) 
+                              + ( unitVector2 * 9 ) )
+                << ( position + ( unitVector * 19.0 ) )
+                << ( position - ( unitVector * 9 ) 
+                              - ( unitVector2 * 9 ) );
 }
 
 QRegion GpsTracking::update(const QSize &canvasSize, double radius,
                             Quaternion invRotAxis) 
 {
-#ifdef HAVE_LIBGPS
-    if( m_gpsdData !=0 ){
+#ifndef HAVE_LIBGPS
+    Q_UNUSED( canvasSize );
+    Q_UNUSED( radius );
+    Q_UNUSED( invRotAxis );
+#else
+    if( m_gpsdData != 0 ) {
         m_gpsdData =m_gpsd->query( "p" );
         
         m_gpsTracking ->setPosition( m_gpsdData->fix.latitude,
@@ -97,18 +103,13 @@ QRegion GpsTracking::update(const QSize &canvasSize, double radius,
         if (m_gpsTrackSeg == 0 ){
             m_gpsTrackSeg = new TrackSegment();
         }
-        if ( !( m_gpsPreviousPosition->position()  
-                == m_gpsTracking->position() ) )
-        {
+        if ( m_gpsPreviousPosition->position() != m_gpsTracking->position() ) {
             m_gpsTrackSeg->append( m_gpsPreviousPosition );
             m_gpsPreviousPosition = m_gpsCurrentPosition;
-            m_gpsCurrentPosition = new TrackPoint( *m_gpsTracking );
+            m_gpsCurrentPosition  = new TrackPoint( *m_gpsTracking );
         }
-            
-        
     } else {
-        
-        if (m_gpsTrackSeg != 0  && (m_gpsTrackSeg->size() >0)) {
+        if ( m_gpsTrackSeg != 0  && m_gpsTrackSeg->size() > 0 ) ) {
             m_gpsTrack->append( m_gpsTrackSeg );
             m_gpsTrackSeg = 0;
         } 
@@ -120,13 +121,12 @@ QRegion GpsTracking::update(const QSize &canvasSize, double radius,
     QRect temp2(previousDraw.boundingRect().toRect());
     
     temp1.adjust( -5, -5, 10, 10);
-    temp2.adjust( -5,-5, 10, 10);
+    temp2.adjust( -5, -5, 10, 10);
     
     return QRegion(temp1).united( QRegion(temp2) );
     
 #endif
     return QRegion();
-
 }
 
 void GpsTracking::draw( ClipPainter *painter,
@@ -137,12 +137,9 @@ void GpsTracking::draw( ClipPainter *painter,
     painter->setBrush( Qt::white );
     painter->drawPolygon( currentDraw, Qt::OddEvenFill );
     
-    if( m_gpsTrackSeg != 0) {
-       m_gpsTrackSeg -> draw ( painter, canvasSize, radius, 
-                               invRotAxis );
-    
+    if ( m_gpsTrackSeg != 0) {
+       m_gpsTrackSeg->draw( painter, canvasSize, radius, invRotAxis );
     }
-    
 }
 
 
