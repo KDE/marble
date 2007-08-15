58a9ff11c4f706d496afe19d94767f4d4a9c4d82
Torsten Rahn <tackat@kde.org>
2007-08-15 12:06:31 +0000

    * src/lib/MarbleDirs.h:     Adding some explanations how MarbleDirs is     meant to work.
    svn path=/trunk/KDE/kdeedu/marble/; revision=700382

--- src/lib/MarbleDirs.h
+++ src/lib/MarbleDirs.h
@@ -26,6 +26,50 @@
 
 #include <config-marble.h>
 
+/**
+ * @short A class that manages data look-up for Marble.
+ *
+ * The class basically does for Marble what KStandardDirs does for KDE.
+ * Given that the MarbleWidget is a Qt4-only library and given that it 
+ * comes with its own model and data we need this class.
+ *
+ * The class needs to respect the requirements of the different plattforms,
+ * so to avoid very different implementations for each plattform we 
+ * specify how data should get looked up:
+ * 
+ * Generally there are two places of which Marble will draw it's data from:
+ * 
+ * "localDir" and "systemDir".
+ * 
+ * look-up of the data should happen in the localDir first.
+ * Only if the look-up in the localDir failed then MarbleDirs should 
+ * look up the data in the systemDir.
+ * 
+ * homeDir:
+ * The place for homeDir should match space that is fully accessible to 
+ * the user. On Unix-like plattforms this matches 
+ * QDir::homePath() + "/.marble/data"
+ * 
+ * systemDir:
+ * Initially the systemDir should match the place where cmake installed the 
+ * data for marble. It should be possible to change that location using
+ * the cmake MARBLE_DATA_PATH option at compile time.
+ * 
+ * To allow kiosk-like setups and for custom setups in general
+ * it should be possible to change the place of the systemDir at runtime. 
+ * Therefore we introduce a global variable "MarbleDataPath" in the 
+ * MarbleDirs.h source code.
+ * Initially MarbleDataPath is empty. The systemDir will point to 
+ * MarbleDataPath as soon as it gets changed to a valid non-empty path. So 
+ * as soon as MarbleDataPath contains a valid path the path specified by 
+ * cmake will get ignored.
+
+ * It's the duty of each application that uses the MarbleWidget to retrieve 
+ * the value of the MarbleDataPath from the MarbleWidget and to save it 
+ * in its settings and restore it on start-up of the application.
+ * 
+ */
+
 class MarbleDirs
 {
  public:
