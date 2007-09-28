be3fc0c6128a87fd5a05a39c80dd367d8be2099f
Tim Sutton <tim@linfiniti.com>
2007-09-28 17:50:37 +0000

    Fix for compile error on MSVC express 2005. 2 is ambiguous so we need to use 2.0
    svn path=/branches/marble/0.4/marble/; revision=718441

--- src/lib/TileCreator.cpp
+++ src/lib/TileCreator.cpp
@@ -68,7 +68,7 @@ void TileCreator::run()
     } 
 
     // Calculating Maximum Tile Level
-    float approxMaxTileLevel = log( imageWidth / ( 2 * tileSize ) ) / log( 2 );
+    float approxMaxTileLevel = log( imageWidth / ( 2.0 * tileSize ) ) / log( 2.0 );
 
     int  maxTileLevel = 0;
     if ( approxMaxTileLevel == int( approxMaxTileLevel ) )
