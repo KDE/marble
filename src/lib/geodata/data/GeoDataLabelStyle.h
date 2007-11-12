61719b4d046c0328716d17a8f793b76f8a402391
Inge Wallin <inge@lysator.liu.se>
2007-11-12 18:56:34 +0000

    Add Apidox
    svn path=/trunk/KDE/kdeedu/marble/; revision=735839

--- src/lib/geodata/data/GeoDataLabelStyle.h
+++ src/lib/geodata/data/GeoDataLabelStyle.h
@@ -6,38 +6,77 @@
 // the source code.
 //
 // Copyright 2007      Murad Tagirov <tmurad@gmail.com>
+// Copyright 2007      Inge Wallin   <ingwa@kde.org>
 //
 
 
 #ifndef GEODATALABELSTYLE_H
 #define GEODATALABELSTYLE_H
 
-#include "GeoDataColorStyle.h"
 
 #include <QtGui/QFont>
 
+#include "GeoDataColorStyle.h"
+
+
+/**
+ * @short specifies how the <b>name</b> of a GeoDataFeature is drawn
+ *
+ * A GeoDataLabelStyle specifies how the <b>name</b> of a
+ * GeoDataFeature is drawn in the viewer. A custom color, color mode
+ * (both inherited from GeoDataColorStyle, and scale for the label
+ * (name) can be specified.
+ */
 class GeoDataLabelStyle : public GeoDataColorStyle
 {
   public:
+    /// Construct a new GeoDataLabelStyle
     GeoDataLabelStyle();
+    /**
+     * @brief Construct a new GeoDataLabelStyle
+     * @param  font   the font to use when showing the name
+     * @param  color  the color to use when showing the name @see GeoDataColorStyle
+     */
     GeoDataLabelStyle( const QFont &font, const QColor &color );
 
+    /**
+     * @brief Set the scale of the label
+     * @param  scale  the new scale
+     */
     void setScale( const float &scale );
+    /**
+     * @brief Return the current scale of the label
+     * @return  the current scale
+     */
     float scale() const;
 
+    /**
+     * @brief Set the font of the label
+     * @param  font  the new font
+     */
     void setFont( const QFont &font );
+    /**
+     * @brief Return the current font of the label
+     * @return  the current font
+     */
     QFont font() const;
 
-    /*
-     * Serializable methods
+    /**
+     * @brief  Serialize the style to a stream.
+     * @param  stream  the stream
      */
     virtual void pack( QDataStream& stream ) const;
+    /**
+     * @brief  Unserialize the style from a stream
+     * @param  stream  the stream
+     */
     virtual void unpack( QDataStream& stream );
 
   protected:
-    float m_scale;
-
-    QFont m_font;   // No KML property
+    /// The current scale of the label
+    float  m_scale;
+    /// The current font of the label
+    QFont  m_font;   // Not a KML property
 };
 
 #endif // GEODATALABELSTYLE_H
