c4760fc0c4d39396d5a001317d43a065eacbc43d
Inge Wallin <inge@lysator.liu.se>
2007-11-12 13:55:48 +0000

    Add APIDOX
    svn path=/trunk/KDE/kdeedu/marble/; revision=735713

--- src/lib/geodata/data/GeoDataStyleSelector.h
+++ src/lib/geodata/data/GeoDataStyleSelector.h
@@ -6,32 +6,62 @@
 // the source code.
 //
 // Copyright 2007      Murad Tagirov <tmurad@gmail.com>
+// Copyright 2007      Inge Wallin   <ingwa@kde.org>
 //
 
 
 #ifndef GEODATASTYLESELECTOR_H
 #define GEODATASTYLESELECTOR_H
 
+
 #include <QtCore/QString>
+
 #include "GeoDataObject.h"
 
+
+/**
+ * @short a base class for the style classes
+ *
+ * A GeoDataStyleSelector is a base class for the style classes Style
+ * and StyleMap classes. The StyleMap class selects a style based on
+ * the current mode of a Placemark (highlighted or normal). An element
+ * derived from StyleSelector is uniquely identified by its id and its
+ * url.
+ *
+ * @See GeoDataStyle
+ * @See GeoDataStyleMap
+ */
 class GeoDataStyleSelector : public GeoDataObject
 {
   public:
+    /**
+     * @brief Set a new style id.
+     * @param  value  the new id
+     */
     void setStyleId( const QString &value );
+    /**
+     * @brief Return the style id.
+     * @return the style id
+     */
     QString styleId() const;
 
-    /*
-     * Serializable methods
+    /**
+     * @brief Serialize the styleselector to a stream
+     * @param  stream  the stream
      */
     virtual void pack( QDataStream& stream ) const;
+    /**
+     * @brief  Unserialize the styleselector from a stream
+     * @param  stream  the stream
+     */
     virtual void unpack( QDataStream& stream );
 
   protected:
     GeoDataStyleSelector();
 
   protected:
-    QString m_styleId;
+    /// The style id.
+    QString  m_styleId;
 };
 
 #endif // GEODATASTYLESELECTOR_H
