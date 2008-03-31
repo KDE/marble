8612c706f822e6e850ac750d405f85c0f5c46edd
Adriaan de Groot <groot@kde.org>
2008-03-31 00:05:31 +0000

    Sun locates the PI sunwhere else
    svn path=/trunk/KDE/kdeedu/marble/; revision=792040

--- src/lib/SunLocator.h
+++ src/lib/SunLocator.h
@@ -4,6 +4,10 @@
 #define SUNLOCATOR_H
 
 #include <cmath>
+// M_PI is sometimes defined in <cmath>
+#ifndef M_PI 
+#define M_PI 3.14159265358979323846264338327950288419717
+#endif
 
 #include <QtCore/QObject>
 #include <QtGui/QColor>
