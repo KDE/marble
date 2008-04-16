0c65fefca9037c483939ad88a4d2dc7675e32492
Patrick Spendrin <ps_ml@gmx.de>
2008-04-16 20:56:32 +0000

    export the dgml elements
    svn path=/trunk/KDE/kdeedu/marble/; revision=797787

--- src/lib/geodata/handlers/dgml/DGMLElementDictionary.h
+++ src/lib/geodata/handlers/dgml/DGMLElementDictionary.h
@@ -22,49 +22,50 @@
 #ifndef DGMLElementDictionary_h
 #define DGMLElementDictionary_h
 
+#include <marble_export.h>
 
 // Lists all known DGML 2.0 tags
 // http://edu.kde.org/marble/apis/dgml/documentation/dgmltags.html)
 namespace GeoSceneElementDictionary {
-    extern const char* dgmlTag_nameSpace20;
+    extern MARBLE_EXPORT const char* dgmlTag_nameSpace20;
 
-    extern const char* dgmlTag_Available;
-    extern const char* dgmlTag_Coastline;
-    extern const char* dgmlTag_Color;
-    extern const char* dgmlTag_CustomPlugin;
-    extern const char* dgmlTag_Dem;
-    extern const char* dgmlTag_Description;
-    extern const char* dgmlTag_Discrete;
-    extern const char* dgmlTag_Dgml;
-    extern const char* dgmlTag_Document;
-    extern const char* dgmlTag_Enabled;
-    extern const char* dgmlTag_Filter;
-    extern const char* dgmlTag_Geodata;
-    extern const char* dgmlTag_Head;
-    extern const char* dgmlTag_Icon;
-    extern const char* dgmlTag_InstallMap;
-    extern const char* dgmlTag_Item;
-    extern const char* dgmlTag_Layer;
-    extern const char* dgmlTag_Legend;
-    extern const char* dgmlTag_Map;
-    extern const char* dgmlTag_Maximum;
-    extern const char* dgmlTag_Minimum;
-    extern const char* dgmlTag_Name;
-    extern const char* dgmlTag_Palette;
-    extern const char* dgmlTag_Pick;
-    extern const char* dgmlTag_Prefix;
-    extern const char* dgmlTag_Property;
-    extern const char* dgmlTag_Section;
-    extern const char* dgmlTag_Settings;
-    extern const char* dgmlTag_SourceDir;
-    extern const char* dgmlTag_SourceFile;
-    extern const char* dgmlTag_Target;
-    extern const char* dgmlTag_Text;
-    extern const char* dgmlTag_Texture;
-    extern const char* dgmlTag_Theme;
-    extern const char* dgmlTag_Vector;
-    extern const char* dgmlTag_Visible;
-    extern const char* dgmlTag_Zoom;
+    extern MARBLE_EXPORT  const char* dgmlTag_Available;
+    extern MARBLE_EXPORT  const char* dgmlTag_Coastline;
+    extern MARBLE_EXPORT  const char* dgmlTag_Color;
+    extern MARBLE_EXPORT  const char* dgmlTag_CustomPlugin;
+    extern MARBLE_EXPORT  const char* dgmlTag_Dem;
+    extern MARBLE_EXPORT  const char* dgmlTag_Description;
+    extern MARBLE_EXPORT  const char* dgmlTag_Discrete;
+    extern MARBLE_EXPORT  const char* dgmlTag_Dgml;
+    extern MARBLE_EXPORT const char* dgmlTag_Document;
+    extern MARBLE_EXPORT const char* dgmlTag_Enabled;
+    extern MARBLE_EXPORT const char* dgmlTag_Filter;
+    extern MARBLE_EXPORT const char* dgmlTag_Geodata;
+    extern MARBLE_EXPORT const char* dgmlTag_Head;
+    extern MARBLE_EXPORT const char* dgmlTag_Icon;
+    extern MARBLE_EXPORT const char* dgmlTag_InstallMap;
+    extern MARBLE_EXPORT const char* dgmlTag_Item;
+    extern MARBLE_EXPORT const char* dgmlTag_Layer;
+    extern MARBLE_EXPORT const char* dgmlTag_Legend;
+    extern MARBLE_EXPORT const char* dgmlTag_Map;
+    extern MARBLE_EXPORT const char* dgmlTag_Maximum;
+    extern MARBLE_EXPORT const char* dgmlTag_Minimum;
+    extern MARBLE_EXPORT const char* dgmlTag_Name;
+    extern MARBLE_EXPORT const char* dgmlTag_Palette;
+    extern MARBLE_EXPORT const char* dgmlTag_Pick;
+    extern MARBLE_EXPORT const char* dgmlTag_Prefix;
+    extern MARBLE_EXPORT const char* dgmlTag_Property;
+    extern MARBLE_EXPORT const char* dgmlTag_Section;
+    extern MARBLE_EXPORT const char* dgmlTag_Settings;
+    extern MARBLE_EXPORT const char* dgmlTag_SourceDir;
+    extern MARBLE_EXPORT const char* dgmlTag_SourceFile;
+    extern MARBLE_EXPORT const char* dgmlTag_Target;
+    extern MARBLE_EXPORT const char* dgmlTag_Text;
+    extern MARBLE_EXPORT const char* dgmlTag_Texture;
+    extern MARBLE_EXPORT const char* dgmlTag_Theme;
+    extern MARBLE_EXPORT const char* dgmlTag_Vector;
+    extern MARBLE_EXPORT const char* dgmlTag_Visible;
+    extern MARBLE_EXPORT const char* dgmlTag_Zoom;
 }
 
 // Helper macro
