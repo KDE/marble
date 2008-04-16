5dd2cb4bd4bfcb3cb3772df6e88820e78a1eaf57
Patrick Spendrin <ps_ml@gmx.de>
2008-04-16 21:15:05 +0000

    add export - for now geodata is within marblewidget, if it comes out of it, make sure to change this file
    svn path=/trunk/KDE/kdeedu/marble/; revision=797823

---
+++ src/lib/geodata/geodata_export.h
@@ -0,0 +1,26 @@
+//
+// This file is part of the Marble Desktop Globe.
+//
+// This program is free software licensed under the GNU LGPL. You can
+// find a copy of this license in LICENSE.txt in the top directory of
+// the source code.
+//
+// Copyright 2007      Christian Ehrlicher <ch.ehrlicher@gmx.de>"
+// Copyright 2008      Patrick Spendrin <ps_ml@gmx.de>"
+//
+
+#ifndef GEODATA_EXPORT_H
+#define GEODATA_EXPORT_H
+
+#include <QtCore/QtGlobal>
+
+// please make sure to change MAKE_MARBLE_LIB to MAKE_GEODATA_LIB if geodata is an independent library
+#ifndef GEODATA_EXPORT
+# ifdef MAKE_MARBLE_LIB
+#  define GEODATA_EXPORT Q_DECL_EXPORT
+# else
+#  define GEODATA_EXPORT Q_DECL_IMPORT
+# endif
+#endif
+
+#endif // GEODATA_EXPORT_H
