597a4289c362468869074db0491e80e44d509878
Patrick Spendrin <ps_ml@gmx.de>
2008-04-19 22:59:01 +0000

    add a first layout how to run the tilecreator in cmake
    svn path=/trunk/KDE/kdeedu/marble/; revision=798924

--- src/tilecreator/main.cpp
+++ src/tilecreator/main.cpp
@@ -16,6 +16,12 @@ int main(int argc, char *argv[])
 {
     TCCoreApplication app( argc, argv );
     if( argc < 4 ) {
+    /*
+            PREFIX: this is the prefix of the source directory
+            INSTALLMAP: this is the map that you want to install - in the form MAPNAME/MAPNAME.jpg
+            DEM: Digital Elevation Model(grayscale) set to "true" for srtm sources set to "false" else
+            TARGETDIR: the directory where the output should go to
+            */
         qDebug() << "Syntax: tilecreator PREFIX INSTALLMAP DEM TARGETDIR";
         return -1;
     } else {
