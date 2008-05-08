e0ea78aa04fcf10c9d43ffbc7af24a820d49409f
Jens-Michael Hoffmann <jensmh@gmx.de>
2008-05-08 15:42:05 +0000

    Fix indention.
    svn path=/trunk/KDE/kdeedu/marble/; revision=805429

--- src/lib/geodata/handlers/dgml/DgmlAttributeDictionary.cpp
+++ src/lib/geodata/handlers/dgml/DgmlAttributeDictionary.cpp
@@ -28,22 +28,22 @@ namespace GeoSceneAttributeDictionary {
 
 const char* dgmlAttr_nameSpace20 = "http://edu.kde.org/marble/dgml/2.0";
 
-const char* dgmlAttr_backend   = "backend";
-const char* dgmlAttr_bgcolor   = "bgcolor";
-const char* dgmlAttr_color     = "color";
-const char* dgmlAttr_checkable = "checkable";
-const char* dgmlAttr_connect   = "connect";
-const char* dgmlAttr_expire    = "expire";
-const char* dgmlAttr_feature   = "feature";
-const char* dgmlAttr_format    = "format";
+const char* dgmlAttr_backend          = "backend";
+const char* dgmlAttr_bgcolor          = "bgcolor";
+const char* dgmlAttr_color            = "color";
+const char* dgmlAttr_checkable        = "checkable";
+const char* dgmlAttr_connect          = "connect";
+const char* dgmlAttr_expire           = "expire";
+const char* dgmlAttr_feature          = "feature";
+const char* dgmlAttr_format           = "format";
 const char* dgmlAttr_levelZeroColumns = "levelZeroColumns";
-const char* dgmlAttr_levelZeroRows = "levelZeroRows";
-const char* dgmlAttr_mode      = "mode";
-const char* dgmlAttr_name      = "name";
-const char* dgmlAttr_pixmap    = "pixmap";
-const char* dgmlAttr_role      = "role";
-const char* dgmlAttr_spacing   = "spacing";
-const char* dgmlAttr_style     = "style";
-const char* dgmlAttr_text      = "text";
-const char* dgmlAttr_width     = "width";
+const char* dgmlAttr_levelZeroRows    = "levelZeroRows";
+const char* dgmlAttr_mode             = "mode";
+const char* dgmlAttr_name             = "name";
+const char* dgmlAttr_pixmap           = "pixmap";
+const char* dgmlAttr_role             = "role";
+const char* dgmlAttr_spacing          = "spacing";
+const char* dgmlAttr_style            = "style";
+const char* dgmlAttr_text             = "text";
+const char* dgmlAttr_width            = "width";
 }
