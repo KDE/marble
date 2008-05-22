07f93aee980dcaee04a414ff2c4a9d6da6b9bc87
Jens-Michael Hoffmann <jensmh@gmx.de>
2008-05-22 15:54:26 +0000

    Fix indention.
    svn path=/trunk/KDE/kdeedu/marble/; revision=811212

--- src/lib/TextureTile.cpp
+++ src/lib/TextureTile.cpp
@@ -33,8 +33,8 @@ static uint **jumpTableFromQImage32( QImage &img )
     uint     **jumpTable = new uint*[height];
 
     for ( int y = 0; y < height; ++y ) {
-	jumpTable[ y ] = data;
-	data += bpl;
+        jumpTable[ y ] = data;
+        data += bpl;
     }
 
     return jumpTable;
@@ -49,8 +49,8 @@ static uchar **jumpTableFromQImage8( QImage &img )
     uchar    **jumpTable = new uchar*[height];
 
     for ( int y = 0; y < height; ++y ) {
-	jumpTable[ y ] = data;
-	data += bpl;
+        jumpTable[ y ] = data;
+        data += bpl;
     }
 
     return jumpTable;
@@ -77,104 +77,106 @@ TextureTile::~TextureTile()
 
 void TextureTile::loadRawTile( GeoSceneTexture *textureLayer, int level, int x, int y)
 {
-//  qDebug() << "TextureTile::loadRawTile" << level << x << y;
-  
-  m_used = true; // Needed to avoid frequent deletion of tiles
-  
+    // qDebug() << "TextureTile::loadRawTile" << level << x << y;
+
+    m_used = true; // Needed to avoid frequent deletion of tiles
+
     // qDebug() << "Entered loadTile( int, int, int) of Tile" << m_id;
     // m_used = true; // Needed to avoid frequent deletion of tiles
 
-  QString  absfilename;
+    QString  absfilename;
 
-  // qDebug() << "Requested tile level" << level;
+    // qDebug() << "Requested tile level" << level;
 
-  // If the tile level offers the requested tile then load it.
-  // Otherwise cycle from the requested tilelevel down to one where
-  // the requested area is covered.  Then scale the area to create a
-  // replacement for the tile that has been requested.
-  const int levelZeroColumns = textureLayer->levelZeroColumns();
-  const int levelZeroRows = textureLayer->levelZeroRows();
-  const int rowsRequestedLevel = TileLoaderHelper::levelToRow( levelZeroRows, level );
-  const int columnsRequestedLevel = TileLoaderHelper::levelToColumn( levelZeroColumns, level );
+    // If the tile level offers the requested tile then load it.
+    // Otherwise cycle from the requested tilelevel down to one where
+    // the requested area is covered.  Then scale the area to create a
+    // replacement for the tile that has been requested.
+    const int levelZeroColumns = textureLayer->levelZeroColumns();
+    const int levelZeroRows = textureLayer->levelZeroRows();
+    const int rowsRequestedLevel = TileLoaderHelper::levelToRow( levelZeroRows, level );
+    const int columnsRequestedLevel = TileLoaderHelper::levelToColumn( levelZeroColumns, level );
 
-  for ( int i = level; i > -1; --i ) {
+    for ( int i = level; i > -1; --i ) {
 
-      const int rowsCurrentLevel = TileLoaderHelper::levelToRow( levelZeroRows, i );
-      const int columnsCurrentLevel = TileLoaderHelper::levelToColumn( levelZeroColumns, i );
-      double origx1 = (double)(x) / (double)( rowsRequestedLevel );
-      double origy1 = (double)(y) / (double)( columnsRequestedLevel );
-      double testx1 = origx1 * (double)( rowsCurrentLevel );
-      double testy1 = origy1 * (double)( columnsCurrentLevel );
+        const int rowsCurrentLevel = TileLoaderHelper::levelToRow( levelZeroRows, i );
+        const int columnsCurrentLevel = TileLoaderHelper::levelToColumn( levelZeroColumns, i );
+        double origx1 = (double)(x) / (double)( rowsRequestedLevel );
+        double origy1 = (double)(y) / (double)( columnsRequestedLevel );
+        double testx1 = origx1 * (double)( rowsCurrentLevel );
+        double testy1 = origy1 * (double)( columnsCurrentLevel );
 
-      QString relfilename = TileLoaderHelper::relativeTileFileName( textureLayer, i,
-                                                                    (int)(testx1), (int)(testy1) );
-      absfilename = MarbleDirs::path( relfilename );
+        QString relfilename = TileLoaderHelper::relativeTileFileName( textureLayer, i,
+                                                                      (int)(testx1),
+                                                                      (int)(testy1) );
+        absfilename = MarbleDirs::path( relfilename );
 
-      if ( QFile::exists( absfilename ) ) {
-          // qDebug() << "The image filename does exist: " << absfilename ;
+        if ( QFile::exists( absfilename ) ) {
+            // qDebug() << "The image filename does exist: " << absfilename ;
 
-          QImage temptile( absfilename );
-// 	  qDebug() << "TextureTile::loadRawTile "
-// 		   << "depth:" << temptile.depth()
-// 		   << "format:" << temptile.format()
-// 		   << "bytesPerLine:" << temptile.bytesPerLine()
-// 		   << "numBytes:" << temptile.numBytes() ;
+            QImage temptile( absfilename );
+            // qDebug() << "TextureTile::loadRawTile "
+            //          << "depth:" << temptile.depth()
+            //          << "format:" << temptile.format()
+            //          << "bytesPerLine:" << temptile.bytesPerLine()
+            //          << "numBytes:" << temptile.numBytes() ;
 
-          if ( !temptile.isNull() ) {
-              // qDebug() << "Image has been successfully loaded.";
+            if ( !temptile.isNull() ) {
+                // qDebug() << "Image has been successfully loaded.";
 
-              if ( level != i ) { 
-                  // qDebug() << "About to start cropping an existing image.";
-                  QSize tilesize = temptile.size();
-                  double origx2 = (double)(x + 1) / (double)( rowsRequestedLevel );
-                  double origy2 = (double)(y + 1) / (double)( columnsRequestedLevel );
-                  double testx2 = origx2 * (double)( rowsCurrentLevel );
-                  double testy2 = origy2 * (double)( columnsCurrentLevel );
+                if ( level != i ) { 
+                    // qDebug() << "About to start cropping an existing image.";
+                    QSize tilesize = temptile.size();
+                    double origx2 = (double)(x + 1) / (double)( rowsRequestedLevel );
+                    double origy2 = (double)(y + 1) / (double)( columnsRequestedLevel );
+                    double testx2 = origx2 * (double)( rowsCurrentLevel );
+                    double testy2 = origy2 * (double)( columnsCurrentLevel );
 
-                  // Determine the rectangular section of the previous tile data 
-                  // which we intend to copy from:
-                  int left   = (int)( ( testx1 - (int)(testx1) ) * temptile.width() );
-                  int top    = (int)( ( testy1 - (int)(testy1) ) * temptile.height() );
-                  int right  = (int)( ( testx2 - (int)(testx1) ) * temptile.width() ) - 1;
-                  int bottom = (int)( ( testy2 - (int)(testy1) ) * temptile.height() ) - 1;
+                    // Determine the rectangular section of the previous tile data 
+                    // which we intend to copy from:
+                    int left   = (int)( ( testx1 - (int)(testx1) ) * temptile.width() );
+                    int top    = (int)( ( testy1 - (int)(testy1) ) * temptile.height() );
+                    int right  = (int)( ( testx2 - (int)(testx1) ) * temptile.width() ) - 1;
+                    int bottom = (int)( ( testy2 - (int)(testy1) ) * temptile.height() ) - 1;
 
-                  // Important: Scaling a null image during the next step would be fatal
-                  // So we make sure the width and height is at least 1.
-                  int rectWidth  = ( right - left > 1 ) ? right  - left : 1;
-                  int rectHeight = ( bottom - top > 1 ) ? bottom - top  : 1;
+                    // Important: Scaling a null image during the next step would be fatal
+                    // So we make sure the width and height is at least 1.
+                    int rectWidth  = ( right - left > 1 ) ? right  - left : 1;
+                    int rectHeight = ( bottom - top > 1 ) ? bottom - top  : 1;
 
-                  // This should not create any memory leaks as
-                  // 'copy' and 'scaled' return a value (on the
-                  // stack) which gets deep copied always into the
-                  // same place for m_rawtile on the heap:
-                  temptile = temptile.copy( left, top, rectWidth, rectHeight );
-                  temptile = temptile.scaled( tilesize ); // TODO: use correct size
-                  //          qDebug() << "Finished scaling up the Temporary Tile.";
-              }
+                    // This should not create any memory leaks as
+                    // 'copy' and 'scaled' return a value (on the
+                    // stack) which gets deep copied always into the
+                    // same place for m_rawtile on the heap:
+                    temptile = temptile.copy( left, top, rectWidth, rectHeight );
+                    temptile = temptile.scaled( tilesize ); // TODO: use correct size
+                    // qDebug() << "Finished scaling up the Temporary Tile.";
+                }
 
-              m_rawtile = temptile;
-              qDebug() << "m_rawtile.m_depth:" << m_rawtile.depth();
+                m_rawtile = temptile;
+                qDebug() << "m_rawtile.m_depth:" << m_rawtile.depth();
 
-              break;
-          } // !tempfile.isNull()
-          //      else {
-          //         qDebug() << "Image load failed for: " + 
-          //           absfilename.toLocal8Bit();
-          //      }
-      }
-      else {
-	QUrl sourceUrl = TileLoaderHelper::downloadUrl( textureLayer, level, x, y );
-	QString destFileName = TileLoaderHelper::relativeTileFileName( textureLayer, level, x, y );
-	qDebug() << "emit downloadTile(" << sourceUrl << destFileName << ");";
-	emit downloadTile( sourceUrl, destFileName, m_id.toString() );
-      }
-  }
-  
-  qDebug() << "TextureTile::loadRawTile end";
+                break;
+            } // !tempfile.isNull()
+            //      else {
+            //         qDebug() << "Image load failed for: " + 
+            //           absfilename.toLocal8Bit();
+            //      }
+        }
+        else {
+            QUrl sourceUrl = TileLoaderHelper::downloadUrl( textureLayer, level, x, y );
+            QString destFileName = TileLoaderHelper::relativeTileFileName( textureLayer,
+                                                                           level, x, y );
+            qDebug() << "emit downloadTile(" << sourceUrl << destFileName << ");";
+            emit downloadTile( sourceUrl, destFileName, m_id.toString() );
+        }
+    }
 
-  m_depth = m_rawtile.depth();
-  
-  qDebug() << "m_depth =" << m_depth;
+    qDebug() << "TextureTile::loadRawTile end";
+
+    m_depth = m_rawtile.depth();
+
+    qDebug() << "m_depth =" << m_depth;
 }
 
 void TextureTile::loadTile( bool requestTileUpdate )
