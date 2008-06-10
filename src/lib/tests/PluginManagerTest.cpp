0cdeae8ad6eb08df0df043c3468cd3a5a130cb8a
Patrick Spendrin <ps_ml@gmx.de>
2008-06-10 01:46:10 +0000

    add first Unittest for the PluginManager (which currently fails under Windows)
    svn path=/trunk/KDE/kdeedu/marble/; revision=818929

---
+++ src/lib/tests/PluginManagerTest.cpp
@@ -0,0 +1,30 @@
+//
+// This file is part of the Marble Desktop Globe.
+//
+// This program is free software licensed under the GNU LGPL. You can
+// find a copy of this license in LICENSE.txt in the top directory of
+// the source code.
+//
+// Copyright 2008 Patrick Spendrin  <ps_ml@gmx.de>
+//
+
+#include <QtTest/QtTest>
+
+#include "MarbleDirs.h"
+#include "PluginManager.h"
+
+class PluginManagerTest : public QObject {
+    Q_OBJECT
+    private slots:
+        void loadPlugins();
+};
+
+void PluginManagerTest::loadPlugins() {
+    int pluginNumber = MarbleDirs::pluginEntryList( "", QDir::Files ).size();
+    PluginManager *pm = new PluginManager();
+    QCOMPARE( pm->layerInterfaces().size(), pluginNumber );
+}
+
+QTEST_MAIN( PluginManagerTest )
+
+#include "PluginManagerTest.moc"
