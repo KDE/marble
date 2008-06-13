301fc8ccd919e1b25bae9be8634d3fff815e3237
Patrick Spendrin <ps_ml@gmx.de>
2008-06-13 12:37:54 +0000

    add new cmake macros to make the building of marble plugins easier
    svn path=/trunk/KDE/kdeedu/marble/; revision=820170

---
+++ MarbleMacros.cmake
@@ -0,0 +1,86 @@
+# the place to put in common cmake macros
+# this is needed to minimize the amount of errors to do
+macro( marble_add_plugin _target_name )
+set( _src ${ARGN} )
+if( QTONLY )
+    qt4_automoc( ${_src} )
+    add_library( ${_target_name} MODULE ${_src} )
+    target_link_libraries( ${_target_name} ${QT_QTCORE_LIBRARY}
+                                           ${QT_QTDBUS_LIBRARY}
+                                           ${QT_QTGUI_LIBRARY}
+                                           ${QT_QTXML_LIBRARY}
+                                           ${QT_QTSVG_LIBRARY}
+                                           ${QT_QTNETWORK_LIBRARY}
+                                           ${QT_QTMAIN_LIBRARY}
+                                           ${${_target_name}_LIBS}
+                                           marblewidget )
+    install( TARGETS ${_target_name} DESTINATION ${MARBLE_PLUGIN_INSTALL_PATH} )
+else( QTONLY )
+    kde4_add_plugin( ${_target_name} ${_src} )
+    target_link_libraries( ${_target_name} ${QT_QTCORE_LIBRARY}
+                                           ${QT_QTDBUS_LIBRARY}
+                                           ${QT_QTGUI_LIBRARY}
+                                           ${QT_QTXML_LIBRARY}
+                                           ${QT_QTSVG_LIBRARY}
+                                           ${QT_QTNETWORK_LIBRARY}
+                                           ${KDE4_KDECORE_LIBRARY}
+                                           ${KDE4_KDEUI_LIBRARY}
+                                           ${KDE4_KIO_LIBRARY}
+                                           ${QT_QTMAIN_LIBRARY}
+                                           ${${_target_name}_LIBS}
+                                           marblewidget )
+    install( TARGETS ${_target_name} DESTINATION ${MARBLE_PLUGIN_INSTALL_PATH} )
+endif( QTONLY )
+
+set_target_properties( ${_target_name} PROPERTIES 
+                       INSTALL_RPATH_USE_LINK_PATH TRUE  
+                       SKIP_BUILD_RPATH TRUE 
+                       BUILD_WITH_INSTALL_RPATH TRUE 
+                       INSTALL_RPATH ${CMAKE_INSTALL_PREFIX}/${LIB_SUFFIX} )
+
+endmacro( marble_add_plugin _target_name )
+
+# these plugins are slightly different
+
+macro( marble_add_designer_plugin _target_name )
+set( _src ${ARGN} )
+
+qt4_add_resources( _src ../../../marble.qrc )
+
+if( QTONLY )
+    qt4_automoc( ${_src} )
+    add_library( ${_target_name} MODULE ${_src} )
+    target_link_libraries( ${_target_name} ${QT_QTCORE_LIBRARY}
+                                           ${QT_QTDBUS_LIBRARY}
+                                           ${QT_QTGUI_LIBRARY}
+                                           ${QT_QTXML_LIBRARY}
+                                           ${QT_QTSVG_LIBRARY}
+                                           ${QT_QTNETWORK_LIBRARY}
+                                           ${QT_QTMAIN_LIBRARY}
+                                           ${${_target_name}_LIBS}
+                                           marblewidget )
+    install( TARGETS ${_target_name} DESTINATION ${QT_PLUGINS_DIR}/designer )
+else( QTONLY )
+    kde4_add_plugin( ${_target_name} ${_src} )
+    target_link_libraries( ${_target_name} ${QT_QTCORE_LIBRARY}
+                                           ${QT_QTDBUS_LIBRARY}
+                                           ${QT_QTGUI_LIBRARY}
+                                           ${QT_QTXML_LIBRARY}
+                                           ${QT_QTSVG_LIBRARY}
+                                           ${QT_QTNETWORK_LIBRARY}
+                                           ${KDE4_KDECORE_LIBRARY}
+                                           ${KDE4_KDEUI_LIBRARY}
+                                           ${KDE4_KIO_LIBRARY}
+                                           ${QT_QTMAIN_LIBRARY}
+                                           ${${_target_name}_LIBS}
+                                           marblewidget )
+    install( TARGETS ${_target_name} DESTINATION ${PLUGIN_INSTALL_DIR}/plugins/designer )
+endif( QTONLY )
+
+set_target_properties( ${_target_name} PROPERTIES 
+                       INSTALL_RPATH_USE_LINK_PATH TRUE  
+                       SKIP_BUILD_RPATH TRUE 
+                       BUILD_WITH_INSTALL_RPATH TRUE 
+                       INSTALL_RPATH ${CMAKE_INSTALL_PREFIX}/${LIB_SUFFIX} )
+
+endmacro( marble_add_designer_plugin _target_name )
