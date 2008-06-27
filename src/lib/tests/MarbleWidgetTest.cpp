002c46d2e6f1d26862ca6eb5d78c4cd0c521551f
Patrick Spendrin <ps_ml@gmx.de>
2008-06-27 15:31:27 +0000

    add a new Test for the MarbleWidget - simply try to run a basic MarbleWidget
    svn path=/trunk/KDE/kdeedu/marble/; revision=825161

---
+++ src/lib/tests/MarbleWidgetTest.cpp
@@ -0,0 +1,34 @@
+//
+// This file is part of the Marble Desktop Globe.
+//
+// This program is free software licensed under the GNU LGPL. You can
+// find a copy of this license in LICENSE.txt in the top directory of
+// the source code.
+//
+// Copyright 2008 Patrick Spendrin  <ps_ml@gmx.de>
+//
+
+#include <QtGui/QtGui>
+#include <QtTest/QtTest>
+#include "marble/MarbleWidget.h"
+
+class TestMarbleWidget: public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void testWidget();
+
+};
+
+void TestMarbleWidget::testWidget()
+{
+    MarbleWidget widget;
+    widget.setMapThemeId("earth/srtm/srtm.dgml");
+
+    QTest::mouseMove( &widget );
+}
+
+QTEST_MAIN( TestMarbleWidget )
+
+#include "MarbleWidgetTest.moc"
