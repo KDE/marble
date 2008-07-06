8bd7ac5457a45f3f7a0a54afcdefe539346a0ca6
Patrick Spendrin <ps_ml@gmx.de>
2008-07-06 00:40:14 +0000

    add tags for missing properties
    svn path=/trunk/KDE/kdeedu/marble/; revision=828488

---
+++ src/lib/geodata/handlers/kml/KmlAltitudeModeTagHandler.cpp
@@ -0,0 +1,68 @@
+/*
+    Copyright (C) 2008 Patrick Spendrin <ps_ml@gmx.de>
+
+    This file is part of the KDE project
+
+    This library is free software you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    aint with this library see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
+#include "KmlAltitudeModeTagHandler.h"
+
+#include <QtCore/QDebug>
+
+#include "KmlElementDictionary.h"
+
+#include "GeoDataGeometry.h"
+#include "GeoDataParser.h"
+
+using namespace GeoDataElementDictionary;
+using namespace Marble;
+
+KML_DEFINE_TAG_HANDLER( altitudeMode )
+
+KmlaltitudeModeTagHandler::KmlaltitudeModeTagHandler()
+    : GeoTagHandler()
+{
+}
+
+KmlaltitudeModeTagHandler::~KmlaltitudeModeTagHandler()
+{
+}
+
+GeoNode* KmlaltitudeModeTagHandler::parse( GeoParser& parser ) const
+{
+    Q_ASSERT( parser.isStartElement() && parser.isValidElement( kmlTag_altitudeMode ) );
+
+    GeoStackItem parentItem = parser.parentElement();
+    
+    if( parentItem.nodeAs<GeoDataGeometry>() ) {
+        QString content = parser.readElementText().trimmed();
+        
+        if( content == QString( "relativeToGround" ) ) {
+            parentItem.nodeAs<GeoDataGeometry>()->setAltitudeMode( RelativeToGround );
+        } else if( content == QString( "absolute" ) ) {
+            parentItem.nodeAs<GeoDataGeometry>()->setAltitudeMode( Absolute );
+        } else { // clampToGround is Standard
+            parentItem.nodeAs<GeoDataGeometry>()->setAltitudeMode( ClampToGround );
+        }
+#ifdef DEBUG_TAGS
+        qDebug() << "Parsed <" << kmlTag_altitudeMode << "> containing: " << content
+                 << " parent item name: " << parentItem.qualifiedName().first;
+#endif
+    }
+
+    return 0;
+}
