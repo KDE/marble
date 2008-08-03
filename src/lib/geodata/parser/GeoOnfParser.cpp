3c401ed7935788a7d4f3aaee71842b1550d9343e
Jens-Michael Hoffmann <jensmh@gmx.de>
2008-08-03 22:56:43 +0000

    Add parser class for OpenStreetMap name finder.
    svn path=/trunk/KDE/kdeedu/marble/; revision=841718

---
+++ src/lib/geodata/parser/GeoOnfParser.cpp
@@ -0,0 +1,49 @@
+/*
+    Copyright (C) 2008 Jens-Michael Hoffmann <jensmh@gmx.de>
+
+    This file is part of the KDE project
+
+    This library is free software you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    aint with this library see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
+#include "GeoOnfParser.h"
+
+#include "GeoDataDocument.h"
+#include "osm-namefinder/ElementDictionary.h"
+
+using namespace Marble;
+
+GeoOnfParser::GeoOnfParser()
+    : GeoParser( 0 )
+{
+    setNamespaceProcessing( false );
+}
+
+bool GeoOnfParser::isValidDocumentElement() const
+{
+    return isValidElement( OsmNamefinder::tag_searchresults );
+}
+
+bool GeoOnfParser::isValidElement(const QString& tagName) const
+{
+    return GeoParser::isValidElement( tagName )
+        && namespaceUri() == OsmNamefinder::tag_namespace;
+}
+
+GeoDocument * GeoOnfParser::createDocument() const
+{
+    return new GeoDataDocument;
+}
---
+++ src/lib/geodata/parser/GeoOnfParser.h
@@ -0,0 +1,40 @@
+/*
+    Copyright (C) 2008 Jens-Michael Hoffmann <jensmh@gmx.de>
+
+    This file is part of the KDE project
+
+    This library is free software you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    aint with this library see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+#ifndef GeoOnfParser_h
+#define GeoOnfParser_h
+
+#include <geodata_export.h>
+#include "GeoParser.h"
+
+class GeoDocument;
+
+
+class GEODATA_EXPORT GeoOnfParser : public GeoParser {
+public:
+    GeoOnfParser();
+
+private:
+    bool isValidDocumentElement() const;
+    GeoDocument* createDocument() const;
+    bool isValidElement(const QString& tagName) const;
+};
+
+#endif
