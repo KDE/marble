d34281af576dd6d7b814810e79761db1b62875ce
Jens-Michael Hoffmann <jensmh@gmx.de>
2008-08-04 00:48:33 +0000

    - Remove commented out code.     - Use GeoDataDocument instead of GeoDataContainer.
    svn path=/trunk/KDE/kdeedu/marble/; revision=841736

--- src/lib/geodata/handlers/osm-namefinder/SearchResultsTagHandler.cpp
+++ src/lib/geodata/handlers/osm-namefinder/SearchResultsTagHandler.cpp
@@ -26,7 +26,7 @@
 #include <QtCore/QString>
 
 #include "GeoParser.h"
-#include "GeoDataContainer.h"
+#include "GeoDataDocument.h"
 #include "osm-namefinder/AttributeDictionary.h"
 #include "osm-namefinder/ElementDictionary.h"
 
@@ -47,48 +47,9 @@ GeoNode * SearchResultsTagHandler::parse( GeoParser & parser ) const
 
     // FIXME:
     GeoDocument * const document = parser.activeDocument();
-    GeoDataContainer * const searchResults =
-        dynamic_cast<GeoDataContainer * const>( document );
+    GeoDataDocument * const searchResults =
+        dynamic_cast<GeoDataDocument * const>( document );
     Q_ASSERT( searchResults );
-
-//     // attribute date, for example "2007-05-08 12:56:33"
-//     const QString dateStr = parser.attribute( attr_date ).trimmed();
-//     const QDateTime date = QDateTime::fromString( dateStr, "yyyy-MM-dd HH:mm:ss" );
-//     Q_ASSERT( date.isValid() );
-//     searchResults->setDateTimeOfSearch( date );
-
-//     // attribute sourcedate, for example "2007-05-02"
-//     const QString sourceDateStr = parser.attribute( attr_sourcedate ).trimmed();
-//     const QDate sourceDate = QDate::fromString( sourceDateStr, "yyyy-MM-dd" );
-//     Q_ASSERT( sourceDate.isValid() );
-//     searchResults->setDateOfIndexData( sourceDate );
-
-//     // attribute find
-//     const QString find = parser.attribute( attr_find ).trimmed();
-//     searchResults->setOriginalSearchRequest( find );
-
-//     // attribute distancesearch
-//     const QString distanceSearchStr = parser.attribute( attr_distancesearch ).trimmed();
-//     const bool distanceSearch = ( distanceSearchStr == "yes" );
-//     searchResults->setDistanceSearch( distanceSearch );
-
-//     // attribute findname
-//     const QString findName = parser.attribute( attr_findname ).trimmed();
-//     searchResults->setFindName( findName );
-
-//     // attribute findplace
-//     const QString findPlace = parser.attribute( attr_findplace ).trimmed();
-//     searchResults->setFindPlace( findPlace );
-
-//     // attribute findisin
-//     const QString findIsIn = parser.attribute( attr_findisin ).trimmed();
-//     searchResults->setFindIsIn( findIsIn );
-
-//     // attribute foundnearplace
-//     const QString foundNearPlaceStr = parser.attribute( attr_foundnearplace ).trimmed();
-//     const bool foundNearPlace = ( foundNearPlaceStr == "yes" );
-//     searchResults->setFoundNearPlace( foundNearPlace );
-
     return searchResults;
 }
 
