5022d6fadfdeab4729995c0b9021083d43cef528
Inge Wallin <inge@lysator.liu.se>
2008-08-15 15:32:19 +0000

    Add a template for Float Item plugins.
    A template for ordinary Marble plugins to follow.
    
    
    svn path=/trunk/KDE/kdeedu/marble/; revision=847534

---
+++ src/plugins/marble/FITemplate/FITemplateFloatItem.cpp
@@ -0,0 +1,107 @@
+//
+// This file is part of the Marble Desktop Globe.
+//
+// This program is free software licensed under the GNU LGPL. You can
+// find a copy of this license in LICENSE.txt in the top directory of
+// the source code.
+//
+// Copyright 2008 Inge Wallin <inge@lysator.liu.se>"
+//
+
+
+// Local
+#include "FITemplateFloatItem.h"
+
+// Qt
+#include <QtCore/QDebug>
+#include <QtCore/QRect>
+#include <QtGui/QColor>
+#include <QtGui/QPixmap>
+#include <QtSvg/QSvgRenderer>
+
+// Marble
+#include "MarbleDirs.h"
+#include "GeoPainter.h"
+#include "GeoDataCoordinates.h"
+#include "ViewportParams.h"
+
+
+FITemplateFloatItem::FITemplateFloatItem ( const QPointF &point,
+					    const QSizeF &size )
+    : MarbleAbstractFloatItem( point, size ),
+      m_compass(),
+      m_polarity( 0 )
+{
+    m_svgobj = new QSvgRenderer( MarbleDirs::path( "svg/compass.svg" ),
+                                 this );
+}
+
+FITemplateFloatItem::~FITemplateFloatItem ()
+{
+    delete m_svgobj;
+}
+
+QStringList FITemplateFloatItem::backendTypes() const
+{
+    return QStringList( "FITemplate" );
+}
+
+QString FITemplateFloatItem::name() const
+{
+    return tr( "FITemplate" );
+}
+
+QString FITemplateFloatItem::guiString() const
+{
+    return tr( "Float Item &Template" );
+}
+
+QString FITemplateFloatItem::nameId() const
+{
+    return QString( "compass" );
+}
+
+QString FITemplateFloatItem::description() const
+{
+    return tr( "This is a template class for float items." );
+}
+
+QIcon FITemplateFloatItem::icon () const
+{
+    return QIcon();
+}
+
+
+void FITemplateFloatItem::initialize ()
+{
+    // Initialize your float item here
+}
+
+bool FITemplateFloatItem::isInitialized () const
+{
+    // Return whether your float item is initialized here.
+    return true;
+}
+
+QPainterPath FITemplateFloatItem::backgroundShape() const
+{
+    // Return a QPainterPath here that contains the outer shape of
+    // your float item.
+}
+
+bool FITemplateFloatItem::needsUpdate( ViewportParams *viewport )
+{
+    // Return whether your float item needs to be redrawn.
+}
+
+bool FITemplateFloatItem::renderFloatItem( GeoPainter     *painter,
+					   ViewportParams *viewport,
+					   GeoSceneLayer  *layer )
+{
+    // Here you should render your float item 
+}
+
+
+Q_EXPORT_PLUGIN2(FITemplateFloatItem, FITemplateFloatItem )
+
+#include "FITemplateFloatItem.moc"
---
+++ src/plugins/marble/FITemplate/FITemplateFloatItem.h
@@ -0,0 +1,101 @@
+//
+// This file is part of the Marble Desktop Globe.
+//
+// This program is free software licensed under the GNU LGPL. You can
+// find a copy of this license in LICENSE.txt in the top directory of
+// the source code.
+//
+// Copyright 2008 Inge Wallin <inge@lysator.liu.se>"
+//
+
+//
+// This class is a template Float Item plugin.
+//
+
+
+#ifndef FITEMPLATE_FLOAT_ITEM_H
+#define FITEMPLATE_FLOAT_ITEM_H
+
+
+// Qt
+#include <QtCore/QObject>
+
+// Marble
+#include "GeoDataLatLonAltBox.h"
+#include "MarbleAbstractFloatItem.h"
+
+
+class QSvgRenderer;
+
+
+/**
+ * @short The class that creates a ... Float Item
+ *
+ */
+
+class FITemplateFloatItem  : public MarbleAbstractFloatItem
+{
+    Q_OBJECT
+    Q_INTERFACES( MarbleRenderPluginInterface )
+
+ public:
+    explicit FITemplateFloatItem( const QPointF &point = QPointF( -1.0, 10.0 ),
+				  const QSizeF &size = QSizeF( 75.0, 75.0 ) );
+
+    // ----------------------------------------------------------------
+    // The following functions are defined in MarbleRenderPluginInterface.h
+    // and MUST be part of the plugin.  See that file for documentation.
+    //
+    // Note that the class MarbleAbstractFloatItem provides default 
+    // implementations for many of them.
+    //
+
+    ~FITemplateFloatItem ();
+
+    QStringList backendTypes() const;
+
+    // Provided by MarbleAbstractFloatItem and should not be implemented.
+    //
+    // QString renderPolicy() const;
+    // QStringList renderPosition() const;
+
+    QString name() const;
+
+    QString guiString() const;
+
+    QString nameId() const;
+
+    QString description() const;
+
+    QIcon icon() const;
+
+    void initialize();
+
+    bool isInitialized() const;
+
+    // Provided by MarbleAbstractFloatItem and should not be implemented.
+    //
+    // bool render( GeoPainter *painter, ViewportParams *viewport,
+    //              const QString &renderPos, GeoSceneLayer *layer);
+
+    QPainterPath backgroundShape() const;
+
+    // End of MarbleRenderPluginInterface functions.
+    // ----------------------------------------------------------------
+
+    bool needsUpdate( ViewportParams *viewport );
+
+    bool renderFloatItem( GeoPainter *painter, ViewportParams *viewport,
+			  GeoSceneLayer * layer = 0 );
+
+ private:
+    Q_DISABLE_COPY( FITemplateFloatItem )
+
+    QSvgRenderer  *m_svgobj;
+    QPixmap        m_compass;
+
+    /// allowed values: -1, 0, 1; default here: 0. FIXME: Declare enum
+    int            m_polarity;
+};
+
+#endif
