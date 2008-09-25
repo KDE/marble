0b9dbc4e2aea19eb5dfd15325a5ff81a2d1bb901
Henry de Valence <hdevalence@gmail.com>
2008-09-25 03:12:00 +0000

    Beginnings of UTM. right now there is only a bit done; in fact the helper classes are just empty so far. Not yet added to building since it's not ready.
    svn path=/trunk/KDE/kdeedu/marble/; revision=864555

---
+++ src/lib/Projections/UtmProjection.cpp
@@ -0,0 +1,83 @@
+//
+// This file is part of the Marble Desktop Globe.
+//
+// This program is free software licensed under the GNU LGPL. You can
+// find a copy of this license in LICENSE.txt in the top directory of
+// the source code.
+//
+// Copyright 2007-2008 Inge Wallin  <ingwa@kde.org>
+// Copyright 2008 Henry de Valence <hdevalence@gmail.com>
+//
+
+#include "UtmProjection.h"
+
+#include "UtmProjectionHelper.h"
+
+#include <math.h>
+
+using namespace Marble;
+
+static UtmProjectionHelper theHelper;
+
+UtmProjection::UtmProjection()
+{
+    // These are the upper and lower bounds of the utm grid.
+    // For some reason (proably norway, they made the other irregularities)
+    // the northern one is extended 4 degrees.
+    m_maxLat  = 84.0 * DEG2RAD;
+    m_minLat  = -80.0 * DEG2RAD;
+    m_traversableMaxLat = false;
+    m_repeatX = true;
+}
+
+UtmProjection::~UtmProjection()
+{
+}
+
+
+AbstractProjectionHelper* UtmProjection::helper()
+{
+    return &theHelper;
+}
+
+bool UtmProjection::screenCoordinates( const qreal lon, const qreal lat,
+                                       const ViewportParams *params,
+                                       int& x, int& y,
+                                       CoordinateType coordType )
+{
+    if( lat > m_maxLat || lat < m_minLat ) {
+        return false;
+    }
+
+    // maxw = max dist away from the slice (radians)
+    const qreal  maxw = 3.0*DEG2RAD;
+    // fullw = width of the slice (radians)
+    const qreal fullw = 6.0*DEG2RAD;
+
+    int lonz = (int) ( floor( ( lon+M_PI ) / ( fullw ) ) - 30 );
+    qreal clon = maxw + ( ( fullw ) * num );
+
+    int Rk = viewport->radius() * 0.9996;
+
+    //x = Rk arctanh( sin lambda ) width of slice (pixels)
+    int slicew = (int) ( ( Rk * atanh( sin( maxw ) ) ) * 2 );
+
+    // Let (x, y) be the position on the screen of the placemark..
+    x = (int)( Rk * atanh( cos(lat) * sin( lon - clon ) ) );
+    y = (int)( Rk * atanh( tan(lat) / cos( lon - clon ) ) );
+
+    //now we must adjust the coords from the slice
+    x += slicew*lonz;
+
+    return true;
+}
+
+bool UtmProjection::geoCoordinates( const int x, const int y,
+                                    const ViewportParams *params,
+                                    qreal& lon, qreal& lat,
+                                    GeoDataCoordinates::Unit unit )
+{
+
+}
+
+
---
+++ src/lib/Projections/UtmProjection.h
@@ -0,0 +1,100 @@
+//
+// This file is part of the Marble Desktop Globe.
+//
+// This program is free software licensed under the GNU LGPL. You can
+// find a copy of this license in LICENSE.txt in the top directory of
+// the source code.
+//
+// Copyright 2007      Inge Wallin  <ingwa@kde.org>
+// Copyright 2008  Henry de Valence <hdevalence@gmail.com>
+//
+
+#ifndef UTMPROJECTION_H
+#define UTMPROJECTION_H
+
+
+namespace Marble {
+
+
+/**
+ * @short A class to implement the Universal Transverse Mercator projection.
+ */
+
+class UtmProjection : public AbstractProjection
+{
+    // Not a QObject so far because we don't need to send signals.
+ public:
+
+    /**
+     * @brief Construct a new UtmProjection.
+     */
+    explicit UtmProjection();
+
+    virtual ~UtmProjection();
+
+    AbstractProjectionHelper *helper();
+
+    /**
+     * @brief Get the screen coordinates corresponding to geographical coordinates in the map.
+     * @param lon    the lon coordinate of the requested pixel position
+     * @param lat    the lat coordinate of the requested pixel position
+     * @param x      the x coordinate of the pixel is returned through this parameter
+     * @param y      the y coordinate of the pixel is returned through this parameter
+     * @return @c true  if the geographical coordinates are visible on the screen
+     *         @c false if the geographical coordinates are not visible on the screen
+     */
+    bool screenCoordinates( const qreal lon, const qreal lat,
+                            const ViewportParams *params,
+                            int& x, int& y,
+                            CoordinateType coordType = originalCoordinates );
+
+    bool screenCoordinates( const GeoDataCoordinates &geopoint,
+                            const ViewportParams *params,
+                            int &x, int &y, bool &globeHidesPoint );
+
+    bool screenCoordinates( const GeoDataCoordinates &geopoint,
+                            const ViewportParams * viewport,
+                            int *x, int &y, int &pointRepeatNum,
+                            bool &globeHidesPoint );
+
+    /**
+     * @brief Get the earth coordinates corresponding to a pixel in the map.
+     * @param x      the x coordinate of the pixel
+     * @param y      the y coordinate of the pixel
+     * @param lon    the longitude angle is returned through this parameter
+     * @param lat    the latitude angle is returned through this parameter
+     * @return @c true  if the pixel (x, y) is within the globe
+     *         @c false if the pixel (x, y) is outside the globe, i.e. in space.
+     */
+    bool geoCoordinates( const int x, const int y,
+                         const ViewportParams *params,
+                         qreal& lon, qreal& lat,
+                         GeoDataCoordinates::Unit = GeoDataCoordinates::Degree );
+
+    /**
+     * @brief Get a quaternion representing a point on the earth corresponding to a pixel in the map.
+     * @param x  the x coordinate of the pixel
+     * @param y  the y coordinate of the pixel
+     * @param q  the out parameter where the result is returned
+     * @return @c true  if the pixel (x, y) is within the globe
+     *         @c false if the pixel (x, y) is outside the globe, i.e. in space
+     */
+    bool    geoCoordinates( int x, int y,
+                            const ViewportParams *params,
+                            Quaternion &q );
+
+    GeoDataLatLonAltBox latLonAltBox( const QRect &screenRect,
+                                      const ViewportParams *viewport );
+
+    bool  mapCoversViewport( const ViewportParams *viewport ) const;
+
+ private:
+    //MercatorProjectionPrivate  * const d;
+};
+
+
+}
+
+
+#endif
+
