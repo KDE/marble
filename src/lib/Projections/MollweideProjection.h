ca4c8ef7c35fab558cceaf2cf1951f307a80f44a
Henry de Valence <hdevalence@gmail.com>
2008-09-25 03:16:48 +0000

    Add files for (so far) unimplemented Mollweide projection
    svn path=/trunk/KDE/kdeedu/marble/; revision=864557

---
+++ src/lib/Projections/MollweideProjection.h
@@ -0,0 +1,97 @@
+//
+// This file is part of the Marble Desktop Globe.
+//
+// This program is free software licensed under the GNU LGPL. You can
+// find a copy of this license in LICENSE.txt in the top directory of
+// the source code.
+//
+// Copyright 2007      Inge Wallin  <ingwa@kde.org>
+// Copyright 2008  Henry de Valence <hdevalence@gmail.com>
+//
+
+#ifndef MOLLWEIDEPROJECTION_H
+#define MOLLWEIDEPROJECTION_H
+
+
+namespace Marble {
+
+
+/**
+ * @short A class to implement the Mollweide projection.
+ */
+
+class MollweideProjection : public AbstractProjection
+{
+    // Not a QObject so far because we don't need to send signals.
+ public:
+
+    /**
+     * @brief Construct a new MollweideProjection.
+     */
+    explicit MollweideProjection();
+
+    virtual ~MollweideProjection();
+
+    AbstractProjectionHelper *helper();
+
+    /**
+     * @brief Get the screen coordinates corresponding to geographical coordinates in the map.
+     * @param lon    the lon coordinate of the requested pixel position
+     * @param lat    the lat coordinate of the requested pixel position
+     * @param x      the x coordinate of the pixel is returned through this parameter
+     * @param y      the y coordinate of the pixel is returned through this parameter
+     * @return @c true  if the geographical coordinates are visible on the screen
+     *         @c false if the geographical coordinates are not visible on the screen
+     */
+    bool screenCoordinates( const qreal lon, const qreal lat,
+                            const ViewportParams *params,
+                            int& x, int& y,
+                            CoordinateType coordType = originalCoordinates );
+
+    bool screenCoordinates( const GeoDataCoordinates &geopoint,
+                            const ViewportParams *params,
+                            int &x, int &y, bool &globeHidesPoint );
+
+    bool screenCoordinates( const GeoDataCoordinates &geopoint,
+                            const ViewportParams * viewport,
+                            int *x, int &y, int &pointRepeatNum,
+                            bool &globeHidesPoint );
+
+    /**
+     * @brief Get the earth coordinates corresponding to a pixel in the map.
+     * @param x      the x coordinate of the pixel
+     * @param y      the y coordinate of the pixel
+     * @param lon    the longitude angle is returned through this parameter
+     * @param lat    the latitude angle is returned through this parameter
+     * @return @c true  if the pixel (x, y) is within the globe
+     *         @c false if the pixel (x, y) is outside the globe, i.e. in space.
+     */
+    bool geoCoordinates( const int x, const int y,
+                         const ViewportParams *params,
+                         qreal& lon, qreal& lat,
+                         GeoDataCoordinates::Unit = GeoDataCoordinates::Degree );
+
+    /**
+     * @brief Get a quaternion representing a point on the earth corresponding to a pixel in the map.
+     * @param x  the x coordinate of the pixel
+     * @param y  the y coordinate of the pixel
+     * @param q  the out parameter where the result is returned
+     * @return @c true  if the pixel (x, y) is within the globe
+     *         @c false if the pixel (x, y) is outside the globe, i.e. in space
+     */
+    bool    geoCoordinates( int x, int y,
+                            const ViewportParams *params,
+                            Quaternion &q );
+
+    GeoDataLatLonAltBox latLonAltBox( const QRect &screenRect,
+                                      const ViewportParams *viewport );
+
+    bool  mapCoversViewport( const ViewportParams *viewport ) const;
+};
+
+
+}
+
+
+#endif
+
