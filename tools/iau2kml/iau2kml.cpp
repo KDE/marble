fb5636cf0be999e0be04d8cd37d772d218cc1a1a
Torsten Rahn <tackat@kde.org>
2008-10-19 14:19:34 +0000

    - Preparing for the return to the moon
    svn path=/trunk/KDE/kdeedu/marble/; revision=873472

---
+++ tools/iau2kml/iau2kml.cpp
@@ -0,0 +1,147 @@
+//
+// This file is part of the Marble Desktop Globe.
+//
+// This program is free software licensed under the GNU LGPL. You can
+// find a copy of this license in LICENSE.txt in the top directory of
+// the source code.
+//
+// Copyright 2006-2007 Torsten Rahn <tackat@kde.org>"
+// Copyright 2007      Inge Wallin  <ingwa@kde.org>"
+//
+
+
+// #include <zlib.h>
+
+#include <QtCore/QCoreApplication>
+#include <QtCore/QDebug>
+#include <QtCore/QFile>
+#include <QtCore/QStringList>
+
+
+QString escapeXml( const QString &str )
+{
+    QString xml = str;
+    xml.replace('&', "&amp;");
+    xml.replace('<', "&lt;");
+    xml.replace('>', "&gt;");
+    xml.replace('\'', "&apos;");
+    xml.replace('"', "&quot;");
+
+    return xml;
+}
+
+
+int main(int argc, char *argv[])
+{
+    QString  sourcefilename;
+    QString  targetfilename;
+
+    QCoreApplication  app( argc, argv );
+
+    for ( int i = 1; i < argc; ++i ) {
+        if ( strcmp( argv[ i ], "-o" ) != 0 )
+            continue;
+
+        targetfilename = QString(argv[i+1]);
+        sourcefilename = QString(argv[i+2]);
+
+        qDebug() << "Source: " << sourcefilename;
+        qDebug() << "Target: " << targetfilename;
+
+        QFile  sourcefile( sourcefilename );
+        sourcefile.open( QIODevice::ReadOnly );
+
+        // Read the data serialized from the file.
+        QTextStream  sourcestream( &sourcefile );
+        sourcestream.setCodec("UTF-8");
+
+        QFile  targetfile( targetfilename );
+        targetfile.open( QIODevice::ReadWrite );
+
+        QTextStream  targetstream( &targetfile );
+        targetstream.setCodec("UTF-8");
+
+        // gzFile gzDoc = gzopen( targetfilename.toLatin1(), "w");
+        // QTextStream targetstream( new QString() );
+
+        targetstream << "<?xml version=\"1.0\" encoding=\"UTF-8\"?> \n"
+                     << "<kml xmlns=\"http://earth.google.com/kml/2.0\"> \n"
+                     << "<Document> \n";
+
+        targetstream << "    <Schema name=\"MarblePlacemark\" parent=\"Placemark\"> \n";
+        targetstream << "       <SimpleField name=\"pop\" type=\"int\"></SimpleField> \n";
+        targetstream << "       <SimpleField name=\"role\" type=\"string\"></SimpleField> \n";
+        targetstream << "    </Schema> \n";
+
+        targetstream << "\n";
+
+        QString  rawline;
+        QString  nameString;
+        QString  latString;
+        QString  lonString;
+        QString  popString;
+        QString  roleString;
+        QString  description;
+        float    lat;
+        float    lon;
+        int          population;
+        QStringList  splitline;
+
+        while ( !sourcestream.atEnd() ) {
+
+            rawline = sourcestream.readLine();
+            if ( !rawline.startsWith("\"E\"|\"m\"|\"" ) ) {
+                continue;
+            }
+            rawline = rawline.replace( "\"|", "|" );
+            rawline = rawline.replace( "|\"", "|" );
+            if ( rawline.startsWith("\"") && rawline.endsWith("\"") ) {
+                rawline = rawline.left( rawline.length() - 1 );
+                rawline = rawline.right( rawline.length() - 2 );
+            }
+
+            splitline = rawline.split('|');
+
+            nameString  = splitline[2];
+            latString   = splitline[3];
+            lonString   = splitline[4];
+            popString   = splitline[10];
+            roleString  = splitline[18];
+            description = splitline[19];
+
+            population = (int) ( 1000.0 * popString.toFloat() );
+
+            lon = lonString.toFloat();
+
+            lat = latString.toFloat();
+
+            targetstream << "    <Placemark> \n";
+            targetstream << "        <name>" << escapeXml( nameString ) << "</name> \n";
+//            targetstream << "        <role>" << escapeXml( roleString ) << "</role> \n";
+//            targetstream << "        <pop>"
+//                         << escapeXml( QString::number( population ) ) << "</pop> \n";
+            targetstream << "        <description>" << escapeXml( description ) << "</description> \n";
+            targetstream << "        <Point>\n"
+                         << "            <coordinates>"
+                         << escapeXml( QString::number( lon ) )
+                         << ","
+                         << escapeXml( QString::number( lat ) )
+                         << "</coordinates> \n"
+                         << "        </Point> \n";
+            targetstream << "    </Placemark> \n";
+        }
+
+        targetstream << "</Document> \n"
+                     << "</kml> \n";
+        qDebug("...");
+
+        sourcefile.close();
+        targetfile.close();
+
+        qDebug("Finished!");
+        return 0;
+    }
+
+    qDebug(" asc2kml -o targetfile sourcefile");
+    app.exit();
+}
