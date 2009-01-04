aeee0455bafcf1f38cf746d04eca47afe85e1066
Eckhart Wörner <ewoerner@kde.org>
2009-01-04 12:49:52 +0000

    krazy: Check single-char QString operations for efficiency (1 issue) * replace issues
    svn path=/trunk/KDE/kdeedu/marble/; revision=905442

--- src/lib/MarbleModel.cpp
+++ src/lib/MarbleModel.cpp
@@ -796,7 +796,7 @@ void MarbleModelPrivate::geoDataDocumentAdded( GeoDataDocument* document )
 
     foreach(GeoDataPlacemark* placemark, document->placemarks())
     {
-        QString styleUrl = placemark->styleUrl().replace("#", "");
+        QString styleUrl = placemark->styleUrl().remove('#');
         placemark->setStyle( document->style( styleUrl ) );
     };
     m_fileviewmodel->append( item );
