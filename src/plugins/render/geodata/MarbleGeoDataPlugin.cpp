f349d081034bbc9c1111ea448884eab270df9efe
Patrick Spendrin <ps_ml@gmx.de>
2009-01-05 19:22:03 +0000

    remove unused code that was commented out
    svn path=/trunk/KDE/kdeedu/marble/; revision=906211

--- src/plugins/render/geodata/MarbleGeoDataPlugin.cpp
+++ src/plugins/render/geodata/MarbleGeoDataPlugin.cpp
@@ -86,109 +86,15 @@ QIcon MarbleGeoDataPlugin::icon () const
 
 void MarbleGeoDataPlugin::initialize ()
 {
-
-//    dataFacade()->geoDataModel()->addGeoDataFile( GEODATA_DATA_PATH "/europe.kml" );
-//    dataFacade()->geoDataModel()->addGeoDataFile( GEODATA_DATA_PATH "/germany.kml" );
-//    dataFacade()->geoDataModel()->addGeoDataFile( GEODATA_DATA_PATH "/poland.kml" );
-//    dataFacade()->geoDataModel()->addGeoDataFile( GEODATA_DATA_PATH "/czech.kml" );
-//    dataFacade()->geoDataModel()->addGeoDataFile( GEODATA_DATA_PATH "/Rabet.kml" );
-
-//    GeoDataDocument* rootDoc = dataFacade()->geoDataModel()->geoDataRoot();
-    
     m_view = new MarbleGeoDataView();
-
-/*    m_currentBrush = QColor( 0xff, 0x0, 0x0 );
-    m_currentPen = QColor( 0xff, 0x0, 0x0 );*/
 }
 
 bool MarbleGeoDataPlugin::isInitialized () const
 {
     return true;
 }
-/*
-void MarbleGeoDataPlugin::setBrushStyle( GeoPainter *painter, GeoDataDocument* root, QString mapped )
-{
-    if( root->style( mapped ) && root->style( mapped )->polyStyle() ) {
-        if( m_currentBrush.color() != root->style( mapped )->polyStyle()->color() ) {
-            qDebug() << "BrushColor:" << root->style( mapped )->polyStyle()->color() << m_currentBrush.color();
-            m_currentBrush.setColor( root->style( mapped )->polyStyle()->color() );
-            painter->setBrush( m_currentBrush );
-        }
-    }
-}
-
-void MarbleGeoDataPlugin::setPenStyle( GeoPainter *painter, GeoDataDocument* root, QString mapped )
-{
-    if( root->style( mapped ) && root->style( mapped )->lineStyle() ) {
-        if( m_currentPen.color() != root->style( mapped )->lineStyle()->color() || 
-            m_currentPen.widthF() != root->style( mapped )->lineStyle()->width() ) {
-            qDebug() << "PenColor:" << root->style( mapped )->lineStyle()->color() << m_currentPen.color();
-            qDebug() << "PenWidth:" << root->style( mapped )->lineStyle()->width() << m_currentPen.widthF();
-            m_currentPen.setColor( root->style( mapped )->lineStyle()->color() );
-            m_currentPen.setWidthF( root->style( mapped )->lineStyle()->width() );
-            painter->setPen( m_currentPen );
-        }
-    }
-}
-
-bool MarbleGeoDataPlugin::renderGeoDataGeometry( GeoPainter *painter, GeoDataGeometry *object, QString styleUrl )
-{
-    painter->save();
-    painter->autoMapQuality();
-
-    GeoDataDocument* root = dataFacade()->geoDataModel()->geoDataRoot();
-/// hard coded to use only the "normal" style
-    QString mapped = styleUrl;
-    GeoDataStyleMap* styleMap = root->styleMap( styleUrl.remove( '#' ) );
-    if( styleMap ) {
-        mapped = styleMap->value( QString( "normal" ) );
-    }
-    mapped.remove( '#' );
-
-
-    if( object->geometryId() == GeoDataPolygonId ) {
-        setBrushStyle( painter, root, mapped );
-        setPenStyle( painter, root, mapped );
-        painter->drawPolygon( dynamic_cast<GeoDataPolygon*>( object )->outerBoundary() );
-    }
-    if( object->geometryId() == GeoDataLinearRingId ) {
-        painter->setBrush( QColor( 0, 0, 0, 0 ) );
-        setPenStyle( painter, root, mapped );
-        painter->drawPolygon( *dynamic_cast<GeoDataLinearRing*>( object ) );
-    }
-    if( object->geometryId() == GeoDataLineStringId ) {
-        setPenStyle( painter, root, mapped );
-        painter->drawPolyline( *dynamic_cast<GeoDataLineString*>( object ) );
-    }
-    if( object->geometryId() == GeoDataMultiGeometryId ) {
-        painter->restore();
-        
-        painter->save();
-    }
-    painter->restore();
-    return true;
-}
-
-bool MarbleGeoDataPlugin::renderGeoDataFeature( GeoPainter *painter, GeoDataFeature *feature )
-{
-    if( !feature ) return false;
-    
-    if( feature->featureId() == GeoDataDocumentId || feature->featureId() == GeoDataFolderId ) {
-        Q_FOREACH( GeoDataFeature *childFeature, static_cast<GeoDataContainer*>( feature )->features() ) {
-            renderGeoDataFeature( painter, childFeature );
-        }
-    }
-    if( feature->featureId() == GeoDataPlacemarkId ) {
-        renderGeoDataGeometry( painter, dynamic_cast<GeoDataPlacemark*>( feature )->geometry(), dynamic_cast<GeoDataPlacemark*>( feature )->styleUrl() );
-    }
-    return true;
-}
-*/
 bool MarbleGeoDataPlugin::render( GeoPainter *painter, ViewportParams *viewport, const QString& renderPos, GeoSceneLayer * layer )
 {
-//    GeoDataDocument* rootDoc = dataFacade()->geoDataModel()->geoDataRoot();
-
-//    renderGeoDataFeature( painter, dynamic_cast<GeoDataFeature*>( rootDoc ) );
     if( !m_view->model() ) m_view->setModel( dataFacade()->renderModel() );
     m_view->setGeoPainter( painter );
     return true;
