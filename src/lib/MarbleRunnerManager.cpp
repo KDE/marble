2bbd73c5b7c79d56aec09f40b53183f72856c79c
Torsten Rahn <tackat@kde.org>
2009-02-06 18:33:44 +0000

    - Further patch from Matias Kallio to make Marble SVN compile on Qt 4.3
    Now Marble 0.7.x should fully compile even with Qt 4.3
    
    
    svn path=/branches/KDE/4.2/kdeedu/marble/; revision=922336

--- src/lib/MarbleRunnerManager.cpp
+++ src/lib/MarbleRunnerManager.cpp
@@ -91,7 +91,14 @@ void MarbleRunnerManager::newText(QString text)
 
 void MarbleRunnerManager::slotRunnerFinished( MarbleAbstractRunner* runner, QVector<GeoDataPlacemark*> result )
 {
-    m_runners.removeOne( runner );
+#if QT_VERSION >= 0x040400
+    m_runners.removeOne(runner);
+#else
+    int index = m_runners.indexOf(runner);
+    if (index != -1) {
+      m_runners.removeAt(index);
+    }
+#endif
     qDebug() << "Runner finished, active runners: " << m_runners.size();
     qDebug() << "Runner reports" << result.size() << "results";
     if( result.isEmpty() )
