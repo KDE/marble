926e6db13a045c20742ff640545cc145bfadc82b
Patrick Spendrin <ps_ml@gmx.de>
2009-04-16 22:24:22 +0000

    tab to 4 spaces
    svn path=/trunk/KDE/kdeedu/marble/; revision=955085

--- src/QtMarbleConfigDialog.h
+++ src/QtMarbleConfigDialog.h
@@ -30,78 +30,78 @@ class QtMarbleConfigDialog : public QDialog
     Q_OBJECT
     
     public:
-	QtMarbleConfigDialog( QWidget *parent = 0 );
-	~QtMarbleConfigDialog();
-	
-	// View Settings
-	Marble::DistanceUnit distanceUnit();
-	Marble::AngleUnit angleUnit();
-	Marble::MapQuality stillQuality();
-	Marble::MapQuality animationQuality();
-	int labelLocalization();
-	QFont mapFont();
-	
-	// Navigation Settings
-	int dragLocation();
-	int onStartup();
-	bool animateTargetVoyage();
-	
-	// Cache Settings
-	int volatileTileCacheLimit();
-	int persistentTileCacheLimit();
-	QString proxyUrl();
-	int proxyPort();
-	
-	QString proxyUser();
-	QString proxyPass();
-	bool proxyHttp();
-	bool proxySocks5();
-	bool proxyAuth();
-	
+    QtMarbleConfigDialog( QWidget *parent = 0 );
+    ~QtMarbleConfigDialog();
+
+    // View Settings
+    Marble::DistanceUnit distanceUnit();
+    Marble::AngleUnit angleUnit();
+    Marble::MapQuality stillQuality();
+    Marble::MapQuality animationQuality();
+    int labelLocalization();
+    QFont mapFont();
+
+    // Navigation Settings
+    int dragLocation();
+    int onStartup();
+    bool animateTargetVoyage();
+
+    // Cache Settings
+    int volatileTileCacheLimit();
+    int persistentTileCacheLimit();
+    QString proxyUrl();
+    int proxyPort();
+
+    QString proxyUser();
+    QString proxyPass();
+    bool proxyHttp();
+    bool proxySocks5();
+    bool proxyAuth();
+
     Q_SIGNALS:
-	/**
-	 * This signal is emitted when the user changed settings and
-	 * chose to Apply.
-	 */
-	void settingsChanged();
-	
-	/**
-	 * The user clicked on the button to clear volatile tile cache.
-	 */
-	void clearVolatileCacheClicked();
-	
-	/**
-	 * The user clicked on the button to clear persistent tile cache.
-	 */
-	void clearPersistentCacheClicked();
+    /**
+     * This signal is emitted when the user changed settings and
+     * chose to Apply.
+     */
+    void settingsChanged();
+
+    /**
+     * The user clicked on the button to clear volatile tile cache.
+     */
+    void clearVolatileCacheClicked();
+
+    /**
+     * The user clicked on the button to clear persistent tile cache.
+     */
+    void clearPersistentCacheClicked();
 
     public Q_SLOTS:
-	/**
-	 * Read settings and update interface.
-	 */
-	void readSettings();
-	
+    /**
+     * Read settings and update interface.
+     */
+    void readSettings();
+
     private Q_SLOTS:
-	/**
-	 * Write settings to disk.
-	 */
-	void writeSettings();
-	
-	/**
-	 * Synchronize the loaded settings with the file on hard disk.
-	 */
-	void syncSettings();
-	
+    /**
+     * Write settings to disk.
+     */
+    void writeSettings();
+
+    /**
+     * Synchronize the loaded settings with the file on hard disk.
+     */
+    void syncSettings();
+
     private:
-	Q_DISABLE_COPY( QtMarbleConfigDialog )
-	
-	void initSettings();
-	
-	Ui::MarbleViewSettingsWidget       *ui_viewSettings;
-	Ui::MarbleNavigationSettingsWidget *ui_navigationSettings;
-	MarbleCacheSettingsWidget          *w_cacheSettings;
-	
-	QSettings *settings;
+    Q_DISABLE_COPY( QtMarbleConfigDialog )
+
+    void initSettings();
+
+    Ui::MarbleViewSettingsWidget       *ui_viewSettings;
+    Ui::MarbleNavigationSettingsWidget *ui_navigationSettings;
+    MarbleCacheSettingsWidget          *w_cacheSettings;
+
+    QSettings *settings;
 };
 
 } // Marble namespace
