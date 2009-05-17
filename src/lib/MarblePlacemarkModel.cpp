c6f52740f9a65c134cc1b23c68c1d89c8c8ac082
Eckhart Wörner <ewoerner@kde.org>
2009-05-17 10:47:15 +0000

    Use reference, don't mess with a local copy
    svn path=/trunk/KDE/kdeedu/marble/; revision=969023

--- src/lib/MarblePlacemarkModel.cpp
+++ src/lib/MarblePlacemarkModel.cpp
@@ -262,7 +262,7 @@ void MarblePlacemarkModel::createFilterProperties( PlacemarkContainer &container
 
     QVector<GeoDataPlacemark>::Iterator i;
     for ( i = container.begin(); i != container.end(); ++i ) {
-        GeoDataPlacemark placemark = *i;
+        GeoDataPlacemark& placemark = *i;
 
         bool hasPopularity = false;
 
