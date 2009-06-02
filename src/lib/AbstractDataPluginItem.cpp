2265aa8e45c858771d24ef23b92cc1b21bc707f4
Bastian Holst <bastianholst@gmx.de>
2009-06-02 20:46:06 +0000

    Marbles AbstractDataPluginItemPrivate variables should be initialized.
    svn path=/trunk/KDE/kdeedu/marble/; revision=976860

--- src/lib/AbstractDataPluginItem.cpp
+++ src/lib/AbstractDataPluginItem.cpp
@@ -27,7 +27,10 @@ namespace Marble {
 class AbstractDataPluginItemPrivate {
  public:
     AbstractDataPluginItemPrivate()
-        : m_addedAngularResolution( 0 )
+        : m_id( "" ),
+          m_coordinates(),
+          m_target( "" ),
+          m_addedAngularResolution( 0 )
     {
     };
     
