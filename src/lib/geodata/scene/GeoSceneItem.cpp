e8c956ce8dbdee0c063d69e48221a03c9ad3879d
Jens-Michael Hoffmann <jensmh@gmx.de>
2009-07-10 15:20:06 +0000

    Just use QStrings default ctor.
    svn path=/trunk/KDE/kdeedu/marble/; revision=994444

--- src/lib/geodata/scene/GeoSceneItem.cpp
+++ src/lib/geodata/scene/GeoSceneItem.cpp
@@ -29,8 +29,8 @@ namespace Marble
 GeoSceneItem::GeoSceneItem( const QString& name )
     : m_icon(new GeoSceneIcon),
       m_name( name ),
-      m_text( "" ),
-      m_connectTo( "" ),
+      m_text(),
+      m_connectTo(),
       m_checkable( false ),
       m_spacing( 12 )
 {
