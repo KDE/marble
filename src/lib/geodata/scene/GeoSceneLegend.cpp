53e86bace27a9954a0436a4a6ac91e10e004a10a
Jens-Michael Hoffmann <jensmh@gmx.de>
2009-07-10 15:09:25 +0000

    - Remove redundant assignment to start iterator, - cache end iterator.
    svn path=/trunk/KDE/kdeedu/marble/; revision=994422

--- src/lib/geodata/scene/GeoSceneLegend.cpp
+++ src/lib/geodata/scene/GeoSceneLegend.cpp
@@ -80,7 +80,8 @@ GeoSceneSection* GeoSceneLegend::section( const QString& name )
     GeoSceneSection* section = 0;
 
     QVector<GeoSceneSection*>::const_iterator it = d->m_sections.constBegin();
-    for (it = d->m_sections.constBegin(); it != d->m_sections.constEnd(); ++it) {
+    QVector<GeoSceneSection*>::const_iterator end = d->m_sections.constEnd();
+    for (; it != end; ++it) {
         if ( (*it)->name() == name )
             section = *it;
     }
