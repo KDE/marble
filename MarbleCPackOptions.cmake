6c5f3081ab308f12d38bf4f9145ce8017e3f4526
Andrew Manson <g.real.ate@gmail.com>
2009-07-24 10:59:16 +0000

    Adding some CPack aditions to the CMake file.
    svn path=/trunk/KDE/kdeedu/marble/; revision=1001829

---
+++ MarbleCPackOptions.cmake
@@ -0,0 +1,8 @@
+############
+# File for all the CPack Variables. Will be added to as more is know about
+# CPack
+############
+
+set(CPACK_DEBIAN_PACKAGE_MAINTAINER "Torsten Rahn")
+#Needed for Deb Creation
+set(CPACK_PACKAGE_CONTACT "example@example.example")
