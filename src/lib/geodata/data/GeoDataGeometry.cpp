85396d990cc2feb31654239e04060135ba0930ce
Torsten Rahn <tackat@kde.org>
2009-07-27 12:45:42 +0000

    - Apply "Remove GeoGraphicsItem inheritance in GeoDataObjects" patch by Gridel Thibaut
    svn path=/trunk/KDE/kdeedu/marble/; revision=1002972

--- src/lib/geodata/data/GeoDataGeometry.cpp
+++ src/lib/geodata/data/GeoDataGeometry.cpp
@@ -138,15 +138,6 @@ void GeoDataGeometry::setAltitudeMode( const AltitudeMode altitudeMode )
     d->m_altitudeMode = altitudeMode;
 }
 
-void GeoDataGeometry::paint( GeoPainter *painter, ViewportParams *viewport,
-                             const QString& renderPos, GeoSceneLayer * layer )
-{
-    Q_UNUSED(painter);
-    Q_UNUSED(viewport);
-    Q_UNUSED(renderPos);
-    Q_UNUSED(layer);
-}
-
 void GeoDataGeometry::pack( QDataStream& stream ) const
 {
     GeoDataObject::pack( stream );
@@ -164,11 +155,6 @@ void GeoDataGeometry::unpack( QDataStream& stream )
     stream >> d->m_extrude;
     stream >> am;
     d->m_altitudeMode = (AltitudeMode) am;
-}
-
-bool GeoDataGeometry::isGeoProjected()
-{
-    return true;
 }
 
 }
--- src/lib/geodata/data/GeoDataGeometry.h
+++ src/lib/geodata/data/GeoDataGeometry.h
@@ -43,8 +43,7 @@ class GeoDataModel; // not implemented yet
  * @see GeoDataPolygon
  */
 
-class GEODATA_EXPORT GeoDataGeometry : public GeoDataObject,
-                                       public GeoGraphicsItem
+class GEODATA_EXPORT GeoDataGeometry : public GeoDataObject
 {
     friend class GeoDataPoint;
     friend class GeoDataPolygon;
@@ -73,15 +72,10 @@ class GEODATA_EXPORT GeoDataGeometry : public GeoDataObject,
     AltitudeMode altitudeMode() const;
     void setAltitudeMode( const AltitudeMode altitudeMode );
 
-    void paint( GeoPainter *painter, ViewportParams *viewport,
-                const QString& renderPos, GeoSceneLayer * layer = 0 );
-
     /// Serialize the contents of the feature to @p stream.
     virtual void pack( QDataStream& stream ) const;
     /// Unserialize the contents of the feature from @p stream.
     virtual void unpack( QDataStream& stream );
-
-    virtual bool isGeoProjected();
 
     void detach();
  private:
