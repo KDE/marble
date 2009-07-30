f2c6a2c8cabcdd6000c6ec7b52f026f42521d0aa
Ariya Hidayat <ariya@kde.org>
2009-07-30 18:47:12 +0000

    Fix potential divide-by-zero.
    svn path=/trunk/KDE/kdeedu/marble/; revision=1004705

--- src/lib/MarbleWidget.cpp
+++ src/lib/MarbleWidget.cpp
@@ -833,7 +833,7 @@ void MarbleWidget::paintEvent(QPaintEvent *evt)
     customPaint( &painter );
     d->m_map->d->paintOverlay( painter, dirtyRect );
 
-    qreal fps = 1000.0 / (qreal)( t.elapsed() );
+    qreal fps = 1000.0 / (qreal)( t.elapsed() + 1 );
     d->m_map->d->paintFps( painter, dirtyRect, fps );
     emit d->m_map->framesPerSecond( fps );
 }
