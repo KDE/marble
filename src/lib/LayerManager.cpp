da4b32e55462fd665545d09a36c90d58e1ab0b5b
Ariya Hidayat <ariya@kde.org>
2009-07-30 16:36:11 +0000

    Last fix to build with Qt < 4.5.
    Reviewed-by: Torsten Rahn.
    
    svn path=/trunk/KDE/kdeedu/marble/; revision=1004632

--- src/lib/LayerManager.cpp
+++ src/lib/LayerManager.cpp
@@ -107,7 +107,14 @@ QList<AbstractDataPluginItem *> LayerManager::whichItemAt( const QPoint& curpos 
     QList<AbstractDataPluginItem *> itemList;
     
     foreach( AbstractDataPlugin *plugin, d->m_dataPlugins ) {
+#if QT_VERSION >= 0x040500
         itemList.append( plugin->whichItemAt( curpos ) );
+#else
+        QList<AbstractDataPluginItem *> subList =  plugin->whichItemAt( curpos );
+        foreach( AbstractDataPluginItem *item, subList ) {
+            itemList.append( item );
+        }
+#endif
     }
     return itemList;
 }
