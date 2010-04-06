45909dc1ff439662f102f946e198d27f34edc573
Bastian Holst <bastianholst@gmx.de>
2010-04-06 10:07:15 +0000

    adding copyright note to MarbleDebug.* (with permission)
    svn path=/trunk/KDE/kdeedu/marble/; revision=1111627

--- src/lib/MarbleDebug.cpp
+++ src/lib/MarbleDebug.cpp
@@ -1,3 +1,13 @@
+//
+// This file is part of the Marble Desktop Globe.
+//
+// This program is free software licensed under the GNU LGPL. You can
+// find a copy of this license in LICENSE.txt in the top directory of
+// the source code.
+//
+// Copyright 2009      Patrick Spendrin <ps_ml@gmx.de>
+//
+
 #include "MarbleDebug.h"
 namespace Marble {
     bool MarbleDebug::enable = false;
