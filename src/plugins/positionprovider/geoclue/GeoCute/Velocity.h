ee7cd8ea418babbf16cdb6a2cb9bc978e8e1e33a
Bastian Holst <bastianholst@gmx.de>
2010-04-06 13:49:46 +0000

    Adding missing license texts with Eckhart Wörner's permission
    svn path=/trunk/KDE/kdeedu/marble/; revision=1111698

--- src/plugins/positionprovider/geoclue/GeoCute/Velocity.h
+++ src/plugins/positionprovider/geoclue/GeoCute/Velocity.h
@@ -1,3 +1,13 @@
+//
+// This file is part of the Marble Desktop Globe.
+//
+// This program is free software licensed under the GNU LGPL. You can
+// find a copy of this license in LICENSE.txt in the top directory of
+// the source code.
+//
+// Copyright 2009      Eckhart Wörner <ewoerner@kde.org>
+//
+
 #ifndef GEOCUTE_VELOCITY_H
 #define GEOCUTE_VELOCITY_H
 
