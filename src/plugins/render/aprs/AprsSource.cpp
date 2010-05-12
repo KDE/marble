f7c39f9833c9a3902e59e74b7709be9733fa5f63
Wes Hardaker <wjhns25@hardakers.net>
2010-05-12 14:07:02 +0000

    fix odd double-code-copy problem from the last APRS commit
    svn path=/trunk/KDE/kdeedu/marble/; revision=1125852

--- src/plugins/render/aprs/AprsSource.cpp
+++ src/plugins/render/aprs/AprsSource.cpp
@@ -33,38 +33,3 @@ AprsSource::setSocket( QIODevice *setto )
     m_socket = setto;
 }
 
-//
-// This file is part of the Marble Desktop Globe.
-//
-// This program is free software licensed under the GNU LGPL. You can
-// find a copy of this license in LICENSE.txt in the top directory of
-// the source code.
-//
-// Copyright 2010 Wes Hardaker <hardaker@users.sourceforge.net>
-//
-
-#include "AprsSource.h"
-
-using namespace Marble;
-
-AprsSource::AprsSource( QIODevice *insocket )
-    : m_socket( insocket )
-{
-}
-
-AprsSource::~AprsSource()
-{
-}
-
-QIODevice *
-AprsSource::socket() 
-{
-    return m_socket;
-}
-
-void
-AprsSource::setSocket( QIODevice *setto ) 
-{
-    m_socket = setto;
-}
-
