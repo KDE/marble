c9357e557b355c9c4415db86fd3a96b5555e06ef
Dennis Nienhüser <earthwings@gentoo.org>
2010-07-17 21:59:10 +0000

    Change license header to match that of the other marble files (point to LICENSE.txt). Acknowledged by Henry de Valence on IRC.
    svn path=/trunk/KDE/kdeedu/marble/; revision=1151114

--- src/lib/LatLonEdit.cpp
+++ src/lib/LatLonEdit.cpp
@@ -1,21 +1,11 @@
-//Copyright 2008 Henry de Valence <hdevalence@gmail.com>
-/*
-** This program is free software; you can redistribute it and/or
-** modify it under the terms of the GNU Lesser General Public License as
-** published by the Free Software Foundation; either version 2 of
-** the License or (at your option) version 3 or any later version
-** accepted by the membership of KDE e.V. (or its successor approved
-** by the membership of KDE e.V.), which shall act as a proxy
-** defined in Section 14 of version 3 of the license.
-**
-** This program is distributed in the hope that it will be useful,
-** but WITHOUT ANY WARRANTY; without even the implied warranty of
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-** GNU Lesser General Public License for more details.
-**
-** You should have received a copy of the GNU Lesser General Public License
-** along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+//
+// This file is part of the Marble Desktop Globe.
+//
+// This program is free software licensed under the GNU LGPL. You can
+// find a copy of this license in LICENSE.txt in the top directory of
+// the source code.
+//
+// Copyright 2008 Henry de Valence <hdevalence@gmail.com>
 
 #include "LatLonEdit.h"
 #include "ui_LatLonEdit.h"
