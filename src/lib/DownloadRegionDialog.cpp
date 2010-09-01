bd85bccc937f6e21a4d44ee49a64c08fd24fa7b7
Dennis Nienhüser <earthwings@gentoo.org>
2010-09-01 16:57:33 +0000

    Make download region dialog usable on Maemo. Backport of commit 1169429, will be in Marble 0.10.2. CCMAIL: kspt.tor@gmail.com
    svn path=/branches/KDE/4.5/kdeedu/marble/; revision=1170617

--- src/lib/DownloadRegionDialog.cpp
+++ src/lib/DownloadRegionDialog.cpp
@@ -24,6 +24,7 @@
 #include <QtGui/QRadioButton>
 #include <QtGui/QShowEvent>
 #include <QtGui/QVBoxLayout>
+#include <QtGui/QScrollArea>
 
 #include "AbstractScanlineTextureMapper.h"
 #include "GeoDataLatLonAltBox.h"
@@ -186,8 +187,21 @@ DownloadRegionDialog::DownloadRegionDialog( MarbleModel const * const model, QWi
     layout->addWidget( d->createSelectionMethodBox() );
     layout->addWidget( d->m_tileLevelRangeWidget );
     layout->addLayout( d->createTilesCounter() );
-    layout->addWidget( d->createOkCancelButtonBox() );
-    setLayout( layout );
+    
+    if ( MarbleGlobal::getInstance()->profiles() & MarbleGlobal::SmallScreen ) {
+        QWidget* widget = new QWidget( this );
+        widget->setLayout( layout );
+        QScrollArea* scrollArea = new QScrollArea( this );
+        scrollArea->setFrameShape( QFrame::NoFrame );
+        scrollArea->setWidget( widget );
+        QVBoxLayout * const mainLayout = new QVBoxLayout;
+        mainLayout->addWidget( scrollArea );
+        mainLayout->addWidget( d->createOkCancelButtonBox() );
+        setLayout( mainLayout );
+    } else {
+        layout->addWidget( d->createOkCancelButtonBox() );
+        setLayout( layout );
+    }  
 
     connect( d->m_latLonBoxWidget, SIGNAL( valueChanged() ), SLOT( updateTilesCount() ));
     connect( d->m_tileLevelRangeWidget, SIGNAL( topLevelChanged( int )),
