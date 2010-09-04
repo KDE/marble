10da970bf7692bf1a3f05d49176c79fd885cd835
Max Brazhnikov <makc@freebsd.org>
2010-09-04 20:40:21 +0000

    merge r1146867: Rewrite Findlibgps.cmake without using PkgConfig.
    svn path=/branches/KDE/4.5/kdeedu/marble/; revision=1171683

--- Findlibgps.cmake
+++ Findlibgps.cmake
@@ -1,10 +1,16 @@
-# - Find Gpsd libs - gps daemon library
-#
-# This module finds if Gpsd is installed.
-#
-# Copyright (c) 2006, Andrew Manson, <g.real.ate@gmail.com>
-#
+# LIBGPS_FOUND - system has the LIBGPS library
+# LIBGPS_INCLUDE_DIR - the LIBGPS include directory
+# LIBGPS_LIBRARIES - The libraries needed to use LIBGPS
 
-include(FindPkgConfig)
+if(LIBGPS_INCLUDE_DIR AND LIBGPS_LIBRARIES)
+  set(LIBGPS_FOUND TRUE)
+else(LIBGPS_INCLUDE_DIR AND LIBGPS_LIBRARIES)
 
-pkg_check_modules(libgps libgps>=2.35)
+  find_path(LIBGPS_INCLUDE_DIR NAMES gps.h)
+  find_library(LIBGPS_LIBRARIES NAMES gps)
+
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(libgps DEFAULT_MSG LIBGPS_INCLUDE_DIR LIBGPS_LIBRARIES)
+
+  mark_as_advanced(LIBGPS_INCLUDE_DIR LIBGPS_LIBRARIES)
+endif(LIBGPS_INCLUDE_DIR AND LIBGPS_LIBRARIES)
