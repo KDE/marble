d3a0f2444f69a64855a934e85334d9c5147d98f5
Dennis Nienhüser <earthwings@gentoo.org>
2010-10-09 13:51:03 +0000

    Support writing kml MultiGeometry tags.
    svn path=/trunk/KDE/kdeedu/marble/; revision=1184227

---
+++ src/lib/geodata/writers/kml/KmlMultiGeometryTagWriter.cpp
@@ -0,0 +1,42 @@
+//
+// This file is part of the Marble Desktop Globe.
+//
+// This program is free software licensed under the GNU LGPL. You can
+// find a copy of this license in LICENSE.txt in the top directory of
+// the source code.
+//
+// Copyright 2010      Dennis Nienhüser <earthwings@gentoo.org>
+//
+
+#include "KmlMultiGeometryTagWriter.h"
+
+#include "GeoDataMultiGeometry.h"
+#include "GeoDataTypes.h"
+#include "GeoWriter.h"
+#include "KmlElementDictionary.h"
+
+namespace Marble
+{
+
+static GeoTagWriterRegistrar s_writerMultiGeometry(
+    GeoTagWriter::QualifiedName( GeoDataTypes::GeoDataMultiGeometryType,
+                                 kml::kmlTag_nameSpace22 ),
+    new KmlMultiGeometryTagWriter );
+
+bool KmlMultiGeometryTagWriter::write( const GeoDataObject &node, GeoWriter& writer ) const
+{
+    const GeoDataMultiGeometry &geometry = static_cast<const GeoDataMultiGeometry&>( node );
+
+    writer.writeStartElement( kml::kmlTag_MultiGeometry );
+
+    for ( int i = 0; i < geometry.size(); ++i )
+    {
+        writeElement( geometry.at( i ), writer );
+    }
+
+    writer.writeEndElement();
+
+    return true;
+}
+
+}
