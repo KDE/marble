c70db391bc1e202d97e863f904d4cd625df6c64b
Frederik Schwarzer <schwarzerf@gmail.com>
2010-12-28 14:55:59 +0000

    typo
    svn path=/trunk/KDE/kdeedu/marble/; revision=1209915

--- src/kdemain.cpp
+++ src/kdemain.cpp
@@ -256,7 +256,7 @@ int main ( int argc, char *argv[] )
     options.add( "enableCurrentLocation", ki18n( "Enable tab to show the current location" ) );
     options.add( "enableFileView",
                  ki18n( "Enable tab to see gpxFileView" ) );
-    options.add( "tile-id", ki18n( "Show tile id's" ) );
+    options.add( "tile-id", ki18n( "Show tile IDs" ) );
     options.add( "marbledatapath <data path>", ki18n( "Use a different directory which contains map data" ) );
     if( profiles & MarbleGlobal::SmallScreen ) {
         options.add( "nosmallscreen", ki18n( "Do not use the interface optimized for small screens" ) );
