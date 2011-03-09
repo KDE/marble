2b07a1ab10cfa4624d31723133ee536b539fcf4d
Dennis Nienhüser <earthwings@gentoo.org>
2011-03-09 20:30:52 +0000

    Add a FindPhonon.cmake check to be able to use phonon from kdesupport. Patch by Christoph Feck, thanks! Extended to make phonon an optional dependency and use a null audio output when it's missing so that the routing plugin can still be used without phonon (no sound output then of course). CCMAIL: christoph@maxiom.de CCMAIL: marble-devel@kde.org
    svn path=/trunk/KDE/kdeedu/marble/; revision=1224259

---
+++ src/plugins/render/routing/NullAudioOutput.cpp
@@ -0,0 +1,79 @@
+//
+// This file is part of the Marble Virtual Globe.
+//
+// This program is free software licensed under the GNU LGPL. You can
+// find a copy of this license in LICENSE.txt in the top directory of
+// the source code.
+//
+// Copyright 2011      Dennis Nienhüser <earthwings@gentoo.org>
+//
+
+#include "AudioOutput.h"
+
+#include "MarbleDirs.h"
+#include "MarbleDebug.h"
+#include "routing/instructions/RoutingInstruction.h"
+
+namespace Marble
+{
+
+class AudioOutputPrivate
+{
+public:
+  void audioOutputFinished();
+};
+
+void AudioOutputPrivate::audioOutputFinished()
+{
+    // nothing to do
+}
+
+AudioOutput::AudioOutput( QObject* parent ) : QObject( parent ),
+    d( new AudioOutputPrivate )
+{
+    // nothing to do
+}
+
+AudioOutput::~AudioOutput()
+{
+    delete d;
+}
+
+void AudioOutput::update( int, qreal, RoutingInstruction::TurnType )
+{
+    // nothing to do
+}
+
+void AudioOutput::setMuted( bool )
+{
+    // nothing to do
+}
+
+void AudioOutput::setSpeaker( const QString & )
+{
+    // nothing to do
+}
+
+QStringList AudioOutput::speakers() const
+{
+    return QStringList();
+}
+
+void AudioOutput::setSoundEnabled( bool )
+{
+    // nothing to do
+}
+
+void AudioOutput::announceStart()
+{
+    // nothing to do
+}
+
+void AudioOutput::announceDestination()
+{
+    // nothing to do
+}
+
+}
+
+#include "AudioOutput.moc"
