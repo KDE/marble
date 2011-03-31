e9af930c515ad81735b62b4cb7c57a8770badfcc
Dennis Nienhüser <earthwings@gentoo.org>
2011-03-31 19:59:42 +0000

    Version number bump: 1.0.80 (1.1 Beta 1) for the application, 0.11.16 for the library. The latter is 0x000b10 to reflect that we have two active, binary compatible branches of the lib, this one more recent.
    svn path=/branches/marble/marble-1.1/; revision=1226657

--- src/lib/global.h
+++ src/lib/global.h
@@ -240,14 +240,14 @@ QColor const oxygenForestGreen4 = QColor::fromRgb( 55, 164, 44 );
 
 // String for about dialog and http user agent
 // FIXME: check if blanks are allowed in user agent version numbers
-const QString MARBLE_VERSION_STRING = QString::fromLatin1( "0.11.0 Release Candidate 2" );
+const QString MARBLE_VERSION_STRING = QString::fromLatin1( "0.11.16" );
 
 // API Version id:
 // form : 0xMMmmpp
 //        MM = major revision.
 //        mm = minor revision.
 //        pp = patch revision.
-#define MARBLE_VERSION 0x000b00
+#define MARBLE_VERSION 0x000b10
 
 static const char NOT_AVAILABLE[] = QT_TR_NOOP("not available");
 
