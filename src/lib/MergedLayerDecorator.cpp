KDE/marble

commit 8101c98e1fe6a42c5ad7a1f37c4f2074427af20e

cleanup StackedTile, TextureTile, and TileLoader
* StackedTile::isExpired() is never read
* TextureTile: lastModified(), setExpireSecs(), and isExpired() are only used during creation
* TextureTile::stackedTileId() can be reconstructed from TextureTile::id()
* the blending of a TextureTile can be passed via its constructor rather than by setBlending()
* TileLoader::reloadTile( TileId, ... ) is never used

src/lib/MergedLayerDecorator.cpp
@@ -125,7 +125,7 @@ bool MergedLayerDecorator::showTileId() const
 QImage MergedLayerDecorator::loadDataset()
 {
     const TileId decorationTileId( m_cityLightsTextureLayer->sourceDir(), m_id.zoomLevel(), m_id.x(), m_id.y());
-    QImage image = *m_tileLoader->loadTile( decorationTileId, decorationTileId, DownloadBrowse )->image();
+    QImage image = *m_tileLoader->loadTile( decorationTileId, DownloadBrowse )->image();
     return image;
 }
 

src/lib/StackedTileLoader.cpp
@@ -219,10 +219,8 @@ StackedTile* StackedTileLoader::loadTile( TileId const & stackedTileId )
                              stackedTileId.x(), stackedTileId.y() );
         mDebug() << "StackedTileLoader::loadTile: tile" << textureLayer->sourceDir()
                  << tileId.toString() << textureLayer->tileSize();
-        QSharedPointer<TextureTile> const tile = d->m_tileLoader->loadTile( stackedTileId, tileId,
-                                                                            DownloadBrowse );
+        QSharedPointer<TextureTile> const tile = d->m_tileLoader->loadTile( tileId, DownloadBrowse );
         if ( tile ) {
-            tile->setBlending( textureLayer->blending() );
             tiles.append( tile );
         }
     }

src/lib/TextureTile.cpp
@@ -22,10 +22,9 @@
 namespace Marble
 {
 
-TextureTile::TextureTile( TileId const & tileId, QImage const * image )
+TextureTile::TextureTile( TileId const & tileId, QImage const * image, const Blending * blending )
     : m_id( tileId ),
-      m_blending( 0 ),
-      m_expireSecs( std::numeric_limits<int>::max() ),
+      m_blending( blending ),
       m_image( image )
 {
     Q_ASSERT( image );

src/lib/TextureTile.h
@@ -21,7 +21,6 @@
 
 #include "TileId.h"
 
-class QByteArray;
 class QImage;
 
 namespace Marble
@@ -58,10 +57,9 @@ class StackedTileLoader;
 class TextureTile
 {
     friend class TileLoader;
-    friend class StackedTileLoader;
 
  public:
-    TextureTile( TileId const & tileId, QImage const * image );
+    TextureTile( TileId const & tileId, QImage const * image, const Blending * blending );
     ~TextureTile();
 
 /*!
@@ -69,25 +67,7 @@ class TextureTile
     \return A TileId object that encodes zoom level, position and map theme.
 */     
     TileId const & id() const;
-    
-/*!
-    \brief Returns a unique ID for the StackedTile that contains this tile.
-    \return A TileId object that encodes zoom level, position and map theme.
-*/         
-    TileId const & stackedTileId() const;
-    
-/*!
-    \brief Returns the time at which the tile was previously loaded onto the harddisc.
-    \return A QDateTime object that holds the modification time of the tile.
-*/         
-    QDateTime const & lastModified() const;
-    
-/*!
-    \brief Returns whether the tile is considered outdated.
-    \return Whether the tile needs to be reloaded according to its last modification time.
-*/         
-    bool isExpired() const;
-    
+
 /*!
     \brief Returns the QImage that describes the look of the TextureTile
     \return A non-zero pointer to a QImage associated with the tile.
@@ -107,16 +87,9 @@ class TextureTile
     Q_DISABLE_COPY( TextureTile )
 
     void setImage( QImage * const );
-    void setBlending( Blending const * const );
-    void setStackedTileId( TileId const & );
-    void setLastModified( QDateTime const & );
-    void setExpireSecs( int const );
 
     TileId const m_id;
-    TileId m_stackedTileId;
-    Blending const * m_blending;
-    QDateTime m_lastModified;
-    int m_expireSecs;
+    Blending const * const m_blending;
     QImage const * m_image;
 };
 
@@ -126,21 +99,6 @@ class TextureTile
 inline TileId const & TextureTile::id() const
 {
     return m_id;
-}
-
-inline TileId const & TextureTile::stackedTileId() const
-{
-    return m_stackedTileId;
-}
-
-inline QDateTime const & TextureTile::lastModified() const
-{
-    return m_lastModified;
-}
-
-inline bool TextureTile::isExpired() const
-{
-    return m_lastModified.secsTo( QDateTime::currentDateTime() ) >= m_expireSecs;
 }
 
 inline QImage const * TextureTile::image() const
@@ -169,26 +127,6 @@ inline void TextureTile::setImage( QImage * const image )
     delete m_image;
 
     m_image = image;
-}
-
-inline void TextureTile::setBlending( Blending const * const blending )
-{
-    m_blending = blending;
-}
-
-inline void TextureTile::setStackedTileId( TileId const & id )
-{
-    m_stackedTileId = id;
-}
-
-inline void TextureTile::setLastModified( QDateTime const & lastModified )
-{
-    m_lastModified = lastModified;
-}
-
-inline void TextureTile::setExpireSecs( int const expireSecs )
-{
-    m_expireSecs = expireSecs;
 }
 
 }

src/lib/TileLoader.cpp
@@ -49,22 +49,21 @@ TileLoader::TileLoader( HttpDownloadManager * const downloadManager, MapThemeMan
 //     - if not expired: create TextureTile, set state to "uptodate", return it => done
 //     - if expired: create TextureTile, state is set to Expired by default, trigger dl,
 
-QSharedPointer<TextureTile> TileLoader::loadTile( TileId const & stackedTileId,
-                                                  TileId const & tileId,
-                                                  DownloadUsage const usage )
+QSharedPointer<TextureTile> TileLoader::loadTile( TileId const & tileId, DownloadUsage const usage )
 {
+    GeoSceneTexture const * const textureLayer = findTextureLayer( tileId );
     QString const fileName = tileFileName( tileId );
     QImage const image( fileName );
     if ( !image.isNull() ) {
         // file is there, so create and return a tile object in any case,
         // but check if an update should be triggered
-        GeoSceneTexture const * const textureLayer = findTextureLayer( tileId );
-        QSharedPointer<TextureTile> const tile( new TextureTile( tileId, new QImage( image ) ));
-        tile->setStackedTileId( stackedTileId );
-        tile->setLastModified( QFileInfo( fileName ).lastModified() );
-        tile->setExpireSecs( textureLayer->expire() );
+        QSharedPointer<TextureTile> const tile( new TextureTile( tileId, new QImage( image ), textureLayer->blending() ));
 
-        if ( !tile->isExpired() ) {
+        const QDateTime lastModified = QFileInfo( fileName ).lastModified();
+        const int expireSecs = textureLayer->expire();
+        const bool isExpired = lastModified.secsTo( QDateTime::currentDateTime() ) >= expireSecs;
+
+        if ( !isExpired ) {
             mDebug() << "TileLoader::loadTile" << tileId.toString() << "StateUptodate";
         } else {
             mDebug() << "TileLoader::loadTile" << tileId.toString() << "StateExpired";
@@ -79,52 +78,11 @@ QSharedPointer<TextureTile> TileLoader::loadTile( TileId const & stackedTileId,
     QImage * replacementTile = scaledLowerLevelTile( tileId );
     Q_ASSERT( replacementTile && !replacementTile->isNull() );
 
-    QSharedPointer<TextureTile> const tile( new TextureTile( tileId, replacementTile ));
-    tile->setStackedTileId( stackedTileId );
+    QSharedPointer<TextureTile> const tile( new TextureTile( tileId, replacementTile, textureLayer->blending() ));
 
     m_waitingForUpdate.insert( tileId, tile );
     triggerDownload( tileId, usage );
 
-    return tile;
-}
-
-// This method loads a tile from the filesystem and additionally triggers a
-// download of that tile (without checking expiration). It is called by upper
-// layer (StackedTileLoader) when the tile that should be reloaded is not
-// currently loaded in memory.
-// It is (theoretically) used for map refresh/reload (F5 key) and will be used
-// for the (hopefully) coming download region feature.
-//
-// post condition
-//     - tile object is being returned, with download triggered,
-//       pointer is kept in m_waitingForUpdate until tile is downloaded
-QSharedPointer<TextureTile> TileLoader::reloadTile( TileId const & stackedTileId,
-                                                    TileId const & tileId,
-                                                    DownloadUsage const usage )
-{
-    QSharedPointer<TextureTile> tile =
-        m_waitingForUpdate.value( tileId, QSharedPointer<TextureTile>() );
-    if ( tile )
-        // tile is being downloaded already and waiting for update,
-        // no need to reload 2 times => just return the tile
-        return tile;
-
-    QString const fileName = tileFileName( tileId );
-    QImage const image( fileName );
-    if ( !image.isNull() ) {
-        tile = QSharedPointer<TextureTile>( new TextureTile( tileId, new QImage( image ) ));
-        tile->setLastModified( QFileInfo( fileName ).lastModified() );
-    }
-    else {
-        QImage * const replacementTile = scaledLowerLevelTile( tileId );
-        tile = QSharedPointer<TextureTile>( new TextureTile( tileId, replacementTile ));
-    }
-
-    GeoSceneTexture const * const textureLayer = findTextureLayer( tileId );
-    tile->setExpireSecs( textureLayer->expire() );
-    tile->setStackedTileId( stackedTileId );
-    m_waitingForUpdate.insert( tileId, tile );
-    triggerDownload( tileId, usage );
     return tile;
 }
 
@@ -164,8 +122,8 @@ void TileLoader::updateTile( QByteArray const & data, QString const & tileId )
         return;
 
     tile->setImage( image );
-    tile->setLastModified( QDateTime::currentDateTime() );
-    emit tileCompleted( tile->stackedTileId() );
+    TileId stackedTileId( 0, id.zoomLevel(), id.x(), id.y() );
+    emit tileCompleted( stackedTileId );
 }
 
 void TileLoader::updateTextureLayers()

src/lib/TileLoader.h
@@ -31,7 +31,7 @@ class QUrl;
 
 namespace Marble
 {
-class HttpDownloadManager; // remove?
+class HttpDownloadManager;
 class GeoSceneTexture;
 class MapThemeManager;
 
@@ -42,10 +42,7 @@ class TileLoader: public QObject
  public:
     TileLoader( HttpDownloadManager * const, MapThemeManager const * mapThemeManager );
 
-    QSharedPointer<TextureTile> loadTile( TileId const & stackedTileId, TileId const & tileId,
-                                          DownloadUsage const );
-    QSharedPointer<TextureTile> reloadTile( TileId const & stackedTileId, TileId const & tileId,
-                                            DownloadUsage const );
+    QSharedPointer<TextureTile> loadTile( TileId const & tileId, DownloadUsage const );
     void reloadTile( QSharedPointer<TextureTile> const & tile, DownloadUsage const );
     void downloadTile( TileId const & tileId );
 

src/lib/StackedTile.cpp
@@ -24,12 +24,12 @@
 
 using namespace Marble;
 
-static uint **jumpTableFromQImage32( QImage &img )
+static const uint **jumpTableFromQImage32( const QImage &img )
 {
     const int  height = img.height();
     const int  bpl    = img.bytesPerLine() / 4;
-    uint      *data = reinterpret_cast<QRgb*>(img.bits());
-    uint     **jumpTable = new uint*[height];
+    const uint  *data = reinterpret_cast<const QRgb*>(img.bits());
+    const uint **jumpTable = new const uint*[height];
 
     for ( int y = 0; y < height; ++y ) {
         jumpTable[ y ] = data;
@@ -40,12 +40,12 @@ static uint **jumpTableFromQImage32( QImage &img )
 }
 
 
-static uchar **jumpTableFromQImage8( QImage &img )
+static const uchar **jumpTableFromQImage8( const QImage &img )
 {
     const int  height = img.height();
     const int  bpl    = img.bytesPerLine();
-    uchar     *data = img.bits();
-    uchar    **jumpTable = new uchar*[height];
+    const uchar  *data = img.bits();
+    const uchar **jumpTable = new const uchar*[height];
 
     for ( int y = 0; y < height; ++y ) {
         jumpTable[ y ] = data;
@@ -61,9 +61,7 @@ StackedTilePrivate::StackedTilePrivate( const TileId& id ) :
       jumpTable8(0),
       jumpTable32(0),
       m_resultTile(),
-      m_byteCount(0),
-      m_depth(0),
-      m_isGrayscale( false )
+      m_byteCount(0)
 {
 }
 
@@ -75,16 +73,16 @@ StackedTilePrivate::~StackedTilePrivate()
 
 uint StackedTilePrivate::pixel( int x, int y ) const
 {
-    if ( m_depth == 8 ) {
-        if ( m_isGrayscale )
+    if ( m_resultTile.depth() == 8 ) {
+        if ( m_resultTile.isGrayscale() )
             return (jumpTable8)[y][x];
         else
             return m_resultTile.color( (jumpTable8)[y][x] );
     }
-    if ( m_depth == 32 )
+    if ( m_resultTile.depth() == 32 )
         return (jumpTable32)[y][x];
-    
-    if ( m_depth == 1 && !m_isGrayscale )
+
+    if ( m_resultTile.depth() == 1 && !m_resultTile.isGrayscale() )
         return m_resultTile.color((jumpTable8)[y][x/8] >> 7);
 
     return m_resultTile.pixel( x, y );
@@ -113,7 +111,7 @@ uint StackedTilePrivate::pixelF( qreal x, qreal y, const QRgb& topLeftValue ) co
                             + (bottomLeftValue & topLeftValue);
         else 
             leftValue = bottomLeftValue;
-#else        
+#else
         // blending the color values of the top left and bottom left point
         qreal ml_red   = ( 1.0 - fY ) * qRed  ( topLeftValue  ) + fY * qRed  ( bottomLeftValue  );
         qreal ml_green = ( 1.0 - fY ) * qGreen( topLeftValue  ) + fY * qGreen( bottomLeftValue  );
@@ -243,16 +241,6 @@ StackedTile::~StackedTile()
 {
 }
 
-bool StackedTile::isExpired() const
-{
-    bool result = false;
-    QVector<QSharedPointer<TextureTile> >::const_iterator pos = d->m_tiles.constBegin();
-    QVector<QSharedPointer<TextureTile> >::const_iterator const end = d->m_tiles.constEnd();
-    for (; pos != end; ++pos )
-        result |= (*pos)->isExpired();
-    return result;
-}
-
 void StackedTile::initJumpTables()
 {
     //    mDebug() << "Entered initJumpTables( bool ) of Tile" << d->m_id;
@@ -302,7 +290,7 @@ uint StackedTile::pixelF( qreal x, qreal y, const QRgb& topLeftValue ) const
 
 int StackedTile::depth() const
 {
-    return d->m_depth;
+    return d->m_resultTile.depth();
 }
 
 int StackedTile::numBytes() const
@@ -329,7 +317,7 @@ void StackedTile::initResultTile()
 {
     // TODO: Free all the TextureTiles once the completion status is reached 
     // to save memory.
-    
+
     Q_ASSERT( !d->m_tiles.isEmpty() );
     // if there are more than one active texture layers, we have to convert the
     // result tile into QImage::Format_ARGB32_Premultiplied to make blending possible
@@ -352,7 +340,5 @@ void StackedTile::initResultTile()
     initJumpTables();
 
     // for now, this seems to be the best place for initializing this stuff
-    d->m_depth = d->m_resultTile.depth();
-    d->m_isGrayscale = d->m_resultTile.isGrayscale();
     d->calcByteCount();
 }

src/lib/StackedTile.h
@@ -26,9 +26,7 @@ namespace Marble
 {
 
 class StackedTilePrivate;
-class GeoSceneTexture;
 class TextureTile;
-class StackedTileLoader;
 
 /*!
     \class StackedTile
@@ -67,7 +65,6 @@ class StackedTile : public AbstractTile
 
     int depth() const;
     int numBytes() const;
-    bool isExpired() const;
 
 /*!
     \brief Returns the stack of TextureTiles

commit 0b8a17b09b977b53a8e3d0f843257c279df049fc

have the TileLoader return QImages rather than TextureTiles
This patch fixes a bug introduced in 8101c98e, where a wrong blending may be assigned if a texture layer is used in multiple map themes, causing the map to stay black.
This happens if sourceDir() from different GeoSceneTextures are the same, which then map to the same hash value in the TileId.
As a result, only one GeoSceneTexture "survives" in the TileLoader, such that the blending from the wrong GeoSceneTexture may be applied to a TextureTile.
Having the TileLoader return a plain QImage and having the TextureTile created in StackedTileLoader solves the issue, since in the correct GeoSceneTexture is used in the StackedTileLoader.

As a nice side effect, MergedLayerDecorator::loadDataset() doesn't need to rely on QSharedPointer to delete the temporarily used TextureTile.

src/lib/MergedLayerDecorator.cpp
@@ -36,7 +36,6 @@
 
 #include <QtGui/QPainter>
 #include <QtCore/QPointer>
-#include "TextureTile.h"
 
 using namespace Marble;
 
@@ -125,7 +124,7 @@ bool MergedLayerDecorator::showTileId() const
 QImage MergedLayerDecorator::loadDataset()
 {
     const TileId decorationTileId( m_cityLightsTextureLayer->sourceDir(), m_id.zoomLevel(), m_id.x(), m_id.y());
-    QImage image = *m_tileLoader->loadTile( decorationTileId, DownloadBrowse )->image();
+    const QImage image = m_tileLoader->loadTile( decorationTileId, DownloadBrowse );
     return image;
 }
 

src/lib/StackedTileLoader.cpp
@@ -219,10 +219,9 @@ StackedTile* StackedTileLoader::loadTile( TileId const & stackedTileId )
                              stackedTileId.x(), stackedTileId.y() );
         mDebug() << "StackedTileLoader::loadTile: tile" << textureLayer->sourceDir()
                  << tileId.toString() << textureLayer->tileSize();
-        QSharedPointer<TextureTile> const tile = d->m_tileLoader->loadTile( tileId, DownloadBrowse );
-        if ( tile ) {
-            tiles.append( tile );
-        }
+        const QImage tileImage = d->m_tileLoader->loadTile( tileId, DownloadBrowse );
+        QSharedPointer<TextureTile> tile( new TextureTile( tileId, tileImage, textureLayer->blending() ) ); 
+        tiles.append( tile );
     }
     Q_ASSERT( !tiles.isEmpty() );
 
@@ -352,7 +351,9 @@ void StackedTileLoader::updateTile( TileId const & tileId )
         QVector<QSharedPointer<TextureTile> > *tiles = displayedTile->tiles();
         for ( int i = 0; i < tiles->count(); ++ i) {
             if ( (*tiles)[i]->id() == tileId ) {
-                (*tiles)[i] = d->m_tileLoader->loadTile( tileId, DownloadBrowse );
+                const QImage tileImage = d->m_tileLoader->loadTile( tileId, DownloadBrowse );
+                const Blending *blending = (*tiles)[i]->blending();
+                (*tiles)[i] = QSharedPointer<TextureTile>( new TextureTile( tileId, tileImage, blending ) );
             }
         }
         mergeDecorations( displayedTile );

src/lib/TextureTile.cpp
@@ -22,18 +22,16 @@
 namespace Marble
 {
 
-TextureTile::TextureTile( TileId const & tileId, QImage const * image, const Blending * blending )
+TextureTile::TextureTile( TileId const & tileId, QImage const & image, const Blending * blending )
     : m_id( tileId ),
       m_blending( blending ),
       m_image( image )
 {
-    Q_ASSERT( image );
-    Q_ASSERT( !image->isNull() );
+    Q_ASSERT( !image.isNull() );
 }
 
 TextureTile::~TextureTile()
 {
-    delete m_image;
 }
 
 }

src/lib/TextureTile.h
@@ -26,8 +26,6 @@ class QImage;
 namespace Marble
 {
 class Blending;
-class TileLoader;
-class StackedTileLoader;
 
 /*!
     \class TextureTile
@@ -57,7 +55,7 @@ class StackedTileLoader;
 class TextureTile
 {
  public:
-    TextureTile( TileId const & tileId, QImage const * image, const Blending * blending );
+    TextureTile( TileId const & tileId, QImage const & image, const Blending * blending );
     ~TextureTile();
 
 /*!
@@ -86,7 +84,7 @@ class TextureTile
 
     TileId const m_id;
     Blending const * const m_blending;
-    QImage const * const m_image;
+    QImage const m_image;
 };
 
 
@@ -99,7 +97,7 @@ inline TileId const & TextureTile::id() const
 
 inline QImage const * TextureTile::image() const
 {
-    return m_image;
+    return &m_image;
 }
 
 inline Blending const * TextureTile::blending() const
@@ -109,10 +107,7 @@ inline Blending const * TextureTile::blending() const
 
 inline int TextureTile::byteCount() const
 {
-    Q_ASSERT( m_image );
-
-    // FIXME: once Qt 4.6 is required for Marble, use QImage::byteCount()
-    return m_image->numBytes();
+    return m_image.byteCount();
 }
 
 }

src/lib/TileLoader.cpp
@@ -27,7 +27,6 @@
 #include "MapThemeManager.h"
 #include "MarbleDebug.h"
 #include "MarbleDirs.h"
-#include "TextureTile.h"
 #include "TileLoaderHelper.h"
 
 namespace Marble
@@ -49,7 +48,7 @@ TileLoader::TileLoader( HttpDownloadManager * const downloadManager, MapThemeMan
 //     - if not expired: create TextureTile, set state to "uptodate", return it => done
 //     - if expired: create TextureTile, state is set to Expired by default, trigger dl,
 
-QSharedPointer<TextureTile> TileLoader::loadTile( TileId const & tileId, DownloadUsage const usage )
+QImage TileLoader::loadTile( TileId const & tileId, DownloadUsage const usage )
 {
     GeoSceneTexture const * const textureLayer = findTextureLayer( tileId );
     QString const fileName = tileFileName( tileId );
@@ -57,7 +56,6 @@ QSharedPointer<TextureTile> TileLoader::loadTile( TileId const & tileId, Downloa
     if ( !image.isNull() ) {
         // file is there, so create and return a tile object in any case,
         // but check if an update should be triggered
-        QSharedPointer<TextureTile> const tile( new TextureTile( tileId, new QImage( image ), textureLayer->blending() ));
 
         const QDateTime lastModified = QFileInfo( fileName ).lastModified();
         const int expireSecs = textureLayer->expire();
@@ -70,20 +68,19 @@ QSharedPointer<TextureTile> TileLoader::loadTile( TileId const & tileId, Downloa
             m_waitingForUpdate.insert( tileId );
             triggerDownload( tileId, usage );
         }
-        return tile;
+
+        return image;
     }
 
     // tile was not locally available => trigger download and look for tiles in other levels
     // for scaling
-    QImage * replacementTile = scaledLowerLevelTile( tileId );
-    Q_ASSERT( replacementTile && !replacementTile->isNull() );
-
-    QSharedPointer<TextureTile> const tile( new TextureTile( tileId, replacementTile, textureLayer->blending() ));
+    QImage replacementTile = scaledLowerLevelTile( tileId );
+    Q_ASSERT( !replacementTile.isNull() );
 
     m_waitingForUpdate.insert( tileId );
     triggerDownload( tileId, usage );
 
-    return tile;
+    return replacementTile;
 }
 
 // This method triggers a download of the given tile (without checking
@@ -196,8 +193,7 @@ void TileLoader::triggerDownload( TileId const & id, DownloadUsage const usage )
     emit downloadTile( sourceUrl, destFileName, id.toString(), usage );
 }
 
-    // TODO: get lastModified time stamp into the TextureTile
-QImage * TileLoader::scaledLowerLevelTile( TileId const & id )
+QImage TileLoader::scaledLowerLevelTile( TileId const & id )
 {
     mDebug() << "TileLoader::scaledLowerLevelTile" << id.toString();
 
@@ -229,12 +225,12 @@ QImage * TileLoader::scaledLowerLevelTile( TileId const & id )
             mDebug() << "QImage::copy:" << startX << startY << partWidth << partHeight;
             QImage const part = toScale.copy( startX, startY, partWidth, partHeight );
             mDebug() << "QImage::scaled:" << toScale.size();
-            return new QImage( part.scaled( toScale.size() ) );
+            return part.scaled( toScale.size() );
         }
     }
 
     Q_ASSERT_X( false, "scaled image", "level zero image missing" ); // not reached
-    return 0;
+    return QImage();
 }
 
 }

src/lib/TileLoader.h
@@ -19,10 +19,9 @@
 #include <QtCore/QHash>
 #include <QtCore/QObject>
 #include <QtCore/QSet>
-#include <QtCore/QSharedPointer>
 #include <QtCore/QString>
+#include <QtGui/QImage>
 
-#include "TextureTile.h"
 #include "TileId.h"
 #include "global.h"
 
@@ -43,7 +42,7 @@ class TileLoader: public QObject
  public:
     TileLoader( HttpDownloadManager * const, MapThemeManager const * mapThemeManager );
 
-    QSharedPointer<TextureTile> loadTile( TileId const & tileId, DownloadUsage const );
+    QImage loadTile( TileId const & tileId, DownloadUsage const );
     void reloadTile( TileId const &tileId, DownloadUsage const );
     void downloadTile( TileId const & tileId );
 
@@ -64,7 +63,7 @@ class TileLoader: public QObject
     GeoSceneTexture const * findTextureLayer( TileId const & ) const;
     QString tileFileName( TileId const & ) const;
     void triggerDownload( TileId const &, DownloadUsage const );
-    QImage * scaledLowerLevelTile( TileId const & );
+    QImage scaledLowerLevelTile( TileId const & );
 
     MapThemeManager const * const m_mapThemeManager;
 