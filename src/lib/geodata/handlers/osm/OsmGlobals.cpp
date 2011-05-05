90aa5994681d85ddcbbca30b14441d1a05f89f62
Konstantin Oblaukhov <oblaukhov.konstantin@gmail.com>
2011-05-05 16:46:01 +0700

    Code alignment.

--- src/lib/geodata/handlers/osm/OsmGlobals.cpp
+++ src/lib/geodata/handlers/osm/OsmGlobals.cpp
@@ -34,66 +34,66 @@ QMap< QString, GeoDataFeature::GeoDataVisualCategory > OsmGlobals::visualCategor
 
 void OsmGlobals::setupCategories()
 {
-    m_visualCategories["amenity=restaurant"] = GeoDataFeature::FoodRestaurant;
-    m_visualCategories["amenity=fast_food"] = GeoDataFeature::FoodFastFood;
-    m_visualCategories["amenity=pub"] = GeoDataFeature::FoodPub;
-    m_visualCategories["amenity=bar"] = GeoDataFeature::FoodBar;
-    m_visualCategories["amenity=cafe"] = GeoDataFeature::FoodCafe;
-    m_visualCategories["amenity=biergarten"] = GeoDataFeature::FoodBiergarten;
+    m_visualCategories["amenity=restaurant"]         = GeoDataFeature::FoodRestaurant;
+    m_visualCategories["amenity=fast_food"]          = GeoDataFeature::FoodFastFood;
+    m_visualCategories["amenity=pub"]                = GeoDataFeature::FoodPub;
+    m_visualCategories["amenity=bar"]                = GeoDataFeature::FoodBar;
+    m_visualCategories["amenity=cafe"]               = GeoDataFeature::FoodCafe;
+    m_visualCategories["amenity=biergarten"]         = GeoDataFeature::FoodBiergarten;
 
-    m_visualCategories["amenity=school"] = GeoDataFeature::EducationSchool;
-    m_visualCategories["amenity=college"] = GeoDataFeature::EducationCollege;
-    m_visualCategories["amenity=library"] = GeoDataFeature::AmenityLibrary;
-    m_visualCategories["amenity=university"] = GeoDataFeature::EducationUniversity;
+    m_visualCategories["amenity=school"]             = GeoDataFeature::EducationSchool;
+    m_visualCategories["amenity=college"]            = GeoDataFeature::EducationCollege;
+    m_visualCategories["amenity=library"]            = GeoDataFeature::AmenityLibrary;
+    m_visualCategories["amenity=university"]         = GeoDataFeature::EducationUniversity;
 
-    m_visualCategories["amenity=bus_station"] = GeoDataFeature::TransportBusStation;
-    m_visualCategories["amenity=car_sharing"] = GeoDataFeature::TransportCarShare;
-    m_visualCategories["amenity=fuel"] = GeoDataFeature::TransportFuel;
-    m_visualCategories["amenity=parking"] = GeoDataFeature::TransportParking;
+    m_visualCategories["amenity=bus_station"]        = GeoDataFeature::TransportBusStation;
+    m_visualCategories["amenity=car_sharing"]        = GeoDataFeature::TransportCarShare;
+    m_visualCategories["amenity=fuel"]               = GeoDataFeature::TransportFuel;
+    m_visualCategories["amenity=parking"]            = GeoDataFeature::TransportParking;
 
-    m_visualCategories["amenity=bank"] = GeoDataFeature::MoneyBank;
+    m_visualCategories["amenity=bank"]               = GeoDataFeature::MoneyBank;
 
-    m_visualCategories["amenity=pharmacy"] = GeoDataFeature::HealthPharmacy;
-    m_visualCategories["amenity=hospital"] = GeoDataFeature::HealthHospital;
-    m_visualCategories["amenity=doctors"] = GeoDataFeature::HealthDoctors;
+    m_visualCategories["amenity=pharmacy"]           = GeoDataFeature::HealthPharmacy;
+    m_visualCategories["amenity=hospital"]           = GeoDataFeature::HealthHospital;
+    m_visualCategories["amenity=doctors"]            = GeoDataFeature::HealthDoctors;
 
-    m_visualCategories["amenity=cinema"] = GeoDataFeature::TouristCinema;
-    m_visualCategories["amenity=theatre"] = GeoDataFeature::TouristTheatre;
+    m_visualCategories["amenity=cinema"]             = GeoDataFeature::TouristCinema;
+    m_visualCategories["amenity=theatre"]            = GeoDataFeature::TouristTheatre;
     
-    m_visualCategories["amenity=place_of_worship"] = GeoDataFeature::ReligionPlaceOfWorship;
+    m_visualCategories["amenity=place_of_worship"]   = GeoDataFeature::ReligionPlaceOfWorship;
 
     //FIXME: alcohol != beverages
-    m_visualCategories["shop=alcohol"] = GeoDataFeature::ShoppingBeverages;
-    m_visualCategories["shop=hifi"] = GeoDataFeature::ShoppingHifi;
-    m_visualCategories["shop=supermarket"] = GeoDataFeature::ShoppingSupermarket;
+    m_visualCategories["shop=alcohol"]               = GeoDataFeature::ShoppingBeverages;
+    m_visualCategories["shop=hifi"]                  = GeoDataFeature::ShoppingHifi;
+    m_visualCategories["shop=supermarket"]           = GeoDataFeature::ShoppingSupermarket;
     
-    m_visualCategories["religion"] = GeoDataFeature::ReligionPlaceOfWorship;
-    m_visualCategories["religion=bahai"] = GeoDataFeature::ReligionBahai;
-    m_visualCategories["religion=buddhist"] = GeoDataFeature::ReligionBuddhist;
-    m_visualCategories["religion=christian"] = GeoDataFeature::ReligionChristian;
-    m_visualCategories["religion=hindu"] = GeoDataFeature::ReligionHindu;
-    m_visualCategories["religion=jain"] = GeoDataFeature::ReligionJain;
-    m_visualCategories["religion=jewish"] = GeoDataFeature::ReligionJewish;
-    m_visualCategories["religion=shinto"] = GeoDataFeature::ReligionShinto;
-    m_visualCategories["religion=sikh"] = GeoDataFeature::ReligionSikh;
+    m_visualCategories["religion"]                   = GeoDataFeature::ReligionPlaceOfWorship;
+    m_visualCategories["religion=bahai"]             = GeoDataFeature::ReligionBahai;
+    m_visualCategories["religion=buddhist"]          = GeoDataFeature::ReligionBuddhist;
+    m_visualCategories["religion=christian"]         = GeoDataFeature::ReligionChristian;
+    m_visualCategories["religion=hindu"]             = GeoDataFeature::ReligionHindu;
+    m_visualCategories["religion=jain"]              = GeoDataFeature::ReligionJain;
+    m_visualCategories["religion=jewish"]            = GeoDataFeature::ReligionJewish;
+    m_visualCategories["religion=shinto"]            = GeoDataFeature::ReligionShinto;
+    m_visualCategories["religion=sikh"]              = GeoDataFeature::ReligionSikh;
     
-    m_visualCategories["highway"] = GeoDataFeature::HighwayUnknown;
-    m_visualCategories["highway=path"] = GeoDataFeature::HighwayPath;
-    m_visualCategories["highway=track"] = GeoDataFeature::HighwayTrack;
-    m_visualCategories["highway=service"] = GeoDataFeature::HighwayService;
-    m_visualCategories["highway=living_street"] = GeoDataFeature::HighwayRoad;
-    m_visualCategories["highway=unclassified"] = GeoDataFeature::HighwayRoad;
-    m_visualCategories["highway=residental"] = GeoDataFeature::HighwayRoad;
-    m_visualCategories["highway=tertiary_link"] = GeoDataFeature::HighwayTertiary;
-    m_visualCategories["highway=tertiary"] = GeoDataFeature::HighwayTertiary;
-    m_visualCategories["highway=secondary_link"] = GeoDataFeature::HighwaySecondary;
-    m_visualCategories["highway=secondary"] = GeoDataFeature::HighwaySecondary;
-    m_visualCategories["highway=primary_link"] = GeoDataFeature::HighwayPrimary;
-    m_visualCategories["highway=primary"] = GeoDataFeature::HighwayPrimary;
-    m_visualCategories["highway=trunk_link"] = GeoDataFeature::HighwayTrunk;
-    m_visualCategories["highway=trunk"] = GeoDataFeature::HighwayTrunk;
-    m_visualCategories["highway=motorway_link"] = GeoDataFeature::HighwayMotorway;
-    m_visualCategories["highway=motorway"] = GeoDataFeature::HighwayMotorway;
+    m_visualCategories["highway"]                    = GeoDataFeature::HighwayUnknown;
+    m_visualCategories["highway=path"]               = GeoDataFeature::HighwayPath;
+    m_visualCategories["highway=track"]              = GeoDataFeature::HighwayTrack;
+    m_visualCategories["highway=service"]            = GeoDataFeature::HighwayService;
+    m_visualCategories["highway=living_street"]      = GeoDataFeature::HighwayRoad;
+    m_visualCategories["highway=unclassified"]       = GeoDataFeature::HighwayRoad;
+    m_visualCategories["highway=residental"]         = GeoDataFeature::HighwayRoad;
+    m_visualCategories["highway=tertiary_link"]      = GeoDataFeature::HighwayTertiary;
+    m_visualCategories["highway=tertiary"]           = GeoDataFeature::HighwayTertiary;
+    m_visualCategories["highway=secondary_link"]     = GeoDataFeature::HighwaySecondary;
+    m_visualCategories["highway=secondary"]          = GeoDataFeature::HighwaySecondary;
+    m_visualCategories["highway=primary_link"]       = GeoDataFeature::HighwayPrimary;
+    m_visualCategories["highway=primary"]            = GeoDataFeature::HighwayPrimary;
+    m_visualCategories["highway=trunk_link"]         = GeoDataFeature::HighwayTrunk;
+    m_visualCategories["highway=trunk"]              = GeoDataFeature::HighwayTrunk;
+    m_visualCategories["highway=motorway_link"]      = GeoDataFeature::HighwayMotorway;
+    m_visualCategories["highway=motorway"]           = GeoDataFeature::HighwayMotorway;
 }
 
 }
