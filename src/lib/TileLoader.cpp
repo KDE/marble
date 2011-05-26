172c8c61f2f6c72f376f96871fad3a4a3a72c941
Bernhard Beschow <bbeschow@cs.tu-berlin.de>
2011-05-26 20:56:27 +0200

    simplify TileLoader::scaledLowerLevelTile()
    Create the fallback image after the loop rather than inside. Besides simplifying the loop body, it also removes
    previously dead code after the loop.

--- src/lib/TileLoader.cpp
+++ src/lib/TileLoader.cpp
@@ -258,16 +258,7 @@ QImage TileLoader::scaledLowerLevelTile( TileId const & id )
                                         id.x() >> deltaLevel, id.y() >> deltaLevel );
         QString const fileName = tileFileName( replacementTileId );
         mDebug() << "TileLoader::scaledLowerLevelTile" << "trying" << fileName;
-        QImage toScale( fileName );
-
-        if ( level == 0 && toScale.isNull() ) {
-            mDebug() << "No level zero tile installed in map theme dir. Falling back to a transparent image for now.";
-            GeoSceneTexture const * const textureLayer = findTextureLayer( replacementTileId );
-            QSize tileSize = textureLayer->tileSize();
-            Q_ASSERT( !tileSize.isEmpty() ); // assured by textureLayer
-            toScale = QImage( tileSize, QImage::Format_ARGB32_Premultiplied );
-            toScale.fill( qRgba( 0, 0, 0, 0 ) );
-        }
+        QImage const toScale( fileName );
 
         if ( !toScale.isNull() ) {
             // which rect to scale?
@@ -284,8 +275,14 @@ QImage TileLoader::scaledLowerLevelTile( TileId const & id )
         }
     }
 
-    Q_ASSERT_X( false, "scaled image", "level zero image missing" ); // not reached
-    return QImage();
+    mDebug() << "No level zero tile installed in map theme dir. Falling back to a transparent image for now.";
+    GeoSceneTexture const * const textureLayer = findTextureLayer( id );
+    QSize const tileSize = textureLayer->tileSize();
+    Q_ASSERT( !tileSize.isEmpty() ); // assured by textureLayer
+    QImage fallbackImage = QImage( tileSize, QImage::Format_ARGB32_Premultiplied );
+    fallbackImage.fill( qRgba( 0, 0, 0, 0 ) );
+
+    return fallbackImage;
 }
 
 }
