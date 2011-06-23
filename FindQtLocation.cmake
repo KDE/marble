a1972ac17d83e18c5f058f6166dd116edc5c1940
Daniel Marth <danielmarth@gmx.at>
2011-06-23 20:58:02 +0200

    Improved CMake-files to fix the compilation of QtMobilityPositionProviderPlugin, which is based on QtLocation in QtMobility. Even though a FindQtMobility.cmake file is added, this is not a dependency change. [Files missing in last commit] (cherry picked from commit 7104dbda2d277f917436f8fce6ce2b410bfa64ea)

--- FindQtLocation.cmake
+++ FindQtLocation.cmake
@@ -5,6 +5,10 @@
 # Copyright (c) 2011, Daniel Marth, <danielmarth@gmx.at>
 #
 
-include(FindPkgConfig)
+FIND_PACKAGE( QtMobility )
+IF( QTMOBILITY_FOUND )
+  FIND_PATH( QTLOCATION_INCLUDE_DIR NAMES QGeoPositionInfoSource PATH_SUFFIXES QtLocation )
+  FIND_LIBRARY( QTLOCATION_LIBRARIES NAMES QtLocation )
+ENDIF( QTMOBILITY_FOUND )
 
-pkg_check_modules(QtLocation QtLocation>=1.0.1)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS( QtLocation DEFAULT_MSG QTLOCATION_INCLUDE_DIR QTLOCATION_LIBRARIES )
