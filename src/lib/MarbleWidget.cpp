654993143f263cf0a85538ca2dcb899649f7ab70
Daniel Marth <danielmarth@gmx.at>
2011-07-10 13:48:10 +0200

    Fixed typo and install directory.

--- src/lib/MarbleWidget.cpp
+++ src/lib/MarbleWidget.cpp
@@ -199,7 +199,7 @@ void MarbleWidgetPrivate::construct()
 
     // When some fundamental things change in the model, we got to
     // show this in the view, i.e. here.
-    m_widget->connect( m_model,  SIGNAL( themeChanged( QZString ) ),
+    m_widget->connect( m_model,  SIGNAL( themeChanged( QString ) ),
 		       m_widget, SIGNAL( themeChanged( QString ) ) );
     m_widget->connect( m_model, SIGNAL( modelChanged() ),
                        m_widget, SLOT( update() ) );
