KDE/marble

commit 0111ac9b63b68c81207dbdf6aefa75f3526fa800

Fix memory leaks with MapWizard and a QMenu never being deleted nor reused

src/lib/MarbleWidgetPopupMenu.cpp
@@ -128,7 +128,11 @@ void MarbleWidgetPopupMenu::showLmbMenu( int xpos, int ypos )
         return;
     }
 
-    m_lmbMenu->clear();
+    if (!m_lmbMenu->isEmpty()) {
+        // just clear()-ing the menu won't delete the submenus
+        m_lmbMenu->deleteLater();
+        m_lmbMenu = new QMenu( m_widget );
+    }
     m_featurelist = m_widget->whichFeatureAt( curpos );
 
     int  actionidx = 1;

src/QtMainWindow.cpp
@@ -1413,6 +1413,8 @@ void MainWindow::showMapWizard()
         settings.setValue( "wmsServers", mapWizard->wmsServers() );
         settings.setValue( "staticUrlServers", mapWizard->staticUrlServers() );
     settings.endGroup();
+
+    mapWizard->deleteLater();
 }
 
 void MainWindow::showGoToDialog()

src/marble_part.cpp
@@ -1264,6 +1264,7 @@ void MarblePart::showMapWizard()
     mapWizard->exec();
     MarbleSettings::setWmsServers( mapWizard->wmsServers() );
     MarbleSettings::setStaticUrlServers( mapWizard->staticUrlServers() );
+    mapWizard->deleteLater();
 }
 
 void MarblePart::editSettings()

commit befacc97b564c03d018c002b768a8f812d631bee

Bugfix: The placemark info dialog was only displayed the first time it was opened
This fixes a regression introduced in 0111ac9b63b68c81207dbdf6aefa75f3526fa800
The QMenu was deleted then recreated, but a call to connect() was missing.
To prevent this, this patch only deletes the children of the QMenu.

src/lib/MarbleWidgetPopupMenu.cpp
@@ -129,9 +129,11 @@ void MarbleWidgetPopupMenu::showLmbMenu( int xpos, int ypos )
     }
 
     if (!m_lmbMenu->isEmpty()) {
+        m_lmbMenu->clear();
         // just clear()-ing the menu won't delete the submenus
-        m_lmbMenu->deleteLater();
-        m_lmbMenu = new QMenu( m_widget );
+        foreach( QObject *child, m_lmbMenu->children() ) {
+            child->deleteLater();
+        }
     }
     m_featurelist = m_widget->whichFeatureAt( curpos );
 