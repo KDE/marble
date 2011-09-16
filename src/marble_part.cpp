5097400c13ffd467ce3822c03371a0d417cc6976
Bernhard Beschow <bbeschow@cs.tu-berlin.de>
2011-09-16 15:44:17 +0200

    recognize the .pnt file format also in the KDE version

--- src/marble_part.cpp
+++ src/marble_part.cpp
@@ -214,7 +214,11 @@ bool MarblePart::openUrl( const KUrl &url )
 bool MarblePart::openFile()
 {
     QStringList fileNames = KFileDialog::getOpenFileNames( KUrl(),
-                                    i18n("*.gpx *.kml *.osm|All Supported Files\n*.gpx|GPS Data\n*.kml|Google Earth KML\n*.osm|OpenStreetMap data"),
+                                    i18n("*.gpx *.kml *.osm *.pnt|All Supported Files\n"
+                                         "*.gpx|GPS Data\n"
+                                         "*.kml|Google Earth KML\n"
+                                         "*.osm|OpenStreetMap Data\n"
+                                         "*.pnt|Micro World Data Bank II"),
                                             widget(), i18n("Open File")
                                            );
     foreach( const QString &fileName, fileNames ) {
