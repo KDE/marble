73892aeeb4f24b97ad84f658bbd003802208cd57
Niko Sams <niko.sams@gmail.com>
2011-09-21 21:06:30 +0200

    add api docs

--- src/lib/TileCreator.h
+++ src/lib/TileCreator.h
@@ -25,12 +25,33 @@ namespace Marble
 
 class TileCreatorPrivate;
 
+/**
+ * Base Class for custom tile source
+ *
+ * Implement this class to have more control over the tile creating process. This
+ * is needed when creating with a high tileLevel where the whole source image can't
+ * be loaded at once.
+ **/
 class MARBLE_EXPORT TileCreatorSource
 {
 public:
     virtual ~TileCreatorSource() {}
+
+    /**
+     * Must return the full size of the source image
+     */
     virtual QSize fullImageSize() const = 0;
-    virtual QImage tile( int n, int m, int maxTileLevel ) = 0;
+
+    /**
+     * Must return one specific tile
+     *
+     * tileLevel can be used to calculate the number of tiles in a row or column
+     */
+    virtual QImage tile( int n, int m, int tileLevel ) = 0;
+
+    /**
+     * The source image path, used to automatically detect the targetDir
+     */
     virtual QString sourcePath() const = 0;
 };
 
@@ -39,14 +60,16 @@ class MARBLE_EXPORT TileCreator : public QThread
     Q_OBJECT
 
  public:
-    /*
+    /**
      * Constructor for standard Image source
      */
     TileCreator( const QString& sourceDir, const QString& installMap, 
                  const QString& dem,       const QString& targetDir=QString() );
 
-    /*
+    /**
      * Constructor for own, custom source class
+     *
+     * Ownership of source is taken by TileCreator
      */
     TileCreator( TileCreatorSource *source, const QString& dem, const QString& targetDir );
 
