KDE/marble

commit fb7d48f0a15951421464642970bd5bb49daf0910

RoutingManager: move private slots into private class

src/lib/routing/RoutingManager.cpp
@@ -74,6 +74,10 @@ public:
     void saveRoute( const QString &filename );
 
     void loadRoute( const QString &filename );
+
+    void addRoute( GeoDataDocument* route );
+
+    void recalculateRoute( bool deviated );
 };
 
 RoutingManagerPrivate::RoutingManagerPrivate( MarbleModel *model, RoutingManager* manager, QObject *parent ) :
@@ -267,18 +271,18 @@ void RoutingManager::updateRoute()
     }
 }
 
-void RoutingManager::retrieveRoute( GeoDataDocument* route )
+void RoutingManagerPrivate::addRoute( GeoDataDocument* route )
 {
     if ( route ) {
-        d->m_alternativeRoutesModel.addRoute( route );
+        m_alternativeRoutesModel.addRoute( route );
     }
 
-    if ( !d->m_haveRoute ) {
-        d->m_haveRoute = route != 0;
-        emit stateChanged( Retrieved, &d->m_routeRequest );
+    if ( !m_haveRoute ) {
+        m_haveRoute = route != 0;
+        emit q->stateChanged( RoutingManager::Retrieved, &m_routeRequest );
     }
 
-    emit routeRetrieved( route );
+    emit q->routeRetrieved( route );
 }
 
 AlternativeRoutesModel* RoutingManager::alternativeRoutesModel()
@@ -365,21 +369,21 @@ void RoutingManager::setGuidanceModeEnabled( bool enabled )
     adjustNavigation()->setRecenter( enabled ? AdjustNavigation::RecenterOnBorder : AdjustNavigation::DontRecenter );
 }
 
-void RoutingManager::recalculateRoute( bool deviated )
+void RoutingManagerPrivate::recalculateRoute( bool deviated )
 {
-    if ( d->m_guidanceModeEnabled && deviated ) {
-        for ( int i=d->m_routeRequest.size()-3; i>=0; --i ) {
-            if ( d->m_routeRequest.visited( i ) ) {
-                d->m_routeRequest.remove( i );
+    if ( m_guidanceModeEnabled && deviated ) {
+        for ( int i=m_routeRequest.size()-3; i>=0; --i ) {
+            if ( m_routeRequest.visited( i ) ) {
+                m_routeRequest.remove( i );
             }
         }
 
-        if ( d->m_routeRequest.size() == 2 && d->m_routeRequest.visited( 0 ) && !d->m_routeRequest.visited( 1 ) ) {
-            d->m_routeRequest.setPosition( 0, d->m_marbleModel->positionTracking()->currentLocation() );
-            updateRoute();
-        } else if ( d->m_routeRequest.size() != 0 && !d->m_routeRequest.visited( d->m_routeRequest.size()-1 ) ) {
-            d->m_routeRequest.insert( 0, d->m_marbleModel->positionTracking()->currentLocation() );
-            updateRoute();
+        if ( m_routeRequest.size() == 2 && m_routeRequest.visited( 0 ) && !m_routeRequest.visited( 1 ) ) {
+            m_routeRequest.setPosition( 0, m_marbleModel->positionTracking()->currentLocation() );
+            q->updateRoute();
+        } else if ( m_routeRequest.size() != 0 && !m_routeRequest.visited( m_routeRequest.size()-1 ) ) {
+            m_routeRequest.insert( 0, m_marbleModel->positionTracking()->currentLocation() );
+            q->updateRoute();
         }
     }
 }

src/lib/routing/RoutingManager.h
@@ -137,10 +137,10 @@ Q_SIGNALS:
 
     void routeRetrieved( GeoDataDocument* route );
 
-private Q_SLOTS:
-    void retrieveRoute( GeoDataDocument* route );
+private:
+    Q_PRIVATE_SLOT( d, void addRoute( GeoDataDocument* route ) )
 
-    void recalculateRoute( bool deviated );
+    Q_PRIVATE_SLOT( d, void recalculateRoute( bool deviated ) )
 
 private:
     friend class RoutingManagerPrivate;

commit 6d3fe6bd2c79be398dceed1fa633767553dd5a8f

RoutingManager: fix slot connection
* regression from fb7d48f0a15951421464642970bd5bb49daf0910

src/lib/routing/RoutingManager.cpp
@@ -222,7 +222,7 @@ RoutingManager::RoutingManager( MarbleModel *marbleModel, QObject *parent ) : QO
         d( new RoutingManagerPrivate( marbleModel, this, this ) )
 {
     connect( &d->m_runnerManager, SIGNAL( routeRetrieved( GeoDataDocument* ) ),
-             this, SLOT( retrieveRoute( GeoDataDocument* ) ) );
+             this, SLOT( addRoute( GeoDataDocument* ) ) );
     connect( &d->m_alternativeRoutesModel, SIGNAL( currentRouteChanged( GeoDataDocument* ) ),
              &d->m_routingModel, SLOT( setCurrentRoute( GeoDataDocument* ) ) );
     connect( &d->m_routingModel, SIGNAL( deviatedFromRoute( bool ) ),