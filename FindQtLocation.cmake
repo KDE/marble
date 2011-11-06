f5498edac1fa0ccc959c727a9e4f7ecdb0593939
Bernhard Beschow <bbeschow@cs.tu-berlin.de>
2011-11-06 14:28:18 +0100

    for finding QtLocation, try pkg-config first, then fall back to manual mode
    * fixes finding QtLocation on Harmattan

--- FindQtLocation.cmake
+++ FindQtLocation.cmake
@@ -3,12 +3,27 @@
 # This module determines whether QtLocation is installed.
 #
 # Copyright (c) 2011, Daniel Marth, <danielmarth@gmx.at>
+# Copyright (c) 2011, Bernhard Beschow, <bbeschow@cs.tu-berlin.de>
 #
 
-FIND_PACKAGE( QtMobility )
-IF( QTMOBILITY_FOUND )
-  FIND_PATH( QTLOCATION_INCLUDE_DIR NAMES QGeoPositionInfoSource PATH_SUFFIXES QtLocation )
-  FIND_LIBRARY( QTLOCATION_LIBRARIES NAMES QtLocation )
-ENDIF( QTMOBILITY_FOUND )
+# try using pkg-config first
+include(FindPkgConfig)
 
-FIND_PACKAGE_HANDLE_STANDARD_ARGS( QtLocation DEFAULT_MSG QTLOCATION_INCLUDE_DIR QTLOCATION_LIBRARIES )
+pkg_check_modules(QtLocation QtLocation>=1.0.1)
+
+IF(QtLocation_FOUND)
+  SET(QTLOCATION_FOUND ${QtLocation_FOUND})
+  SET(QTLOCATION_INCLUDE_DIR ${QtLocation_INCLUDE_DIRS})
+  SET(QTLOCATION_LIBRARIES ${QtLocation_LIBRARIES})
+ENDIF(QtLocation_FOUND)
+
+# no success using pkg-config, try manually
+IF(NOT QTLOCATION_FOUND)
+  FIND_PACKAGE(QtMobility)
+  IF(QTMOBILITY_FOUND)
+    FIND_PATH(QTLOCATION_INCLUDE_DIR NAMES QGeoPositionInfoSource PATH_SUFFIXES QtLocation)
+    FIND_LIBRARY(QTLOCATION_LIBRARIES NAMES QtLocation)
+  ENDIF(QTMOBILITY_FOUND)
+
+  FIND_PACKAGE_HANDLE_STANDARD_ARGS( QtLocation DEFAULT_MSG QTLOCATION_INCLUDE_DIR QTLOCATION_LIBRARIES )
+ENDIF(NOT QTLOCATION_FOUND)
