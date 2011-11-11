KDE/marble

commit cb4b859ca63ef8eff0fa23a4dbe981e18cf5045d

Display selected placemarks labels like highlighted text, add "glowing" labels
Make the satellites placemarks glow by default so that they're visible against
the starry sky.

src/lib/PlacemarkPainter.cpp
@@ -23,6 +23,8 @@
 #include "MarblePlacemarkModel.h"
 #include "ViewportParams.h"
 #include "VisiblePlacemark.h"
+#include <QPalette>
+#include <QApplication>
 
 using namespace Marble;
 
@@ -114,28 +116,48 @@ void PlacemarkPainter::drawPlacemarks( QPainter* painter,
     }
 }
 
-inline void PlacemarkPainter::drawLabelText(QPainter& labelPainter,
-                                            const QString &name, const QFont &labelFont )
+inline void PlacemarkPainter::drawLabelText(QPainter &labelPainter, const QString &text,
+                                            const QFont &labelFont, LabelStyle labelStyle )
 {
     QFont font = labelFont;
-    font.setWeight( 75 );
+    QFontMetrics metrics = QFontMetrics( font );
+    int fontAscent = metrics.ascent();
 
-    QPen    outlinepen( Qt::white );
-    outlinepen.setWidthF( s_labelOutlineWidth );
-    QBrush  outlinebrush( Qt::black );
+    switch ( labelStyle ) {
+    case Selected: {
+        labelPainter.setFont( font );
+        QRect textRect( 0, 0, metrics.width( text ), metrics.height() );
+        labelPainter.fillRect( textRect, QApplication::palette().highlight() );
+        labelPainter.setPen( QPen( QApplication::palette().highlightedText(), 1 ) );
+        labelPainter.drawText( 0, fontAscent, text );
+        break;
+    }
+    case Glow: {
+        font.setWeight( 75 );
+        fontAscent = QFontMetrics( font ).ascent();
 
-    QPainterPath   outlinepath;
+        QPen outlinepen( Qt::white );
+        outlinepen.setWidthF( s_labelOutlineWidth );
+        QBrush  outlinebrush( Qt::black );
 
-    int fontascent = QFontMetrics( font ).ascent();
-    const QPointF  baseline( s_labelOutlineWidth / 2.0, fontascent );
-    outlinepath.addText( baseline, font, name );
-    labelPainter.setRenderHint( QPainter::Antialiasing, true );
-    labelPainter.setPen( outlinepen );
-    labelPainter.setBrush( outlinebrush );
-    labelPainter.drawPath( outlinepath );
-    labelPainter.setPen( Qt::NoPen );
-    labelPainter.drawPath( outlinepath );
-    labelPainter.setRenderHint( QPainter::Antialiasing, false );
+        QPainterPath outlinepath;
+
+        const QPointF  baseline( s_labelOutlineWidth / 2.0, fontAscent );
+        outlinepath.addText( baseline, font, text );
+        labelPainter.setRenderHint( QPainter::Antialiasing, true );
+        labelPainter.setPen( outlinepen );
+        labelPainter.setBrush( outlinebrush );
+        labelPainter.drawPath( outlinepath );
+        labelPainter.setPen( Qt::NoPen );
+        labelPainter.drawPath( outlinepath );
+        labelPainter.setRenderHint( QPainter::Antialiasing, false );
+        break;
+    }
+    default: {
+        labelPainter.setFont( font );
+        labelPainter.drawText( 0, fontAscent, text );
+    }
+    }
 }
 
 inline void PlacemarkPainter::drawLabelPixmap( VisiblePlacemark *mark, bool isSelected )
@@ -162,6 +184,13 @@ inline void PlacemarkPainter::drawLabelPixmap( VisiblePlacemark *mark, bool isSe
 	 && m_defaultLabelColor != Qt::black )
         labelColor = m_defaultLabelColor;
 
+    LabelStyle labelStyle = Normal;
+    if ( isSelected ) {
+        labelStyle = Selected;
+    } else if ( style->labelStyle().glow() ) {
+        labelStyle = Glow;
+    }
+
     // Due to some XOrg bug this requires a workaround via
     // QImage in some cases (at least with Qt 4.2).
     if ( !m_useXWorkaround ) {
@@ -170,15 +199,7 @@ inline void PlacemarkPainter::drawLabelPixmap( VisiblePlacemark *mark, bool isSe
 
         labelPainter.begin( &labelPixmap );
 
-        if ( !isSelected ) {
-            labelPainter.setFont( labelFont );
-            labelPainter.setPen( labelColor );
-            int fontascent = QFontMetrics( labelFont ).ascent();
-            labelPainter.drawText( 0, fontascent, labelName );
-        }
-        else {
-            drawLabelText( labelPainter, labelName, labelFont  );
-        }
+        drawLabelText( labelPainter, labelName, labelFont, labelStyle );
 
         labelPainter.end();
     } else {
@@ -189,15 +210,7 @@ inline void PlacemarkPainter::drawLabelPixmap( VisiblePlacemark *mark, bool isSe
 
         labelPainter.begin( &image );
 
-        if ( !isSelected ) {
-            labelPainter.setFont( labelFont );
-            labelPainter.setPen( labelColor );
-            int fontascent = QFontMetrics( labelFont ).ascent();
-            labelPainter.drawText( 0, fontascent, labelName );
-        }
-        else {
-            drawLabelText( labelPainter, labelName, labelFont );
-        }
+        drawLabelText( labelPainter, labelName, labelFont, labelStyle );
 
         labelPainter.end();
 

src/lib/PlacemarkPainter.h
@@ -52,8 +52,13 @@ class PlacemarkPainter : public QObject
     void setDefaultLabelColor( const QColor& color );
 
  private:
+    enum LabelStyle {
+        Normal = 0,
+        Glow,
+        Selected
+    };
 
-    void drawLabelText( QPainter& textpainter, const QString &name, const QFont &labelFont );
+    void drawLabelText( QPainter &labelPainter, const QString &text, const QFont &labelFont, LabelStyle labelStyle );
     void drawLabelPixmap( VisiblePlacemark *mark, bool isSelected );
 
     bool testXBug();

src/lib/geodata/data/GeoDataLabelStyle.cpp
@@ -30,14 +30,16 @@ class GeoDataLabelStylePrivate
     GeoDataLabelStylePrivate() 
         : m_scale( 1.0 ),
           m_alignment( GeoDataLabelStyle::Corner ),
-          m_font( QFont("Sans Serif").family(), defaultSize, 50, false )
+          m_font( QFont("Sans Serif").family(), defaultSize, 50, false ),
+          m_glow( false )
     {
     }
 
     explicit GeoDataLabelStylePrivate( const QFont &font )
         : m_scale( 1.0 ),
           m_alignment( GeoDataLabelStyle::Corner ),
-          m_font( font )
+          m_font( font ),
+          m_glow( false )
     {
     }
 
@@ -52,6 +54,8 @@ class GeoDataLabelStylePrivate
     GeoDataLabelStyle::Alignment m_alignment;
     /// The current font of the label
     QFont  m_font;   // Not a KML property
+    /// Whether or not the text should glow
+    bool m_glow; // Not a KML property
 };
 
 GeoDataLabelStyle::GeoDataLabelStyle()
@@ -118,6 +122,15 @@ QFont GeoDataLabelStyle::font() const
     return d->m_font;
 }
 
+bool GeoDataLabelStyle::glow() const
+{
+    return d->m_glow;
+}
+
+void GeoDataLabelStyle::setGlow(bool on)
+{
+    d->m_glow = on;
+}
 
 void GeoDataLabelStyle::pack( QDataStream& stream ) const
 {

src/lib/geodata/data/GeoDataLabelStyle.h
@@ -90,6 +90,21 @@ class GEODATA_EXPORT GeoDataLabelStyle : public GeoDataColorStyle
     QFont font() const;
 
     /**
+     * @brief Return true if the text of the label should glow, false otherwise
+     * @see setGlow
+     */
+    bool glow() const;
+
+    /**
+     * @brief Enable or disable a glow effect around the text of the label.
+     *
+     * The default is false.
+     *
+     * Note that this is not a KML property.
+     */
+    void setGlow( bool on );
+
+    /**
      * @brief  Serialize the style to a stream.
      * @param  stream  the stream
      */

src/lib/layers/PlacemarkLayout.cpp
@@ -45,9 +45,9 @@ PlacemarkLayout::PlacemarkLayout( QAbstractItemModel  *placemarkModel,
                                   QObject* parent )
     : QObject( parent ),
       m_selectionModel( selectionModel ),
-      m_showPlaces( true ),
       m_clock( clock ),
       m_placemarkPainter( 0 ),
+      m_showPlaces( true ),
       m_maxLabelHeight( 0 ),
       m_styleResetRequested( true )
 {
@@ -496,24 +496,11 @@ bool PlacemarkLayout::render( GeoPainter *painter,
 
         // ----------------------------------------------------------------
         // End of checks. Here the actual layouting starts.
-        int textWidth = 0;
 
         // Find the corresponding visible placemark
         VisiblePlacemark *mark = m_visiblePlacemarks.value( placemark );
 
         GeoDataStyle* style = placemark->style();
-
-        // Specify font properties
-        if ( mark ) {
-            textWidth = mark->labelRect().width();
-        }
-        else {
-            QFont labelFont = style->labelStyle().font();
-            labelFont.setWeight( 75 ); // Needed to calculate the correct pixmap size; 
-
-            textWidth = ( QFontMetrics( labelFont ).width( placemark->name() )
-                  + (int)( 2 * s_labelOutlineWidth ) );
-        }
 
         // Choose Section
         const QVector<VisiblePlacemark*> currentsec = rowsection.at( y / m_maxLabelHeight );
@@ -521,7 +508,7 @@ bool PlacemarkLayout::render( GeoPainter *painter,
         // Find out whether the area around the placemark is covered already.
         // If there's not enough space free don't add a VisiblePlacemark here.
 
-        QRect labelRect = roomForLabel( style, currentsec, x, y, textWidth );
+        QRect labelRect = roomForLabel( style, currentsec, x, y, placemark->name() );
         if ( labelRect.isNull() )
             continue;
 
@@ -653,20 +640,10 @@ bool PlacemarkLayout::render( GeoPainter *painter,
 
         // ----------------------------------------------------------------
         // End of checks. Here the actual layouting starts.
-        int textWidth = 0;
 
         // Find the corresponding visible placemark
         VisiblePlacemark *mark = m_visiblePlacemarks.value( placemark );
         GeoDataStyle* style = placemark->style();
-
-        // Specify font properties
-        if ( mark ) {
-            textWidth = mark->labelRect().width();
-        }
-        else {
-            QFont labelFont = style->labelStyle().font();
-            textWidth = ( QFontMetrics( labelFont ).width( placemark->name() ) );
-        }
 
         // Choose Section
         const QVector<VisiblePlacemark*> currentsec = rowsection.at( y / m_maxLabelHeight );
@@ -674,7 +651,7 @@ bool PlacemarkLayout::render( GeoPainter *painter,
          // Find out whether the area around the placemark is covered already.
         // If there's not enough space free don't add a VisiblePlacemark here.
 
-        QRect labelRect = roomForLabel( style, currentsec, x, y, textWidth );
+        QRect labelRect = roomForLabel( style, currentsec, x, y, placemark->name() );
         if ( labelRect.isNull() )
             continue;
 
@@ -731,7 +708,7 @@ GeoDataCoordinates PlacemarkLayout::placemarkIconCoordinates( GeoDataPlacemark *
 QRect PlacemarkLayout::roomForLabel( GeoDataStyle * style,
                                       const QVector<VisiblePlacemark*> &currentsec,
                                       const int x, const int y,
-                                      const int textWidth )
+                                      const QString &labelText )
 {
     bool  isRoom      = false;
 
@@ -740,6 +717,16 @@ QRect PlacemarkLayout::roomForLabel( GeoDataStyle * style,
     QFont labelFont = style->labelStyle().font();
     int textHeight = QFontMetrics( labelFont ).height();
 //    mDebug() << textHeight;
+
+    int textWidth;
+    if ( style->labelStyle().glow() ) {
+        labelFont.setWeight( 75 ); // Needed to calculate the correct pixmap size;
+        textWidth = ( QFontMetrics( labelFont ).width( labelText )
+            + (int)( 2 * s_labelOutlineWidth ) );
+    } else {
+        QFont labelFont = style->labelStyle().font();
+        textWidth = ( QFontMetrics( labelFont ).width( labelText ) );
+    }
 
     if ( style->labelStyle().alignment() == GeoDataLabelStyle::Corner ) {
         int  xpos = symbolwidth / 2 + x + 1;

src/lib/layers/PlacemarkLayout.h
@@ -129,7 +129,7 @@ class PlacemarkLayout : public QObject, public LayerInterface
     QRect  roomForLabel( GeoDataStyle * style,
                          const QVector<VisiblePlacemark*> &currentsec,
                          const int x, const int y,
-                         const int textWidth );
+                         const QString &labelText );
 
     int    placemarksOnScreenLimit() const;
 

src/plugins/render/satellites/SatellitesItem.cpp
@@ -57,6 +57,7 @@ SatellitesItem::SatellitesItem( const QString &name, elsetrec satrec, const Marb
     placemark()->setStyle( style );
     placemark()->style()->lineStyle().setColor( oxygenBrickRed4 );
     placemark()->style()->lineStyle().setPenStyle( Qt::NoPen );
+    placemark()->style()->labelStyle().setGlow( true );
 
     update();
 }

commit 8b29f6309d77a009f9a161009d245f65a1b3c187

Fixes: Placemark label color Regression caused by cb4b859c

src/lib/PlacemarkPainter.cpp
@@ -184,12 +184,15 @@ inline void PlacemarkPainter::drawLabelPixmap( VisiblePlacemark *mark, bool isSe
 	 && m_defaultLabelColor != Qt::black )
         labelColor = m_defaultLabelColor;
 
+    labelFont.setWeight(75);
+
     LabelStyle labelStyle = Normal;
     if ( isSelected ) {
         labelStyle = Selected;
     } else if ( style->labelStyle().glow() ) {
         labelStyle = Glow;
     }
+
 
     // Due to some XOrg bug this requires a workaround via
     // QImage in some cases (at least with Qt 4.2).
@@ -199,6 +202,7 @@ inline void PlacemarkPainter::drawLabelPixmap( VisiblePlacemark *mark, bool isSe
 
         labelPainter.begin( &labelPixmap );
 
+        labelPainter.setPen( labelColor );
         drawLabelText( labelPainter, labelName, labelFont, labelStyle );
 
         labelPainter.end();
@@ -210,6 +214,7 @@ inline void PlacemarkPainter::drawLabelPixmap( VisiblePlacemark *mark, bool isSe
 
         labelPainter.begin( &image );
 
+        labelPainter.setPen( labelColor );
         drawLabelText( labelPainter, labelName, labelFont, labelStyle );
 
         labelPainter.end();