7e74dad6ece26546e727693284b2845bc5071ffd
Dennis Nienhüser <earthwings@gentoo.org>
2012-02-11 11:37:02 +0100

    No debug spam

--- src/plugins/runner/nominatim/OsmNominatimRunner.cpp
+++ src/plugins/runner/nominatim/OsmNominatimRunner.cpp
@@ -168,7 +168,7 @@ void OsmNominatimRunner::handleSearchResult( QNetworkReply* reply )
         }
 
         QString country = place.firstChildElement("country").text();
-        qDebug() << "place " << name << ", " << road << ", " << city << ", " << administrative << ", " << country;
+        //qDebug() << "place " << name << ", " << road << ", " << city << ", " << administrative << ", " << country;
 
         QString description;
         for (int i=0; i<place.childNodes().size(); ++i) {
