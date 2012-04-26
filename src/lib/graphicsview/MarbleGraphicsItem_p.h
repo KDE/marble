KDE/marble

commit 9bef33979f7c11a2ba5149871bc93cde47634200

MarbleGraphicsItem: const'ify some pointers, turn a pointer attribute into a value
* QSet is implicitly shared, which means m_children is basically a pointer, too (but a safer one)

src/lib/graphicsview/MarbleGraphicsItem_p.h
@@ -37,7 +37,7 @@ class MarbleGraphicsItemPrivate
           m_cacheMode( MarbleGraphicsItem::NoCache ),
           m_visibility( true ),
           m_parent( parent ),
-          m_children( 0 ),
+          m_children(),
           m_layout( 0 ),
           m_marbleGraphicsItem( marbleGraphicsItem ),
           m_zValue( 0 )
@@ -56,13 +56,8 @@ class MarbleGraphicsItemPrivate
         }
 
         // Delete all children
-        if ( m_children ) {
-            // See above: The children will remove itself from the list
-            while ( !m_children->isEmpty() ) {
-                delete *m_children->begin();
-            }
-            delete m_children;
-        }
+        // See above: The children will remove itself from the list
+        qDeleteAll( m_children );
 
         // Delete Layout
         delete m_layout;
@@ -70,18 +65,12 @@ class MarbleGraphicsItemPrivate
 
     void addChild( MarbleGraphicsItem *child )
     {
-        if ( m_children == 0 ) {
-            m_children = new QSet<MarbleGraphicsItem *>();
-        }
-
-        m_children->insert( child );
+        m_children.insert( child );
     }
 
     void removeChild( MarbleGraphicsItem *child )
     {
-        if ( m_children ) {
-            m_children->remove( child );
-        }
+        m_children.remove( child );
     }
 
     virtual QList<QPointF> positions() const
@@ -137,10 +126,8 @@ class MarbleGraphicsItemPrivate
     void updateLabelPositions()
     {
         // This has to be done recursively because we need a correct size from all children.
-        if ( m_children ) {
-            foreach ( MarbleGraphicsItem *item, *m_children ) {
-                item->p()->updateLabelPositions();
-            }
+        foreach ( MarbleGraphicsItem *item, m_children ) {
+            item->p()->updateLabelPositions();
         }
 
         // Adjust positions
@@ -164,16 +151,16 @@ class MarbleGraphicsItemPrivate
     bool m_visibility;
 
     // The parent of the item
-    MarbleGraphicsItem *m_parent;
-    // The set of children. WARNING: This is not initialized by default.
-    QSet<MarbleGraphicsItem *> *m_children;
+    MarbleGraphicsItem *const m_parent;
+    // The set of children.
+    QSet<MarbleGraphicsItem *> m_children;
 
     // The layout handling the positions of the children
     AbstractMarbleGraphicsLayout *m_layout;
     
     QString m_toolTip;
 
-    MarbleGraphicsItem *m_marbleGraphicsItem;
+    MarbleGraphicsItem *const m_marbleGraphicsItem;
     
     qreal m_zValue;
 };

src/lib/graphicsview/MarbleGraphicsItem.cpp
@@ -100,10 +100,8 @@ bool MarbleGraphicsItem::paintEvent( GeoPainter *painter, ViewportParams *viewpo
             paint( &pixmapPainter, viewport, renderPos, layer );
 
             // Paint children
-            if ( p()->m_children ) {
-                foreach ( MarbleGraphicsItem *item, *p()->m_children ) {
-                    item->paintEvent( &pixmapPainter, viewport, renderPos, layer );
-                }
+            foreach ( MarbleGraphicsItem *item, p()->m_children ) {
+                item->paintEvent( &pixmapPainter, viewport, renderPos, layer );
             }
             // Update the pixmap in cache
 #if QT_VERSION < 0x040600
@@ -129,10 +127,8 @@ bool MarbleGraphicsItem::paintEvent( GeoPainter *painter, ViewportParams *viewpo
             paint( painter, viewport, renderPos, layer );
 
             // Paint children
-            if ( p()->m_children ) {
-                foreach ( MarbleGraphicsItem *item, *p()->m_children ) {
-                    item->paintEvent( painter, viewport, renderPos, layer );
-                }
+            foreach ( MarbleGraphicsItem *item, p()->m_children ) {
+                item->paintEvent( painter, viewport, renderPos, layer );
             }
 
             painter->restore();
@@ -242,10 +238,8 @@ void MarbleGraphicsItem::setSize( const QSizeF& size )
     p()->m_size = size;
     update();
 
-    if ( p()->m_children ) {
-        foreach ( MarbleGraphicsItem *item, *p()->m_children ) {
-            item->p()->setParentSize( size );
-        }
+    foreach ( MarbleGraphicsItem *item, p()->m_children ) {
+        item->p()->setParentSize( size );
     }
 }
 
@@ -304,14 +298,14 @@ bool MarbleGraphicsItem::eventFilter( QObject *object, QEvent *e )
     
     QMouseEvent *event = static_cast<QMouseEvent*> (e);
     
-    if( p()->m_children ) {
+    if( !p()->m_children.isEmpty() ) {
         QList<QPointF> absolutePositions = p()->absolutePositions();
         
         foreach( const QPointF& absolutePosition, absolutePositions ) {
             QPoint shiftedPos = event->pos() - absolutePosition.toPoint();
             
             if ( QRect( QPoint( 0, 0 ), size().toSize() ).contains( shiftedPos ) ) {
-                foreach( MarbleGraphicsItem *child, *p()->m_children ) {
+                foreach( MarbleGraphicsItem *child, p()->m_children ) {
                     QList<QRectF> childRects = child->boundingRects();
                     
                     foreach( const QRectF& childRect, childRects ) {

commit c44fd333afa03a5eb630f1c062af0b80852763db

fix crash when closing introduced in 9bef33979f7c11a2ba5149871bc93cde47634200

src/lib/graphicsview/MarbleGraphicsItem_p.h
@@ -56,8 +56,7 @@ class MarbleGraphicsItemPrivate
         }
 
         // Delete all children
-        // See above: The children will remove itself from the list
-        qDeleteAll( m_children );
+        qDeleteAll( m_children.toList() ); // delete using a copy, since children may invalidate m_children's iterator
 
         // Delete Layout
         delete m_layout;