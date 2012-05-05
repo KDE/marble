e8df08ecf3fb2959ff147d9c5a64beac73e1ee9d
Dennis Nienhüser <earthwings@gentoo.org>
2012-05-05 18:52:01 +0200

    Remove view attribute settings.
    A speed difference on harmattan was not measurable and the attributes
    lead to rendering problems when using plasma components.

--- src/qt-components/marble-touch/main.cpp
+++ src/qt-components/marble-touch/main.cpp
@@ -70,10 +70,6 @@ int main( int argc, char *argv[] )
     // Create main window based on QML.
     QDeclarativeView view;
     view.setSource( QUrl( "qrc:/main.qml" ) );
-    view.setAttribute( Qt::WA_OpaquePaintEvent );
-    view.setAttribute( Qt::WA_NoSystemBackground );
-    view.viewport()->setAttribute( Qt::WA_OpaquePaintEvent );
-    view.viewport()->setAttribute( Qt::WA_NoSystemBackground );
 
 #ifdef __arm__
     // Window takes up full screen on arm (mobile) devices.
