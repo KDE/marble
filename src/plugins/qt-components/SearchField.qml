KDE/marble

commit 4c74096ed6d8ffcf2b9aa5b1243eb8f7affd5869

Fix search field height

src/plugins/qt-components/SearchField.qml
@@ -19,9 +19,6 @@ TextField {
 
     id: searchField
 
-    /** @todo: What's the exact height of a text field? Should be defined somewhere, use it here */
-    height: visible ? 50 : 0
-
     placeholderText: "Search..."
     // Icon to clear text in the textfield.
     ToolIcon {

commit fc5dcdbea9cd2fa2c22c25ce4b010744eae44288

Fix regression introduced in 4c74096e (cherry picked from commit 84c32eb45ed5f00ad0879c93c5baaa9e85172de2)

src/plugins/qt-components/SearchField.qml
@@ -18,6 +18,7 @@ TextField {
     property bool busy: false
 
     id: searchField
+    height: visible ? undefined : 0
 
     placeholderText: "Search..."
     // Icon to clear text in the textfield.