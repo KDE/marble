7bfbed43730a7a840ab3156fe219ab6eb05c4085
Dennis Nienhüser <earthwings@gentoo.org>
2012-05-13 22:49:51 +0200

    Fix bounds calculation
    CCBUG: 299959

--- src/lib/routing/Route.cpp
+++ src/lib/routing/Route.cpp
@@ -25,7 +25,7 @@ Route::Route() :
 void Route::addRouteSegment( const RouteSegment &segment )
 {
     if ( segment.isValid() ) {
-        m_bounds = m_bounds.united( segment.bounds() );
+        m_bounds = m_bounds.isEmpty() ? segment.bounds() : m_bounds.united( segment.bounds() );
         m_distance += segment.distance();
         m_path << segment.path();
         if ( segment.maneuver().position().longitude() != 0.0 || segment.maneuver().position().latitude() != 0.0 ) {
