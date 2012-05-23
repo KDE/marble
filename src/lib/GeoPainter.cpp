aee75aed399ac169c8dccd16ee300515ec5e24e6
Dennis Nienhüser <earthwings@gentoo.org>
2012-05-23 22:43:40 +0200

    Fix painting of polygons with holes.
    REVIEW: 105019

--- src/lib/GeoPainter.cpp
+++ src/lib/GeoPainter.cpp
@@ -747,11 +747,30 @@ void GeoPainter::drawPolygon ( const GeoDataPolygon & polygon,
     // In QPathClipper We Trust ...
     // ... and in the speed of a threesome of nested foreachs!
 
+    QVector<QPolygonF> outline;
+    QPen const oldPen = pen();
+    // When inner boundaries exist, the outline of the polygon must be painted
+    // separately to avoid connections between the outer and inner boundaries
+    // To avoid performance penalties the separate painting is only done when
+    // it's really needed. See review 105019 for details.
+    bool const needOutlineWorkaround = !polygon.innerBoundaries().isEmpty();
+    if ( needOutlineWorkaround ) {
+        foreach( QPolygonF* polygon, outerPolygons ) {
+            outline << *polygon;
+        }
+        setPen( QPen( Qt::NoPen ) );
+    }
+
     QVector<GeoDataLinearRing> innerBoundaries = polygon.innerBoundaries(); 
     foreach( const GeoDataLinearRing& itInnerBoundary, innerBoundaries ) {
         QVector<QPolygonF*> innerPolygons;
         d->createPolygonsFromLinearRing( itInnerBoundary, innerPolygons );
-        
+        if ( needOutlineWorkaround ) {
+            foreach( QPolygonF* polygon, innerPolygons ) {
+                outline << *polygon;
+            }
+        }
+
         foreach( QPolygonF* itOuterPolygon, outerPolygons ) {
             foreach( QPolygonF* itInnerPolygon, innerPolygons ) {
                 *itOuterPolygon = itOuterPolygon->subtracted( *itInnerPolygon );
@@ -762,6 +781,13 @@ void GeoPainter::drawPolygon ( const GeoDataPolygon & polygon,
 
     foreach( QPolygonF* itOuterPolygon, outerPolygons ) {
         ClipPainter::drawPolygon( *itOuterPolygon, fillRule );
+    }
+
+    if ( needOutlineWorkaround ) {
+        setPen( oldPen );
+        foreach( const QPolygonF &polygon, outline ) {
+            ClipPainter::drawPolyline( polygon );
+        }
     }
 
     qDeleteAll( outerPolygons );    
