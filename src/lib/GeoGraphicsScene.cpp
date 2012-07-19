2f4a8c18dbcfe3752d83fc39eaef918b7ecbb849
Dennis Nienhüser <earthwings@gentoo.org>
2012-07-19 21:18:25 +0200

    5 levels of nesting is too much, insert a new method for readability
    (cherry picked from commit 3d659760c90c58a50cb18acddc77db6ebbf79898)

--- src/lib/GeoGraphicsScene.cpp
+++ src/lib/GeoGraphicsScene.cpp
@@ -30,6 +30,8 @@ class GeoGraphicsScenePrivate
 {
 public:
 
+    void addItems(const TileId &tileId, QList<GeoGraphicsItem*> &result, int maxZoomLevel ) const;
+
     QMap<TileId, QList<GeoGraphicsItem*> > m_items;
 };
 
@@ -96,22 +98,7 @@ QList< GeoGraphicsItem* > GeoGraphicsScene::items( const Marble::GeoDataLatLonAl
         coords.getCoords( &x1, &y1, &x2, &y2 );
         for ( int x = x1; x <= x2; ++x ) {
             for ( int y = y1; y <= y2; ++y ) {
-                TileId const tileId( "", level, x, y );
-                const QList< GeoGraphicsItem* > &objects = d->m_items.value(tileId);
-                QList< GeoGraphicsItem* >::iterator before = result.begin();
-                QList< GeoGraphicsItem* >::const_iterator currentItem = objects.constBegin();
-                while( currentItem != objects.end() ) {
-                    while( ( currentItem != objects.end() )
-                      && ( ( before == result.end() ) || ( (*currentItem)->zValue() < (*before)->zValue() ) ) ) {
-                        if( (*currentItem)->minZoomLevel() <= maxZoomLevel ) {
-                            before = result.insert( before, *currentItem );
-                        }
-                        ++currentItem;
-                    }
-                    if ( before != result.end() ) {
-                        ++before;
-                    }
-                }
+                d->addItems( TileId ( "", level, x, y ), result, maxZoomLevel );
             }
         }
     }
@@ -188,6 +175,26 @@ void GeoGraphicsScene::addItem( GeoGraphicsItem* item )
         }
     }
 }
-};
+
+void GeoGraphicsScenePrivate::addItems( const TileId &tileId, QList<GeoGraphicsItem *> &result, int maxZoomLevel ) const
+{
+    const QList< GeoGraphicsItem* > &objects = m_items.value(tileId);
+    QList< GeoGraphicsItem* >::iterator before = result.begin();
+    QList< GeoGraphicsItem* >::const_iterator currentItem = objects.constBegin();
+    while( currentItem != objects.end() ) {
+        while( ( currentItem != objects.end() )
+          && ( ( before == result.end() ) || ( (*currentItem)->zValue() < (*before)->zValue() ) ) ) {
+            if( (*currentItem)->minZoomLevel() <= maxZoomLevel ) {
+                before = result.insert( before, *currentItem );
+            }
+            ++currentItem;
+        }
+        if ( before != result.end() ) {
+            ++before;
+        }
+    }
+}
+
+}
 
 #include "GeoGraphicsScene.moc"
