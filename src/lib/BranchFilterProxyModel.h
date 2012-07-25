357b5ec811952d196b67eec2a83e11996d3ba945
Thibaut Gridel <tgridel@free.fr>
2012-07-25 17:06:45 +0200

    BranchFilterProxyModel: move from BookmarkManagerDialog

---
+++ src/lib/BranchFilterProxyModel.h
@@ -0,0 +1,39 @@
+//
+// This file is part of the Marble Virtual Globe.
+//
+// This program is free software licensed under the GNU LGPL. You can
+// find a copy of this license in LICENSE.txt in the top directory of
+// the source code.
+//
+// Copyright 2010   Dennis Nienhüser <earthwings@gentoo.org>
+// Copyright 2012   Thibaut Gridel <tgridel@free.fr>
+
+#ifndef BRANCHFILTERPROXYMODEL_H
+#define BRANCHFILTERPROXYMODEL_H
+
+#include "GeoDataTreeModel.h"
+
+#include <QSortFilterProxyModel>
+
+namespace Marble
+{
+
+class BranchFilterProxyModel : public QSortFilterProxyModel
+{
+public:
+    explicit BranchFilterProxyModel( QObject *parent = 0 );
+
+    void setBranchIndex( GeoDataTreeModel *sourceModel, const QModelIndex &index );
+
+protected:
+    bool filterAcceptsRow( int sourceRow, const QModelIndex &sourceParent ) const;
+
+private:
+    GeoDataTreeModel *m_treeModel;
+    QPersistentModelIndex m_branchIndex;
+
+};
+
+}
+
+#endif // BRANCHFILTERPROXYMODEL_H
