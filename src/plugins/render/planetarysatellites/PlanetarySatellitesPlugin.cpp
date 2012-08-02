9f0da491602984e83fe7ac1097f378f50a17226c
Rene Kuettner <rene@bitkanal.net>
2012-08-02 22:38:49 +0200

    PlanetarySatellitesPlugin: add plugin skeleton
    Add basic files for the upcoming PlanetarySatellites plugin.

---
+++ src/plugins/render/planetarysatellites/PlanetarySatellitesPlugin.cpp
@@ -0,0 +1,139 @@
+//
+// This file is part of the Marble Virtual Globe.
+//
+// This program is free software licensed under the GNU LGPL. You can
+// find a copy of this license in LICENSE.txt in the top directory of
+// the source code.
+//
+// Copyright 2012 Rene Kuettner <rene@bitkanal.net>
+//
+
+#include "PlanetarySatellitesPlugin.h"
+
+#include "MarbleDebug.h"
+#include "MarbleModel.h"
+#include "GeoDataPlacemark.h"
+#include "ViewportParams.h"
+
+#include <QtCore/QUrl>
+#include <QtGui/QPushButton>
+
+namespace Marble
+{
+
+PlanetarySatellitesPlugin::PlanetarySatellitesPlugin()
+    : RenderPlugin( 0 ),
+      m_isInitialized( false )
+{
+}
+
+PlanetarySatellitesPlugin::PlanetarySatellitesPlugin( const MarbleModel *marbleModel )
+    : RenderPlugin( marbleModel ),
+     m_isInitialized( false )
+{
+    connect( this, SIGNAL(enabledChanged(bool)), SLOT(enableModel(bool)) );
+    connect( this, SIGNAL(visibilityChanged(bool,QString)), SLOT(visibleModel(bool)) );
+
+    setVisible( false );
+}
+
+PlanetarySatellitesPlugin::~PlanetarySatellitesPlugin()
+{
+}
+
+QStringList PlanetarySatellitesPlugin::backendTypes() const
+{
+    return QStringList( "planetarysatellites" );
+}
+
+QString PlanetarySatellitesPlugin::renderPolicy() const
+{
+    return QString( "ALWAYS" );
+}
+
+QStringList PlanetarySatellitesPlugin::renderPosition() const
+{
+    return QStringList( "ORBIT" );
+}
+
+QString PlanetarySatellitesPlugin::name() const
+{
+    return tr( "Planetary Satellites" );
+}
+
+QString PlanetarySatellitesPlugin::nameId() const
+{
+    return "planetarysatellites";
+}
+
+QString PlanetarySatellitesPlugin::guiString() const
+{
+    return tr( "&Planetary Satellites" );
+}
+
+QString PlanetarySatellitesPlugin::version() const
+{
+    return "1.0";
+}
+
+QString PlanetarySatellitesPlugin::description() const
+{
+    return tr( "This plugin displays planetary satellites, space probes and their orbits." );
+}
+
+QString PlanetarySatellitesPlugin::copyrightYears() const
+{
+    return "2012";
+}
+
+QList<PluginAuthor> PlanetarySatellitesPlugin::pluginAuthors() const
+{
+    return QList<PluginAuthor>()
+            << PluginAuthor( "Rene Kuettner", "rene@bitkanal.net" );
+}
+
+QString PlanetarySatellitesPlugin::aboutDataText() const
+{
+    // FIXME: add data about text
+    return tr( "???" );
+}
+
+QIcon PlanetarySatellitesPlugin::icon() const
+{
+    return QIcon();
+}
+
+RenderPlugin::RenderType PlanetarySatellitesPlugin::renderType() const
+{
+    return Online;
+}
+
+void PlanetarySatellitesPlugin::initialize()
+{
+    m_isInitialized = true;
+}
+
+bool PlanetarySatellitesPlugin::isInitialized() const
+{
+    return m_isInitialized;
+}
+
+bool PlanetarySatellitesPlugin::render( GeoPainter *painter, ViewportParams *viewport, const QString &renderPos, GeoSceneLayer *layer )
+{
+    Q_UNUSED( painter );
+    Q_UNUSED( viewport );
+    Q_UNUSED( renderPos );
+    Q_UNUSED( layer );
+
+    /*if( marbleModel()->planetId() == "earth" ) {
+    } else {
+    }*/
+
+    return true;
+}
+
+}
+
+Q_EXPORT_PLUGIN2( PlanetarySatellitesPlugin, Marble::PlanetarySatellitesPlugin )
+
+#include "PlanetarySatellitesPlugin.moc"
---
+++ src/plugins/render/planetarysatellites/PlanetarySatellitesPlugin.h
@@ -0,0 +1,66 @@
+//
+// This file is part of the Marble Virtual Globe.
+//
+// This program is free software licensed under the GNU LGPL. You can
+// find a copy of this license in LICENSE.txt in the top directory of
+// the source code.
+//
+// Copyright 2012 Rene Kuettner <rene@bitkanal.net>
+//
+
+#ifndef MARBLE_NONEARTHSATELLITESPLUGIN_H
+#define MARBLE_NONEARTHSATELLITESPLUGIN_H
+
+#include "RenderPlugin.h"
+
+#include <QtCore/QObject>
+
+class QCheckBox;
+
+namespace Ui
+{
+}
+
+namespace Marble
+{
+
+/**
+ * @brief This plugin displays planetary satellites, space probes
+ *        and their orbits.
+ */
+class PlanetarySatellitesPlugin : public RenderPlugin
+{
+    Q_OBJECT
+    Q_INTERFACES( Marble::RenderPluginInterface )
+    MARBLE_PLUGIN( PlanetarySatellitesPlugin )
+
+public:
+    PlanetarySatellitesPlugin();
+    explicit PlanetarySatellitesPlugin( const MarbleModel *marbleModel );
+    virtual ~PlanetarySatellitesPlugin();
+    
+    QStringList backendTypes() const;
+    QString renderPolicy() const;
+    QStringList renderPosition() const;
+    QString name() const;
+    QString nameId() const;
+    QString guiString() const;
+    QString version() const;
+    QString description() const;
+    QString copyrightYears() const;
+    QList<PluginAuthor> pluginAuthors() const;
+    QString aboutDataText() const;
+    QIcon icon() const;
+    RenderType renderType() const;
+    void initialize();
+    bool isInitialized() const;
+
+    bool render( GeoPainter *painter, ViewportParams *viewport, const QString &renderPos, GeoSceneLayer *layer );
+
+private:
+    bool m_isInitialized;
+};
+
+}
+
+#endif // MARBLE_NONEARTHSATELLITESPLUGIN_H
