33ac56bc7d1fcc42958ab56c7aa6bb22146f51cd
Ander Pijoan <ander.pijoan@deusto.es>
2012-08-14 15:37:32 +0200

    Kothic Json parser for vector data tiles
    During GSoC 2012 for vector tile rendering, Kothic's servers json tiles
    (hosted on http://osmosnimki.ru/vtile/ - please don't use it
    heavily-loaded projects for now) have been used while OpenStreetMap
    finishes building its own data tile server. These Kothic tiles have a
    very similar format
    (https://github.com/kothic/kothic-js/wiki/Tiles-format) to the ones that
    OSM will have.
    
    A problem was found with QScriptValue for multidimensional arrays. When
    having for example [[20,10],[20,30].[30,30]] if we extract the property
    it will flatten to 20,10,20,30,30,30. This parser doesn't create
    separate inner geometries because of this. It will connect the inner
    geometry with its outer.
    
    It will set the geometries name and style according to what
    OsmVisualCategory generates for the parsed tags.
    
    This parser creates a GeoDataDocument with all the extracted data.
    REVIEW: 106008

---
+++ src/plugins/runner/json/JsonPlugin.cpp
@@ -0,0 +1,73 @@
+/*
+ This file is part of the Marble Virtual Globe.
+
+ This program is free software licensed under the GNU LGPL. You can
+ find a copy of this license in LICENSE.txt in the top directory of
+ the source code.
+
+ Copyright 2012 Ander Pijoan <ander.pijoan@deusto.es>
+*/
+
+#include "JsonPlugin.h"
+#include "JsonRunner.h"
+
+namespace Marble
+{
+
+JsonPlugin::JsonPlugin( QObject *parent ) :
+    ParseRunnerPlugin( parent )
+{
+}
+
+QString JsonPlugin::name() const
+{
+    return tr( "Json File Parser" );
+}
+
+QString JsonPlugin::nameId() const
+{
+    return "Json";
+}
+
+QString JsonPlugin::version() const
+{
+    return "1.0";
+}
+
+QString JsonPlugin::description() const
+{
+    return tr( "Create GeoDataDocument from Json Files" );
+}
+
+QString JsonPlugin::copyrightYears() const
+{
+    return "2012";
+}
+
+QList<PluginAuthor> JsonPlugin::pluginAuthors() const
+{
+    return QList<PluginAuthor>()
+            << PluginAuthor( "Ander Pijoan", "ander.pijoan@deusto.es" );
+}
+
+QString JsonPlugin::fileFormatDescription() const
+{
+    return tr( "Json or GeoJson" );
+}
+
+QStringList JsonPlugin::fileExtensions() const
+{
+    return QStringList() << "js";
+}
+
+MarbleAbstractRunner* JsonPlugin::newRunner() const
+{
+    return new JsonRunner;
+}
+
+}
+
+Q_EXPORT_PLUGIN2( JsonPlugin, Marble::JsonPlugin )
+
+#include "JsonPlugin.moc"
+
