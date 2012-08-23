0c1a6219ab03025a4ef0c636cf088a66088793f7
Rene Kuettner <rene@bitkanal.net>
2012-08-24 00:11:40 +0200

    SatellitesPlugin: use dict instead of list

--- tools/gen-satellites-data/lib/HorizonsClient.py
+++ tools/gen-satellites-data/lib/HorizonsClient.py
@@ -37,29 +37,29 @@ class HorizonsClient(object):
             'Saturn'    : 699,
             'Uranus'    : 799,
             'Neptun'    : 899 }
-        self._commands = [
-                ( re.escape("$$SOE"), self.NORESPONSE ),
-                ( re.escape("$$EOE"), self.DATA),
-                ( re.escape("Horizons> "), self.DONE ),
-                ( re.escape("Continue [ <cr>=yes, n=no, ? ] : "), "n" ),
-                ( re.escape("[E]phemeris, [F]tp, [M]ail, [R]edisplay, ?, <cr>: "), "e" ),
-                ( re.escape("Observe, Elements, Vectors  [o,e,v,?] : "), "v" ),
-                ( re.escape("Coordinate center [ <id>,coord,geo  ] : "), "500@{planet_id}" ),
-                ( re.escape("Confirm selected station    [ y/n ] --> "), "y" ),
-                ( re.escape("Reference plane [eclip, frame, body ] : "), "frame" ),
-                ( re.escape("Output interval [ex: 10m, 1h, 1d, ? ] : "), "{interval_days}d" ),
-                ( re.escape("Accept default output [ cr=(y), n, ?] : "), "n" ),
-                ( re.escape("Output reference frame [J2000, B1950] : "), "J2000" ),
-                ( re.escape("Corrections [ 1=NONE, 2=LT, 3=LT+S ]  : "), "1" ),
-                ( re.escape("Output units [1=KM-S, 2=AU-D, 3=KM-D] : "), "1" ),
-                ( re.escape("Spreadsheet CSV format    [ YES, NO ] : "), "YES" ),
-                ( re.escape("Label cartesian output    [ YES, NO ] : "), "NO" ),
-                ( re.escape("Select output table type  [ 1-6, ?  ] : "), "2" ),
-                ( re.escape("[A]gain, [N]ew-case, [F]tp, [K]ermit, [M]ail, [R]edisplay, ? : "), "n" ),
-                ( re.escape("Use previous center  [ cr=(y), n, ? ] : "), "n" ),
-                ( re.compile("Starting CT .* : "), "{datetimestart}"),
-                ( re.compile("Ending   CT .* : "), "{datetimeend}")
-            ]
+        self._lastCommandIndex = None
+        self._commands = { 
+            re.escape("$$SOE"): self.NORESPONSE,
+            re.escape("$$EOE"): self.DATA,
+            re.escape("Horizons> "): self.DONE,
+            re.escape("Continue [ <cr>=yes, n=no, ? ] : "): "n",
+            re.escape("[E]phemeris, [F]tp, [M]ail, [R]edisplay, ?, <cr>: "): "e",
+            re.escape("Observe, Elements, Vectors  [o,e,v,?] : "): "v",
+            re.escape("Coordinate center [ <id>,coord,geo  ] : "): "500@{planet_id}",
+            re.escape("Confirm selected station    [ y/n ] --> "): "y",
+            re.escape("Reference plane [eclip, frame, body ] : "): "frame",
+            re.escape("Output interval [ex: 10m, 1h, 1d, ? ] : "): "{interval_days}d",
+            re.escape("Accept default output [ cr=(y), n, ?] : "): "n",
+            re.escape("Output reference frame [J2000, B1950] : "): "J2000",
+            re.escape("Corrections [ 1=NONE, 2=LT, 3=LT+S ]  : "): "1",
+            re.escape("Output units [1=KM-S, 2=AU-D, 3=KM-D] : "): "1",
+            re.escape("Spreadsheet CSV format    [ YES, NO ] : "): "YES",
+            re.escape("Label cartesian output    [ YES, NO ] : "): "NO",
+            re.escape("Select output table type  [ 1-6, ?  ] : "): "2",
+            re.escape("[A]gain, [N]ew-case, [F]tp, [K]ermit, [M]ail, [R]edisplay, ? : "): "n",
+            re.escape("Use previous center  [ cr=(y), n, ? ] : "): "n",
+            re.compile("Starting CT .* : "): "{datetimestart}",
+            re.compile("Ending   CT .* : "): "{datetimeend}" }
 
     def connect(self):
         self._connection = telnetlib.Telnet(self._host, self._port)
@@ -81,12 +81,13 @@ class HorizonsClient(object):
 
         planetId = self._planetIds[planet]
         
-        cmds = [x for (x,y) in self._commands]
+        cmds = self._commands.keys()
         resp = [y.format(planet=planet,
                          planet_id=planetId,
                          datetimestart=dateTimeStart,
                          datetimeend=dateTimeEnd,
-                         interval_days=intervalDays) for (x,y) in self._commands]
+                         interval_days=intervalDays)
+                for y in self._commands.values()]
         
         done = False
         parsed = ""
@@ -110,6 +111,13 @@ class HorizonsClient(object):
         idx, match, data = self._connection.expect(cmds, 120)
         if(self._debug):
             print(data)
+        if(self._lastCommandIndex == idx):
+            print("Horizons repeated the last message:")
+            print(data)
+            print("Probably something went wrong. Aborting!")
+            sys.exit(1)
+        else:
+            self._lastCommandIndex = idx
         return (resp[idx], data)
  
     def _jdate_to_unix(self, jstamp):
