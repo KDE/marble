KDE/marble

commit 3c7e383f65440efcf89f9be9662f96145cef2a12

Fix planet order in planet combo box
Known planets first in specific order, afterwards moons and unknown
celestial bodies.
REVIEW: 107727

src/lib/MapViewWidget.cpp
@@ -40,6 +40,48 @@ using namespace Marble;
 namespace Marble
 {
 
+class CelestialSortFilterProxyModel : public QSortFilterProxyModel
+{
+public:
+    CelestialSortFilterProxyModel()
+    {
+        // here we will set m_priority for default order
+        const int prefix = 100;
+        m_priority["Mercury"] = prefix-1;
+        m_priority["Venus"] = prefix-2;
+        m_priority["Earth"] = prefix-3;
+        m_priority["Mars"] = prefix-4;
+        m_priority["Jupiter"] = prefix-5;
+        m_priority["Saturn"] = prefix-6;
+        m_priority["Uranus"] = prefix-7;
+        m_priority["Neptune"] = prefix-8;
+        m_priority["Pluto"] = prefix-9;
+    }
+    ~CelestialSortFilterProxyModel() {}
+
+protected:
+    bool lessThan(const QModelIndex &left, const QModelIndex &right) const {
+        QString first = sourceModel()->data(left).toString();
+        QString second = sourceModel()->data(right).toString();
+        // both are in the list
+        if (m_priority.contains(first) && m_priority.contains(second)) {
+            return m_priority[first] > m_priority[second];
+        }
+        // only left in the list
+        if (m_priority.contains(first) && !m_priority.contains(second)) {
+            return true;
+        }
+        // only right in the list
+        if (!m_priority.contains(first) && m_priority.contains(second)) {
+            return false;
+        }
+        return QSortFilterProxyModel::lessThan(left, right);
+    }
+
+private:
+    QMap<QString, int> m_priority;
+};
+
 class MapViewWidget::Private {
  public:
     Private( MapViewWidget *parent )
@@ -48,8 +90,10 @@ class MapViewWidget::Private {
           m_mapThemeModel( 0 ),
           m_mapSortProxy(),
           m_celestialList(),
+          m_celestialListProxy(),
           m_settings( "kde.org", "Marble Desktop Globe" )
     {
+        m_celestialListProxy.setSourceModel(&m_celestialList);
     }
 
     void updateMapFilter()
@@ -93,6 +137,7 @@ class MapViewWidget::Private {
     MapThemeSortFilterProxyModel m_mapSortProxy;
 
     QStandardItemModel m_celestialList;
+    CelestialSortFilterProxyModel m_celestialListProxy;
     QSettings m_settings;
 };
 
@@ -142,7 +187,7 @@ MapViewWidget::MapViewWidget( QWidget *parent, Qt::WindowFlags f )
              this,                                 SLOT( projectionSelected( int ) ) );
 
     d->m_mapViewUi.projectionComboBox->setEnabled( true );
-    d->m_mapViewUi.celestialBodyComboBox->setModel( &d->m_celestialList );
+    d->m_mapViewUi.celestialBodyComboBox->setModel( &d->m_celestialListProxy );
 
     connect( d->m_mapViewUi.celestialBodyComboBox, SIGNAL( activated( int ) ),
              this,                                 SLOT( setCelestialBody( int ) ) );
@@ -208,6 +253,7 @@ void MapViewWidget::Private::updateMapThemeView()
         if ( !mapThemeId.isEmpty() )
             q->setMapThemeId( mapThemeId );
     }
+    m_celestialListProxy.sort(0);
 }
 
 void MapViewWidget::setMapThemeId( const QString &theme )

commit a264db0e8b4597db9612adeb8091afa9a956f5c3

Respect proxy model for selection updates.
Fixes a regression from 3c7e383: Selecting a celestial body jumps to
the wrong one.

src/lib/MapViewWidget.cpp
@@ -100,7 +100,8 @@ class MapViewWidget::Private {
     void updateMapFilter()
     {
         int currentIndex = m_mapViewUi.celestialBodyComboBox->currentIndex();
-        QStandardItem * selectedItem = m_celestialList.item( currentIndex, 1 );
+        int const row = m_celestialListProxy.mapToSource( m_celestialListProxy.index( currentIndex, 0 ) ).row();
+        QStandardItem * selectedItem = m_celestialList.item( row, 1 );
 
         if ( selectedItem ) {
             QString selectedId;
@@ -301,8 +302,9 @@ void MapViewWidget::setMapThemeId( const QString &theme )
             QStandardItem * selectedItem = itemList.first();
 
             if ( selectedItem ) {
-                int selectedIndex = selectedItem->row();
-                d->m_mapViewUi.celestialBodyComboBox->setCurrentIndex( selectedIndex );
+                QModelIndex const selectedIndex = d->m_celestialList.index( selectedItem->row(), 0 );
+                int const proxyIndex = d->m_celestialListProxy.mapFromSource( selectedIndex ).row();
+                d->m_mapViewUi.celestialBodyComboBox->setCurrentIndex( proxyIndex );
                 d->m_mapSortProxy.setFilterRegExp( QRegExp( selectedId, Qt::CaseInsensitive,QRegExp::FixedString ) );
             }
 