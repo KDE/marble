5973d161f125e8c137f11763dddf69c7a0f3cbf7
Torsten Rahn <rahn@kde.org>
2012-12-24 16:54:41 +0100

    - Fix NP and SP - Add initial Celestial Equator and Ecliptic implementation.

--- src/plugins/render/stars/StarsPlugin.cpp
+++ src/plugins/render/stars/StarsPlugin.cpp
@@ -403,36 +403,93 @@ bool StarsPlugin::render( GeoPainter *painter, ViewportParams *viewport,
     const qreal centerLat = viewport->centerLatitude();
 
     const Quaternion skyAxis = Quaternion::fromEuler( -centerLat , centerLon + skyRotationAngle, 0.0 );
-
     matrix skyAxisMatrix;
     skyAxis.inverse().toMatrix( skyAxisMatrix );
 
     const qreal  skyRadius = 0.6 * sqrt( ( qreal )viewport->width() * viewport->width() + viewport->height() * viewport->height() );
 
-    QPen polesPen( QColor( Marble::Oxygen::aluminumGray6 ) );
-    polesPen.setWidth( 2 );
-    painter->setPen( polesPen );
-    Quaternion qpos = Quaternion::fromEuler( 0.0, M_PI/2, 0.0 );
-    qpos.rotateAroundAxis( skyAxisMatrix );
-    int x1, y1;
-    x1 = ( int )( viewport->width()  / 2 + skyRadius * qpos.v[Q_X] );
-    y1 = ( int )( viewport->height() / 2 - skyRadius * qpos.v[Q_Y] );
-    painter->drawLine( x1, y1, x1+10, y1 );
-    painter->drawLine( x1+5, y1-5, x1+5, y1+5 );
-    painter->drawText( x1+8, y1+12, "NP" );
-    x1 = ( int )( viewport->width()  / 2 - skyRadius * qpos.v[Q_X] );
-    y1 = ( int )( viewport->height() / 2 + skyRadius * qpos.v[Q_Y] );
-    painter->drawLine( x1, y1, x1+10, y1 );
-    painter->drawLine( x1+5, y1-5, x1+5, y1+5 );
-    painter->drawText( x1+8, y1+12, "SP" );
+    const bool renderPoles = true;
+
+    if ( renderPoles ) {
+
+	QPen polesPen( QColor( Marble::Oxygen::aluminumGray6 ) );
+	polesPen.setWidth( 2 );
+	painter->setPen( polesPen );
+
+	Quaternion qpos;
+	qpos = Quaternion::fromSpherical( 0, 90 * DEG2RAD );
+	qpos.rotateAroundAxis( skyAxisMatrix );
+
+	int x1, y1;
+	x1 = ( int )( viewport->width()  / 2 + skyRadius * qpos.v[Q_X] );
+	y1 = ( int )( viewport->height() / 2 - skyRadius * qpos.v[Q_Y] );
+	painter->drawLine( x1, y1, x1+10, y1 );
+	painter->drawLine( x1+5, y1-5, x1+5, y1+5 );
+	painter->drawText( x1+8, y1+12, "NP" );
+	x1 = ( int )( viewport->width()  / 2 - skyRadius * qpos.v[Q_X] );
+	y1 = ( int )( viewport->height() / 2 + skyRadius * qpos.v[Q_Y] );
+	painter->drawLine( x1, y1, x1+10, y1 );
+	painter->drawLine( x1+5, y1-5, x1+5, y1+5 );
+	painter->drawText( x1+8, y1+12, "SP" );
+    }
+
+    QPen eclipticPen( QBrush(Marble::Oxygen::aluminumGray6), 1, Qt::DotLine );
+
+    const bool renderEcliptic = true;
+
+    if ( renderEcliptic ) {
+
+        const qreal  skyRadius      = 0.6 * sqrt( ( qreal )viewport->width() * viewport->width() + viewport->height() * viewport->height() );
+
+        const Quaternion skyAxis = Quaternion::fromEuler( -centerLat , centerLon + skyRotationAngle, 0.0 );
+        matrix skyAxisMatrix;
+        skyAxis.inverse().toMatrix( skyAxisMatrix );
+
+        const Quaternion eclipticAxis = Quaternion::fromEuler( 0.0, 0.0, -23.5 * DEG2RAD );
+        matrix eclipticAxisMatrix;
+        (eclipticAxis * skyAxis).inverse().toMatrix( eclipticAxisMatrix );
+
+        painter->setPen(eclipticPen);
+
+        int previousX = -1;
+        int previousY = -1;
+
+        for ( int i = 0; i <= 36; ++i) {
+            Quaternion qpos;
+            qpos = Quaternion::fromSpherical( i * 10 * DEG2RAD, 0 );
+            qpos.rotateAroundAxis( eclipticAxisMatrix );
+
+            int x = ( int )( viewport->width()  / 2 + skyRadius * qpos.v[Q_X] );
+            int y = ( int )( viewport->height() / 2 - skyRadius * qpos.v[Q_Y] );
+
+            if ( qpos.v[Q_Z] < 0 && previousX >= 0 ) painter->drawLine(previousX, previousY, x, y);
+
+            previousX = x;
+            previousY = y;
+        }
+
+        previousX = -1;
+        previousY = -1;
+
+        for ( int i = 0; i <= 36; ++i) {
+            Quaternion qpos;
+            qpos = Quaternion::fromSpherical( i * 10 * DEG2RAD, 0 );
+            qpos.rotateAroundAxis( skyAxisMatrix );
+
+            int x = ( int )( viewport->width()  / 2 + skyRadius * qpos.v[Q_X] );
+            int y = ( int )( viewport->height() / 2 - skyRadius * qpos.v[Q_Y] );
+
+            if ( qpos.v[Q_Z] < 0 && previousX > 0 ) painter->drawLine(previousX, previousY, x, y);
+
+            previousX = x;
+            previousY = y;
+        }
+    }
 
     QPen starPen( Qt::NoPen );
     QPen constellationPenSolid( m_constellationBrush, 1, Qt::SolidLine );
     QPen constellationPenDash(  m_constellationBrush, 1, Qt::DashLine );
     QBrush starBrush( Qt::white );
-
-    painter->setPen( starPen );
-    painter->setBrush( starBrush );
 
     const bool renderStars = !viewport->mapCoversViewport() && viewport->projection() == Spherical;
 
