61f0603c881ad3e036f6104aefcb0422a6686c71
Illya Kovalevskyy <illya.kovalevskyy@gmail.com>
2013-01-05 12:06:52 +0200

    Changes in classes PopupItem and MapInfoDialog
     + API docs for PopupItem and MapInfoDialog were created
     + Minor changes in C++ code of MapInfoDialog
    
    REVIEW: 108173

--- src/lib/MapInfoDialog.h
+++ src/lib/MapInfoDialog.h
@@ -26,6 +26,14 @@ namespace Marble
 class PopupItem;
 class MarbleModel;
 
+/**
+ * @brief The MapInfoDialog class
+ *
+ * A popup dialog opening on top of the map. The content is shown in a QWebView,
+ * acting like a minimalistic web browser. The dialog is either shown aligned to
+ * a geo position or shown at a specific screen position.
+ *
+ */
 class MARBLE_EXPORT MapInfoDialog : public QObject, public LayerInterface
 {
     Q_OBJECT
@@ -40,21 +48,97 @@ public:
     virtual bool eventFilter( QObject *, QEvent * );
     qreal zValue() const;
 
+    /**
+     * @brief Is popup item visible
+     *
+     * If popup item visible, it will return `true`,
+     * otherwise - `false`
+     *
+     * @return visibility of the item
+     */
     bool visible() const;
+
+    /**
+     * @brief Set visibility of the item
+     *
+     * If @p visible is `true`, popup will be visible,
+     * otherwise - popup won't be visible.
+     *
+     * @param visible visibility of the item
+     */
     void setVisible( bool visible );
 
+    /**
+     * @brief Sets coordinates
+     *
+     * Use a geo position as the dialog base position. The dialog will be shown if
+     * it is visible and if the map viewport includes the given coordinates.
+     * This invalidates a screen position set with setPosition(), if any.
+     *
+     * Alignment of the dialog from the point of view of the coordinates. For example,
+     * Qt::AlignRight | Qt::AlignVCenter shows the dialog to the right of the geo position,
+     * vertically centered. An arrow points from the dialog to the geo position.
+     *
+     * @param coordinates geo coordinates
+     * @param alignment alignment of popup when it visible
+     */
     void setCoordinates( const GeoDataCoordinates &coordinates, Qt::Alignment alignment );
+
+    /**
+     * @brief Sets URL of the browser
+     *
+     * @see PopupItem::setUrl();
+     *
+     * @param url url for web brower
+     */
     void setUrl( const QUrl &url );
+
+    /**
+     * @brief Sets size of popup item
+     *
+     * Sets the @p size of the dialog (including the arrow, if any).
+     *
+     * @param size popup size, arrows in count
+     */
     void setSize( const QSizeF &size );
+
+    /**
+     * @brief Sets the position of the dialog to the given screen position.
+     * @warning Any geo position set with setCoordinates() is invalidated.
+     */
     void setPosition( const QPointF &position );
+
+    /**
+     * @brief Sets content of the browser
+     *
+     * @see PopupItem::setContent();
+     *
+     * @param html content (in html format)
+     */
     void setContent( const QString &html );
+
+    /**
+     * @brief Sets background color of the header
+     *
+     * @see PopupItem::setBackgroundColor();
+     *
+     * @param color color to set
+     */
     void setBackgroundColor( const QColor &color );
+
+    /**
+     * @brief Sets text color of the header
+     *
+     * @see PopupItem::setTextColor();
+     *
+     * @param color color to set
+     */
     void setTextColor( const QColor &color );
 
 Q_SIGNALS:
     void repaintNeeded();
 
-public slots:
+private slots:
     void hidePopupItem();
 
 private:
--- src/lib/PopupItem.h
+++ src/lib/PopupItem.h
@@ -26,6 +26,15 @@ class QPushButton;
 namespace Marble
 {
 
+/**
+ * @brief The PopupItem Class
+ *
+ * This class represents graphics item for information bubble.
+ * Mostly used by @see MapInfoDialog.
+ *
+ * It has nice API for QWebView and methods for styling it.
+ *
+ */
 class PopupItem : public QObject, public BillboardGraphicsItem
 {
     Q_OBJECT
@@ -33,20 +42,115 @@ public:
     explicit PopupItem( QObject* parent = 0 );
     ~PopupItem();
 
+    /**
+     * @brief Print button visibility indicator
+     *
+     * There is a button in the header of item with print icon.
+     * It used to print the content of QWebView inside.
+     * This method indicates visibility of this button.
+     *
+     * @see setPrintButtonVisible();
+     *
+     * @return visibility of the print button
+     */
     bool isPrintButtonVisible() const;
-    void setPrintButtonVisible(bool);
 
+    /**
+     * @brief Sets visibility of the print button
+     *
+     * There is a button in the header of item with print icon.
+     * It used to print the content of QWebView inside
+     *
+     * This method sets visibility of this button.
+     *
+     * If @p display is `true`, button will be displayed,
+     * otherwise - button won't be displayed
+     *
+     * @param display visibility of the print button
+     */
+    void setPrintButtonVisible(bool display);
+
+    /**
+     * @brief Set URL for web window
+     *
+     * There is a small web browser inside.
+     * It can show open websites.
+     *
+     * This method sets @p url for its window.
+     *
+     * @param url new url for web window
+     */
     void setUrl( const QUrl &url );
+
+    /**
+     * @brief Set content of the popup
+     *
+     * There is a small web browser inside. It can show custom HTML.
+     * This method sets custom @p html for its window
+     *
+     * @param html custom html for popup
+     */
     void setContent( const QString &html );
+
+    /**
+     * @brief Sets text color of the header
+     *
+     * Frame of the web browser is called bubble. Bubble has
+     * a header - part of the bubble at the top. Usually
+     * it contains the name of the page which can be set via
+     * TITLE html tag in HTML document loaded.
+     * This method sets text @p color of the header.
+     *
+     * @param color text color of the header
+     */
     void setTextColor( const QColor &color );
+
+    /**
+     * @brief Sets background color of the bubble
+     *
+     * Frame of the web browser is called bubble. This method
+     * sets background @p color of this bubble.
+     *
+     * @param color background color of the bubble
+     */
     void setBackgroundColor( const QColor &color );
-    QWidget* transform( QPoint &point ) const;
-    void clearHistory();
+
     virtual bool eventFilter( QObject *, QEvent *e );
 
 private slots:
+    /**
+     * @brief Print content of the web browser
+     *
+     * Popup Item has built-in mini-browser. This function
+     * executes print dialog for printing its content.
+     *
+     */
     void printContent();
+
+    /**
+     * @brief Updates Back Button (web surfing history)
+     *
+     * When you are browsing the site you may need to visit
+     * the page, you have visited before (Go Back).
+     *
+     * For this action Popup Item has a button Go Back placed
+     * in the left of the header.
+     *
+     * @note it's visible only if web surfing history is not clear or
+     * you are not on its first page.
+     *
+     * @see goBack();
+     *
+     */
     void updateBackButton();
+
+    /**
+     * @brief Go Back (web surfing history)
+     *
+     * This method moves you one step backwards in
+     * web surfing history.
+     *
+     */
     void goBack();
 
 protected:
@@ -59,6 +163,8 @@ Q_SIGNALS:
 private:
     QPixmap pixmap( const QString &imageid );
     void colorize( QImage &img, const QColor &col );
+    QWidget* transform( QPoint &point ) const;
+    void clearHistory();
 
     QWidget *m_widget;
     QPushButton *m_printButton;
