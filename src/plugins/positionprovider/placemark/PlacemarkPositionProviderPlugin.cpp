KDE/marble

commit b40fd58247d18b85c4de61418665425b8e7264b8

Several EBN Krazy fixes
Fixing various issues found by the EnglishBreakfastNetwork's
code checker Krazy.

Submitted by Mayank Madan <maddiemadan@gmail.com>, completing
a Google Code-in task.

CCMAIL: maddiemadan@gmail.com
REVIEW: 108048
DIGEST: Google Code-in contribution

src/plugins/positionprovider/placemark/PlacemarkPositionProviderPlugin.cpp
@@ -76,8 +76,8 @@ void PlacemarkPositionProviderPlugin::initialize()
 {
     if ( marbleModel() ) {
         setPlacemark( marbleModel()->trackedPlacemark() );
-        connect( marbleModel(), SIGNAL( trackedPlacemarkChanged( const GeoDataPlacemark* ) ),
-                 this, SLOT( setPlacemark( const GeoDataPlacemark * ) ) );
+        connect( marbleModel(), SIGNAL(trackedPlacemarkChanged(GeoDataPlacemark*)),
+                 this, SLOT(setPlacemark(GeoDataPlacemark*)) );
     } else {
         mDebug() << "PlacemarkPositionProviderPlugin: MarbleModel not set, cannot track placemarks.";
     }
@@ -142,9 +142,9 @@ void PlacemarkPositionProviderPlugin::setPlacemark( const GeoDataPlacemark *plac
     m_status      = placemark ? PositionProviderStatusAvailable : PositionProviderStatusUnavailable;
     m_speed       = 0.0;
 
-    disconnect( marbleModel()->clock(), SIGNAL( timeChanged() ), this, SLOT( updatePosition() ) );
+    disconnect( marbleModel()->clock(), SIGNAL(timeChanged()), this, SLOT(updatePosition()) );
     if ( placemark ) {
-        connect( marbleModel()->clock(), SIGNAL( timeChanged() ), this, SLOT( updatePosition() ) );
+        connect( marbleModel()->clock(), SIGNAL(timeChanged()), this, SLOT(updatePosition()) );
     }
 
     if ( oldPlacemark != m_placemark && m_placemark != 0 ) {

src/plugins/network/kio/KIONetworkPlugin.cpp
@@ -29,14 +29,14 @@ void KIOHttpJob::execute()
     m_job = KIO::storedGet( KUrl( sourceUrl() ), KIO::Reload,
                             KIO::HideProgressInfo | KIO::Overwrite );
 
-    connect( m_job, SIGNAL( canceled( KJob * ) ),
-             this, SLOT( slotCanceled( KJob * ) ) );
-    connect( m_job, SIGNAL( finished( KJob * ) ),
-             this, SLOT( slotFinished( KJob * ) ) );
-    connect( m_job, SIGNAL( infoMessage( KJob *, QString, QString ) ),
-             this, SLOT( slotInfoMessage( KJob *, QString, QString ) ) );
-    connect( m_job, SIGNAL( warning( KJob *, QString, QString ) ),
-             this, SLOT( slotWarning( KJob *, QString, QString ) ) );
+    connect( m_job, SIGNAL(canceled(KJob*)),
+             this, SLOT(slotCanceled(KJob*)) );
+    connect( m_job, SIGNAL(finished(KJob*)),
+             this, SLOT(slotFinished(KJob*)) );
+    connect( m_job, SIGNAL(infoMessage(KJob*,QString,QString)),
+             this, SLOT(slotInfoMessage(KJob*,QString,QString)) );
+    connect( m_job, SIGNAL(warning(KJob*,QString,QString)),
+             this, SLOT(slotWarning(KJob*,QString,QString)) );
 }
 
 void KIOHttpJob::slotCanceled( KJob *job )

src/plugins/network/qhttp/QHttpNetworkPlugin.cpp
@@ -43,8 +43,8 @@ void QHttpHttpJob::execute()
         m_buffer = new QBuffer( &m_data );
         m_buffer->open( QIODevice::WriteOnly );
 
-        connect( m_http, SIGNAL( requestFinished( int, bool ) ),
-                 this, SLOT( httpRequestFinished( int, bool ) ) );
+        connect( m_http, SIGNAL(requestFinished(int,bool)),
+                 this, SLOT(httpRequestFinished(int,bool)) );
     }
 
     m_http->setHost( sourceUrl().host(),

src/plugins/positionprovider/geoclue/GeoCluePositionProviderPlugin.cpp
@@ -45,10 +45,10 @@ void GeoCluePositionProviderPlugin::initialize()
 {
     m_positionProvider = GeoCute::PositionProvider::detailed();
     if (m_positionProvider) {
-        connect( m_positionProvider, SIGNAL( positionChanged(GeoCute::Position) ),
-                 SLOT( updatePosition(GeoCute::Position) ) );
-        connect( m_positionProvider, SIGNAL( statusChanged(GeoCute::Status) ),
-                 SLOT( updateStatus(GeoCute::Status) ) );
+        connect( m_positionProvider, SIGNAL(positionChanged(GeoCute::Position)),
+                 SLOT(updatePosition(GeoCute::Position)) );
+        connect( m_positionProvider, SIGNAL(statusChanged(GeoCute::Status)),
+                 SLOT(updateStatus(GeoCute::Status)) );
     }
 }
 

src/plugins/positionprovider/gpsd/GpsdConnection.cpp
@@ -33,7 +33,7 @@ GpsdConnection::GpsdConnection( QObject* parent )
 {
     m_oldLocale = setlocale( LC_NUMERIC, NULL );
     setlocale( LC_NUMERIC, "C" );
-    connect( &m_timer, SIGNAL( timeout() ), this, SLOT( update() ) );
+    connect( &m_timer, SIGNAL(timeout()), this, SLOT(update()) );
 }
 
 GpsdConnection::~GpsdConnection()

src/plugins/positionprovider/gpsd/GpsdPositionProviderPlugin.cpp
@@ -65,10 +65,10 @@ void GpsdPositionProviderPlugin::initialize()
     emit statusChanged( m_status );
 
     m_thread = new GpsdThread;
-    connect( m_thread, SIGNAL( gpsdInfo( gps_data_t ) ),
-             this, SLOT( update( gps_data_t ) ) );
-    connect( m_thread, SIGNAL( statusChanged( PositionProviderStatus ) ),
-             this, SIGNAL( statusChanged( PositionProviderStatus ) ) );
+    connect( m_thread, SIGNAL(gpsdInfo(gps_data_t)),
+             this, SLOT(update(gps_data_t)) );
+    connect( m_thread, SIGNAL(statusChanged(PositionProviderStatus)),
+             this, SIGNAL(statusChanged(PositionProviderStatus)) );
     m_thread->start();
 }
 

src/plugins/positionprovider/gpsd/GpsdThread.cpp
@@ -32,10 +32,10 @@ void GpsdThread::run()
     qRegisterMetaType<gps_data_t>( "gps_data_t" );
     qRegisterMetaType<PositionProviderStatus>("PositionProviderStatus");
     m_connection = new GpsdConnection;
-    connect( m_connection, SIGNAL( statusChanged( PositionProviderStatus) ),
-             this, SIGNAL( statusChanged( PositionProviderStatus) ) );
-    connect( m_connection, SIGNAL( gpsdInfo( gps_data_t ) ),
-             this, SIGNAL( gpsdInfo( gps_data_t ) ) );
+    connect( m_connection, SIGNAL(statusChanged(PositionProviderStatus)),
+             this, SIGNAL(statusChanged(PositionProviderStatus)) );
+    connect( m_connection, SIGNAL(gpsdInfo(gps_data_t)),
+             this, SIGNAL(gpsdInfo(gps_data_t)) );
     m_connection->initialize();
     exec();
 }

src/plugins/positionprovider/maemo/MaemoPositionProviderPlugin.cpp
@@ -159,7 +159,7 @@ QDateTime MaemoPositionProviderPlugin::timestamp() const
 MaemoPositionProviderPlugin::MaemoPositionProviderPlugin() :
         d( new MaemoPositionProviderPluginPrivate )
 {
-    connect( &d->m_timer, SIGNAL( timeout() ), this, SLOT( update() ) );
+    connect( &d->m_timer, SIGNAL(timeout()), this, SLOT(update()) );
 }
 
 MaemoPositionProviderPlugin::~MaemoPositionProviderPlugin()

commit 835526f925f39cf34a679f840acc2ff449c5a2c3

Fix signal/slot connection broken in b40fd58.
CCMAIL: krammer@kde.org
CCMAIL: maddiemadan@gmail.com

Object::connect: No such signal Marble::MarbleModel::trackedPlacemarkChanged(GeoDataPlacemark*) in .../src/src/plugins/positionprovider/placemark/PlacemarkPositionProviderPlugin.cpp:79

src/plugins/positionprovider/placemark/PlacemarkPositionProviderPlugin.cpp
@@ -76,8 +76,8 @@ void PlacemarkPositionProviderPlugin::initialize()
 {
     if ( marbleModel() ) {
         setPlacemark( marbleModel()->trackedPlacemark() );
-        connect( marbleModel(), SIGNAL(trackedPlacemarkChanged(GeoDataPlacemark*)),
-                 this, SLOT(setPlacemark(GeoDataPlacemark*)) );
+        connect( marbleModel(), SIGNAL(trackedPlacemarkChanged(const GeoDataPlacemark*)),
+                 this, SLOT(setPlacemark(const GeoDataPlacemark*)) );
     } else {
         mDebug() << "PlacemarkPositionProviderPlugin: MarbleModel not set, cannot track placemarks.";
     }