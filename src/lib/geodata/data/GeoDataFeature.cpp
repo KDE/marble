KDE/marble

commit fb08fee2b68c75e16c1cc2d6f361683abb1be751

Support relative icon paths in kml icon styles
REVIEW: 108039

src/lib/geodata/data/GeoDataFeature.cpp
@@ -564,7 +564,9 @@ void GeoDataFeature::setStyleUrl( const QString &value)
                 styleUrl = styleMap.value( QString( "normal" ) );
                 styleUrl.remove('#');
             }
-            setStyle( &doc->style( styleUrl ) );
+            // Not calling setStyle here because we don't want
+            // re-parenting of the style
+            d->m_style = &doc->style( styleUrl );
             found = true;
         }
         object = object->parent();
@@ -635,9 +637,10 @@ const GeoDataStyle* GeoDataFeature::style() const
     }
 }
 
-void GeoDataFeature::setStyle( const GeoDataStyle* style )
+void GeoDataFeature::setStyle( GeoDataStyle* style )
 {
     detach();
+    style->setParent( this );
     d->m_style = style;
 }
 

src/lib/geodata/data/GeoDataDocument.cpp
@@ -78,6 +78,7 @@ void GeoDataDocument::addStyle( const GeoDataStyle& style )
 {
     detach();
     p()->m_styleHash.insert( style.styleId(), style );
+    p()->m_styleHash[ style.styleId() ].setParent( this );
 }
 
 void GeoDataDocument::removeStyle( const QString& styleId )
@@ -104,6 +105,7 @@ void GeoDataDocument::addStyleMap( const GeoDataStyleMap& map )
 {
     detach();
     p()->m_styleMapHash.insert( map.styleId(), map );
+    p()->m_styleMapHash[ map.styleId() ].setParent( this );
 }
 
 void GeoDataDocument::removeStyleMap( const QString& mapId )

src/lib/geodata/data/GeoDataFeature.h
@@ -383,7 +383,7 @@ class GEODATA_EXPORT GeoDataFeature : public GeoDataObject
      * Sets the style of the placemark.
      * @param  style  the new style to be used.
      */
-    void setStyle( const GeoDataStyle* style );
+    void setStyle( GeoDataStyle *style );
 
     /**
      * Return the ExtendedData assigned to the feature.

src/lib/geodata/data/GeoDataIconStyle.cpp
@@ -99,7 +99,7 @@ QImage GeoDataIconStyle::icon() const
     if(!d->m_icon.isNull())
         return d->m_icon;
     else if(!d->m_iconPath.isEmpty()) {
-        d->m_icon = QImage(d->m_iconPath);
+        d->m_icon = QImage( resolvePath( d->m_iconPath ) );
         return d->m_icon;
     }
     else

src/lib/geodata/data/GeoDataObject.cpp
@@ -13,6 +13,8 @@
 
 #include <QtCore/QtGlobal>
 #include <QtCore/QDataStream>
+#include <QtCore/QFileInfo>
+#include <QtCore/QUrl>
 
 #include "GeoDataDocument.h"
 
@@ -98,6 +100,24 @@ int GeoDataObject::targetId() const
 void GeoDataObject::setTargetId( int value )
 {
     d->m_targetId = value;
+}
+
+QString GeoDataObject::resolvePath( const QString &relativePath ) const
+{
+    QUrl const url( relativePath );
+    QFileInfo const fileInfo( url.path() );
+    if ( url.isRelative() && fileInfo.isRelative() ) {
+        GeoDataDocument const * document = dynamic_cast<GeoDataDocument const*>( this );
+        if ( document ) {
+            QFileInfo const documentFile = document->fileName();
+            QFileInfo const absoluteImage( documentFile.absolutePath() + '/' + url.path() );
+            return absoluteImage.absoluteFilePath();
+        } else if ( d->m_parent ) {
+            return d->m_parent->resolvePath( relativePath );
+        }
+    }
+
+    return relativePath;
 }
 
 void GeoDataObject::pack( QDataStream& stream ) const

src/lib/geodata/data/GeoDataObject.h
@@ -83,6 +83,8 @@ public:
      */
     void setTargetId( int value );
 
+    QString resolvePath( const QString &relativePath ) const;
+
     /// Reimplemented from Serializable
     virtual void pack( QDataStream& stream ) const;
     /// Reimplemented from Serializable

src/lib/geodata/data/GeoDataOverlay.cpp
@@ -28,30 +28,11 @@ public:
     QString m_iconPath;
 
     GeoDataOverlayPrivate();
-
-    QString resolve( const GeoDataObject *object, const QString &filename ) const;
 };
 
 GeoDataOverlayPrivate::GeoDataOverlayPrivate() : m_color( Qt::white ), m_drawOrder( 0 )
 {
     // nothing to do
-}
-
-QString GeoDataOverlayPrivate::resolve( const GeoDataObject* object, const QString &filename ) const
-{
-    QFileInfo fileInfo( filename );
-    if ( fileInfo.isRelative() ) {
-        GeoDataDocument const * document = dynamic_cast<GeoDataDocument const*>( object );
-        if ( document ) {
-            QFileInfo documentFile = document->fileName();
-            QFileInfo absoluteImage( documentFile.absolutePath() + '/' + filename );
-            return absoluteImage.absoluteFilePath();
-        } else {
-            return resolve( object->parent(), filename );
-        }
-    }
-
-    return filename;
 }
 
 GeoDataOverlay::GeoDataOverlay() : d( new GeoDataOverlayPrivate )
@@ -121,7 +102,7 @@ QString GeoDataOverlay::iconFile() const
 
 QString GeoDataOverlay::absoluteIconFile() const
 {
-    return d->resolve( this, d->m_iconPath );
+    return resolvePath( d->m_iconPath );
 }
 
 }

src/lib/geodata/data/GeoDataStyle.cpp
@@ -82,6 +82,7 @@ const char* GeoDataStyle::nodeType() const
 void GeoDataStyle::setIconStyle( const GeoDataIconStyle& style )
 {
     d->m_iconStyle = style;
+    d->m_iconStyle.setParent( this );
 }
 
 void GeoDataStyle::setLineStyle( const GeoDataLineStyle& style )

commit 8dd234cf18488aa30bfb91832ea684fdfd39eb1b

- Fix .osm parsing crash regression introduced by fb08fee2  Support relative icon paths in kml icon styles

src/lib/geodata/data/GeoDataFeature.cpp
@@ -640,7 +640,8 @@ const GeoDataStyle* GeoDataFeature::style() const
 void GeoDataFeature::setStyle( GeoDataStyle* style )
 {
     detach();
-    style->setParent( this );
+    if (style)
+        style->setParent( this );
     d->m_style = style;
 }
 