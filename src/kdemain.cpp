30a1f768f33db1f8bd8db1f5accdfefb66b8019c
Rene Kuettner <rene@bitkanal.net>
2013-01-13 19:34:59 +0100

    Add ESA SoCiS 2012 participant to the KDE-version's about dialog

--- src/kdemain.cpp
+++ src/kdemain.cpp
@@ -92,6 +92,9 @@ int main ( int argc, char *argv[] )
     aboutData.addAuthor( ki18n( "Guillaume Martres" ),
                          ki18n( " Satellites" ),
                          "smarter@ubuntu.com" );
+    aboutData.addAuthor( ki18n( "Rene Kuettner" ),
+                         ki18n( " Satellites, Eclipses" ),
+                         "rene@bitkanal.net" );
     aboutData.addAuthor( ki18n( "Friedrich W. H. Kossebau" ),
                          ki18n( "Plasma Integration, Bugfixes" ),
                          "kossebau@kde.org" );
@@ -133,6 +136,10 @@ int main ( int argc, char *argv[] )
                          "mboquien@free.fr" );
 
     // ESA Summer of Code in Space
+    aboutData.addAuthor( ki18n( "Rene Kuettner" ),
+                         ki18n( "ESA Summer of Code in Space  2012 Project:"
+                                " Visualization of planetary satellites" ),
+                         "rene@bitkanal.net" );
     aboutData.addAuthor( ki18n( "Guillaume Martres" ),
                          ki18n( "ESA Summer of Code in Space  2011 Project:"
                                 " Visualisation of Satellite Orbits" ),
