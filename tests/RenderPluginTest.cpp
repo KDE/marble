1fb82e1a2d6de5e19c81a617292cbc8e8d3a1edb
Bernhard Beschow <bbeschow@cs.tu-berlin.de>
2013-01-16 12:33:12 +0100

    add generic test case for settings equality
    (cherry picked from commit f875e05d1d0cbc32904d9e4880727f9723b2418b)

--- tests/RenderPluginTest.cpp
+++ tests/RenderPluginTest.cpp
@@ -36,6 +36,9 @@ class RenderPluginTest : public QObject
     void setEnabled_data();
     void setEnabled();
 
+    void setSettingsAllEqual_data();
+    void setSettingsAllEqual();
+
     void restoreDefaultSettings_data();
     void restoreDefaultSettings();
 
@@ -136,6 +139,35 @@ void RenderPluginTest::setEnabled()
     QCOMPARE( instance->enabled(), enabledByDefault );
 
     delete instance;
+}
+
+void RenderPluginTest::setSettingsAllEqual_data()
+{
+    QTest::addColumn<QVariant>( "result" );
+    QTest::addColumn<QVariant>( "expected" );
+
+    foreach ( const RenderPlugin *plugin, m_model.pluginManager()->renderPlugins() ) {
+        const RenderPlugin *const expected = plugin->newInstance( &m_model );
+
+        RenderPlugin *const result = plugin->newInstance( &m_model );
+        result->setSettings( expected->settings() );
+
+        const QHash<QString, QVariant> settings = expected->settings();
+        QHash<QString, QVariant>::const_iterator itpoint = settings.begin();
+        QHash<QString, QVariant>::const_iterator const endpoint = settings.end();
+        for (; itpoint != endpoint; ++itpoint ) {
+            const QString testName = QString( "%1 %2" ).arg( plugin->nameId() ).arg( itpoint.key() );
+            QTest::newRow( testName.toAscii() ) << result->settings().value( itpoint.key() ) << expected->settings().value( itpoint.key() );
+        }
+    }
+}
+
+void RenderPluginTest::setSettingsAllEqual()
+{
+    QFETCH( QVariant, result );
+    QFETCH( QVariant, expected );
+
+    QCOMPARE( result, expected );
 }
 
 void RenderPluginTest::restoreDefaultSettings_data()
