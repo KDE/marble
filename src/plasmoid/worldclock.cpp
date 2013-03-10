54755cd966c088d15e59c268d8298868ce6cc638
Bernhard Beschow <bbeschow@cs.tu-berlin.de>
2013-03-10 15:26:02 +0100

    use screenCoordinates() from ViewportParams rather than from AbstractProjection

--- src/plasmoid/worldclock.cpp
+++ src/plasmoid/worldclock.cpp
@@ -51,7 +51,6 @@
 #include "GeoPainter.h"
 #include "LatLonEdit.h"
 #include "ViewportParams.h"
-#include "AbstractProjection.h"
 
 namespace Marble
 {
@@ -233,8 +232,7 @@ QString WorldClock::getZone()
     qreal lat, lon;
     // get the hover zone only if the hove point exists
     bool ok = !m_hover.isNull() && 
-              m_map->viewport()->currentProjection()->geoCoordinates(
-                m_hover.x(), m_hover.y(), m_map->viewport(), lon, lat );
+              m_map->viewport()->geoCoordinates( m_hover.x(), m_hover.y(), lon, lat );
 
     if( !ok ) {
         return KSystemTimeZones::local().name();
@@ -368,8 +366,7 @@ void WorldClock::paintInterface(QPainter *p,
     qreal tzy = 0;
     qreal lon = m_locations.value(m_locationkey).longitude() * DEG2RAD;
     qreal lat = m_locations.value(m_locationkey).latitude() * DEG2RAD;
-    bool ok = m_map->viewport()->currentProjection()
-              ->screenCoordinates(lon, lat, m_map->viewport(), tzx, tzy);
+    bool ok = m_map->viewport()->screenCoordinates(lon, lat, tzx, tzy);
     if ( ok /*&& m_isHovered*/ ) {
         QPoint tz( tzx, tzy );
         tz += m_t;
