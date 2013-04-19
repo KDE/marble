KDE/marble

commit d0851c32a1e5ead6155dbe4eea741f5769ffb931

[API change] remove GeoPainter::autoMapQuality()
* surround code with QPainter::save() and QPainter::restore() if changing the painter's renderHints

src/lib/GeoPainter.cpp
@@ -191,13 +191,6 @@ GeoPainter::~GeoPainter()
 }
 
 
-void GeoPainter::autoMapQuality()
-{
-    const bool antialiased = d->m_mapQuality == HighQuality || d->m_mapQuality == PrintQuality;
-    setRenderHint( QPainter::Antialiasing, antialiased );
-}
-
-
 MapQuality GeoPainter::mapQuality() const
 {
     return d->m_mapQuality;

src/lib/GeoPainter.h
@@ -119,18 +119,6 @@ class MARBLE_EXPORT GeoPainter : public ClipPainter
 
     
 /*!
-    \brief Automatically adjusts the painter to the current map quality.
-
-    Automatically adjusts the usage of antialiasing according to the current
-    map quality. It's considered good practice to call this method before
-    starting to draw with a painter. This avoids that objects don't get
-    painted in the wrong quality and it ensures that performance is always
-    optimal.
-*/
-    void autoMapQuality();
-
-    
-/*!
     \brief Returns the map quality.
     \return The map quality that got assigned to the painter.
 */

src/lib/layers/GeometryLayer.cpp
@@ -246,7 +246,6 @@ bool GeometryLayer::render( GeoPainter *painter, ViewportParams *viewport,
     Q_UNUSED( layer )
 
     painter->save();
-    painter->autoMapQuality();
 
     int maxZoomLevel = qMin<int>( qLn( viewport->radius() *4 / 256 ) / qLn( 2.0 ), GeometryLayerPrivate::maximumZoomLevel() );
 

src/lib/routing/RoutingLayer.cpp
@@ -683,7 +683,6 @@ bool RoutingLayer::render( GeoPainter *painter, ViewportParams *viewport,
     Q_UNUSED( layer )
 
     painter->save();
-    painter->autoMapQuality();
 
     if ( d->m_placemarkModel) {
         d->renderPlacemarks( painter );

src/plugins/render/annotate/AnnotatePlugin.cpp
@@ -176,7 +176,6 @@ bool AnnotatePlugin::render( GeoPainter *painter, ViewportParams *viewport, cons
     Q_UNUSED(renderPos);
     Q_UNUSED(layer);
 
-    painter->autoMapQuality();
     QListIterator<SceneGraphicsItem*> iter( m_graphicsItems );
     while( iter.hasNext() ) {
         iter.next()->paint( painter, viewport );

src/plugins/render/aprs/AprsPlugin.cpp
@@ -421,8 +421,6 @@ bool AprsPlugin::render( GeoPainter *painter, ViewportParams *viewport, const QS
 
     painter->save();
 
-    painter->autoMapQuality();
-    
     if ( !( viewport->viewLatLonAltBox() == m_lastBox ) ) {
         m_lastBox = viewport->viewLatLonAltBox();
         QString towrite = "#filter a/" + 

src/plugins/render/graticule/GraticulePlugin.cpp
@@ -263,8 +263,6 @@ bool GraticulePlugin::render( GeoPainter *painter, ViewportParams *viewport,
 
     painter->save();
 
-    painter->autoMapQuality();
-
     if ( m_currentNotation != GeoDataCoordinates::defaultNotation() ) {
         initLineMaps( GeoDataCoordinates::defaultNotation() );
     }

src/plugins/render/measure/MeasureToolPlugin.cpp
@@ -191,7 +191,6 @@ bool MeasureToolPlugin::render( GeoPainter *painter,
     painter->save();
 
     // Prepare for painting the measure line string and paint it.
-    painter->autoMapQuality();
     painter->setPen( m_pen );
 
     if ( m_showSegmentLabels ) {

commit bc851589737a195e9bb31ffbf8804044c3e7c1a1

enable antialiasing based on map quality
* fixes regression introduced in commit d0851c32a1e5ead6155dbe4eea741f5769ffb931

src/lib/GeoPainter.cpp
@@ -170,6 +170,8 @@ GeoPainter::GeoPainter( QPaintDevice* pd, const ViewportParams *viewport,
     : ClipPainter( pd, clip ),
       d( new GeoPainterPrivate( viewport, mapQuality ) )
 {
+    const bool antialiased = mapQuality == HighQuality || mapQuality == PrintQuality;
+    setRenderHint( QPainter::Antialiasing, antialiased );
 }
 
 