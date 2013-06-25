KDE/marble

commit ad5d1579bac56def0eb5846a2a683a0628f7a02e

have TextureLayer invalidate itself when visible LatLonBox changes (rather than relying on MarbleMap)

src/lib/layers/TextureLayer.cpp
@@ -58,6 +58,7 @@ public:
     TileLoader m_loader;
     MergedLayerDecorator m_layerDecorator;
     StackedTileLoader    m_tileLoader;
+    GeoDataLatLonBox m_latLonBox;
     int m_tileZoomLevel;
     TextureMapperInterface *m_texmapper;
     TextureColorizer *m_texcolorizer;
@@ -80,6 +81,7 @@ TextureLayer::Private::Private( HttpDownloadManager *downloadManager,
     , m_loader( downloadManager, pluginManager )
     , m_layerDecorator( &m_loader, sunLocator )
     , m_tileLoader( &m_layerDecorator )
+    , m_latLonBox()
     , m_tileZoomLevel( -1 )
     , m_texmapper( 0 )
     , m_texcolorizer( 0 )
@@ -212,6 +214,11 @@ bool TextureLayer::render( GeoPainter *painter, ViewportParams *viewport,
 
     if ( !d->m_texmapper )
         return false;
+
+    if ( d->m_latLonBox != viewport->viewLatLonAltBox() ) {
+        d->m_latLonBox = viewport->viewLatLonAltBox();
+        d->m_texmapper->setRepaintNeeded();
+    }
 
     // choose the smaller dimension for selecting the tile level, leading to higher-resolution results
     const int levelZeroWidth = d->m_tileLoader.tileSize().width() * d->m_tileLoader.tileColumnCount( 0 );

src/lib/MarbleMap.cpp
@@ -655,7 +655,6 @@ void MarbleMap::rotateBy( const qreal& deltaLon, const qreal& deltaLat )
 void MarbleMap::centerOn( const qreal lon, const qreal lat )
 {
     d->m_viewport.centerOn( lon * DEG2RAD, lat * DEG2RAD );
-    d->m_textureLayer.setNeedsUpdate();
 
     emit visibleLatLonAltBoxChanged( d->m_viewport.viewLatLonAltBox() );
 }

commit 83124b5b63a5c4ce64596e347856495ace054f86

fix regression introduced in commit ad5d1579bac56def0eb5846a2a683a0628f7a02e
* texture of planet didn't rotate with globe when whole planet was visible

src/lib/layers/TextureLayer.cpp
@@ -58,7 +58,7 @@ public:
     TileLoader m_loader;
     MergedLayerDecorator m_layerDecorator;
     StackedTileLoader    m_tileLoader;
-    GeoDataLatLonBox m_latLonBox;
+    GeoDataCoordinates m_centerCoordinates;
     int m_tileZoomLevel;
     TextureMapperInterface *m_texmapper;
     TextureColorizer *m_texcolorizer;
@@ -81,7 +81,7 @@ TextureLayer::Private::Private( HttpDownloadManager *downloadManager,
     , m_loader( downloadManager, pluginManager )
     , m_layerDecorator( &m_loader, sunLocator )
     , m_tileLoader( &m_layerDecorator )
-    , m_latLonBox()
+    , m_centerCoordinates()
     , m_tileZoomLevel( -1 )
     , m_texmapper( 0 )
     , m_texcolorizer( 0 )
@@ -219,8 +219,10 @@ bool TextureLayer::render( GeoPainter *painter, ViewportParams *viewport,
     if ( !d->m_texmapper )
         return false;
 
-    if ( d->m_latLonBox != viewport->viewLatLonAltBox() ) {
-        d->m_latLonBox = viewport->viewLatLonAltBox();
+    if ( d->m_centerCoordinates.longitude() != viewport->centerLongitude() ||
+         d->m_centerCoordinates.latitude() != viewport->centerLatitude() ) {
+        d->m_centerCoordinates.setLongitude( viewport->centerLongitude() );
+        d->m_centerCoordinates.setLatitude( viewport->centerLatitude() );
         d->m_texmapper->setRepaintNeeded();
     }
 