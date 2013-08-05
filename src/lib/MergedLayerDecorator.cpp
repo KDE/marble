d7f506f51fed1fd0f22bef9eb7be4c49a3433437
Adrian Draghici <draghici.adrian.b@gmail.com>
2013-08-05 15:06:26 +0100

    Make use of TileId::toLatLonBox in GroundOverlay rendering

--- src/lib/MergedLayerDecorator.cpp
+++ src/lib/MergedLayerDecorator.cpp
@@ -227,29 +227,7 @@ void MergedLayerDecorator::Private::renderGroundOverlays( QImage *tileImage, con
     /* All tiles are covering the same area. Pick one. */
     const TileId tileId = tiles.first()->id();
 
-    const GeoSceneTextureTile *textureLayer = findRelevantTextureLayers( tileId ).first();
-
-    qreal radius = ( 1 << tileId.zoomLevel() ) * textureLayer->levelZeroColumns() / 2.0;
-
-    qreal lonLeft   = ( tileId.x() - radius ) / radius * M_PI;
-    qreal lonRight  = ( tileId.x() - radius + 1 ) / radius * M_PI;
-
-    radius = ( 1 << tileId.zoomLevel() ) * textureLayer->levelZeroRows() / 2.0;
-    qreal latTop = 0;
-    qreal latBottom = 0;
-
-    switch ( textureLayer->projection() ) {
-    case GeoSceneTiled::Equirectangular:
-        latTop = ( radius - tileId.y() ) / radius *  M_PI / 2.0;
-        latBottom = ( radius - tileId.y() - 1 ) / radius *  M_PI / 2.0;
-        break;
-    case GeoSceneTiled::Mercator:
-        latTop = gd( ( radius - tileId.y() ) / radius * M_PI );
-        latBottom = gd( ( radius - tileId.y() - 1 ) / radius * M_PI );
-        break;
-    }
-
-    GeoDataLatLonBox tileLatLonBox = GeoDataLatLonBox( latTop, latBottom, lonRight, lonLeft );
+    GeoDataLatLonBox tileLatLonBox = tileId.toLatLonBox( findRelevantTextureLayers( tileId ).first() );
 
     /* Map the ground overlay to the image. */
     for ( int i =  0; i < m_groundOverlays.size(); ++i ) {
