KDE/marble

commit 376004bb723e8de16f879e45c312365cebf67f09

remove Qt modules from includes and adjust CMakeLists.txt to add missing modules

CMakeLists.txt
@@ -251,22 +251,26 @@ endif(APPLE)
 ####################################################
 # Specific options for if building with Qt or kde4 libs
 
-if(QTONLY)
-  find_package(Qt4 REQUIRED)
-  set( QT_USE_QTXML      ON )
-  set( QT_USE_QTNETWORK  ON )
-  set( QT_USE_QTTEST     ON )
-  set( QT_USE_QTSCRIPT   ON )
-  include( ${QT_USE_FILE} )
-  marble_set_package_properties( Qt4 PROPERTIES DESCRIPTION "cross-platform application framework" )
-  marble_set_package_properties( Qt4 PROPERTIES URL "http://qt.digia.com/" )
-  marble_set_package_properties( Qt4 PROPERTIES TYPE REQUIRED PURPOSE "core framework" )
-  IF ( NOT QT_QTDECLARATIVE_LIBRARY )
-    # older cmake versions have a FindQt4.cmake without support for declarative,
-    # but the library may still be available
-    FIND_LIBRARY(QT_QTDECLARATIVE_LIBRARY NAMES QtDeclarative HINTS ${QT_LIBRARY_DIR})
-  ENDIF()
+find_package(Qt4 REQUIRED)
+set( QT_USE_QTXML         ON )
+set( QT_USE_QTNETWORK     ON )
+set( QT_USE_QTTEST        ON )
+set( QT_USE_QTSCRIPT      ON )
+set( QT_USE_QTWEBKIT      ON )
+set( QT_USE_QTSVG         ON )
+set( QT_USE_QTDECLARATIVE ON )
+set( QT_USE_QTSQL         ON )
+include( ${QT_USE_FILE} )
+marble_set_package_properties( Qt4 PROPERTIES DESCRIPTION "cross-platform application framework" )
+marble_set_package_properties( Qt4 PROPERTIES URL "http://qt.digia.com/" )
+marble_set_package_properties( Qt4 PROPERTIES TYPE REQUIRED PURPOSE "core framework" )
+IF ( NOT QT_QTDECLARATIVE_LIBRARY )
+  # older cmake versions have a FindQt4.cmake without support for declarative,
+  # but the library may still be available
+  FIND_LIBRARY(QT_QTDECLARATIVE_LIBRARY NAMES QtDeclarative HINTS ${QT_LIBRARY_DIR})
+ENDIF()
 
+if(QTONLY)
   # add a flag to be able to distinguish between qt 
   # and kde mode in the sources
   add_definitions(-DQTONLY)

commit bf3393cdb2bd670d6eec93dacd64a51a79942f26

find QtDeclarative library again on Maemo5
* broken since commit 376004bb723e8de16f879e45c312365cebf67f09

CMakeLists.txt
@@ -264,10 +264,11 @@ include( ${QT_USE_FILE} )
 marble_set_package_properties( Qt4 PROPERTIES DESCRIPTION "cross-platform application framework" )
 marble_set_package_properties( Qt4 PROPERTIES URL "http://qt.digia.com/" )
 marble_set_package_properties( Qt4 PROPERTIES TYPE REQUIRED PURPOSE "core framework" )
-IF ( NOT QT_QTDECLARATIVE_LIBRARY )
+IF ( NOT QT_QTDECLARATIVE_FOUND )
   # older cmake versions have a FindQt4.cmake without support for declarative,
   # but the library may still be available
-  FIND_LIBRARY(QT_QTDECLARATIVE_LIBRARY NAMES QtDeclarative HINTS ${QT_LIBRARY_DIR})
+  FIND_PACKAGE(QtDeclarative)
+  include_directories(${QT_QTDECLARATIVE_INCLUDE_DIR})
 ENDIF()
 
 if(QTONLY)

cmake/modules/FindQtDeclarative.cmake
@@ -1,0 +1,8 @@
+if (NOT QT_QTDECLARATIVE_FOUND)
+    find_path(QT_QTDECLARATIVE_INCLUDE_DIR NAMES QtDeclarative PATHS ${QT_HEADERS_DIR}/QtDeclarative NO_DEFAULT_PATH)
+    find_library(QT_QTDECLARATIVE_LIBRARY NAMES QtDeclarative HINTS ${QT_LIBRARY_DIR})
+
+    if (QT_QTDECLARATIVE_INCLUDE_DIR AND QT_QTDECLARATIVE_LIBRARY)
+        set(QT_QTDECLARATIVE_FOUND ON)
+    endif ()
+endif ()