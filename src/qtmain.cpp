08bc7cfe848f01306ba3d6bc844dbc71a53dc252
Bernhard Beschow <bbeschow@cs.tu-berlin.de>
2013-09-01 15:17:29 +0200

    remove Maemo5 reminders

--- src/qtmain.cpp
+++ src/qtmain.cpp
@@ -52,22 +52,7 @@ int main(int argc, char *argv[])
     app.setOrganizationDomain( "kde.org" );
     // Widget translation
 
-#ifdef Q_WS_MAEMO_5
-    // Work around http://bugreports.qt.nokia.com/browse/QTBUG-1313
-    QProcessEnvironment env = QProcessEnvironment::systemEnvironment();
-    QString lang( "C" );
-    QStringList const locales = QStringList() << "LC_ALL" << "LC_MESSAGES" << "LANG" << "LANGUAGE";
-    foreach( const QString &locale, locales ) {
-        if ( env.contains( locale ) && !env.value( locale ).isEmpty() ) {
-            lang = env.value( locale, "C" );
-            break;
-        }
-    }
-
-    lang = lang.section( '_', 0, 0 );
-#else
     QString      lang = QLocale::system().name().section('_', 0, 0);
-#endif
     QTranslator  translator;
     translator.load( "marble-" + lang, MarbleDirs::path(QString("lang") ) );
     app.installTranslator(&translator);
