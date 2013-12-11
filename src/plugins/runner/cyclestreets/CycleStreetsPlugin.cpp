3cfc22679069fa5e05220d2031ee175a3e49c75f
Dennis Nienhüser <earthwings@gentoo.org>
2013-12-11 22:25:39 +0100

    Move class declaration on top.

--- src/plugins/runner/cyclestreets/CycleStreetsPlugin.cpp
+++ src/plugins/runner/cyclestreets/CycleStreetsPlugin.cpp
@@ -18,6 +18,66 @@
 namespace Marble
 {
 
+class CycleStreetsConfigWidget : public RoutingRunnerPlugin::ConfigWidget
+{
+public:
+    CycleStreetsConfigWidget();
+    virtual void loadSettings( const QHash<QString, QVariant> &settings );
+    virtual QHash<QString, QVariant> settings() const;
+
+private:
+    Ui::CycleStreetsConfigWidget *ui_configWidget;
+};
+
+CycleStreetsConfigWidget::CycleStreetsConfigWidget()
+    : RoutingRunnerPlugin::ConfigWidget(),
+      ui_configWidget( new Ui::CycleStreetsConfigWidget )
+{
+    ui_configWidget->setupUi( this );
+
+    ui_configWidget->plan->addItem( tr( "balanced" ), "balanced" );
+    ui_configWidget->plan->addItem( tr( "fastest" ), "fastest" );
+    ui_configWidget->plan->addItem( tr( "quietest" ), "quietest" );
+    ui_configWidget->plan->addItem( tr( "shortest" ), "shortest" );
+
+    if ( MarbleGlobal::getInstance()->locale()->measurementSystem() == QLocale::MetricSystem ) {
+        ui_configWidget->speed->addItem( tr( "slow (16 km/h)" ), "16" );
+        ui_configWidget->speed->addItem( tr( "normal (20 km/h)" ), "20" );
+        ui_configWidget->speed->addItem( tr( "fast (24 km/h)") , "24" );
+    } else {
+        ui_configWidget->speed->addItem( tr( "slow (10 mph)" ), "16" );
+        ui_configWidget->speed->addItem( tr( "normal (12 mph)" ), "20" );
+        ui_configWidget->speed->addItem( tr( "fast (15 mph)") , "24" );
+    }
+}
+
+void CycleStreetsConfigWidget::loadSettings( const QHash<QString, QVariant> &settings_ )
+{
+    QHash<QString, QVariant> settings = settings_;
+
+    // Check if all fields are filled and fill them with default values.
+    if ( !settings.contains( "plan" ) ) {
+        settings.insert( "plan", "balanced" );
+    }
+    if ( !settings.contains( "speed" ) ) {
+        settings.insert( "speed", "20" );
+    }
+    ui_configWidget->plan->setCurrentIndex(
+                ui_configWidget->plan->findData( settings.value( "plan" ) ));
+    ui_configWidget->speed->setCurrentIndex(
+                ui_configWidget->speed->findData( settings.value( "speed" ) ));
+}
+
+QHash<QString, QVariant> CycleStreetsConfigWidget::settings() const
+{
+    QHash<QString,QVariant> settings;
+    settings.insert( "plan",
+                     ui_configWidget->plan->itemData( ui_configWidget->plan->currentIndex() ) );
+    settings.insert( "speed",
+                     ui_configWidget->speed->itemData( ui_configWidget->speed->currentIndex() ) );
+    return settings;
+}
+
 CycleStreetsPlugin::CycleStreetsPlugin( QObject *parent ) :
     RoutingRunnerPlugin( parent )
 {
@@ -66,62 +126,6 @@ RoutingRunner *CycleStreetsPlugin::newRunner() const
 {
     return new CycleStreetsRunner;
 }
-
-class CycleStreetsConfigWidget : public RoutingRunnerPlugin::ConfigWidget
-{
-public:
-
-    CycleStreetsConfigWidget()
-        : RoutingRunnerPlugin::ConfigWidget()
-    {
-        ui_configWidget = new Ui::CycleStreetsConfigWidget;
-        ui_configWidget->setupUi( this );
-
-        ui_configWidget->plan->addItem( tr( "balanced" ), "balanced" );
-        ui_configWidget->plan->addItem( tr( "fastest" ), "fastest" );
-        ui_configWidget->plan->addItem( tr( "quietest" ), "quietest" );
-        ui_configWidget->plan->addItem( tr( "shortest" ), "shortest" );
-
-        if ( MarbleGlobal::getInstance()->locale()->measurementSystem() == QLocale::MetricSystem ) {
-            ui_configWidget->speed->addItem( tr( "slow (16 km/h)" ), "16" );
-            ui_configWidget->speed->addItem( tr( "normal (20 km/h)" ), "20" );
-            ui_configWidget->speed->addItem( tr( "fast (24 km/h)") , "24" );
-        } else {
-            ui_configWidget->speed->addItem( tr( "slow (10 mph)" ), "16" );
-            ui_configWidget->speed->addItem( tr( "normal (12 mph)" ), "20" );
-            ui_configWidget->speed->addItem( tr( "fast (15 mph)") , "24" );
-        }
-    }
-
-    virtual void loadSettings( const QHash<QString, QVariant> &settings_ )
-    {
-        QHash<QString, QVariant> settings = settings_;
-
-        // Check if all fields are filled and fill them with default values.
-        if ( !settings.contains( "plan" ) ) {
-            settings.insert( "plan", "balanced" );
-        }
-        if ( !settings.contains( "speed" ) ) {
-            settings.insert( "speed", "20" );
-        }
-        ui_configWidget->plan->setCurrentIndex(
-                    ui_configWidget->plan->findData( settings.value( "plan" ) ));
-        ui_configWidget->speed->setCurrentIndex(
-                    ui_configWidget->speed->findData( settings.value( "speed" ) ));
-    }
-
-    virtual QHash<QString, QVariant> settings() const
-    {
-        QHash<QString,QVariant> settings;
-        settings.insert( "plan",
-                         ui_configWidget->plan->itemData( ui_configWidget->plan->currentIndex() ) );
-        settings.insert( "speed",
-                         ui_configWidget->speed->itemData( ui_configWidget->speed->currentIndex() ) );
-        return settings;
-    }
-private:
-    Ui::CycleStreetsConfigWidget *ui_configWidget;
-};
 
 RoutingRunnerPlugin::ConfigWidget *CycleStreetsPlugin::configWidget()
 {
