KDE/marble

commit 705dcf2db2074c2d7e9cc82947d62f926cbc8a2d

Use non-deprecated ctor

src/plugins/render/routing/AudioOutput.cpp
@@ -53,7 +53,7 @@ AudioOutputPrivate::AudioOutputPrivate( AudioOutput* parent ) :
 
 void AudioOutputPrivate::audioOutputFinished()
 {
-    m_output->setCurrentSource( QString() );
+    m_output->setCurrentSource( Phonon::MediaSource() );
     m_output->clearQueue();
 }
 
@@ -72,7 +72,7 @@ void AudioOutputPrivate::reset()
 {
     if ( m_output ) {
         m_output->stop();
-        m_output->setCurrentSource( QString() );
+        m_output->setCurrentSource( Phonon::MediaSource() );
         m_output->clearQueue();
     }
 
@@ -83,7 +83,7 @@ void AudioOutputPrivate::playInstructions()
 {
     setupAudio();
     if ( m_output ) {
-        m_output->enqueue( m_voiceNavigation.instruction() );
+        m_output->enqueue( QUrl::fromLocalFile( m_voiceNavigation.instruction() ) );
         m_output->play();
     }
 }

commit 52601ccba747366d5b9d6a64c28c5a4bef9f2062

include required header
* fixes compilation under Maemo5 broken since commit 705dcf2db2074c2d7e9cc82947d62f926cbc8a2d

src/plugins/render/routing/AudioOutput.cpp
@@ -15,6 +15,7 @@
 #include "routing/VoiceNavigationModel.h"
 
 #include <QDirIterator>
+#include <QUrl>
 #include <phonon/MediaObject>
 #include <phonon/MediaSource>
 #include <phonon/AudioOutput>