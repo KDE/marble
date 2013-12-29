KDE/marble

commit a457f09400aa1cefdedebf0bb8764f61d628884b

Improve support for rendering of the BalloonStyle by adding a setStyle function to PopupLayer
REVIEW: 114712
CCMAIL: tackat@kde.org

src/lib/marble/layers/PopupLayer.cpp
@@ -119,6 +119,19 @@ void PopupLayer::setContent( const QString &html )
     emit repaintNeeded();
 }
 
+void PopupLayer::setStyle( const GeoDataStyle *style )
+{
+    if (style == 0) {
+	m_popupItem->setBackgroundColor(QColor(Qt::white));
+	m_popupItem->setTextColor(QColor(Qt::black));
+	return;
+    }
+    if (&style->balloonStyle()) {
+        m_popupItem->setBackgroundColor(style->balloonStyle().backgroundColor());
+        m_popupItem->setTextColor(style->balloonStyle().textColor());
+    }
+}
+
 void PopupLayer::setBackgroundColor(const QColor &color)
 {
     if(color.isValid()) {

src/lib/marble/MarbleWidgetPopupMenu.cpp
@@ -331,6 +331,7 @@ void MarbleWidgetPopupMenu::slotInfoDialog()
         } else {
             d->setupDialogGeoPlaces(popup, index);
         }
+        popup->setStyle(index->style());
         popup->popup();
     }
 }

src/lib/marble/layers/PopupLayer.h
@@ -130,6 +130,13 @@ public:
     void setContent( const QString &html );
 
     /**
+     * @brief Sets style of the popup
+     *
+     * @param style the style to set
+     */
+    void setStyle( const GeoDataStyle *style );
+
+    /**
      * @brief Sets background color of the header
      *
      * @see PopupItem::setBackgroundColor();

commit f1707410bee04aa708541a6aebaf1a1503ad9014

Fix build error caused by commit a457f094
The build error was caused by forgetting to commit the
include "GeoDataStyle.h"
line.

src/lib/marble/layers/PopupLayer.cpp
@@ -13,6 +13,7 @@
 #include "PopupLayer.h"
 #include "MarbleWidget.h"
 #include "PopupItem.h"
+#include "GeoDataStyle.h"
 
 #include <QMouseEvent>
 #include <QApplication>