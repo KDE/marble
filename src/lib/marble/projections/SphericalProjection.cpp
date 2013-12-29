7314466f64bfaeac248981ec9abe1314ef970ebb
Bernhard Beschow <bbeschow@cs.tu-berlin.de>
2013-12-29 18:36:44 +0100

    no need for private header
    * class is neither exported nor subclassed

--- src/lib/marble/projections/SphericalProjection.cpp
+++ src/lib/marble/projections/SphericalProjection.cpp
@@ -7,12 +7,13 @@
 //
 // Copyright 2007        Inge Wallin   <ingwa@kde.org>
 // Copyright 2007-2012   Torsten Rahn  <rahn@kde.org>
+// Copyright 2009        Patrick Spendrin <ps_ml@gmx.de>
 // Copyright 2012        Cezar Mocan   <mocancezar@gmail.com>
 //
 
 // Local
 #include "SphericalProjection.h"
-#include "SphericalProjection_p.h"
+#include "AbstractProjection_p.h"
 
 #include "MarbleDebug.h"
 
@@ -31,6 +32,56 @@ static const int maxTessellationNodes = 200;
 namespace Marble
 {
 
+class SphericalProjectionPrivate : public AbstractProjectionPrivate
+{
+  public:
+    explicit SphericalProjectionPrivate( SphericalProjection * parent );
+
+    // This method tessellates a line segment in a way that the line segment
+    // follows great circles. The count parameter specifies the
+    // number of nodes generated for the polygon. If the
+    // clampToGround flag is added the polygon contains count + 2
+    // nodes as the clamped down start and end node get added.
+
+    void tessellateLineSegment(  const GeoDataCoordinates &aCoords,
+                                qreal ax, qreal ay,
+                                const GeoDataCoordinates &bCoords,
+                                qreal bx, qreal by,
+                                QVector<QPolygonF*> &polygons,
+                                const ViewportParams *viewport,
+                                TessellationFlags f = 0 ) const;
+
+    void processTessellation(   const GeoDataCoordinates &previousCoords,
+                               const GeoDataCoordinates &currentCoords,
+                               int count,
+                               QVector<QPolygonF*> &polygons,
+                               const ViewportParams *viewport,
+                               TessellationFlags f = 0 ) const;
+
+    void crossHorizon( const GeoDataCoordinates & bCoord,
+                       QVector<QPolygonF*> &polygons,
+                       const ViewportParams *viewport ) const;
+
+    virtual bool lineStringToPolygon( const GeoDataLineString &lineString,
+                              const ViewportParams *viewport,
+                              QVector<QPolygonF*> &polygons ) const;
+
+    void horizonToPolygon( const ViewportParams *viewport,
+                           const GeoDataCoordinates & disappearCoords,
+                           const GeoDataCoordinates & reappearCoords,
+                           QPolygonF* ) const;
+
+    GeoDataCoordinates findHorizon( const GeoDataCoordinates & previousCoords,
+                                    const GeoDataCoordinates & currentCoords,
+                                    const ViewportParams *viewport,
+                                    TessellationFlags f = 0,
+                                    int recursionCounter = 0 ) const;
+
+    bool globeHidesPoint( const GeoDataCoordinates &coordinates,
+                          const ViewportParams *viewport ) const;
+
+    Q_DECLARE_PUBLIC( SphericalProjection )
+};
 
 SphericalProjection::SphericalProjection()
     : AbstractProjection( new SphericalProjectionPrivate( this ) )
