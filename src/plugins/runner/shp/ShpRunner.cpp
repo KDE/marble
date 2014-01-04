cee0e095aa7d40bea1b4d3ac455267195127c1b9
Torsten Rahn <torsten.rahn@lge.com>
2014-01-04 15:16:50 +0100

    Fixes the shapefile runner to iterate properly over donut-polygons. This bug prevented the caspian sea to show up in the natural earth shape files.

--- src/plugins/runner/shp/ShpRunner.cpp
+++ src/plugins/runner/shp/ShpRunner.cpp
@@ -125,9 +125,10 @@ void ShpRunner::parseFile( const QString &fileName, DocumentRole role = UnknownD
             case SHPT_POLYGON: {
                 if ( shape->nParts != 1 ) {
                     GeoDataPolygon *poly = new GeoDataPolygon;
-                    for( int j=0; j<shape->nParts-1; ++j ) {
+                    for( int j=0; j<shape->nParts; ++j ) {
                         GeoDataLinearRing ring;
-                        for( int k=shape->panPartStart[j]; k<shape->panPartStart[j+1]; ++k ) {
+                        int itEnd = (j + 1 < shape->nParts) ? shape->panPartStart[j+1] : shape->nVertices;
+                        for( int k=shape->panPartStart[j]; k<itEnd; ++k ) {
                             ring.append( GeoDataCoordinates(
                                          shape->padfX[k], shape->padfY[k],
                                          0, GeoDataCoordinates::Degree ) );
