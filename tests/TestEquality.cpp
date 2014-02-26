4fab350d883d34aa0484c6405024b03193532cf2
Dennis Nienhüser <earthwings@gentoo.org>
2014-02-26 04:16:16 -0500

    Split into two methods, rename variables, avoid boolean.

--- tests/TestEquality.cpp
+++ tests/TestEquality.cpp
@@ -22,7 +22,8 @@ class TestEquality : public QObject
     Q_OBJECT
 private slots:
     void initTestCase();
-    void simpleParseTest();
+    void aliasTest();
+    void accuracyTest();
 };
 
 
@@ -31,82 +32,56 @@ void TestEquality::initTestCase()
     MarbleDebug::setEnabled( true );
 }
 
-void TestEquality::simpleParseTest()
+void TestEquality::aliasTest()
 {
-    GeoDataAlias test1;
-    test1.setSourceHref("test1");
-    test1.setTargetHref("test1");
+    GeoDataAlias alias;
+    alias.setSourceHref("test1");
+    alias.setTargetHref("test1");
 
-    GeoDataAlias test1_other;
-    test1_other.setSourceHref("test1");
-    test1_other.setTargetHref("test1");
+    GeoDataAlias alias2;
+    alias2.setSourceHref("test1");
+    alias2.setTargetHref("test1");
 
-    /* Tests for GeoDataAlias */
+    QCOMPARE( alias, alias2 );
 
-    // Test 1
-    bool rez = test1 == test1_other;
-    QCOMPARE( rez, true );
+    alias.setSourceHref("test2");
+    alias2.setSourceHref("Test2");
+    QCOMPARE( alias == alias2, false );
+    QVERIFY( alias != alias2 );
 
-    // Test 2
-    test1.setSourceHref("test2");
-    test1_other.setSourceHref("Test2");
+    alias.setSourceHref("test3");
+    alias.setTargetHref("test3");
+    alias2.setSourceHref("test3");
+    alias2.setTargetHref("test2");
+    QCOMPARE( alias == alias2, false );
 
-    rez = test1 == test1_other;
-    QCOMPARE( rez, false );
+    alias2.setSourceHref("Test3");
+    QCOMPARE( alias != alias2, true );
 
-    // Test 3
-    rez = test1 != test1_other;
-    QCOMPARE( rez, true );
+    alias.setSourceHref("Test3");
+    alias.setTargetHref("test2");
+    QCOMPARE( alias, alias2 );
+}
 
-    // Test 4
-    test1.setSourceHref("test3");
-    test1.setTargetHref("test3");
-    test1_other.setSourceHref("test3");
-    test1_other.setTargetHref("test2");
+void TestEquality::accuracyTest()
+{
+    GeoDataAccuracy accuracy1, accuracy2;
 
-    rez = test1 == test1_other;
-    QCOMPARE( rez, false );
+    accuracy1.level = GeoDataAccuracy::Country;
+    accuracy1.vertical = 2;
+    accuracy1.horizontal = 3;
+    accuracy2.level = GeoDataAccuracy::Country;
+    accuracy2.vertical = 2;
+    accuracy2.horizontal = 3;
+    QCOMPARE( accuracy1, accuracy2 );
 
-    // Test 5
-    test1_other.setSourceHref("Test3");
+    accuracy1.vertical = 1;
+    QCOMPARE( accuracy1 != accuracy2, true );
 
-    rez = test1 != test1_other;
-    QCOMPARE( rez, true );
+    accuracy1.horizontal = 1;
+    accuracy2.horizontal = 1;
 
-    // Test 6
-    test1.setSourceHref("Test3");
-    test1.setTargetHref("test2");
-
-    rez = test1 == test1_other;
-    QCOMPARE( rez, true );
-
-
-    /* Tests for GeoDataAccuracy */
-    GeoDataAccuracy test2, test2_other;
-
-    // Test1
-    test2.level = GeoDataAccuracy::Country;
-    test2.vertical = 2;
-    test2.horizontal = 3;
-    test2_other.level = GeoDataAccuracy::Country;
-    test2_other.vertical = 2;
-    test2_other.horizontal = 3;
-
-    rez = test2 == test2_other;
-    QCOMPARE( rez, true );
-
-    // Test2
-    test2.vertical = 1;
-
-    rez = test2 != test2_other;
-    QCOMPARE( rez, true );
-
-    // Test 3
-    test2.horizontal = 1;
-    test2_other.horizontal = 1;
-
-    rez = test2 == test2_other;
-    QCOMPARE( rez, false );
+    QCOMPARE( accuracy1 == accuracy2, false );
 }
 
 QTEST_MAIN( TestEquality )
