b351696d07798482e9dc54665cb2abb293914610
Dennis Nienhüser <earthwings@gentoo.org>
2014-06-06 16:00:22 +0200

    Increase version for easier checking against 2bdadb2

--- src/lib/marble/MarbleGlobal.h
+++ src/lib/marble/MarbleGlobal.h
@@ -229,14 +229,14 @@ const qreal SEC2HOUR = 1.0 / HOUR2SEC;
 
 // String for about dialog and http user agent
 // FIXME: check if blanks are allowed in user agent version numbers
-const QString MARBLE_VERSION_STRING = QString::fromLatin1( "0.18.20 (0.19 Alpha)" );
+const QString MARBLE_VERSION_STRING = QString::fromLatin1( "0.18.21 (0.19 Alpha)" );
 
 // API Version id:
 // form : 0xMMmmpp
 //        MM = major revision.
 //        mm = minor revision.
 //        pp = patch revision.
-#define MARBLE_VERSION 0x001214
+#define MARBLE_VERSION 0x001215
 
 static const char NOT_AVAILABLE[] = QT_TR_NOOP("not available");
 
