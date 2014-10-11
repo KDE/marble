KDE/marble

commit b1c64e92b1008f9b0cdd47e437339a982a73c554

[API change] remove autoNavigation from RoutingManager as stated in the comments

src/lib/marble/CurrentLocationWidget.cpp
@@ -63,6 +63,7 @@ class CurrentLocationWidgetPrivate
     void updateRecenterComboBox( AutoNavigation::CenterMode centerMode );
     void updateAutoZoomCheckBox( bool autoZoom );
     void updateActivePositionProvider( PositionProviderPlugin* );
+    void updateGuidanceMode();
     void saveTrack();
     void openTrack();
     void clearTrack();
@@ -105,8 +106,8 @@ void CurrentLocationWidget::setMarbleWidget( MarbleWidget *widget )
 {
     d->m_widget = widget;
 
+    delete d->m_adjustNavigation;
     d->m_adjustNavigation = new AutoNavigation( widget->model(), widget->viewport(), this );
-    d->m_widget->model()->routingManager()->setAutoNavigation( d->m_adjustNavigation );
 
     const PluginManager* pluginManager = d->m_widget->model()->pluginManager();
     d->m_positionProviderPlugins = pluginManager->positionProviderPlugins();
@@ -139,10 +140,6 @@ void CurrentLocationWidget::setMarbleWidget( MarbleWidget *widget )
              SIGNAL(statusChanged(PositionProviderStatus)),this,
              SLOT(adjustPositionTrackingStatus(PositionProviderStatus)) );
 
-    disconnect( d->m_adjustNavigation, SIGNAL(recenterModeChanged(AutoNavigation::CenterMode)),
-             this, SLOT(updateRecenterComboBox(AutoNavigation::CenterMode)) );
-    disconnect( d->m_adjustNavigation, SIGNAL(autoZoomToggled(bool)),
-             this, SLOT(updateAutoZoomCheckBox(bool)) );
     disconnect( d->m_widget->model(), SIGNAL(trackedPlacemarkChanged(const GeoDataPlacemark*)),
              this, SLOT(trackPlacemark()) );
 
@@ -175,6 +172,8 @@ void CurrentLocationWidget::setMarbleWidget( MarbleWidget *widget )
 
     connect( d->m_widget, SIGNAL(visibleLatLonAltBoxChanged(GeoDataLatLonAltBox)),
              d->m_adjustNavigation, SLOT(inhibitAutoAdjustments()) );
+    connect( d->m_widget->model()->routingManager(), SIGNAL(guidanceModeEnabledChanged(bool)),
+             this, SLOT(updateGuidaceMode()) );
 
     connect (d->m_currentLocationUi.showTrackCheckBox, SIGNAL(clicked(bool)),
              d->m_widget->model()->positionTracking(), SLOT(setTrackVisible(bool)));
@@ -240,6 +239,14 @@ void CurrentLocationWidgetPrivate::updateActivePositionProvider( PositionProvide
     m_currentLocationUi.recenterComboBox->setEnabled( plugin );
     m_currentLocationUi.autoZoomCheckBox->setEnabled( plugin );
 
+}
+
+void CurrentLocationWidgetPrivate::updateGuidanceMode()
+{
+    const bool enabled = m_widget->model()->routingManager()->guidanceModeEnabled();
+
+    m_adjustNavigation->setAutoZoom( enabled );
+    m_adjustNavigation->setRecenter( enabled ? AutoNavigation::RecenterOnBorder : AutoNavigation::DontRecenter );
 }
 
 void CurrentLocationWidgetPrivate::receiveGpsCoordinates( const GeoDataCoordinates &position, qreal speed )

src/lib/marble/CurrentLocationWidget.h
@@ -90,6 +90,8 @@ class MARBLE_EXPORT CurrentLocationWidget : public QWidget
 
      Q_PRIVATE_SLOT( d, void updateActivePositionProvider( PositionProviderPlugin* ) )
 
+     Q_PRIVATE_SLOT( d, void updateGuidanceMode() )
+
      Q_PRIVATE_SLOT( d, void saveTrack() )
      Q_PRIVATE_SLOT( d, void openTrack() )
      Q_PRIVATE_SLOT( d, void clearTrack() )

src/lib/marble/routing/RoutingManager.cpp
@@ -16,7 +16,6 @@
 #include "RoutingModel.h"
 #include "RoutingProfilesModel.h"
 #include "RoutingRunnerPlugin.h"
-#include "AutoNavigation.h"
 #include "GeoWriter.h"
 #include "GeoDataDocument.h"
 #include "GeoDataExtendedData.h"
@@ -65,8 +64,6 @@ public:
     RoutingRunnerManager m_runnerManager;
 
     bool m_haveRoute;
-
-    AutoNavigation *m_adjustNavigation;
 
     bool m_guidanceModeEnabled;
 
@@ -117,7 +114,6 @@ RoutingManagerPrivate::RoutingManagerPrivate( MarbleModel *model, RoutingManager
         m_alternativeRoutesModel( parent ),
         m_runnerManager( model, q ),
         m_haveRoute( false ),
-        m_adjustNavigation( 0 ),
         m_guidanceModeEnabled( false ),
         m_shutdownPositionTracking( false ),
         m_guidanceModeWarning( true ),
@@ -438,16 +434,6 @@ AlternativeRoutesModel* RoutingManager::alternativeRoutesModel()
     return &d->m_alternativeRoutesModel;
 }
 
-void RoutingManager::setAutoNavigation( AutoNavigation* adjustNavigation )
-{
-    d->m_adjustNavigation = adjustNavigation;
-}
-
-const AutoNavigation* RoutingManager::adjustNavigation() const
-{
-    return d->m_adjustNavigation;
-}
-
 void RoutingManager::writeSettings() const
 {
     d->saveRoute( d->stateFile() );
@@ -543,11 +529,6 @@ void RoutingManager::setGuidanceModeEnabled( bool enabled )
     } else if ( positionProvider && !enabled && d->m_shutdownPositionTracking ) {
         d->m_shutdownPositionTracking = false;
         d->m_positionTracking->setPositionProviderPlugin( 0 );
-    }
-
-    if ( d->m_adjustNavigation ) {
-        d->m_adjustNavigation->setAutoZoom( enabled );
-        d->m_adjustNavigation->setRecenter( enabled ? AutoNavigation::RecenterOnBorder : AutoNavigation::DontRecenter );
     }
 
     emit guidanceModeEnabledChanged( d->m_guidanceModeEnabled );

src/lib/marble/routing/RoutingManager.h
@@ -27,7 +27,6 @@ class MarbleModel;
 class GeoDataDocument;
 class GeoDataFolder;
 class AlternativeRoutesModel;
-class AutoNavigation;
 class RoutingProfilesModel;
 
 /**
@@ -72,15 +71,6 @@ public:
       * alternative routes
       */
     AlternativeRoutesModel* alternativeRoutesModel();
-
-    // This is a temporary location for AutoNavigation. The code would be refactored soon.
-    void setAutoNavigation( AutoNavigation * adjustNavigation );
-
-    /**
-     * returns the instance of AutoNavigation
-     * This is a temporary location for AutoNavigation. The code would be refactored soon.
-     */
-    const AutoNavigation* adjustNavigation() const;
 
     /**
       * Returns the current route request

src/plugins/declarative/Navigation.cpp
@@ -117,6 +117,7 @@ void Navigation::setMap( MarbleWidget* widget )
         connect( d->m_marbleWidget->model()->routingManager()->routingModel(),
                 SIGNAL(positionChanged()), this, SLOT(update()) );
 
+        delete d->m_autoNavigation;
         d->m_autoNavigation = new Marble::AutoNavigation( d->m_marbleWidget->model(), d->m_marbleWidget->viewport(), this );
         connect( d->m_autoNavigation, SIGNAL(zoomIn(FlyToMode)),
                  d->m_marbleWidget, SLOT(zoomIn()) );
@@ -129,8 +130,6 @@ void Navigation::setMap( MarbleWidget* widget )
                  d->m_autoNavigation, SLOT(inhibitAutoAdjustments()) );
         connect( d->m_marbleWidget->model()->positionTracking(), SIGNAL(statusChanged(PositionProviderStatus)),
                  &d->m_voiceNavigation, SLOT(handleTrackingStatusChange(PositionProviderStatus)) );
-
-        d->m_marbleWidget->model()->routingManager()->setAutoNavigation( d->m_autoNavigation );
     }
     emit mapChanged();
 }
@@ -144,6 +143,8 @@ void Navigation::setGuidanceModeEnabled( bool enabled )
 {
     if ( d->m_marbleWidget ) {
         d->m_marbleWidget->model()->routingManager()->setGuidanceModeEnabled( enabled );
+        d->m_autoNavigation->setAutoZoom( enabled );
+        d->m_autoNavigation->setRecenter( enabled ? Marble::AutoNavigation::RecenterOnBorder : Marble::AutoNavigation::DontRecenter );
 
         if ( enabled && !d->m_muted ) {
             //d->m_audio.announceStart();

commit 7321ae24d7c11444ce641a6a1439f7e652a150ea

fix typo in slot connection introduced in b1c64e92b1008f9b0cdd47e437339a982a73c554

src/lib/marble/CurrentLocationWidget.cpp
@@ -173,7 +173,7 @@ void CurrentLocationWidget::setMarbleWidget( MarbleWidget *widget )
     connect( d->m_widget, SIGNAL(visibleLatLonAltBoxChanged(GeoDataLatLonAltBox)),
              d->m_adjustNavigation, SLOT(inhibitAutoAdjustments()) );
     connect( d->m_widget->model()->routingManager(), SIGNAL(guidanceModeEnabledChanged(bool)),
-             this, SLOT(updateGuidaceMode()) );
+             this, SLOT(updateGuidanceMode()) );
 
     connect (d->m_currentLocationUi.showTrackCheckBox, SIGNAL(clicked(bool)),
              d->m_widget->model()->positionTracking(), SLOT(setTrackVisible(bool)));