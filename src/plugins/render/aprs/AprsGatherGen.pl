046d262b44b19056430dc375f38211c90ea214a0
Pino Toscano <pino@kde.org>
2014-10-29 09:08:05 +0100

    aprs: update AprsGatherGen.pl
    Fix/update its output so it is almost the same as the generated
    AprsGatherer_mic_e.h; the only difference is the lack of escaping of
    ' and \ as single character.

--- src/plugins/render/aprs/AprsGatherGen.pl
+++ src/plugins/render/aprs/AprsGatherGen.pl
@@ -9,15 +9,14 @@
 # Copyright 2010 Wes Hardaker <hardaker@users.sourceforge.net>
 
 
-print "
-//
+print "//
 // This file is part of the Marble Virtual Globe.
 //
 // This program is free software licensed under the GNU LGPL. You can
 // find a copy of this license in LICENSE.txt in the top directory of
 // the source code.
 //
-// Copyright 2010 Wes Hardaker <hardaker@users.sourceforge.net>
+// Copyright 2010 Wes Hardaker <hardaker\@users.sourceforge.net>
 //
 
 ";
@@ -25,76 +24,77 @@ print "
 print "    // Generated automatically by $0\n";
 
 foreach my $c ('0'..'9') {
-    print "    DstcallDigits['$c']          = $c;\n";
-    print "    DstcallSouthEast['$c']       = true;\n";
-    print "    DstcallLongitudeOffset['$c'] = 0;\n";
-    print "    DstcallMessageBit['$c']      = 0;\n\n";
+    print "    m_dstCallDigits['$c']          = $c;\n";
+    print "    m_dstCallSouthEast['$c']       = true;\n";
+    print "    m_dstCallLongitudeOffset['$c'] = 0;\n";
+    print "    m_dstCallMessageBit['$c']      = 0;\n\n";
 }
 
 foreach my $c ('A'..'J') {
-    print "    DstcallDigits['$c']          = " . (ord($c)-ord('A')) . ";\n";
-    print "    DstcallMessageBit['$c']      = 2;\n\n";    # 1(Custom)
+    print "    m_dstCallDigits['$c']          = " . (ord($c)-ord('A')) . ";\n";
+    print "    m_dstCallMessageBit['$c']      = 2;\n\n";    # 1(Custom)
 }
 
-    print "    DstcallDigits['K']          = 0;\n";     # technically ' '
-    print "    DstcallMessageBit['K']      = 2;\n\n";   # 1(Custom)
+    print "    m_dstCallDigits['K']          = 0;\n";     # technically ' '
+    print "    m_dstCallMessageBit['K']      = 2;\n\n";   # 1(Custom)
 
-    print "    DstcallDigits['L']          = 0;\n";     # technically ' '
-    print "    DstcallMessageBit['L']      = 0;\n";
-    print "    DstcallLongitudeOffset['L'] = 0;\n";
-    print "    DstcallSouthEast['L']       = true;\n\n";  
+    print "    m_dstCallDigits['L']          = 0;\n";     # technically ' '
+    print "    m_dstCallMessageBit['L']      = 0;\n";
+    print "    m_dstCallLongitudeOffset['L'] = 0;\n";
+    print "    m_dstCallSouthEast['L']       = true;\n\n";  
 
 foreach my $c ('P'..'Y') {
-    print "    DstcallDigits['$c']          = " . (ord($c)-ord('P')) . ";\n";
-    print "    DstcallSouthEast['$c']       = false;\n";
-    print "    DstcallLongitudeOffset['$c'] = 100;\n";
-    print "    DstcallMessageBit['$c']      = 1;\n\n";    # 1 (Standard)
+    print "    m_dstCallDigits['$c']          = " . (ord($c)-ord('P')) . ";\n";
+    print "    m_dstCallSouthEast['$c']       = false;\n";
+    print "    m_dstCallLongitudeOffset['$c'] = 100;\n";
+    print "    m_dstCallMessageBit['$c']      = 1;\n\n";    # 1 (Standard)
 }
 
 
-    print "    DstcallDigits['Z']          = 0;\n";     # technically ' '
-    print "    DstcallMessageBit['Z']      = 1;\n";     # 1 (Standard)
-    print "    DstcallLongitudeOffset['Z'] = 100;\n";
-    print "    DstcallSouthEast['Z']       = false;\n"; 
+    print "    m_dstCallDigits['Z']          = 0;\n";     # technically ' '
+    print "    m_dstCallMessageBit['Z']      = 1;\n";     # 1 (Standard)
+    print "    m_dstCallLongitudeOffset['Z'] = 100;\n";
+    print "    m_dstCallSouthEast['Z']       = false;\n"; 
 
 print "\n\n";
 
 foreach my $c (118..127) {
-    print "    InfoFieldByte1[$c] = $c-1128;\n";
+    print "    m_infoFieldByte1[$c] = $c-1128;\n";
 }
 
 foreach my $c (38..127) {
-    print "    InfoFieldByte1[$c] = $c-28;\n";
+    print "    m_infoFieldByte1[$c] = $c-28;\n";
 }
 
 foreach my $c (108..117) {
-    print "    InfoFieldByte1[$c] = $c-8;\n";
+    print "    m_infoFieldByte1[$c] = $c-8;\n";
 }
 
 foreach my $c (110..179) {
-    print "    InfoFieldByte1[$c] = $c-8;\n";
+    print "    m_infoFieldByte1[$c] = $c-8;\n";
 }
 
 print "\n\n";
 
-print "    StandardMessageText[0] = \"M0: Off Duty\";\n";
-print "    StandardMessageText[1] = \"M1: En Route\";\n";
-print "    StandardMessageText[2] = \"M2: In Service\";\n";
-print "    StandardMessageText[3] = \"M3: Returning\";\n";
-print "    StandardMessageText[4] = \"M4: Committed\";\n";
-print "    StandardMessageText[5] = \"M5: Special\";\n";
-print "    StandardMessageText[6] = \"M6: Priority\";\n";
-print "    StandardMessageText[7] = \"M7: EMERGENCY\";\n\n";
+print "    m_standardMessageText[0] = \"M0: Off Duty\";\n";
+print "    m_standardMessageText[1] = \"M1: En Route\";\n";
+print "    m_standardMessageText[2] = \"M2: In Service\";\n";
+print "    m_standardMessageText[3] = \"M3: Returning\";\n";
+print "    m_standardMessageText[4] = \"M4: Committed\";\n";
+print "    m_standardMessageText[5] = \"M5: Special\";\n";
+print "    m_standardMessageText[6] = \"M6: Priority\";\n";
+print "    m_standardMessageText[7] = \"M7: EMERGENCY\";\n\n";
 
 for (my $i = 0; $i <= 7; $i++) {
-    print "    CustomMessageText[$i] = \"Custom-$i\";\n";
+    print "    m_customMessageText[$i] = \"Custom-$i\";\n";
 }
-print "    CustomMessageText[7] = \"M7: EMERGENCY\";\n";
+print "    m_customMessageText[7] = \"M7: EMERGENCY\";\n";
 
+print "\n";
 
 foreach my $chr (ord('!') .. ord('~')) {
-    printf ("pixmaps[QPair<QChar, QChar>('/','%c')] = " .
-	    "\"bitmaps/aprs-primary/%03d.png\";\n", $chr, $chr-ord('!'));
-    printf ("pixmaps[QPair<QChar, QChar>('\\\\','%c')] = " .
-	    "\"bitmaps/aprs-secondary/%03d.png\";\n", $chr, $chr-ord('!'));
+    printf ("    m_pixmaps[QPair<QChar, QChar>('/','%c')] = " .
+	    "\"aprs/primary/%02d.png\";\n", $chr, $chr-ord('!'));
+    printf ("    m_pixmaps[QPair<QChar, QChar>('\\\\','%c')] = " .
+	    "\"aprs/secondary/%02d.png\";\n", $chr, $chr-ord('!'));
 }
