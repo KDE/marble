a32be51fc2b65839028f8a5de37469c069f42923
Alejandro Garcia Montoro <alejandro.garciamontoro@gmail.com>
2015-05-02 18:09:19 +0200

    Fixes coding style issues: variables declaration and comments

--- src/lib/marble/geodata/data/GeoDataCoordinates.cpp
+++ src/lib/marble/geodata/data/GeoDataCoordinates.cpp
@@ -621,10 +621,10 @@ namespace GeoDataUTM
     * Reference: Hoffmann-Wellenhof, B., Lichtenegger, H., and Collins, J.,
     *   GPS: Theory and Practice, 3rd ed.  New York: Springer-Verlag Wien, 1994.
     *
-    * @param y The UTM northing coordinate, in meters.
+    * @param northing The UTM northing coordinate, in meters.
     * @return The footpoint latitude, in radians.
     */
-    qreal footpointLatitude (qreal y);
+    qreal footpointLatitude (qreal northing);
 
     /**
     * Converts a latitude/longitude pair to x and y coordinates in the
@@ -660,32 +660,37 @@ namespace GeoDataUTM
 
 qreal GeoDataUTM::arcLengthOfMeridian (qreal phi)
 {
-    qreal alpha, beta, gamma, delta, epsilon, n;
+    qreal alpha;
+    qreal beta;
+    qreal gamma;
+    qreal delta;
+    qreal epsilon;
+    qreal n;
     qreal result;
 
-    /* Precalculate n */
+    // Precalculate n
     n = (GeoDataUTM::sm_a - GeoDataUTM::sm_b) / (GeoDataUTM::sm_a + GeoDataUTM::sm_b);
 
-    /* Precalculate alpha */
+    // Precalculate alpha
     alpha = ((GeoDataUTM::sm_a + GeoDataUTM::sm_b) / 2.0)
        * (1.0 + (qPow (n, 2.0) / 4.0) + (qPow (n, 4.0) / 64.0));
 
-    /* Precalculate beta */
+    // Precalculate beta
     beta = (-3.0 * n / 2.0) + (9.0 * qPow (n, 3.0) / 16.0)
        + (-3.0 * qPow (n, 5.0) / 32.0);
 
-    /* Precalculate gamma */
+    // Precalculate gamma
     gamma = (15.0 * qPow (n, 2.0) / 16.0)
         + (-15.0 * qPow (n, 4.0) / 32.0);
 
-    /* Precalculate delta */
+    // Precalculate delta
     delta = (-35.0 * qPow (n, 3.0) / 48.0)
         + (105.0 * qPow (n, 5.0) / 256.0);
 
-    /* Precalculate epsilon */
+    // Precalculate epsilon
     epsilon = (315.0 * qPow (n, 4.0) / 512.0);
 
-/* Now calculate the sum of the series and return */
+    // Now calculate the sum of the series and return
     result = alpha * (phi + (beta * qSin (2.0 * phi))
             + (gamma * qSin (4.0 * phi))
             + (delta * qSin (6.0 * phi))
@@ -699,71 +704,84 @@ qreal GeoDataUTM::centralMeridianUTM (qreal zone)
     return DEG2RAD*(-183.0 + (zone * 6.0));
 }
 
-qreal GeoDataUTM::footpointLatitude (qreal y)
+qreal GeoDataUTM::footpointLatitude (qreal northing)
 {
-    qreal y_, alpha_, beta_, gamma_, delta_, epsilon_, n;
+    qreal y;
+    qreal alpha;
+    qreal beta;
+    qreal gamma;
+    qreal delta;
+    qreal epsilon;
+    qreal n;
     qreal result;
 
-    /* Precalculate n (Eq. 10.18) */
+    // Precalculate n (Eq. 10.18)
     n = (GeoDataUTM::sm_a - GeoDataUTM::sm_b) / (GeoDataUTM::sm_a + GeoDataUTM::sm_b);
 
-    /* Precalculate alpha_ (Eq. 10.22) */
-    /* (Same as alpha in Eq. 10.17) */
-    alpha_ = ((GeoDataUTM::sm_a + GeoDataUTM::sm_b) / 2.0)
+    // Precalculate alpha (Eq. 10.22)
+    // (Same as alpha in Eq. 10.17)
+    alpha = ((GeoDataUTM::sm_a + GeoDataUTM::sm_b) / 2.0)
         * (1 + (qPow (n, 2.0) / 4) + (qPow (n, 4.0) / 64));
 
-    /* Precalculate y_ (Eq. 10.23) */
-    y_ = y / alpha_;
+    // Precalculate y (Eq. 10.23)
+    y = northing / alpha;
 
-    /* Precalculate beta_ (Eq. 10.22) */
-    beta_ = (3.0 * n / 2.0) + (-27.0 * qPow (n, 3.0) / 32.0)
+    // Precalculate beta (Eq. 10.22)
+    beta = (3.0 * n / 2.0) + (-27.0 * qPow (n, 3.0) / 32.0)
         + (269.0 * qPow (n, 5.0) / 512.0);
 
-    /* Precalculate gamma_ (Eq. 10.22) */
-    gamma_ = (21.0 * qPow (n, 2.0) / 16.0)
+    // Precalculate gamma (Eq. 10.22)
+    gamma = (21.0 * qPow (n, 2.0) / 16.0)
         + (-55.0 * qPow (n, 4.0) / 32.0);
 
-    /* Precalculate delta_ (Eq. 10.22) */
-    delta_ = (151.0 * qPow (n, 3.0) / 96.0)
+    // Precalculate delta (Eq. 10.22)
+    delta = (151.0 * qPow (n, 3.0) / 96.0)
         + (-417.0 * qPow (n, 5.0) / 128.0);
 
-    /* Precalculate epsilon_ (Eq. 10.22) */
-    epsilon_ = (1097.0 * qPow (n, 4.0) / 512.0);
+    // Precalculate epsilon (Eq. 10.22)
+    epsilon = (1097.0 * qPow (n, 4.0) / 512.0);
 
-    /* Now calculate the sum of the series (Eq. 10.21) */
-    result = y_ + (beta_ * qSin (2.0 * y_))
-        + (gamma_ * qSin (4.0 * y_))
-        + (delta_ * qSin (6.0 * y_))
-        + (epsilon_ * qSin (8.0 * y_));
+    // Now calculate the sum of the series (Eq. 10.21)
+    result = y + (beta * qSin (2.0 * y))
+        + (gamma * qSin (4.0 * y))
+        + (delta * qSin (6.0 * y))
+        + (epsilon * qSin (8.0 * y));
 
     return result;
 }
 
 void GeoDataUTM::mapLatLonToXY (qreal phi, qreal lambda, qreal lambda0, qreal *xy)
 {
-    qreal N, nu2, ep2, t, t2, l;
+    qreal N;
+    qreal nu2;
+    qreal ep2;
+    qreal t;
+    qreal t2;
+    qreal l;
     qreal l3coef, l4coef, l5coef, l6coef, l7coef, l8coef;
 
-    /* Precalculate ep2 */
+    // Precalculate ep2
     ep2 = (qPow (GeoDataUTM::sm_a, 2.0) - qPow (GeoDataUTM::sm_b, 2.0)) / qPow (GeoDataUTM::sm_b, 2.0);
 
-    /* Precalculate nu2 */
+    // Precalculate nu2
     nu2 = ep2 * qPow (qCos(phi), 2.0);
 
-    /* Precalculate N */
+    // Precalculate N
     N = qPow (GeoDataUTM::sm_a, 2.0) / (GeoDataUTM::sm_b * qSqrt (1 + nu2));
 
-    /* Precalculate t */
+    // Precalculate t
     t = qTan (phi);
     t2 = t * t;
 
-    /* Precalculate l */
+    // Precalculate l
     l = lambda - lambda0;
 
-    /* Precalculate coefficients for l**n in the equations below
-       so a normal human being can read the expressions for easting
-       and northing
-       -- l**1 and l**2 have coefficients of 1.0 */
+    /*
+     * Precalculate coefficients for l**n in the equations below
+     * so a normal human being can read the expressions for easting
+     * and northing
+     * -- l**1 and l**2 have coefficients of 1.0
+     */
     l3coef = 1.0 - t2 + nu2;
 
     l4coef = 5.0 - t2 + 9 * nu2 + 4.0 * (nu2 * nu2);
@@ -778,13 +796,13 @@ void GeoDataUTM::mapLatLonToXY (qreal phi, qreal lambda, qreal lambda0, qreal *x
 
     l8coef = 1385.0 - 3111.0 * t2 + 543.0 * (t2 * t2) - (t2 * t2 * t2);
 
-    /* Calculate easting (x) */
+    // Calculate easting (x)
     xy[0] = N * qCos(phi) * l
         + (N / 6.0 * qPow (qCos(phi), 3.0) * l3coef * qPow (l, 3.0))
         + (N / 120.0 * qPow (qCos(phi), 5.0) * l5coef * qPow (l, 5.0))
         + (N / 5040.0 * qPow (qCos(phi), 7.0) * l7coef * qPow (l, 7.0));
 
-    /* Calculate northing (y) */
+    // Calculate northing (y)
     xy[1] = arcLengthOfMeridian (phi)
         + (t / 2.0 * N * qPow (qCos(phi), 2.0) * qPow (l, 2.0))
         + (t / 24.0 * N * qPow (qCos(phi), 4.0) * l4coef * qPow (l, 4.0))
@@ -796,11 +814,12 @@ void GeoDataUTM::latLonToUTMXY (qreal lon, qreal lat, qreal zone, qreal *xy)
 {
     GeoDataUTM::mapLatLonToXY (lon, lat, GeoDataUTM::centralMeridianUTM(zone), xy);
 
-    /* Adjust easting and northing for UTM system. */
+    // Adjust easting and northing for UTM system.
     xy[0] = xy[0] * GeoDataUTM::UTMScaleFactor + 500000.0;
     xy[1] = xy[1] * GeoDataUTM::UTMScaleFactor;
-    if (xy[1] < 0.0)
+    if (xy[1] < 0.0) {
         xy[1] = xy[1] + 10000000.0;
+    }
 }
 
 GeoDataCoordinates::Notation GeoDataCoordinates::s_notation = GeoDataCoordinates::DMS;
