9db7fffe048c6647803149ae4866eadfb1d6c6a1
Dennis Nienhüser <nienhueser@kde.org>
2015-08-03 23:01:16 +0200

    Set small screen + high resolution profile on Android.

--- src/apps/marble-maps/main.cpp
+++ src/apps/marble-maps/main.cpp
@@ -12,16 +12,24 @@
 #include <QQmlApplicationEngine>
 
 #include "declarative/MarbleDeclarativePlugin.h"
+#include <MarbleGlobal.h>
 #include "MarbleMaps.h"
+
+using namespace Marble;
 
 int main(int argc, char ** argv)
 {
     QApplication app(argc, argv);
 
+#ifdef Q_OS_ANDROID
+    MarbleGlobal::Profiles profiles = MarbleGlobal::SmallScreen | MarbleGlobal::HighResolution;
+    MarbleGlobal::getInstance()->setProfiles( profiles );
+#endif
+
     MarbleDeclarativePlugin declarativePlugin;
     const char * uri = "org.kde.edu.marble";
     declarativePlugin.registerTypes(uri);
-    qmlRegisterType<Marble::MarbleMaps>(uri, 0, 20, "MarbleMaps");
+    qmlRegisterType<MarbleMaps>(uri, 0, 20, "MarbleMaps");
 
     QQmlApplicationEngine engine(QUrl("qrc:/MainScreen.qml"));
 
