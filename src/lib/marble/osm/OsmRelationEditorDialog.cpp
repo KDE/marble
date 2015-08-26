9283a4cdd2c6a7f2a54f0caeed87cea9e3d91e89
Marius Stanciu <stanciumarius94@gmail.com>
2015-08-26 17:55:52 +0300

    Introduces custom relation editing to placemarks within the AnnotatePlugin
    REVIEW: 124822

---
+++ src/lib/marble/osm/OsmRelationEditorDialog.cpp
@@ -0,0 +1,101 @@
+//
+// This file is part of the Marble Virtual Globe.
+//
+// This program is free software licensed under the GNU LGPL. You can
+// find a copy of this license in LICENSE.txt in the top directory of
+// the source code.
+//
+// Copyright 2015      Stanciu Marius-Valeriu <stanciumarius94@gmail.com>
+//
+
+// self
+#include "OsmRelationEditorDialog.h"
+
+// Marble
+#include "OsmTagEditorWidget.h"
+#include "OsmPlacemarkData.h"
+#include "OsmObjectManager.h"
+#include "GeoDataPlacemark.h"
+#include "GeoDataExtendedData.h"
+#include "GeoDataData.h"
+
+// Qt
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QMessageBox>
+#include <QLabel>
+
+namespace Marble {
+
+
+OsmRelationEditorDialog::OsmRelationEditorDialog( OsmPlacemarkData *relationData, QWidget *parent ) :
+    QDialog( parent )
+{
+    m_relationData = relationData;
+    QVBoxLayout *layout = new QVBoxLayout( this );
+
+    // Name input area
+    QHBoxLayout *nameLayout = new QHBoxLayout();
+    QLabel *nameLabel = new QLabel( tr( "Name" ), this );
+    m_nameLineEdit = new QLineEdit( this );
+    m_nameLineEdit->setText( relationData->tagValue( "name" ) );
+    nameLayout->addWidget( nameLabel );
+    nameLayout->addWidget( m_nameLineEdit );
+    layout->addLayout( nameLayout );
+
+    // Tag editor area
+    // A dummy placemark is needed because the OsmTagEditorWidget works with placemarks
+    m_dummyPlacemark = new GeoDataPlacemark();
+    // "osmRelaation=yes" entry is added to its ExtendedData to let the widget know
+    // its special relation status
+    GeoDataExtendedData extendedData;
+    extendedData.addValue( GeoDataData( "osmRelation", "yes" ) );
+    m_dummyPlacemark->setExtendedData( extendedData );
+    m_dummyPlacemark->setOsmData( *m_relationData );
+    OsmObjectManager::initializeOsmData( m_dummyPlacemark );
+    m_tagEditor = new OsmTagEditorWidget( m_dummyPlacemark, this );
+    layout->addWidget( m_tagEditor );
+
+    // Button box area
+    m_buttonBox = new QDialogButtonBox( QDialogButtonBox::Ok | QDialogButtonBox::Cancel, this );
+    layout->addWidget( m_buttonBox );
+
+    QObject::connect( m_buttonBox, SIGNAL( accepted() ),
+                       this, SLOT( checkFields() ) );
+}
+
+OsmRelationEditorDialog::~OsmRelationEditorDialog()
+{
+    delete m_dummyPlacemark;
+}
+
+void OsmRelationEditorDialog::finish()
+{
+    // Updating the relation data with the edited one
+    m_dummyPlacemark->osmData().addTag( "name", m_nameLineEdit->text() );
+    *m_relationData = m_dummyPlacemark->osmData();
+    accept();
+    deleteLater();
+}
+
+void OsmRelationEditorDialog::checkFields()
+{
+    if ( m_nameLineEdit->text().isEmpty() ) {
+        QMessageBox::warning( this,
+                              tr( "No name specified" ),
+                              tr( "Please specify a name for this relation." ) );
+    }
+    else if ( !m_dummyPlacemark->osmData().containsTagKey( "type" ) ) {
+        QMessageBox::warning( this,
+                              tr( "No type tag specified" ),
+                              tr( "Please add a type tag for this relation." ) );
+    }
+    else {
+        finish();
+    }
+}
+
+}
+
+#include "moc_OsmRelationEditorDialog.cpp"
+
---
+++ src/lib/marble/osm/OsmRelationManagerWidget.h
@@ -0,0 +1,74 @@
+//
+// This file is part of the Marble Virtual Globe.
+//
+// This program is free software licensed under the GNU LGPL. You can
+// find a copy of this license in LICENSE.txt in the top directory of
+// the source code.
+//
+// Copyright 2015      Stanciu Marius-Valeriu <stanciumarius94@gmail.com>
+//
+
+#ifndef MARBLE_OSMRELATIONMANAGERWIDGET_H
+#define MARBLE_OSMRELATIONMANAGERWIDGET_H
+
+#include <QWidget>
+#include <QTreeWidgetItem>
+
+#include "MarbleGlobal.h"
+#include "marble_export.h"
+
+namespace Marble
+{
+
+class OsmRelationManagerWidgetPrivate;
+class GeoDataPlacemark;
+class GeoDataFeature;
+class OsmPlacemarkData;
+
+enum Column {
+    Name = 0,
+    Type = 1,
+    Role = 2
+};
+
+/**
+ * @brief The OsmRelationManagerWidget class is used to view and edit relations for any placemark
+ * within the Annotate Plugin.
+ * It can add the placemark to any relation the Annotate Plugin has loaded, or to newly created ones
+ */
+class MARBLE_EXPORT OsmRelationManagerWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit OsmRelationManagerWidget( GeoDataPlacemark *placemark,
+                                       const QHash<qint64, OsmPlacemarkData> *relations,
+                                       QWidget *parent = 0 );
+    ~OsmRelationManagerWidget();
+
+public Q_SLOTS:
+    /**
+     * @brief addRelation adds the placemark to the relation specified in the action->text();
+     * If the text is "New Relation", a new relation is added
+     */
+    void addRelation( QAction* action );
+    void handleItemChange( QTreeWidgetItem *item, int column );
+    void handleDoubleClick( QTreeWidgetItem * item, int column );
+    void handleRelationContextMenuRequest( QPoint point );
+    /**
+     * @brief update updates the relations list and the suggested relations drop menu
+     */
+    void update();
+
+Q_SIGNALS:
+    void relationCreated( OsmPlacemarkData relationData );
+
+private:
+    friend class OsmRelationManagerWidgetPrivate;
+    OsmRelationManagerWidgetPrivate* const d;
+};
+
+}
+
+#endif
+ 
