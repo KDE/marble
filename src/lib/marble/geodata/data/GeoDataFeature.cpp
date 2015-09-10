d6c7bc359a370e363187f44a5eb4d2d7d67b90b8
Dennis Nienhüser <nienhueser@kde.org>
2015-09-11 01:05:03 +0200

    Improve rendering of steps

--- src/lib/marble/geodata/data/GeoDataFeature.cpp
+++ src/lib/marble/geodata/data/GeoDataFeature.cpp
@@ -490,12 +490,9 @@ void GeoDataFeaturePrivate::initializeDefaultStyles()
     s_defaultStyle[GeoDataFeature::ReligionShinto]           = GeoDataFeaturePrivate::createOsmPOIStyle( osmFont, "black/shintoist.16" );
     s_defaultStyle[GeoDataFeature::ReligionSikh]             = GeoDataFeaturePrivate::createOsmPOIStyle( osmFont, "black/sikhist.16" );
     
-    s_defaultStyle[GeoDataFeature::HighwaySteps]             = GeoDataFeaturePrivate::createStyle( 1, 5, "#F98072", "#F98072", true, true,
-                                                                                   Qt::SolidPattern, Qt::CustomDashLine, Qt::FlatCap,
-                                                                                   false, QVector< qreal >() << 0.2 << 0.2 );
-
     s_defaultStyle[GeoDataFeature::HighwayTrafficSignals]    = GeoDataFeaturePrivate::createOsmPOIStyle( osmFont, "individual/traffic_light" );
 
+    s_defaultStyle[GeoDataFeature::HighwaySteps]             = GeoDataFeaturePrivate::createStyle(0.0, 2.0, "#F98072", "#F98072", true, true, Qt::SolidPattern, Qt::CustomDashLine, Qt::FlatCap, false, QVector< qreal >() << 0.3 << 0.3 );
     s_defaultStyle[GeoDataFeature::HighwayUnknown]           = GeoDataFeaturePrivate::createHighwayStyle( "highway_unknown", "#808080", QColor("#808080").darker(150), QFont("Arial"), "000000", 0.0, 1.0);
     s_defaultStyle[GeoDataFeature::HighwayPath]              = GeoDataFeaturePrivate::createHighwayStyle( "highway_path", "#ECECEC", "#F49B8C", QFont("Arial"), "000000", 0.0, 1.0, Qt::DotLine, Qt::SquareCap, true);
     s_defaultStyle[GeoDataFeature::HighwayTrack]             = GeoDataFeaturePrivate::createHighwayStyle( "highway_track", "#ECECEC", "#A27F38", QFont("Arial"), "000000", 0.0, 1.0, Qt::DashLine, Qt::SquareCap, true );
