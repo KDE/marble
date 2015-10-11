KDE/marble

commit 32992e8866d2326c6f5f68d020a046ec3e231fbd

Regression fix: Adding street outlines for the vector osm map theme resulted in all polylines with solid pens not being drawn (e.g. the borders and rivers in the atlas and plain map theme).
This patch introduces a cosmeticOutline property in the
LineStyle to properly determine outline polyline drawing
and to fix this regression.

src/plugins/runner/osm/OsmWay.cpp
@@ -98,6 +98,7 @@ void OsmWay::create(GeoDataDocument *document, const OsmNodes &nodes) const
 
             GeoDataLineStyle lineStyle = placemark->style()->lineStyle();
             lineStyle.setPhysicalWidth(physicalWidth);
+            lineStyle.setCosmeticOutline(true);
             GeoDataStyle* style = new GeoDataStyle(*placemark->style());
             style->setLineStyle(lineStyle);
             placemark->setStyle(style);

src/lib/marble/geodata/data/GeoDataLineStyle.cpp
@@ -22,7 +22,8 @@ class GeoDataLineStylePrivate
 {
   public:
     GeoDataLineStylePrivate() 
-        : m_width( 1.0 ), m_physicalWidth( 0.0 ), m_capStyle( Qt::FlatCap ),
+        : m_width( 1.0 ), m_physicalWidth( 0.0 ),
+          m_cosmeticOutline( false ), m_capStyle( Qt::FlatCap ),
           m_penStyle( Qt::SolidLine ), m_background( false )
     {
     }
@@ -36,6 +37,7 @@ class GeoDataLineStylePrivate
     float  m_width;
     /// The current real width of the line
     float  m_physicalWidth;
+    bool  m_cosmeticOutline;
     Qt::PenCapStyle m_capStyle;
     Qt::PenStyle m_penStyle;
     bool m_background;
@@ -112,6 +114,16 @@ float GeoDataLineStyle::physicalWidth() const
 void GeoDataLineStyle::setPhysicalWidth( const float& realWidth )
 {
     d->m_physicalWidth = realWidth;
+}
+
+bool GeoDataLineStyle::cosmeticOutline() const
+{
+    return d->m_cosmeticOutline;
+}
+
+void GeoDataLineStyle::setCosmeticOutline(bool enabled)
+{
+    d->m_cosmeticOutline = enabled;
 }
 
 Qt::PenCapStyle GeoDataLineStyle::capStyle() const

src/lib/marble/geodata/data/GeoDataLineStyle.h
@@ -79,6 +79,16 @@ class GEODATA_EXPORT GeoDataLineStyle : public GeoDataColorStyle
      * @return the current width
      */
     float physicalWidth() const;
+
+    /**
+     * @brief Set whether the line has a cosmetic 1 pixel outline
+     */
+    void setCosmeticOutline( bool enabled );
+    /**
+     * @brief Return whether the line has a cosmetic 1 pixel outline
+     */
+    bool cosmeticOutline() const;
+
     
     /**
      * @brief Set pen cap style

src/lib/marble/geodata/graphicsitem/GeoLineStringGraphicsItem.cpp
@@ -32,7 +32,7 @@ const float GeoLineStringGraphicsItem::s_outlineZValue = -0.001;
 void GeoLineStringGraphicsItem::createDecorations()
 {
     if ( style() != nullptr ) {
-        if ( style()->polyStyle().outline() ) {
+        if ( style()->lineStyle().cosmeticOutline() ) {
             GeoLineStringGraphicsItem* outline = new GeoLineStringGraphicsItem(this->feature(), this->m_lineString);
             outline->setZValue(this->zValue() + s_outlineZValue);
 
@@ -119,7 +119,7 @@ void GeoLineStringGraphicsItem::paint( GeoPainter* painter, const ViewportParams
 
     if ( ! ( isDecoration() && currentPen.widthF() < 2.5f ) )
     {
-        if( style()->polyStyle().outline() &&
+        if( style()->lineStyle().cosmeticOutline() &&
             style()->lineStyle().penStyle() == Qt::SolidLine ) {
             if ( isDecoration() ) {
                 painter->drawPolyline( *m_lineString, "", NoLabel );

src/lib/marble/geodata/handlers/dgml/DgmlPenTagHandler.cpp
@@ -43,7 +43,8 @@ GeoNode* DgmlPenTagHandler::parse(GeoParser& parser) const
 
     QString color = parser.attribute(dgmlAttr_color).trimmed();
     QString style = parser.attribute(dgmlAttr_style).toLower().trimmed();
-    qreal  width = parser.attribute(dgmlAttr_width).toDouble();
+    QString widthString = parser.attribute(dgmlAttr_width);
+    qreal  width = widthString.isEmpty() ? 1.0 : widthString.toDouble();
 
     QPen pen;
 
@@ -52,7 +53,7 @@ GeoNode* DgmlPenTagHandler::parse(GeoParser& parser) const
     }
 
     if ( !style.isEmpty() ) {
-        if ( style == "nopen" ) pen.setStyle( Qt::NoPen ); 
+        if ( style == "nopen" ) pen.setStyle( Qt::NoPen );
         if ( style == "solidline" ) pen.setStyle( Qt::SolidLine ); 
         if ( style == "dashline" ) pen.setStyle( Qt::DashLine ); 
         if ( style == "dotline" ) pen.setStyle( Qt::DotLine ); 

commit 5fba18b131c70920b2678f075c32d6e12e8d9029

Fix visual regressions of vector tile street rendering introduced with 32992e8866d2326c6f5f68d020a046ec3e231fbd

src/plugins/runner/osm/OsmWay.cpp
@@ -87,6 +87,9 @@ void OsmWay::create(GeoDataDocument *document, const OsmNodes &nodes) const
             lineString->append(node.coordinates());
         }
 
+        GeoDataLineStyle lineStyle = placemark->style()->lineStyle();
+        lineStyle.setCosmeticOutline(true);
+
         if (placemark->visualCategory() > GeoDataFeature::HighwayService &&
                 placemark->visualCategory() <= GeoDataFeature::HighwayMotorway) {
             bool const isOneWay = m_osmData.containsTag("oneway", "yes") || m_osmData.containsTag("oneway", "-1");
@@ -95,13 +98,13 @@ void OsmWay::create(GeoDataDocument *document, const OsmNodes &nodes) const
             double const margins = placemark->visualCategory() == GeoDataFeature::HighwayMotorway ? 2.0 : (isOneWay ? 1.0 : 0.0);
             double const physicalWidth = margins + lanes * laneWidth;
 
-            GeoDataLineStyle lineStyle = placemark->style()->lineStyle();
             lineStyle.setPhysicalWidth(physicalWidth);
-            lineStyle.setCosmeticOutline(true);
-            GeoDataStyle* style = new GeoDataStyle(*placemark->style());
-            style->setLineStyle(lineStyle);
-            placemark->setStyle(style);
         }
+
+        GeoDataStyle* style = new GeoDataStyle(*placemark->style());
+        style->setLineStyle(lineStyle);
+        placemark->setStyle(style);
+
     }
 
     OsmObjectManager::registerId(m_osmData.id());