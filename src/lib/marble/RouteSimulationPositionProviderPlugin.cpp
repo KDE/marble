dd826c5cebf3abcb16bf7947e534382caeb1c422
Dennis Nienhüser <nienhueser@kde.org>
2016-01-09 22:15:21 +0100

    minor code style changes

--- src/lib/marble/RouteSimulationPositionProviderPlugin.cpp
+++ src/lib/marble/RouteSimulationPositionProviderPlugin.cpp
@@ -158,48 +158,49 @@ void RouteSimulationPositionProviderPlugin::update()
         const QDateTime newDateTime = QDateTime::currentDateTime();
         qreal time= m_currentDateTime.msecsTo(newDateTime)/1000.0;
         if ( m_currentPosition.isValid() ) {
-	    {//speed calculations
-	     //Max speed is set on points (m_lineStringInterpolated) based on formula. (max speed before points is calculated so the acceleration won't be exceeded)
-	    	const double acceleration = 1.5;
-	        const double lookForwardDistance = 1000;
-		double checkedDistance = distanceSphere( m_currentPosition, m_lineStringInterpolated.at(m_currentIndex) )* m_marbleModel->planetRadius();
-		const double maxSpeed = 25;
-		const double minSpeed = 2;
-		double newSpeed = qMin((m_speed + acceleration*time), maxSpeed);
-		for (int i=qMax(1,m_currentIndex); i<m_lineStringInterpolated.size()-1 && checkedDistance<lookForwardDistance; ++i)
-		{
-                    double previousHeading = m_lineStringInterpolated.at( i-1 ).bearing( m_lineStringInterpolated.at( i ), GeoDataCoordinates::Degree, GeoDataCoordinates::FinalBearing );
-                    double curveLength = 10;//we treat one point turn as a curve of length 10
-                    double angleSum = 0;//sum of turn angles in a curve
-                    for (int j=i+1; j<m_lineStringInterpolated.size() && curveLength<35; ++j)
-                    {
-                        double newHeading = m_lineStringInterpolated.at( j-1 ).bearing( m_lineStringInterpolated.at( j ), GeoDataCoordinates::Degree, GeoDataCoordinates::FinalBearing );
-                        double diffrenceHeading = qAbs(previousHeading-newHeading);//angle of turn
-                        if(diffrenceHeading>180)
-                                diffrenceHeading = 360 - diffrenceHeading;
-                        angleSum +=diffrenceHeading;
-                        double maxSpeedAtTurn = qMax(((1 - (angleSum/60.0/curveLength*10.0))*maxSpeed), minSpeed);//speed limit at turn
-		        if( checkedDistance<25 and maxSpeedAtTurn<newSpeed )//if we are near turn don't accelerate, if we will have to slow down
-			    newSpeed = qMin(newSpeed, qMax(m_speed,maxSpeedAtTurn));
-                        // formulas:
-                        //     s = Vc * t + a*t*t/2
-                        //     V0 = Vc + a*t
-                        // V0 = maxCurrentSpeed
-                        // Vc = maxSpeedAtTurn
-                        // s = checkedDistance
-                        // a = acceleration
-                        double delta = maxSpeedAtTurn*maxSpeedAtTurn - 4.0*acceleration/2.0*(-checkedDistance);//delta = b*b-4*a*c
-                        double t = (-maxSpeedAtTurn+sqrt(delta))/(2.0*acceleration/2.0);//(-b+sqrt(delta))/(2*c)
-                        double maxCurrentSpeed = maxSpeedAtTurn + acceleration*t;
-                        newSpeed = qMin(newSpeed, maxCurrentSpeed);
-                        previousHeading = newHeading;
-                        curveLength += distanceSphere( m_lineStringInterpolated.at( j-1 ), m_lineStringInterpolated.at( j ) )* m_marbleModel->planetRadius();
+            //speed calculations
+            //Max speed is set on points (m_lineStringInterpolated) based on formula. (max speed before points is calculated so the acceleration won't be exceeded)
+            const double acceleration = 1.5;
+            const double lookForwardDistance = 1000;
+            double checkedDistance = distanceSphere( m_currentPosition, m_lineStringInterpolated.at(m_currentIndex) )* m_marbleModel->planetRadius();
+            const double maxSpeed = 25;
+            const double minSpeed = 2;
+            double newSpeed = qMin((m_speed + acceleration*time), maxSpeed);
+            for (int i=qMax(1,m_currentIndex); i<m_lineStringInterpolated.size()-1 && checkedDistance<lookForwardDistance; ++i)
+            {
+                double previousHeading = m_lineStringInterpolated.at( i-1 ).bearing( m_lineStringInterpolated.at( i ), GeoDataCoordinates::Degree, GeoDataCoordinates::FinalBearing );
+                double curveLength = 10;//we treat one point turn as a curve of length 10
+                double angleSum = 0;//sum of turn angles in a curve
+                for (int j=i+1; j<m_lineStringInterpolated.size() && curveLength<35; ++j)
+                {
+                    double newHeading = m_lineStringInterpolated.at( j-1 ).bearing( m_lineStringInterpolated.at( j ), GeoDataCoordinates::Degree, GeoDataCoordinates::FinalBearing );
+                    double differenceHeading = qAbs(previousHeading-newHeading);//angle of turn
+                    if(differenceHeading>180) {
+                        differenceHeading = 360 - differenceHeading;
                     }
-                    checkedDistance += distanceSphere( m_lineStringInterpolated.at( i ), m_lineStringInterpolated.at( i+1 ) )* m_marbleModel->planetRadius();
-		}
-		m_speed=newSpeed;
-	    }
-	    //Assume the car's moving at m_speed m/s. The distance moved will be speed*time which is equal to the speed of the car if time is equal to one.
+                    angleSum +=differenceHeading;
+                    double maxSpeedAtTurn = qMax(((1 - (angleSum/60.0/curveLength*10.0))*maxSpeed), minSpeed);//speed limit at turn
+                    if( checkedDistance<25 and maxSpeedAtTurn<newSpeed )//if we are near turn don't accelerate, if we will have to slow down
+                        newSpeed = qMin(newSpeed, qMax(m_speed,maxSpeedAtTurn));
+                    // formulas:
+                    //     s = Vc * t + a*t*t/2
+                    //     V0 = Vc + a*t
+                    // V0 = maxCurrentSpeed
+                    // Vc = maxSpeedAtTurn
+                    // s = checkedDistance
+                    // a = acceleration
+                    double delta = maxSpeedAtTurn*maxSpeedAtTurn - 4.0*acceleration/2.0*(-checkedDistance);//delta = b*b-4*a*c
+                    double t = (-maxSpeedAtTurn+sqrt(delta))/(2.0*acceleration/2.0);//(-b+sqrt(delta))/(2*c)
+                    double maxCurrentSpeed = maxSpeedAtTurn + acceleration*t;
+                    newSpeed = qMin(newSpeed, maxCurrentSpeed);
+                    previousHeading = newHeading;
+                    curveLength += distanceSphere( m_lineStringInterpolated.at( j-1 ), m_lineStringInterpolated.at( j ) )* m_marbleModel->planetRadius();
+                }
+                checkedDistance += distanceSphere( m_lineStringInterpolated.at( i ), m_lineStringInterpolated.at( i+1 ) )* m_marbleModel->planetRadius();
+            }
+            m_speed=newSpeed;
+
+            //Assume the car's moving at m_speed m/s. The distance moved will be speed*time which is equal to the speed of the car if time is equal to one.
             //If the function isn't called once exactly after a second, multiplying by the time will compensate for the error and maintain the speed.
             double fraction = m_speed*time/(distanceSphere( m_currentPosition, newPosition )* m_marbleModel->planetRadius());
 
@@ -210,11 +211,11 @@ void RouteSimulationPositionProviderPlugin::update()
             }
             else if ( fraction > 1 ) {
                 while ( fraction > 1 ) {
-		    ++m_currentIndex;
-		    if ( m_currentIndex >= m_lineStringInterpolated.size() ) {
+                    ++m_currentIndex;
+                    if ( m_currentIndex >= m_lineStringInterpolated.size() ) {
                         isCurrentIndexValid = false;
                         break;
-		    }
+                    }
 
                     newPosition = m_lineStringInterpolated.at( m_currentIndex );
                     fraction = m_speed*time / (distanceSphere( m_currentPosition, newPosition )* m_marbleModel->planetRadius());
@@ -225,10 +226,10 @@ void RouteSimulationPositionProviderPlugin::update()
                     newPosition = newPoint;
                 }
             }
-	    else
-	    {
-	    	m_currentIndex++;
-	    }
+            else
+            {
+                m_currentIndex++;
+            }
 
             m_direction = m_currentPosition.bearing( newPosition, GeoDataCoordinates::Degree, GeoDataCoordinates::FinalBearing );
         }
@@ -242,7 +243,7 @@ void RouteSimulationPositionProviderPlugin::update()
         m_lineStringInterpolated = m_lineString;
         m_currentPosition = GeoDataCoordinates();	//Reset The current position so that the the simulation starts from the correct starting point.
         m_speed = 0;
-	if ( m_status != PositionProviderStatusUnavailable ) {
+        if ( m_status != PositionProviderStatusUnavailable ) {
             m_status = PositionProviderStatusUnavailable;
             emit statusChanged( PositionProviderStatusUnavailable );
         }
