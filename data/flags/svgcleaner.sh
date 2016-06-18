fd68ac00d738cb057216143a0b8f8bc1beb03107
Torsten Rahn <rahn@kde.org>
2016-06-18 19:01:58 +0200

    - Make all svgs render with QtSvg - Convert them to plain svg - Make them smaller

--- data/flags/svgcleaner.sh
+++ data/flags/svgcleaner.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-perl -p0777i -e 's/(\s+)?(sodipodi|inkscape):[a-z\-]+?=".+?"//mig' *.svg
-perl -p0777i -e 's/(\s+)?xmlns:(sodipodi|inkscape)=".+?"//mig' *.svg
+# perl -p0777i -e 's/(\s+)?(sodipodi|inkscape):[a-z\-]+?=".+?"//mig' *.svg
+# perl -p0777i -e 's/(\s+)?xmlns:(sodipodi|inkscape)=".+?"//mig' *.svg
 
 # rounding off the paths
 perl -pi -e 's/([ \t][0-9])([0-9\.]{4})[0-9\.]+,([0-9])([0-9\.]{4})[0-9\.]+/\1\2,\3\4/mig' *.svg
