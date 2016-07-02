KDE/marble

commit 6ee45333a9c3b03e89171e88cfc3679dd3d77b79

Do not crash when toggling projections
Only happened when disabling the last texture layer in a map theme
with mixed vector and texture layers (e.g. disabling osm texture tiles
in vector osm)

src/lib/marble/layers/TextureLayer.cpp
@@ -396,7 +396,7 @@ void TextureLayer::setShowTileId( bool show )
 
 void TextureLayer::setProjection( Projection projection )
 {
-    if ( d->m_textures.isEmpty() ) {
+    if ( d->m_textures.isEmpty() || textureLayerCount() == 0 ) {
         return;
     }
 

commit 1f0b61b1bc1a865b884de8fad1ed4ba85d79a1b7

Fix a regression introduced in 6ee4533.
Fixes texture layer not appearing after activation when the startup
map theme has no texture layers enabled by default and no other map
theme with texture layers enabled was active before.

src/lib/marble/layers/TextureLayer.cpp
@@ -396,7 +396,7 @@ void TextureLayer::setShowTileId( bool show )
 
 void TextureLayer::setProjection( Projection projection )
 {
-    if ( d->m_textures.isEmpty() || textureLayerCount() == 0 ) {
+    if ( d->m_textures.isEmpty() ) {
         return;
     }
 
@@ -411,7 +411,7 @@ void TextureLayer::setProjection( Projection projection )
             d->m_texmapper = new EquirectScanlineTextureMapper( &d->m_tileLoader );
             break;
         case Mercator:
-            if ( d->m_tileLoader.tileProjection() == GeoSceneTileDataset::Mercator ) {
+            if ( d->m_textures.at(0)->projection() == GeoSceneTileDataset::Mercator ) {
                 d->m_texmapper = new TileScalingTextureMapper( &d->m_tileLoader );
             } else {
                 d->m_texmapper = new MercatorScanlineTextureMapper( &d->m_tileLoader );