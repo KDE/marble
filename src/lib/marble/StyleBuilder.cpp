KDE/marble

commit 144ce0215ffd22bd36ae3dd32c1242015e9d22e1

merge visualCategories() into createStyle()

src/lib/marble/StyleBuilder.cpp
@@ -904,10 +904,10 @@ GeoDataStyle::ConstPtr StyleBuilder::createStyle(const StyleParameters &paramete
             style = newStyle;
         }
 
-
         if (style->iconStyle().iconPath().isEmpty()) {
-            QList<GeoDataFeature::GeoDataVisualCategory> categories = visualCategories(osmData);
-            foreach(GeoDataFeature::GeoDataVisualCategory category, categories) {
+            for (auto iter = osmData.tagsBegin(), end = osmData.tagsEnd(); iter != end; ++iter) {
+                const QString keyValue = QString("%1=%2").arg(iter.key()).arg(iter.value());
+                const GeoDataFeature::GeoDataVisualCategory category = OsmPresetLibrary::osmVisualCategory(keyValue);
                 const GeoDataStyle::ConstPtr categoryStyle = presetStyle(category);
                 if (!categoryStyle->iconStyle().icon().isNull()) {
                     GeoDataStyle::Ptr newStyle(new GeoDataStyle(*style));
@@ -1445,19 +1445,6 @@ StyleParameters::StyleParameters(const GeoDataFeature *feature_, int tileLevel_)
     tileLevel(tileLevel_)
 {
     // nothing to do
-}
-
-QList<GeoDataFeature::GeoDataVisualCategory> StyleBuilder::visualCategories(const OsmPlacemarkData &osmData)
-{
-    QList<GeoDataFeature::GeoDataVisualCategory> categories;
-    for (auto iter = osmData.tagsBegin(), end=osmData.tagsEnd(); iter != end; ++iter) {
-        const QString keyValue = QString("%1=%2").arg(iter.key()).arg(iter.value());
-        GeoDataFeature::GeoDataVisualCategory category = OsmPresetLibrary::osmVisualCategory(keyValue);
-        if (category != GeoDataFeature::None) {
-            categories << category;
-        }
-    }
-    return categories;
 }
 
 }

src/lib/marble/StyleBuilder.h
@@ -74,8 +74,6 @@ public:
 private:
     Q_DISABLE_COPY(StyleBuilder)
 
-    static QList<GeoDataFeature::GeoDataVisualCategory> visualCategories(const OsmPlacemarkData &osmData);
-
     class Private;
     Private * const d;
 };

commit c68ad733e51e934779143ca11f8d27dca7066553

remove yellow icons
fixes regression introduced in 144ce0215ffd22bd36ae3dd32c1242015e9d22e1

src/lib/marble/StyleBuilder.cpp
@@ -907,7 +907,7 @@ GeoDataStyle::ConstPtr StyleBuilder::createStyle(const StyleParameters &paramete
                 const QString keyValue = QString("%1=%2").arg(iter.key()).arg(iter.value());
                 const GeoDataFeature::GeoDataVisualCategory category = OsmPresetLibrary::osmVisualCategory(keyValue);
                 const GeoDataStyle::ConstPtr categoryStyle = presetStyle(category);
-                if (!categoryStyle->iconStyle().icon().isNull()) {
+                if (category != GeoDataFeature::None && !categoryStyle->iconStyle().icon().isNull()) {
                     GeoDataStyle::Ptr newStyle(new GeoDataStyle(*style));
                     newStyle->setIconStyle(categoryStyle->iconStyle());
                     style = newStyle;