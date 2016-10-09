0646a5e26e2137944a51632639588ccd0298eeb8
Dennis Nienhüser <nienhueser@kde.org>
2016-10-09 16:24:15 +0200

    Fix refactoring issue introduced in 5c1022d
    m_zoomlevel and tile.zoomLevel() are different (m_zoomlevel is the
    lower base zoom level and tile a higher level one).
    
    (cherry picked from commit eb44ff9711a38223ea70204c7289598b27e6e329)

--- tools/vectorosm-tilecreator/TileDirectory.cpp
+++ tools/vectorosm-tilecreator/TileDirectory.cpp
@@ -392,7 +392,7 @@ void TileDirectory::createTiles() const
 bool TileDirectory::contains(const TileId &tile) const
 {
     GeoDataLatLonBox tileBoundary;
-    m_tileProjection.geoCoordinates(m_zoomLevel, tile.x(), tile.y(), tileBoundary);
+    m_tileProjection.geoCoordinates(tile.zoomLevel(), tile.x(), tile.y(), tileBoundary);
 
     double const west = tileBoundary.west();
     double const east = tileBoundary.east();
