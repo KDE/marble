KDE/marble

commit 5c1022d13a3b68fd3251cf0b57d03087957dc9eb

Introducing GeoSceneAbstractTileProjection for tile x/y <-> lonlat
Summary:
In the current code in some places hard assumptions are made about
the projection used by the tile material. Also are calculations
duplicated.
The new abstract class GeoSceneAbstractTileProjection and its currently
two concrete subclasses GeoSceneEquirectTileProjection & GeoSceneMercatorTileProjection
should allow to make most code agnostic of the tile projection details
and also remove the code duplication, also makes unit testing of the projection easy.

GeoSceneAbstractTileProjection follows the concepts of AbstractProjection
and has conversion methods in the interface, with the output vars as
non-const references at the end of the argument list (for consistency, I personally
prefer yielded stuff as return parameter, or have output vars at least being
first in the argument list and as pointers for improved markup in the calling code).
As the current two implementations need to know about the variables
levelZeroColumns and levelZeroRows, I made these properties of the classes
themselves, to avoid having the classes depend on GeoSceneTileDataset.
Comes at the cost of data duplication and thus more complicated setup,
but also avoids an indirection in the class methods using these values.
For simplicity I put these properties onto GeoSceneAbstractTileProjection
itself, to avoid another intermediate abstract subclass for the concept of
levelZeroColumns and levelZeroRows.

The projection methods do not do any out-of-bounds handling, but expect
the calling code to pass proper value. Which should be valid with the current
callees.

Any methods tileProjection() have been renamed to tileProjectionType(),
to make clear those just return the type, not a projection object.

The patch also makes TileId more dump again and just a property container,
especially no longer knowing about GeoSceneTileDataset.

FUTURE WORK:
* Look into all remaining places which have "if tileProjectionType() == x" to see

tools/vectorosm-tilecreator/TileDirectory.cpp
@@ -356,11 +356,14 @@ void TileDirectory::createTiles() const
         QDir().mkpath(outputDir);
         if (m_tileType == OpenStreetMap) {
             QString const output = QString("-o=%1").arg(outputFile);
-            int const N = pow(2, m_zoomLevel);
-            double const minLon = TileId::tileX2lon(tileId.x(), N) * RAD2DEG;
-            double const maxLon = TileId::tileX2lon(tileId.x()+1, N) * RAD2DEG;
-            double const maxLat = TileId::tileY2lat(tileId.y(), N) * RAD2DEG;
-            double const minLat = TileId::tileY2lat(tileId.y()+1, N) * RAD2DEG;
+
+            GeoDataLatLonBox tileBoundary;
+            m_tileProjection.geoCoordinates(m_zoomLevel, tileId.x(), tileId.y(), tileBoundary);
+
+            double const minLon = tileBoundary.west(GeoDataCoordinates::Degree);
+            double const maxLon = tileBoundary.east(GeoDataCoordinates::Degree);
+            double const maxLat = tileBoundary.north(GeoDataCoordinates::Degree);
+            double const minLat = tileBoundary.south(GeoDataCoordinates::Degree);
             QString const bbox = QString("-b=%1,%2,%3,%4").arg(minLon).arg(minLat).arg(maxLon).arg(maxLat);
             QProcess osmconvert;
             osmconvert.start("osmconvert", QStringList() << "--drop-author" << "--drop-version"
@@ -387,11 +390,13 @@ void TileDirectory::createTiles() const
 
 bool TileDirectory::contains(const TileId &tile) const
 {
-    int const N = pow(2, tile.zoomLevel());
-    double const west = TileId::tileX2lon(tile.x(), N);
-    double const east = TileId::tileX2lon(tile.x()+1, N);
-    double const north = TileId::tileY2lat(tile.y(), N);
-    double const south = TileId::tileY2lat(tile.y()+1, N);
+    GeoDataLatLonBox tileBoundary;
+    m_tileProjection.geoCoordinates(m_zoomLevel, tile.x(), tile.y(), tileBoundary);
+
+    double const west = tileBoundary.west();
+    double const east = tileBoundary.east();
+    double const north = tileBoundary.north();
+    double const south = tileBoundary.south();
     QVector<GeoDataCoordinates> bounds;
     bounds << GeoDataCoordinates(west, north);
     bounds << GeoDataCoordinates(east, north);

src/lib/marble/DownloadRegion.cpp
@@ -57,10 +57,10 @@ int DownloadRegionPrivate::rad2PixelY( qreal const lat, const TextureLayer *text
     qreal const globalHeight = textureLayer->tileSize().height()
             * textureLayer->tileRowCount( m_visibleTileLevel );
     qreal const normGlobalHeight = globalHeight / M_PI;
-    switch ( textureLayer->tileProjection() ) {
-    case GeoSceneTileDataset::Equirectangular:
+    switch (textureLayer->tileProjectionType()) {
+    case GeoSceneAbstractTileProjection::Equirectangular:
         return static_cast<int>( globalHeight * 0.5 - lat * normGlobalHeight );
-    case GeoSceneTileDataset::Mercator:
+    case GeoSceneAbstractTileProjection::Mercator:
         if ( fabs( lat ) < 1.4835 )
             return static_cast<int>( globalHeight * 0.5 - gdInv( lat ) * 0.5 * normGlobalHeight );
         if ( lat >= +1.4835 )

src/lib/marble/MapThemeManager.cpp
@@ -563,7 +563,7 @@ GeoSceneDocument *MapThemeManager::createMapThemeFromOverlay( const GeoDataPhoto
     texture->setSourceDir( sourceDir );
     texture->setFileFormat( extension );
     texture->setInstallMap( fileName );
-    texture->setProjection(GeoSceneTileDataset::Equirectangular);
+    texture->setTileProjection(GeoSceneAbstractTileProjection::Equirectangular);
 
     layer->addDataset(texture);
 

src/lib/marble/MapWizard.cpp
@@ -789,7 +789,7 @@ GeoSceneDocument* MapWizard::createDocument()
         texture->setLevelZeroRows( 1 );
         texture->setLevelZeroColumns( 1 );
         texture->setServerLayout( new WmsServerLayout( texture ) );
-        texture->setProjection( GeoSceneTileDataset::Equirectangular );
+        texture->setTileProjection(GeoSceneAbstractTileProjection::Equirectangular);
     }
     
     else if( d->mapProviderType == MapWizardPrivate::StaticUrlMap )
@@ -803,7 +803,7 @@ GeoSceneDocument* MapWizard::createDocument()
         texture->setLevelZeroRows( 1 );
         texture->setLevelZeroColumns( 1 );
         texture->setServerLayout( new CustomServerLayout( texture ) );
-        texture->setProjection( GeoSceneTileDataset::Mercator );
+        texture->setTileProjection(GeoSceneAbstractTileProjection::Mercator);
     }
     
     else if( d->mapProviderType == MapWizardPrivate::StaticImageMap )
@@ -813,7 +813,7 @@ GeoSceneDocument* MapWizard::createDocument()
         texture->setFileFormat( d->format.toUpper() );
         texture->setInstallMap(document->head()->theme() + QLatin1Char('.') + d->format);
         texture->setServerLayout( new MarbleServerLayout( texture ) );
-        texture->setProjection( GeoSceneTileDataset::Equirectangular );
+        texture->setTileProjection(GeoSceneAbstractTileProjection::Equirectangular);
         int imageWidth = QImage( image ).width();
         int tileSize = c_defaultTileSize;
         

src/lib/marble/MergedLayerDecorator.cpp
@@ -146,11 +146,11 @@ int MergedLayerDecorator::tileRowCount( int level ) const
     return TileLoaderHelper::levelToRow( levelZeroRows, level );
 }
 
-GeoSceneTileDataset::Projection MergedLayerDecorator::tileProjection() const
+GeoSceneAbstractTileProjection::Type MergedLayerDecorator::tileProjectionType() const
 {
     Q_ASSERT( !d->m_textureLayers.isEmpty() );
 
-    return d->m_textureLayers.at( 0 )->projection();
+    return d->m_textureLayers.at( 0 )->tileProjectionType();
 }
 
 QSize MergedLayerDecorator::tileSize() const
@@ -218,7 +218,8 @@ void MergedLayerDecorator::Private::renderGroundOverlays( QImage *tileImage, con
     /* All tiles are covering the same area. Pick one. */
     const TileId tileId = tiles.first()->id();
 
-    GeoDataLatLonBox tileLatLonBox = tileId.toLatLonBox( findRelevantTextureLayers( tileId ).first() );
+    GeoDataLatLonBox tileLatLonBox;
+    findRelevantTextureLayers(tileId).first()->tileProjection()->geoCoordinates(tileId, tileLatLonBox);
 
     /* Map the ground overlay to the image. */
     for ( int i =  0; i < m_groundOverlays.size(); ++i ) {
@@ -246,13 +247,14 @@ void MergedLayerDecorator::Private::renderGroundOverlays( QImage *tileImage, con
         const qreal rad2Pixel = global_height / M_PI;
 
         qreal latPixelPosition = rad2Pixel/2 * gdInv(tileLatLonBox.north());
+        const bool isMercatorTileProjection = (m_textureLayers.at( 0 )->tileProjectionType() ==  GeoSceneAbstractTileProjection::Mercator);
 
         for ( int y = 0; y < tileImage->height(); ++y ) {
              QRgb *scanLine = ( QRgb* ) ( tileImage->scanLine( y ) );
 
              qreal lat = 0;
 
-             if (m_textureLayers.at( 0 )->projection() ==  GeoSceneTileDataset::Mercator) {
+             if (isMercatorTileProjection) {
                   lat = gd(2 * (latPixelPosition - y) * pixel2Rad );
              }
              else {
@@ -584,10 +586,16 @@ QVector<const GeoSceneTextureTileDataset *> MergedLayerDecorator::Private::findR
         if ( !candidate->hasMaximumTileLevel() ||
              candidate->maximumTileLevel() >= stackedTileId.zoomLevel() ) {
             //check if the tile intersects with texture bounds
-            if ( candidate->latLonBox().isNull()
-                || candidate->latLonBox().intersects( stackedTileId.toLatLonBox( candidate ) ) )
-            {
-                result.append( candidate );
+            if (candidate->latLonBox().isNull()) {
+                result.append(candidate);
+            }
+            else {
+                GeoDataLatLonBox bbox;
+                candidate->tileProjection()->geoCoordinates(stackedTileId, bbox);
+
+                if (candidate->latLonBox().intersects(bbox)) {
+                    result.append( candidate );
+                }
             }
         }
     }

src/lib/marble/MergedLayerDecorator.h
@@ -57,7 +57,7 @@ class MergedLayerDecorator
 
     int tileRowCount( int level ) const;
 
-    GeoSceneTextureTileDataset::Projection tileProjection() const;
+    GeoSceneAbstractTileProjection::Type tileProjectionType() const;
 
     QSize tileSize() const;
 

src/lib/marble/ScanlineTextureMapperContext.cpp
@@ -24,7 +24,7 @@ using namespace Marble;
 
 ScanlineTextureMapperContext::ScanlineTextureMapperContext( StackedTileLoader * const tileLoader, int tileLevel )
     : m_tileLoader( tileLoader ),
-      m_textureProjection( tileLoader->tileProjection() ),  // cache texture projection
+      m_textureProjection(tileLoader->tileProjectionType()),  // cache texture projection
       m_tileSize( tileLoader->tileSize() ),  // cache tile size
       m_tileLevel( tileLevel ),
       m_globalWidth( m_tileSize.width() * m_tileLoader->tileColumnCount( m_tileLevel ) ),

src/lib/marble/ScanlineTextureMapperContext.h
@@ -73,7 +73,7 @@ private:
 
 private:
     StackedTileLoader *const m_tileLoader;
-    GeoSceneTileDataset::Projection const m_textureProjection;
+    GeoSceneAbstractTileProjection::Type const m_textureProjection;
     /// size of the tiles of of the current texture layer
     QSize const m_tileSize;
 
@@ -123,9 +123,9 @@ inline qreal ScanlineTextureMapperContext::rad2PixelX( const qreal lon ) const
 inline qreal ScanlineTextureMapperContext::rad2PixelY( const qreal lat ) const
 {
     switch ( m_textureProjection ) {
-    case GeoSceneTileDataset::Equirectangular:
+    case GeoSceneAbstractTileProjection::Equirectangular:
         return -lat * m_normGlobalHeight;
-    case GeoSceneTileDataset::Mercator:
+    case GeoSceneAbstractTileProjection::Mercator:
         if ( fabs( lat ) < 1.4835 ) {
             // We develop the inverse Gudermannian into a MacLaurin Series:
             // In spite of the many elements needed to get decent 

commit eb44ff9711a38223ea70204c7289598b27e6e329

Fix refactoring issue introduced in 5c1022d
m_zoomlevel and tile.zoomLevel() are different (m_zoomlevel is the
lower base zoom level and tile a higher level one).

tools/vectorosm-tilecreator/TileDirectory.cpp
@@ -396,7 +396,7 @@ void TileDirectory::createTiles() const
 int TileDirectory::innerNodes(const TileId &tile) const
 {
     GeoDataLatLonBox tileBoundary;
-    m_tileProjection.geoCoordinates(m_zoomLevel, tile.x(), tile.y(), tileBoundary);
+    m_tileProjection.geoCoordinates(tile.zoomLevel(), tile.x(), tile.y(), tileBoundary);
 
     double const west = tileBoundary.west();
     double const east = tileBoundary.east();