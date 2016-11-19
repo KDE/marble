3742716e06b5bb3c5a6ec534751f35a41b7d60b1
Dennis Nienhüser <nienhueser@kde.org>
2016-11-19 18:56:33 +0100

    Run the easier (hence faster) check first.

--- src/lib/marble/geodata/data/GeoDataLatLonBox.cpp
+++ src/lib/marble/geodata/data/GeoDataLatLonBox.cpp
@@ -347,13 +347,14 @@ bool GeoDataLatLonBox::containsPole( Pole pole ) const
 
 bool GeoDataLatLonBox::contains(qreal lon, qreal lat) const
 {
+    if ( lat < d->m_south || lat > d->m_north ) {
+        return false;
+    }
+
     // We need to take care of the normal case ...
     if ( ( ( lon < d->m_west || lon > d->m_east ) && ( d->m_west < d->m_east ) ) ||
     // ... and the case where the bounding box crosses the date line:
          ( ( lon < d->m_west && lon > d->m_east ) && ( d->m_west > d->m_east ) ) )
-        return false;
-
-    if ( lat < d->m_south || lat > d->m_north )
         return false;
 
     return true;
