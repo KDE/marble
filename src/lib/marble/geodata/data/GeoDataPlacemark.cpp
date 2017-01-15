KDE/marble

commit e79a1d010bc7195e29df2a4f3bfd5e41aaaa3db8

Improve center approximation for line strings with just 2 coordinates

src/lib/marble/geodata/data/GeoDataPlacemark.cpp
@@ -303,7 +303,15 @@ GeoDataCoordinates GeoDataPlacemark::coordinate( const QDateTime &dateTime, bool
             coord = track->coordinatesAt( dateTime );
         } else if (d->m_geometry->nodeType() == GeoDataTypes::GeoDataLineStringType) {
             GeoDataLineString const *lineString = static_cast<const GeoDataLineString *>(d->m_geometry);
-            coord = lineString->isEmpty() ? GeoDataCoordinates() : lineString->at(lineString->size() / 2);
+            auto const size = lineString->size();
+            if (size == 0) {
+                return GeoDataCoordinates();
+            } else if (size < 3) {
+                // Approximate center if there are just two coordinates
+                return lineString->latLonAltBox().center();
+            } else {
+                lineString->at(size / 2);
+            }
         } else {
             coord = d->m_geometry->latLonAltBox().center();
         }

commit 71cd57accea18386b1bf8ce0ea79dd8d63144d08

Return value (regression introduced in e79a1d010)

src/lib/marble/geodata/data/GeoDataPlacemark.cpp
@@ -310,7 +310,7 @@ GeoDataCoordinates GeoDataPlacemark::coordinate( const QDateTime &dateTime, bool
                 // Approximate center if there are just two coordinates
                 return lineString->latLonAltBox().center();
             } else {
-                lineString->at(size / 2);
+                return lineString->at(size / 2);
             }
         } else {
             coord = d->m_geometry->latLonAltBox().center();