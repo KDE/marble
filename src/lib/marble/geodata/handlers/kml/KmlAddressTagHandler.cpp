207423a8a1043915bf86579aa81d39ecd9551b78
Bernhard Beschow <bbeschow@cs.tu-berlin.de>
2017-05-26 14:06:00 +0200

    restore address in GeoDataFeatures when parsing KML

---
+++ src/lib/marble/geodata/handlers/kml/KmlAddressTagHandler.cpp
@@ -0,0 +1,49 @@
+/*
+    Copyright (C) 2017 Bernhard Beschow <bbeschow@cs.tu-berlin.de>
+
+    This file is part of the KDE project
+
+    This library is free software you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    aint with this library see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
+#include "KmlAddressTagHandler.h"
+
+#include "KmlElementDictionary.h"
+#include "GeoDataFeature.h"
+#include "GeoParser.h"
+
+namespace Marble
+{
+namespace kml
+{
+KML_DEFINE_TAG_HANDLER(address)
+
+GeoNode *KmladdressTagHandler::parse(GeoParser &parser) const
+{
+    Q_ASSERT(parser.isStartElement() && parser.isValidElement(QLatin1String(kmlTag_address)));
+
+    GeoStackItem parentItem = parser.parentElement();
+    if (parentItem.is<GeoDataFeature>()) {
+        const auto address = parser.readElementText().trimmed();
+
+        parentItem.nodeAs<GeoDataFeature>()->setAddress(address);
+    }
+
+    return 0;
+}
+
+}
+}
