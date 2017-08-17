KDE/marble

commit b5d1987f74814ccdd7560a79eff2ae35f4631464

introduce and use geodata_cast
Previously, one had to know the relation between GeoNode's nodeType() and static_cast<> for a cast to be valid. With geodata_cast<>, this knowledge isn't required any more. In addition, it makes the code more concise by requiring just one line instead of two.

src/lib/marble/geodata/handlers/kml/KmlMemberTagHandler.cpp
@@ -43,10 +43,10 @@ GeoNode* KmlmemberTagHandler::parse( GeoParser& parser ) const
     */
     if( parser.parentElement( 2 ).is<GeoDataPlacemark>() ) {
         GeoDataPlacemark *placemark = parser.parentElement( 2 ).nodeAs<GeoDataPlacemark>();
-        if ( placemark->geometry()->nodeType() != GeoDataTypes::GeoDataPolygonType ) {
+        GeoDataPolygon *polygon = geodata_cast<GeoDataPolygon>(placemark->geometry());
+        if (polygon) {
             return 0;
         }
-        GeoDataPolygon *polygon = static_cast<GeoDataPolygon*>( placemark->geometry() );
 
         // The memberIndex is used to determine which member this tag represents
         if ( memberIndex == -1 ) {

commit 758ebe8b2d72600b40c6f2f47f448c0c7cc19e96

fix bug introduced in b5d1987f74814ccdd7560a79eff2ae35f4631464
* found by clang-analyzer

src/lib/marble/geodata/handlers/kml/KmlMemberTagHandler.cpp
@@ -44,7 +44,7 @@ GeoNode* KmlmemberTagHandler::parse( GeoParser& parser ) const
     if( parser.parentElement( 2 ).is<GeoDataPlacemark>() ) {
         GeoDataPlacemark *placemark = parser.parentElement( 2 ).nodeAs<GeoDataPlacemark>();
         GeoDataPolygon *polygon = geodata_cast<GeoDataPolygon>(placemark->geometry());
-        if (polygon) {
+        if (!polygon) {
             return 0;
         }
 