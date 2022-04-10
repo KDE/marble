c804cf0ebe1cfb4f836eb404cd5c4a35e954e35f
Volker Krause <vkrause@kde.org>
2022-04-10 11:14:56 +0200

    Build our own static Qt for the vector tile generator
    Recent Marble needs at least Qt 5.14, while the Ubuntu 20.04 currently in
    use on maps.kde.org only has 5.12.
    
    This makes the binary significantly larger of course, but on the other hand
    no longer requires Qt packages to be installed on the server.

--- tools/vectorosm-tilecreator/setup/build/build-tilegenerator.sh
+++ tools/vectorosm-tilecreator/setup/build/build-tilegenerator.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
+# SPDX-License-Identifier: BSD-3-clause
+# SPDX-FileCopyrightText: 2020-2022 Volker Krause <vkrause@kde.org>
+
+function build-static-qt-module() {
+    local module=$1
+    shift
+    cd
+    git clone https://invent.kde.org/qt/qt/$module
+    cd $module
+    git checkout kde/5.15
+    mkdir build
+    cd build
+    if [ $module == "qtbase" ]; then
+        ../configure -prefix ${HOME}/qt-inst $@
+    else
+        ${HOME}/qt-inst/bin/qmake .. $@
+    fi
+    make -j 4
+    make install
+}
+
+build-static-qt-module qtbase --release -qpa offscreen -no-pch -no-icu -no-dbus -no-glib -openssl-linked -no-feature-vulkan -no-feature-sql-mysql -no-feature-sql-psql -no-feature-sql-odbc -no-feature-sha3-fast -static -confirm-license -opensource -make libs -make tools
+build-static-qt-module qtdeclarative -- -no-feature-qml-debug -no-feature-qml-devtools -no-feature-qml-preview -no-feature-qml-profiler
+build-static-qt-module qtsvg
 
 cd
 git clone https://invent.kde.org/education/marble
@@ -6,6 +30,7 @@ cd marble
 mkdir build
 cd build
 cmake -DSTATIC_BUILD=1 \
+    -DCMAKE_PREFIX_PATH=${HOME}/qt-inst \
     -DMARBLE_NO_WEBKITWIDGETS=TRUE \
     -DMARBLE_NO_DBUS=TRUE \
     -DBUILD_MARBLE_TOOLS=ON \
