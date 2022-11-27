84a2b6c7c1483803d80034e88e5f5505865b7899
Fushan Wen <qydwhotmail@gmail.com>
2022-11-27 13:33:54 +0800

    Replace K_EXPORT_PLASMA_RUNNER_WITH_JSON with K_PLUGIN_CLASS_WITH_JSON

--- src/plasmarunner/main.cpp
+++ src/plasmarunner/main.cpp
@@ -5,7 +5,7 @@
 
 #if KRUNNER_VERSION >= QT_VERSION_CHECK(5, 72, 0)
 using namespace Marble;
-K_EXPORT_PLASMA_RUNNER_WITH_JSON(PlasmaRunner, "plasma-runner-marble.json")
+K_PLUGIN_CLASS_WITH_JSON(PlasmaRunner, "plasma-runner-marble.json")
 #else
 K_EXPORT_PLASMA_RUNNER(marble, Marble::PlasmaRunner)
 #endif
