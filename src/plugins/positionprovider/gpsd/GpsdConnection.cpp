KDE/marble

commit 090196aefd9a05926ccdd26fb2353f0ab03ad8d8

Fix for issue #8

src/plugins/positionprovider/gpsd/GpsdConnection.cpp
@@ -92,6 +92,22 @@ void GpsdConnection::initialize()
 
 void GpsdConnection::update()
 {
+#if defined( GPSD_API_MAJOR_VERSION ) && ( GPSD_API_MAJOR_VERSION >= 12 ) && defined( PACKET_SET )
+    gps_data_t* data = nullptr;
+
+    if ( m_gpsd.waiting(gpsUpdateInterval * 1000) )
+    {
+        gps_data_t *currentData = m_gpsd.read();
+
+        if( currentData && currentData->set & PACKET_SET ) {
+            data = currentData;
+        }
+    }
+
+    if ( data ) {
+        emit gpsdInfo( *data );
+    }
+#else
 #if defined( GPSD_API_MAJOR_VERSION ) && ( GPSD_API_MAJOR_VERSION >= 4 ) && defined( PACKET_SET )
     gps_data_t *data = nullptr;
 
@@ -128,6 +144,7 @@ void GpsdConnection::update()
         mDebug() << "Lost connection to gpsd, trying to re-open.";
         initialize();
     }
+#endif
 #endif
 }
 

src/plugins/positionprovider/gpsd/GpsdPositionProviderPlugin.cpp
@@ -74,10 +74,9 @@ void GpsdPositionProviderPlugin::update( gps_data_t data )
 {
     PositionProviderStatus oldStatus = m_status;
     GeoDataCoordinates oldPosition = m_position;
-#if defined(STATUS_UNK) // STATUS_NO_FIX was renamed to STATUS_UNK without bumping API version
-    if ( data.fix.status == STATUS_UNK || std::isnan( data.fix.longitude ) || std::isnan( data.fix.latitude ) )
-#elif defined( GPSD_API_MAJOR_VERSION ) && ( GPSD_API_MAJOR_VERSION >= 10 )
-    if ( data.fix.status == STATUS_NO_FIX || std::isnan( data.fix.longitude ) || std::isnan( data.fix.latitude ) )
+
+#if defined( GPSD_API_MAJOR_VERSION ) && ( GPSD_API_MAJOR_VERSION >= 10 )
+    if ( data.fix.mode <= MODE_NO_FIX || std::isnan( data.fix.longitude ) || std::isnan( data.fix.latitude ) )
 #else
     if ( data.status == STATUS_NO_FIX || std::isnan( data.fix.longitude ) || std::isnan( data.fix.latitude ) )
 #endif

commit 87d110cee1b80d1735dbeb62f46bff4cafe883e6

Read gpsd repeatedly to avoid stale data
In the gpsd position provider plugin, one position report was read from
gpsd every 1000ms. However, reports are available from the GPS at a
higher frequency, and gpsd buffers these (to an unknown depth) and
supplies then on a FIFO basis to the plugin. This means that the plugin
will never keep up with the new fixes, and will constantly be reading
stale data. This has the effect that, if the receiver is moved, it may be
some minutes before this move is registered on the map.

Therefore, we need to read fixes until there are no more
available instead, and throw away all of the fixes except the last one.

This bug appears to have been introduced in commit 090196ae 2022-04-05.
The bug was noticed on a PinePhone v1.2b running Mobian trixie,
gpsd 3.25, and a Quectel EG25-G GPS, but it will probably affect
other configurations as well.

src/plugins/positionprovider/gpsd/GpsdConnection.cpp
@@ -97,7 +97,7 @@ void GpsdConnection::update()
 #if defined(GPSD_API_MAJOR_VERSION) && (GPSD_API_MAJOR_VERSION >= 12) && defined(PACKET_SET)
     gps_data_t *data = nullptr;
 
-    if (m_gpsd.waiting(gpsUpdateInterval * 1000)) {
+    while (m_gpsd.waiting(0)) {
         gps_data_t *currentData = m_gpsd.read();
 
         if (currentData && currentData->set & PACKET_SET) {